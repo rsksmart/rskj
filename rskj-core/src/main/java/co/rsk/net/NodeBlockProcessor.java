da35631c96633eff35f2150e0685ae6adeeb393d
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2017-06-02 11:30:55 -0300

    Rollback sync change

--- rskj-core/src/main/java/co/rsk/net/NodeBlockProcessor.java
+++ rskj-core/src/main/java/co/rsk/net/NodeBlockProcessor.java
@@ -322,7 +322,9 @@ public class NodeBlockProcessor implements BlockProcessor {
         if (peerBestBlockNumber > this.lastKnownBlockNumber)
             this.lastKnownBlockNumber = peerBestBlockNumber;
 
-        for (long n = peerBestBlockNumber; n <= bestBlockNumber && n < peerBestBlockNumber + 25; n++) {
+        int initial = Math.abs(random.nextInt() % 10);
+
+        for (long n = peerBestBlockNumber + initial; n <= bestBlockNumber && n < peerBestBlockNumber + 100; n += 10) {
             logger.trace("Trying to send block {}", n);
             
             final Block b = this.blockchain.getBlockByNumber(n);
