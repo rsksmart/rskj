aca31a0cc126ed533d31b3cc429a145d536c4158
Bernardo Codesido <bernardo@rsk.co>
2017-07-11 16:00:20 -0300

    Fix At most one statement is allowed per line, but 2 statements were found on this line. by Sonar

--- rskj-core/src/main/java/org/ethereum/core/BlockWrapper.java
+++ rskj-core/src/main/java/org/ethereum/core/BlockWrapper.java
@@ -171,8 +171,13 @@ public class BlockWrapper {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         BlockWrapper wrapper = (BlockWrapper) o;
 
