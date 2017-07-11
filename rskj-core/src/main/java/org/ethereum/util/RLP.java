e121022f56327f4fc682cfeb3f7fe686f1eed60d
Bernardo Codesido <bernardo@rsk.co>
2017-07-11 18:12:23 -0300

    Remove unused import

--- rskj-core/src/main/java/org/ethereum/util/RLP.java
+++ rskj-core/src/main/java/org/ethereum/util/RLP.java
@@ -25,7 +25,6 @@ import org.slf4j.LoggerFactory;
 import org.spongycastle.util.encoders.Hex;
 
 import java.math.BigInteger;
-import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
