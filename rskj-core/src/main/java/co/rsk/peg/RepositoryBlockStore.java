3a58c461a123acfa122f8c45d48eb2d24b352def
Bernardo Codesido <bernardo@rsk.co>
2017-07-11 15:35:06 -0300

    Fix At most one statement is allowed per line, but 2 statements were found on this line. by Sonar

--- rskj-core/src/main/java/co/rsk/peg/RepositoryBlockStore.java
+++ rskj-core/src/main/java/co/rsk/peg/RepositoryBlockStore.java
@@ -70,7 +70,10 @@ public class RepositoryBlockStore implements BtcBlockStore{
     @Override
     public synchronized StoredBlock get(Sha256Hash hash) throws BlockStoreException {
         byte[] ba = repository.getStorageBytes(Hex.decode(contractAddress), new DataWord(hash.toString()));
-        if (ba==null) return null;
+        if (ba==null) {
+            return null;
+        }
+        
         StoredBlock storedBlock = byteArrayToStoredBlock(ba);
         return storedBlock;
     }
