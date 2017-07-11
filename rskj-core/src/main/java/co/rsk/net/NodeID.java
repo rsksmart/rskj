d78041f963dbe71c4c6d7aad53a5d5e732b9cb21
Bernardo Codesido <bernardo@rsk.co>
2017-07-11 15:16:53 -0300

    Fix At most one statement is allowed per line, but 2 statements were found on this line. by Sonar

--- rskj-core/src/main/java/co/rsk/net/NodeID.java
+++ rskj-core/src/main/java/co/rsk/net/NodeID.java
@@ -36,8 +36,13 @@ public class NodeID {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         NodeID nodeID1 = (NodeID) o;
 
