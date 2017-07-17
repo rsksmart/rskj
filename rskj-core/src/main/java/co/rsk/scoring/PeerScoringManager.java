1de212b502efff88fd25ad2cad0995bb8a9d60e2
ajlopez <webmaster@ajlopez.com>
2017-07-17 17:12:47 -0300

    Comments in PeerScoringManager

--- rskj-core/src/main/java/co/rsk/scoring/PeerScoringManager.java
+++ rskj-core/src/main/java/co/rsk/scoring/PeerScoringManager.java
@@ -10,6 +10,12 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 /**
+ * PeerScoringManager keeps list of nodes and addresses scoring
+ * Records events by node id and address
+ * Calculates good reputation by node id and address
+ * Starts punishments when the good reputation is lost
+ * Alsa keeps a list of banned addresses and blocks
+ * <p>
  * Created by ajlopez on 28/06/2017.
  */
 public class PeerScoringManager {
@@ -27,6 +33,14 @@ public class PeerScoringManager {
     @GuardedBy("accessLock")
     private Map<InetAddress, PeerScoring> peersByAddress;
 
+    /**
+     * Creates and initialize the scoring manager
+     * usually only one object per running node
+     *
+     * @param nodePeersSize     maximum number of nodes to keep
+     * @param nodeParameters    nodes punishment parameters (@see PunishmentParameters)
+     * @param ipParameters      address punishment parameters
+     */
     public PeerScoringManager(int nodePeersSize, PunishmentParameters nodeParameters, PunishmentParameters ipParameters) {
         this.scoringCalculator = new ScoringCalculator();
         this.nodePunishmentCalculator = new PunishmentCalculator(nodeParameters);
@@ -41,6 +55,14 @@ public class PeerScoringManager {
 
         this.peersByAddress = new HashMap<>();
     }
+
+    /**
+     * Record the event, givent the node id and/or the network address
+     *
+     * @param id        node id or null
+     * @param address   address or null
+     * @param event     event type (@see EventType)
+     */
     public void recordEvent(NodeID id, InetAddress address, EventType event) {
         synchronized (accessLock) {
             if (id != null) {
@@ -63,10 +85,22 @@ public class PeerScoringManager {
         }
     }
 
+    /**
+     * Returns if the given node id has good reputation
+     *
+     * @param id    the node id
+     * @return  <tt>true</tt> if the node has good reputation
+     */
     public boolean hasGoodReputation(NodeID id) {
         return this.getPeerScoring(id).hasGoodReputation();
     }
 
+    /**
+     * Returns if the given networkaddress has good reputation
+     *
+     * @param address   the network address
+     * @return  <tt>true</tt> if the address has good reputation
+     */
     public boolean hasGoodReputation(InetAddress address)
     {
         if (this.addressTable.contains(address))
@@ -75,10 +109,22 @@ public class PeerScoringManager {
         return this.getPeerScoring(address).hasGoodReputation();
     }
 
+    /**
+     * Adds a network address to the set of banned addresses
+     *
+     * @param address   the address to be banned
+     */
     public void addBannedAddress(InetAddress address) {
         this.addressTable.addAddress(address);
     }
 
+    /**
+     * Adds a network address to the set of banned addresses
+     * The address is represented in an string
+     * If it is a block, it has a mask
+     *
+     * @param address   the address or address block to be banned
+     */
     public void addBannedAddress(String address) throws InvalidInetAddressBlockException, InvalidInetAddressException {
         if (InetAddressUtils.hasMask(address))
             this.addBannedAddressBlock(InetAddressUtils.parse(address));
@@ -86,10 +132,22 @@ public class PeerScoringManager {
             this.addBannedAddress(InetAddressUtils.getAddress(address));
     }
 
+    /**
+     * Removes a network address from the set of banned addresses
+     *
+     * @param address   the address to be removed
+     */
     public void removeBannedAddress(InetAddress address) {
         this.addressTable.removeAddress(address);
     }
 
+    /**
+     * Removes a network address from the set of banned addresses
+     * The address is represented in an string
+     * If it is a block, it has a mask
+     *
+     * @param address   the address or address block to be removed
+     */
     public void removeBannedAddress(String address) throws InvalidInetAddressBlockException, InvalidInetAddressException {
         if (InetAddressUtils.hasMask(address))
             this.removeBannedAddressBlock(InetAddressUtils.parse(address));
@@ -97,14 +155,30 @@ public class PeerScoringManager {
             this.removeBannedAddress(InetAddressUtils.getAddress(address));
     }
 
+    /**
+     * Adds a network address block to the set of banned blocks
+     *
+     * @param addressBlock   the address block to be banned
+     */
     public void addBannedAddressBlock(InetAddressBlock addressBlock) {
         this.addressTable.addAddressBlock(addressBlock);
     }
 
+    /**
+     * Removes a network address block from the set of banned blocks
+     *
+     * @param addressBlock   the address block to be removed
+     */
     public void removeBannedAddressBlock(InetAddressBlock addressBlock) {
         this.addressTable.removeAddressBlock(addressBlock);
     }
 
+    /**
+     * Returns the list of peer scoring information
+     * It contains the information recorded by node id and by address
+     *
+     * @return  the list of peer scoring information
+     */
     public List<PeerScoringInformation> getPeersInformation() {
         List<PeerScoringInformation> list = new ArrayList<>(this.peersByNodeID.size() + this.peersByAddress.size());
 
@@ -114,6 +188,12 @@ public class PeerScoringManager {
         return list;
     }
 
+    /**
+     * Returns the list of banned addresses, represented by a textual description
+     * The list includes the banned addresses and the banned blocks
+     *
+     * @return a list of strings describing the banned addresses and blocks
+     */
     public List<String> getBannedAddressList() {
         List<String> list = new ArrayList<>();
 
@@ -148,6 +228,13 @@ public class PeerScoringManager {
         }
     }
 
+    /**
+     * Calculates the reputation for a peer
+     * Starts punishment if needed
+     *
+     * @param scoring       the peer scoring
+     * @param calculator    the calculator to use
+     */
     private void reviewReputation(PeerScoring scoring, PunishmentCalculator calculator) {
         boolean reputation = scoringCalculator.hasGoodReputation(scoring);
 
