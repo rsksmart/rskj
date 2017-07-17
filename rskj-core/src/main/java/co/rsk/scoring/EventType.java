7c568690d40ecdd66cecb66feaecf3793f1c0e43
ajlopez <webmaster@ajlopez.com>
2017-07-17 16:44:54 -0300

    Comments in EventType

--- rskj-core/src/main/java/co/rsk/scoring/EventType.java
+++ rskj-core/src/main/java/co/rsk/scoring/EventType.java
@@ -1,6 +1,9 @@
 package co.rsk.scoring;
 
 /**
+ * EventType is an enumeration to represent the events to be recorded
+ * in peer scoring (@see PeerScoring)
+ * <p>
  * Created by ajlopez on 27/06/2017.
  */
 public enum EventType {
