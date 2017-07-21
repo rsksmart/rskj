f4bfb05304ee21c4c188e954c8a118fe7cd08cf2
ajlopez <webmaster@ajlopez.com>
2017-07-21 16:52:09 -0300

    Sort using comparator

--- rskj-core/src/test/java/co/rsk/net/discovery/NodeDistanceScoreComparatorTest.java
+++ rskj-core/src/test/java/co/rsk/net/discovery/NodeDistanceScoreComparatorTest.java
@@ -3,6 +3,10 @@ package co.rsk.net.discovery;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by ajlopez on 21/07/2017.
  */
@@ -27,5 +31,28 @@ public class NodeDistanceScoreComparatorTest {
         // different distance, same score
         Assert.assertEquals(1, comparator.compare(node1, node4));
         Assert.assertEquals(-1, comparator.compare(node4, node1));
+    }
+
+    @Test
+    public void sortNodesInformation() {
+        NodeInformation node1 = new NodeInformation(null, 10, 20);
+        NodeInformation node2 = new NodeInformation(null, 10, 20);
+        NodeInformation node3 = new NodeInformation(null, 10, 30);
+        NodeInformation node4 = new NodeInformation(null,  5, 20);
+
+        List<NodeInformation> list = new ArrayList<>();
+        list.add(node1);
+        list.add(node2);
+        list.add(node3);
+        list.add(node4);
+
+        Collections.sort(list, new NodeDistanceScoreComparator());
+
+        Assert.assertSame(node4, list.get(0));
+        Assert.assertSame(node3, list.get(1));
+        Assert.assertEquals(node2.getDistance(), list.get(2).getDistance());
+        Assert.assertEquals(node2.getScore(), list.get(2).getScore());
+        Assert.assertEquals(node1.getDistance(), list.get(3).getDistance());
+        Assert.assertEquals(node1.getScore(), list.get(3).getScore());
     }
 }
