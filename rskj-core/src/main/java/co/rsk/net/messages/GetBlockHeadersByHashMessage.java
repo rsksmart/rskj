e43e3373fbbb7ca1329b767ccc8887ce5a54abd4
ajlopez <webmaster@ajlopez.com>
2017-08-23 17:16:53 -0300

    GetBlockHeadersByHashMessage encode, decode

--- rskj-core/src/main/java/co/rsk/net/messages/GetBlockHeadersByHashMessage.java
+++ rskj-core/src/main/java/co/rsk/net/messages/GetBlockHeadersByHashMessage.java
@@ -1,6 +1,11 @@
 package co.rsk.net.messages;
 
 import org.apache.commons.lang3.NotImplementedException;
+import org.ethereum.util.RLP;
+
+import java.math.BigInteger;
+
+import static org.ethereum.util.ByteUtil.byteArrayToInt;
 
 /**
  * Created by ajlopez on 23/08/2017.
@@ -24,7 +29,11 @@ public class GetBlockHeadersByHashMessage extends Message {
 
     @Override
     public byte[] getEncodedMessage() {
-        throw new RuntimeException();
+        byte[] rlpId = RLP.encodeBigInteger(BigInteger.valueOf(this.id));
+        byte[] rlpHash = RLP.encodeElement(this.hash);
+        byte[] rlpCount = RLP.encodeInt(this.count);
+
+        return RLP.encodeList(rlpId, rlpHash, rlpCount);
     }
 
     @Override
