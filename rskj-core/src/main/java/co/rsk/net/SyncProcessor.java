7ac4133c2f32347610af0b87d72b74cbeeaad8f3
ajlopez <webmaster@ajlopez.com>
2017-08-29 17:20:55 -0300

    SyncProcessor with advanced peers

--- rskj-core/src/main/java/co/rsk/net/SyncProcessor.java
+++ rskj-core/src/main/java/co/rsk/net/SyncProcessor.java
@@ -1,5 +1,9 @@
 package co.rsk.net;
 
+import co.rsk.core.bc.BlockChainStatus;
+import org.ethereum.core.Blockchain;
+
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,10 +11,31 @@ import java.util.Map;
  * Created by ajlopez on 29/08/2017.
  */
 public class SyncProcessor {
+    private Blockchain blockchain;
     private Map<NodeID, Status> peers = new HashMap<>();
+
+    public SyncProcessor(Blockchain blockchain) {
+        this.blockchain = blockchain;
+    }
 
     public int getNoPeers() {
         return this.peers.size();
+    }
+
+    public int getNoAdvancedPeers() {
+        BlockChainStatus chainStatus = this.blockchain.getStatus();
+
+        if (chainStatus == null)
+            return this.peers.size();
+
+        BigInteger totalDifficulty = chainStatus.getTotalDifficulty();
+        int count = 0;
+
+        for (Status peer : this.peers.values())
+            if (peer.getTotalDifficulty().compareTo(totalDifficulty) > 0)
+                count++;
+
+        return count;
     }
 
     public void processStatus(MessageSender sender, Status status) {
--- rskj-core/src/test/java/co/rsk/net/SyncProcessorTest.java
+++ rskj-core/src/test/java/co/rsk/net/SyncProcessorTest.java
@@ -1,12 +1,19 @@
 package co.rsk.net;
 
+import co.rsk.blockchain.utils.BlockGenerator;
+import co.rsk.core.bc.BlockChainImpl;
 import co.rsk.net.messages.StatusMessage;
 import co.rsk.net.simples.SimpleMessageSender;
+import co.rsk.test.builders.BlockChainBuilder;
+import org.ethereum.core.Block;
+import org.ethereum.core.Blockchain;
+import org.ethereum.core.ImportResult;
 import org.ethereum.crypto.HashUtil;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.math.BigInteger;
+import java.util.List;
 
 /**
  * Created by ajlopez on 29/08/2017.
@@ -14,22 +21,66 @@ import java.math.BigInteger;
 public class SyncProcessorTest {
     @Test
     public void noPeers() {
-        SyncProcessor processor = new SyncProcessor();
+        Blockchain blockchain = createBlockchain();
+        SyncProcessor processor = new SyncProcessor(blockchain);
 
         Assert.assertEquals(0, processor.getNoPeers());
+        Assert.assertEquals(0, processor.getNoAdvancedPeers());
     }
 
     @Test
-    public void processStatus() {
+    public void processStatusWithAdvancedPeers() {
+        Blockchain blockchain = createBlockchain();
         SimpleMessageSender sender = new SimpleMessageSender(new byte[] { 0x01 });
         byte[] hash = HashUtil.randomHash();
         byte[] parentHash = HashUtil.randomHash();
 
-        Status status = new Status(100, hash, parentHash, BigInteger.TEN);
+        Status status = new Status(100, hash, parentHash, blockchain.getTotalDifficulty().add(BigInteger.TEN));
 
-        SyncProcessor processor = new SyncProcessor();
+        SyncProcessor processor = new SyncProcessor(blockchain);
         processor.processStatus(sender, status);
 
         Assert.assertEquals(1, processor.getNoPeers());
+        Assert.assertEquals(1, processor.getNoAdvancedPeers());
+    }
+
+    @Test
+    public void processStatusWithPeerWithSameDifficulty() {
+        Blockchain blockchain = createBlockchain(100);
+        SimpleMessageSender sender = new SimpleMessageSender(new byte[] { 0x01 });
+        byte[] hash = HashUtil.randomHash();
+        byte[] parentHash = HashUtil.randomHash();
+
+        Status status = new Status(blockchain.getStatus().getBestBlockNumber(), hash, parentHash, blockchain.getStatus().getTotalDifficulty());
+
+        SyncProcessor processor = new SyncProcessor(blockchain);
+        processor.processStatus(sender, status);
+
+        Assert.assertEquals(1, processor.getNoPeers());
+        Assert.assertEquals(0, processor.getNoAdvancedPeers());
+    }
+
+    private static Blockchain createBlockchain() {
+        return createBlockchain(0);
+    }
+
+    private static Blockchain createBlockchain(int size) {
+        BlockChainBuilder builder = new BlockChainBuilder();
+        BlockChainImpl blockChain = builder.build();
+
+        Block genesis = BlockGenerator.getGenesisBlock();
+        genesis.setStateRoot(blockChain.getRepository().getRoot());
+        genesis.flushRLP();
+
+        Assert.assertEquals(ImportResult.IMPORTED_BEST, blockChain.tryToConnect(genesis));
+
+        if (size > 0) {
+            List<Block> blocks = BlockGenerator.getBlockChain(genesis, size);
+
+            for (Block block: blocks)
+                blockChain.tryToConnect(block);
+        }
+
+        return blockChain;
     }
 }
