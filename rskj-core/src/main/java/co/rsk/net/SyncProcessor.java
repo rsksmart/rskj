8c217a2416837a6a943a82921add8d6a3e237c0e
ajlopez <webmaster@ajlopez.com>
2017-08-29 16:54:42 -0300

    Process status, first test

--- rskj-core/src/main/java/co/rsk/net/SyncProcessor.java
+++ rskj-core/src/main/java/co/rsk/net/SyncProcessor.java
@@ -1,10 +1,19 @@
 package co.rsk.net;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by ajlopez on 29/08/2017.
  */
 public class SyncProcessor {
+    private Map<NodeID, Status> peers = new HashMap<>();
+
     public int getNoPeers() {
-        return 0;
+        return this.peers.size();
+    }
+
+    public void processStatus(MessageSender sender, Status status) {
+        peers.put(sender.getNodeID(), status);
     }
 }
