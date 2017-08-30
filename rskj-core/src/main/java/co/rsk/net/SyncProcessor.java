32e1ff6c6ce780c77bacbbf9538ef75d93e6d9c6
ajlopez <webmaster@ajlopez.com>
2017-08-30 11:12:36 -0300

    Find connection point blockchain with genesis vs blockchain with 100 blocks

--- rskj-core/src/main/java/co/rsk/net/SyncProcessor.java
+++ rskj-core/src/main/java/co/rsk/net/SyncProcessor.java
@@ -4,6 +4,7 @@ import co.rsk.core.bc.BlockChainStatus;
 import co.rsk.net.messages.BlockHashRequestMessage;
 import co.rsk.net.messages.BlockHashResponseMessage;
 import co.rsk.net.messages.SkeletonRequestMessage;
+import org.ethereum.core.Block;
 import org.ethereum.core.Blockchain;
 
 import java.math.BigInteger;
@@ -17,7 +18,7 @@ public class SyncProcessor {
     private long nextId;
     private Blockchain blockchain;
     private Map<NodeID, SyncPeerStatus> peers = new HashMap<>();
-    private Map<NodeID, Long> blockHashes = new HashMap<>();
+    private Map<Long, Long> blockHashes = new HashMap<>();
 
     public SyncProcessor(Blockchain blockchain) {
         this.blockchain = blockchain;
@@ -52,18 +53,23 @@ public class SyncProcessor {
     }
 
     public void sendBlockHashRequest(MessageSender sender, long height) {
-        sender.sendMessage(new BlockHashRequestMessage(++nextId, height));
+        blockHashes.put(++nextId, height);
+        sender.sendMessage(new BlockHashRequestMessage(nextId, height));
     }
 
     public void findConnectionPoint(MessageSender sender, long height) {
         long newheight = height / 2;
         this.sendBlockHashRequest(sender, newheight);
-        blockHashes.put(sender.getNodeID(), newheight);
     }
 
     public void processBlockHashResponse(MessageSender sender, BlockHashResponseMessage message) {
-        long height = blockHashes.get(sender.getNodeID());
+        long height = blockHashes.get(message.getId());
 
+        Block block = this.blockchain.getBlockByHash(message.getHash());
+
+        if (block != null)
+            return;
+        
         sendBlockHashRequest(sender, height / 2);
     }
 }
