56871ddc97655710cec346244b3268bc58b1f1ae
ajlopez <webmaster@ajlopez.com>
2017-08-30 20:03:41 -0300

    Better internal peer status, WIP

--- rskj-core/src/main/java/co/rsk/net/SyncPeerStatus.java
+++ rskj-core/src/main/java/co/rsk/net/SyncPeerStatus.java
@@ -4,15 +4,23 @@ package co.rsk.net;
  * Created by ajlopez on 29/08/2017.
  */
 public class SyncPeerStatus {
-    MessageSender sender;
-    Status status;
+    private boolean hasConnectionPoint;
+    private long connectionPoint;
 
-    public SyncPeerStatus(MessageSender sender, Status status) {
-        this.sender = sender;
-        this.status = status;
-    }
-
-    public MessageSender getSender() { return this.sender; }
+    private long findingHeight;
+    private long findingInterval;
 
     public Status getStatus() { return this.status; }
+
+    public void startFindConnectionPoint(long height) {
+        this.findingInterval = height / 2;
+        this.findingHeight = height - this.findingInterval;
+    }
+
+    public boolean hasConnectionPoint() { return this.hasConnectionPoint; }
+
+    public long getFindingHeight() { return this.findingHeight; }
+
+    public long getFindingInterval() { return this.findingInterval; }
 }
+
