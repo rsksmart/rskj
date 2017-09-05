c2f38e82e6dc9328afab874c1c3be8f603df2296
ajlopez <webmaster@ajlopez.com>
2017-09-05 10:53:29 -0300

    Check BodyResponseMessage is expected

--- rskj-core/src/main/java/co/rsk/net/SyncProcessor.java
+++ rskj-core/src/main/java/co/rsk/net/SyncProcessor.java
@@ -183,9 +183,8 @@ public class SyncProcessor {
     public void processBodyResponse(MessageSender sender, BodyResponseMessage message) {
         SyncPeerStatus peerStatus = this.getPeerStatus(sender.getNodeID());
 
-        peerStatus.isExpectedResponse(message.getId(), message.getMessageType());
-        //if (!peerStatus.isExpectedResponse(message.getId(), message.getMessageType()))
-            //return;
+        if (!peerStatus.isExpectedResponse(message.getId(), message.getMessageType()))
+            return;
 
         PendingBodyResponse expected = pendingBodyResponses.get(message.getId());
         if (expected == null || !sender.getNodeID().equals(expected.nodeID)) {
