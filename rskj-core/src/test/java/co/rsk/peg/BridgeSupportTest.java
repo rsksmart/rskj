76d05a3d21c5ed3d9ec51a04897366ed2d87d09e
Diego López León <diegoll@rsk.co>
2017-09-08 10:19:56 -0300

    cleanup previous release BTC transaction implementation

--- rskj-core/src/test/java/co/rsk/peg/BridgeSupportTest.java
+++ rskj-core/src/test/java/co/rsk/peg/BridgeSupportTest.java
@@ -43,6 +43,8 @@ import org.ethereum.config.net.TestNetConfig;
 import org.ethereum.core.*;
 import org.ethereum.db.ReceiptStore;
 import org.ethereum.db.TransactionInfo;
+import org.ethereum.util.RLP;
+import org.ethereum.vm.LogInfo;
 import org.ethereum.vm.PrecompiledContracts;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -64,6 +66,11 @@ import java.util.*;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
 
 /**
  * Created by ajlopez on 6/9/2016.
@@ -608,7 +615,6 @@ public class BridgeSupportTest {
         BridgeStorageProvider provider = new BridgeStorageProvider(repository, PrecompiledContracts.BRIDGE_ADDR);
 
         Assert.assertTrue(provider.getRskTxsWaitingForSignatures().isEmpty());
-        Assert.assertTrue(provider.getRskTxsWaitingForBroadcasting().isEmpty());
     }
 
     @Test
@@ -626,7 +632,6 @@ public class BridgeSupportTest {
         BridgeStorageProvider provider = new BridgeStorageProvider(repository, PrecompiledContracts.BRIDGE_ADDR);
 
         Assert.assertTrue(provider.getRskTxsWaitingForSignatures().isEmpty());
-        Assert.assertTrue(provider.getRskTxsWaitingForBroadcasting().isEmpty());
     }
 
     @Test
@@ -702,7 +707,8 @@ public class BridgeSupportTest {
         track.commit();
 
         track = repository.startTracking();
-        BridgeSupport bridgeSupport = new BridgeSupport(track, contractAddress, (Block) null, null, null, Collections.emptyList());
+        List<LogInfo> logs = new ArrayList<>();
+        BridgeSupport bridgeSupport = new BridgeSupport(track, contractAddress, (Block) null, null, null, logs);
 
         Script inputScript = t.getInputs().get(0).getScriptSig();
         List<ScriptChunk> chunks = inputScript.getChunks();
@@ -746,7 +752,13 @@ public class BridgeSupportTest {
 
         if ("FullySigned".equals(expectedResult)) {
             Assert.assertTrue(provider.getRskTxsWaitingForSignatures().isEmpty());
-            Script retrievedScriptSig = provider.getRskTxsWaitingForBroadcasting().get(sha3Hash).getLeft().getInput(0).getScriptSig();
+            Assert.assertThat(logs, is(not(empty())));
+            Assert.assertThat(logs, hasSize(1));
+            LogInfo releaseTxEvent = logs.get(0);
+            Assert.assertThat(releaseTxEvent.getTopics(), hasSize(1));
+            Assert.assertThat(releaseTxEvent.getTopics(), hasItem(Bridge.RELEASE_BTC_TOPIC));
+            BtcTransaction releaseTx = new BtcTransaction(bridgeConstants.getBtcParams(), RLP.decode2(releaseTxEvent.getData()).get(0).getRLPData());
+            Script retrievedScriptSig = releaseTx.getInput(0).getScriptSig();
             Assert.assertEquals(4, retrievedScriptSig.getChunks().size());
             Assert.assertEquals(true, retrievedScriptSig.getChunks().get(1).data.length > 0);
             Assert.assertEquals(true, retrievedScriptSig.getChunks().get(2).data.length > 0);
@@ -760,7 +772,6 @@ public class BridgeSupportTest {
             }
             Assert.assertEquals(expectSignatureToBePersisted, retrievedScriptSig.getChunks().get(1).data.length > 0);
             Assert.assertEquals(false, retrievedScriptSig.getChunks().get(2).data.length > 0);
-            Assert.assertTrue(provider.getRskTxsWaitingForBroadcasting().isEmpty());
             Assert.assertFalse(provider.getBtcUTXOs().isEmpty());
         }
     }
@@ -788,7 +799,6 @@ public class BridgeSupportTest {
         Assert.assertTrue(provider.getRskTxsWaitingForConfirmations().isEmpty());
         Assert.assertTrue(provider2.getRskTxsWaitingForConfirmations().isEmpty());
         Assert.assertTrue(provider.getRskTxsWaitingForSignatures().isEmpty());
-        Assert.assertTrue(provider.getRskTxsWaitingForBroadcasting().isEmpty());
     }
 
     @Test
@@ -814,7 +824,6 @@ public class BridgeSupportTest {
         Assert.assertFalse(provider.getRskTxsWaitingForConfirmations().isEmpty());
         Assert.assertFalse(provider2.getRskTxsWaitingForConfirmations().isEmpty());
         Assert.assertTrue(provider.getRskTxsWaitingForSignatures().isEmpty());
-        Assert.assertTrue(provider.getRskTxsWaitingForBroadcasting().isEmpty());
     }
 
 
@@ -841,7 +850,6 @@ public class BridgeSupportTest {
         Assert.assertTrue(provider2.getBtcUTXOs().isEmpty());
         Assert.assertTrue(provider2.getRskTxsWaitingForConfirmations().isEmpty());
         Assert.assertTrue(provider2.getRskTxsWaitingForSignatures().isEmpty());
-        Assert.assertTrue(provider2.getRskTxsWaitingForBroadcasting().isEmpty());
         Assert.assertFalse(provider2.getBtcTxHashesAlreadyProcessed().isEmpty());
     }
 
@@ -872,7 +880,6 @@ public class BridgeSupportTest {
         Assert.assertTrue(provider2.getBtcUTXOs().isEmpty());
         Assert.assertTrue(provider2.getRskTxsWaitingForConfirmations().isEmpty());
         Assert.assertTrue(provider2.getRskTxsWaitingForSignatures().isEmpty());
-        Assert.assertTrue(provider2.getRskTxsWaitingForBroadcasting().isEmpty());
         Assert.assertTrue(provider2.getBtcTxHashesAlreadyProcessed().isEmpty());
     }
 
@@ -903,7 +910,6 @@ public class BridgeSupportTest {
         Assert.assertTrue(provider2.getBtcUTXOs().isEmpty());
         Assert.assertTrue(provider2.getRskTxsWaitingForConfirmations().isEmpty());
         Assert.assertTrue(provider2.getRskTxsWaitingForSignatures().isEmpty());
-        Assert.assertTrue(provider2.getRskTxsWaitingForBroadcasting().isEmpty());
         Assert.assertTrue(provider2.getBtcTxHashesAlreadyProcessed().isEmpty());
     }
 
@@ -938,7 +944,6 @@ public class BridgeSupportTest {
         Assert.assertTrue(provider2.getBtcUTXOs().isEmpty());
         Assert.assertTrue(provider2.getRskTxsWaitingForConfirmations().isEmpty());
         Assert.assertTrue(provider2.getRskTxsWaitingForSignatures().isEmpty());
-        Assert.assertTrue(provider2.getRskTxsWaitingForBroadcasting().isEmpty());
         Assert.assertTrue(provider2.getBtcTxHashesAlreadyProcessed().isEmpty());
 
         RskSystemProperties.CONFIG.setBlockchainConfig(blockchainNetConfigOriginal);
@@ -989,7 +994,6 @@ public class BridgeSupportTest {
 
         Assert.assertTrue(provider2.getRskTxsWaitingForConfirmations().isEmpty());
         Assert.assertTrue(provider2.getRskTxsWaitingForSignatures().isEmpty());
-        Assert.assertTrue(provider2.getRskTxsWaitingForBroadcasting().isEmpty());
         Assert.assertTrue(provider2.getBtcTxHashesAlreadyProcessed().isEmpty());
     }
 
@@ -1070,7 +1074,6 @@ public class BridgeSupportTest {
 
         Assert.assertTrue(provider2.getRskTxsWaitingForConfirmations().isEmpty());
         Assert.assertTrue(provider2.getRskTxsWaitingForSignatures().isEmpty());
-        Assert.assertTrue(provider2.getRskTxsWaitingForBroadcasting().isEmpty());
         Assert.assertEquals(1, provider2.getBtcTxHashesAlreadyProcessed().size());
     }
 
@@ -1130,7 +1133,6 @@ public class BridgeSupportTest {
 
         Assert.assertTrue(provider2.getRskTxsWaitingForConfirmations().isEmpty());
         Assert.assertTrue(provider2.getRskTxsWaitingForSignatures().isEmpty());
-        Assert.assertTrue(provider2.getRskTxsWaitingForBroadcasting().isEmpty());
         Assert.assertEquals(1, provider2.getBtcTxHashesAlreadyProcessed().size());
     }
 
