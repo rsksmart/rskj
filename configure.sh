a909fde0fd40d352e275daf86ae8de2f0cc26fb6
herrerameri <herrerameri@gmail.com>
2017-09-21 17:50:32 +0200

    - Change in configure.sh to support Windows platform. The unames included are: MINGW32_NT-10.0-WOW and MINGW64_NT-10.0

--- configure.sh
+++ configure.sh
@@ -15,7 +15,7 @@ function downloadJar(){
 	fi
 	curl https://deps.rsklabs.io/rsk-gradle-witness.jar -o ~/$DOWNLOAD_FILE1
 	curl https://deps.rsklabs.io/gradle-wrapper.jar -o ~/$DOWNLOAD_FILE2
-	if [[ $PLATFORM == 'linux' ]]; then
+	if [[ $PLATFORM == 'linux' || $PLATFORM == 'windows' ]]; then
 		DOWNLOADED_HASH1=$(sha256sum ~/${DOWNLOAD_FILE1} | cut -d' ' -f1)
 		DOWNLOADED_HASH2=$(sha256sum ~/${DOWNLOAD_FILE2} | cut -d' ' -f1)
 	elif [[ $PLATFORM == 'mac' ]]; then
@@ -43,6 +43,10 @@ function platform() {
 		PLATFORM='linux'
 	elif [[ "$unamestr" == 'Darwin' ]]; then
 		PLATFORM='mac'
+	elif [[ "$unamestr" == 'MINGW32_NT-10.0-WOW' ]]; then
+		PLATFORM='windows'
+	elif [[ "$unamestr" == 'MINGW64_NT-10.0' ]]; then
+		PLATFORM='windows'
 	fi
 }
 
