09cdeb71f40105f8ef5dde861d0072e54de89749
Bernardo Codesido <bernardo@rsk.co>
2017-09-29 22:08:07 -0300

    Requested Changes

--- configure.sh
+++ configure.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-PLATFORM=''
 GRADLE_WITNESS="1b8eaa3a788aac37ee78fa65579246d1ad268e3c8cf42cd2caeffc50b3e50056"
 GRADLE_WRAPPER="0f49043be582d7a39b671f924c66bd9337b92fa88ff5951225acc60560053067"
 DOWNLOADED_HASH1=''
@@ -10,6 +9,7 @@ DOWNLOAD_FILE2=$(dd if=/dev/urandom bs=64 count=1 2>/dev/null| od -t x8 -A none 
 unamestr=`uname`
 
 function downloadJar(){
+	platform
 	if [ ! -d ./rskj-core/libs ]; then
 		mkdir ./rskj-core/libs
 	fi
@@ -32,7 +32,7 @@ function downloadJar(){
 	if [[ $GRADLE_WRAPPER != $DOWNLOADED_HASH2 ]]; then
 		rm -f ~/${DOWNLOAD_FILE2}
 		exit 1
-	else 
+	else
 		mv ~/${DOWNLOAD_FILE2} ./gradle/wrapper/gradle-wrapper.jar
 		rm -f ~/${DOWNLOAD_FILE2}
 	fi
@@ -45,10 +45,12 @@ function platform() {
 		PLATFORM='mac'
 	elif [[ "$unamestr" =~ 'MINGW' ]]; then
 		PLATFORM='windows'
+	else
+			echo -e "\e[1m\e[31m[ ERROR ]\e[0m UNRECOGNIZED PLATFORM"
+			exit 2
 	fi
 }
 
-platform
 downloadJar
 
 exit 0
