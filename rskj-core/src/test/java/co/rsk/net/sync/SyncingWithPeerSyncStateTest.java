dc8d78a3f0e5bed842c5191c7bfedc5df68984bc
Martín Coll <mc@rsk.co>
2017-10-02 14:31:34 -0300

    More tests in SyncingWithPeerSyncStateTest

--- rskj-core/src/test/java/co/rsk/net/sync/SyncingWithPeerSyncStateTest.java
+++ rskj-core/src/test/java/co/rsk/net/sync/SyncingWithPeerSyncStateTest.java
@@ -34,4 +34,25 @@ public class SyncingWithPeerSyncStateTest {
         Assert.assertTrue(syncEventsHandler.stopSyncingWasCalled());
     }
 
+    @Test
+    public void itDoesntTimeoutWhenSendingMessages() {
+        SyncConfiguration syncConfiguration = SyncConfiguration.DEFAULT;
+        SimpleSyncEventsHandler syncEventsHandler = new SimpleSyncEventsHandler();
+        SyncState syncState = new SyncingWithPeerSyncState(syncConfiguration, syncEventsHandler);
+
+        syncState.newPeerStatus();
+        Assert.assertFalse(syncEventsHandler.stopSyncingWasCalled());
+
+        for (int i = 0; i < 10; i++) {
+            syncState.messageSent();
+            Assert.assertFalse(syncEventsHandler.stopSyncingWasCalled());
+
+            syncState.tick(Duration.ofSeconds(20));
+            Assert.assertFalse(syncEventsHandler.stopSyncingWasCalled());
+        }
+
+        syncState.tick(Duration.ofSeconds(20));
+        Assert.assertTrue(syncEventsHandler.stopSyncingWasCalled());
+    }
+
 }
