85c2e86e58bf963646a0e9e74e107ba95bf4fa22
ajlopez <webmaster@ajlopez.com>
2017-10-03 14:47:51 -0300

    Refactor new filter processing

--- rskj-core/src/main/java/org/ethereum/rpc/Web3Impl.java
+++ rskj-core/src/main/java/org/ethereum/rpc/Web3Impl.java
@@ -1133,12 +1133,9 @@ public class Web3Impl implements Web3 {
 
             if (fr.address instanceof String) {
                 logFilter.withContractAddress(stringHexToByteArray((String) fr.address));
-            } else if (fr.address instanceof String[]) {
-                List<byte[]> addr = new ArrayList<>();
-                for (String s : ((String[]) fr.address)) {
-                    addr.add(stringHexToByteArray(s));
-                }
-                logFilter.withContractAddress(addr.toArray(new byte[0][]));
+            } else if (fr.address instanceof Collection<?>) {
+                byte[][] addresses = collectionToBytesArray((Collection<?>) fr.address);
+                logFilter.withContractAddress(addresses);
             }
 
             if (fr.topics != null) {
@@ -1147,12 +1144,10 @@ public class Web3Impl implements Web3 {
                         logFilter.withTopic(null);
                     } else if (topic instanceof String) {
                         logFilter.withTopic(new DataWord(stringHexToByteArray((String) topic)).getData());
-                    } else if (topic instanceof String[]) {
-                        List<byte[]> t = new ArrayList<>();
-                        for (String s : ((String[]) topic)) {
-                            t.add(new DataWord(stringHexToByteArray(s)).getData());
-                        }
-                        logFilter.withTopic(t.toArray(new byte[0][]));
+                    } else if (topic instanceof Collection<?>) {
+                        byte[][] topics = collectionToBytesArray((Collection<?>) topic);
+
+                        logFilter.withTopic(topics);
                     }
                 }
             }
@@ -1193,6 +1188,15 @@ public class Web3Impl implements Web3 {
                 logger.debug("eth_newFilter(" + fr + "): " + str);
             }
         }
+    }
+
+    private static byte[][] collectionToBytesArray(Collection<?> topic) {
+        Collection<?> iterable = topic;
+
+        return iterable.stream()
+                .filter(String.class::isInstance).map(String.class::cast)
+                .map(TypeConverter::stringHexToByteArray)
+                .toArray(byte[][]::new);
     }
 
     @Override
