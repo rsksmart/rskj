99e250504c1fc36cd28848d4f0133a068652dd88
Lisandro <lisandro@rsk.co>
2017-11-15 14:45:08 -0300

    Removed timeout from being considered bad reputation

--- rskj-core/src/main/java/co/rsk/scoring/ScoringCalculator.java
+++ rskj-core/src/main/java/co/rsk/scoring/ScoringCalculator.java
@@ -18,7 +18,7 @@ public class ScoringCalculator {
     public boolean hasGoodReputation(PeerScoring scoring) {
         return scoring.getEventCounter(EventType.INVALID_BLOCK) < 1 &&
                 scoring.getEventCounter(EventType.INVALID_MESSAGE) < 1 &&
-                scoring.getEventCounter(EventType.INVALID_HEADER) < 1 &&
-                scoring.getEventCounter(EventType.TIMEOUT_MESSAGE) < 1;
+                scoring.getEventCounter(EventType.INVALID_HEADER) < 1;
+        //TODO: implement empty messages as responses so timeout can be handled as it should
     }
 }
