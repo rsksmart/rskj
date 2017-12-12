49c02cebe821b4332fe43be7028f1929deb39c4f
Lisandro <lisandro@rsk.co>
2017-12-12 16:27:06 -0300

    Removed sync for calcReceiptsTrie, setHash in trie will be reviewed later

--- rskj-core/src/main/java/co/rsk/core/bc/BlockChainImpl.java
+++ rskj-core/src/main/java/co/rsk/core/bc/BlockChainImpl.java
@@ -563,7 +563,7 @@ public class BlockChainImpl implements Blockchain, org.ethereum.facade.Blockchai
         return Block.getTxTrie(transactions).getHash();
     }
 
-    public synchronized static byte[] calcReceiptsTrie(List<TransactionReceipt> receipts) {
+    public static byte[] calcReceiptsTrie(List<TransactionReceipt> receipts) {
         Trie receiptsTrie = new TrieImpl();
 
         if (receipts == null || receipts.isEmpty())
