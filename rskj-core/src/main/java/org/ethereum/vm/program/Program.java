f1244d3110d2cb60d15bf76f3e5da569a9120b4d
SergioDemianLerner <SergioDemianLerner@users.noreply.github.com>
2017-12-22 12:13:49 -0300

    Fixed comments

--- rskj-core/src/main/java/org/ethereum/vm/program/Program.java
+++ rskj-core/src/main/java/org/ethereum/vm/program/Program.java
@@ -62,7 +62,7 @@ import static org.ethereum.util.ByteUtil.EMPTY_BYTE_ARRAY;
  * @since 01.06.2014
  */
 public class Program {
-    // These logs should never be enabled in Info
+    // These logs should never be in Info mode in production
     private static final Logger logger = LoggerFactory.getLogger("VM");
     private static final Logger gasLogger = LoggerFactory.getLogger("gas");
 
@@ -185,11 +185,11 @@ public class Program {
         isGasLogEnabled =gasLogger.isInfoEnabled();
 
         if (isLogEnabled ) {
-            gasLogger.info("WARNING! VM logging is enabled. This will the VM 200 times slower. Do not use in production.");
+            gasLogger.info("WARNING! VM logging is enabled. This will make the VM 200 times slower. Do not use in production.");
         }
 
         if (isGasLogEnabled) {
-            gasLogger.info("WARNING! Gas logging is enabled. This will the VM 200 times slower. Do not use in production.");
+            gasLogger.info("WARNING! Gas logging is enabled. This will the make VM 200 times slower. Do not use in production.");
         }
         this.invoke = programInvoke;
 
