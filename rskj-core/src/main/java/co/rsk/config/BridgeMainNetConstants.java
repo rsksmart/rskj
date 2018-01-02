5f923c567b123785c3aad135b4a81376dcc25c0f
Bernardo Codesido <bernardo@rsk.co>
2018-01-02 18:35:56 -0300

    Add genesis federation

--- rskj-core/src/main/java/co/rsk/config/BridgeMainNetConstants.java
+++ rskj-core/src/main/java/co/rsk/config/BridgeMainNetConstants.java
@@ -20,21 +20,21 @@ public class BridgeMainNetConstants extends BridgeConstants {
     BridgeMainNetConstants() {
         btcParamsString = NetworkParameters.ID_MAINNET;
 
-        BtcECKey federator0PublicKey = BtcECKey.fromPublicOnly(Hex.decode("039a060badbeb24bee49eb2063f616c0f0f0765d4ca646b20a88ce828f259fcdb9"));
-        BtcECKey federator1PublicKey = BtcECKey.fromPublicOnly(Hex.decode("02afc230c2d355b1a577682b07bc2646041b5d0177af0f98395a46018da699b6da"));
-        BtcECKey federator2PublicKey = BtcECKey.fromPublicOnly(Hex.decode("0206262e6bb2dceea515f77fae4928d87002a04b72f721034e1d4fbf3d84b16c72"));
-        BtcECKey federator3PublicKey = BtcECKey.fromPublicOnly(Hex.decode("03481d38fd2113f289347b7fd47e428127de02a078a7e28089ebe0150b74d9dcf7"));
-        BtcECKey federator4PublicKey = BtcECKey.fromPublicOnly(Hex.decode("029868937807b41dac42ff5a5b4a1d1711c4f3454f5826933465aa2614c5e90fdf"));
-        BtcECKey federator5PublicKey = BtcECKey.fromPublicOnly(Hex.decode("03c83e2dc1fbeaa54f0a8e8d482d46a32ef721322a4910a756fb07713f2dddbcb9"));
-        BtcECKey federator6PublicKey = BtcECKey.fromPublicOnly(Hex.decode("02629b1e976a5ed02194c0680f4f7b30f8388b51e935796ccee35e5b0fad915c3a"));
-        BtcECKey federator7PublicKey = BtcECKey.fromPublicOnly(Hex.decode("0344a3c38cd59afcba3edcebe143e025574594b001700dec41e59409bdbd0f2a09"));
-        BtcECKey federator8PublicKey = BtcECKey.fromPublicOnly(Hex.decode("023552f8144c944ffa220724cd4b5f455d75eaf59b17f73bdc1a7177a3e9bf7871"));
-        BtcECKey federator9PublicKey = BtcECKey.fromPublicOnly(Hex.decode("036d9d9bf6fa85bbb00a18b34e0d8baecf32c330c4b1920419c415e1005355f498"));
-        BtcECKey federator10PublicKey = BtcECKey.fromPublicOnly(Hex.decode("03bb42b0d32e781b88319dbc3aadc43c7a032c1931b641f5ae8340b8891bfdedbd"));
-        BtcECKey federator11PublicKey = BtcECKey.fromPublicOnly(Hex.decode("03dece3c5f5b7df1ae3f4542c38dd25932e332d9e960c2c1f24712657626498705"));
-        BtcECKey federator12PublicKey = BtcECKey.fromPublicOnly(Hex.decode("033965f98e9ec741fdd3281f5cf2a2a0ae89958f4bf4f6862ee73ac9bf2b49e0c7"));
-        BtcECKey federator13PublicKey = BtcECKey.fromPublicOnly(Hex.decode("0297d72f4c58b62495adbd49398b39d8fca69c6714ecaec49bd09e9dfcd9dc35cf"));
-        BtcECKey federator14PublicKey = BtcECKey.fromPublicOnly(Hex.decode("03c5dc2281b1bf3a8db339dceb4867bbcca1a633f3a65d5f80f6e8aca35b9b191c"));
+        BtcECKey federator0PublicKey = BtcECKey.fromPublicOnly(Hex.decode("04b53899c390573471ba30e5054f78376c5f797fda26dde7a760789f02908cbad2aafaaa2611606699ec4f82777a268b708dab346de4880cd223969f7bbe5422bf"));
+        BtcECKey federator1PublicKey = BtcECKey.fromPublicOnly(Hex.decode("047319afb15481dbeb3c426bcc37f9a30e7f51ceff586936d85548d9395bcc2344f6f082925b6b823106d21b2aa0572701bf1cd007c41d0c36822aac4cf04412ca"));
+        BtcECKey federator2PublicKey = BtcECKey.fromPublicOnly(Hex.decode("0455a2e9bf100c00fc0a214afd1bf272647c7824eb9cb055480962f0c382596a7049b97cdffcdca0f5cd199f4f4ec3a35f7b40a35a16d79b65579b153e7fddf48f"));
+        BtcECKey federator3PublicKey = BtcECKey.fromPublicOnly(Hex.decode("04566d5ded7c7db1aa7ee4ef6f76989fb42527fcfdcddcd447d6793b7d869e46f7e44c9bf49f31a5b5d9f09aa079e0dd86fb439f0e9f11ded3d305783a25eb4306"));
+        BtcECKey federator4PublicKey = BtcECKey.fromPublicOnly(Hex.decode("0494c817150f78607566e961b3c71df53a22022a80acbb982f83c0c8baac040adcb17171aa9ec8d8587098e0771f686ee61ac35279f9e5aadf9b06b738aa6d3720"));
+        BtcECKey federator5PublicKey = BtcECKey.fromPublicOnly(Hex.decode("0472cd46831f3b6afd4c044d160b7667e8ebf659d6cb51a825a3104df6ee0638c65325dce1ca5a6ceabed7010c2880ca36a07be3be03e858549972812e12dbce9b"));
+        BtcECKey federator6PublicKey = BtcECKey.fromPublicOnly(Hex.decode("04566d5ded7c7db1aa7ee4ef6f76989fb42527fcfdcddcd447d6793b7d869e46f7e44c9bf49f31a5b5d9f09aa079e0dd86fb439f0e9f11ded3d305783a25eb4306"));
+        BtcECKey federator7PublicKey = BtcECKey.fromPublicOnly(Hex.decode("044c532bcd5454f0eb8dd5f6caa9b0bffe91a5918cc45225e41d3d509de9ee0cc755033c6070103fc41a8551d8d765b7126e582cac099283fb177e410c24df25ea"));
+        BtcECKey federator8PublicKey = BtcECKey.fromPublicOnly(Hex.decode("041aabbeb9b27258f98c2bf21f36677ae7bae09eb2d8c958ef41a20a6e88626d261f17f8ec02af309b7b50c06e2baa05a57166266e038a0a7dce7b70386e8260a3"));
+        BtcECKey federator9PublicKey = BtcECKey.fromPublicOnly(Hex.decode("0445ef34f5ee218005c9c21227133e8568a4f3f11aeab919c66ff7b816ae1ffeeae024d50312de76a7950f8c6268fbf454335cf252f961a67c47e67dc06fa590ba"));
+        BtcECKey federator10PublicKey = BtcECKey.fromPublicOnly(Hex.decode("04550cc87fa9061162b1dd395a16662529c9d8094c0feca17905a3244713d65fe85cff7d7a1f67fefc865bfa34927b4457ec4481fa93c297441fae8c1dd0440cee"));
+        BtcECKey federator11PublicKey = BtcECKey.fromPublicOnly(Hex.decode("04481f02b7140acbf3fcdd9f72cf9a7d9484d8125e6df7c9451cfa55ba3b0772657bc2471f34ed4f12c42ab1b1587849feb847576011b8f79ab223e2bd3bfebb04"));
+        BtcECKey federator12PublicKey = BtcECKey.fromPublicOnly(Hex.decode("04f909ae15558c70cc751aff9b1f495199c325b13a9e5b934fd6299cd30ec50be804870f0dad7b9d1e3ed6a852ce42a30c0afcf803a48443e5a096591016a3b663"));
+        BtcECKey federator13PublicKey = BtcECKey.fromPublicOnly(Hex.decode("04c6018fcbd3e89f3cf9c7f48b3232ea3638eb8bf217e59ee290f5f0cfb2fb9259fd2c5fd43652022645cbfa62bf24c759102dca0746b25d69fffed1b2162dbfd4"));
+        BtcECKey federator14PublicKey = BtcECKey.fromPublicOnly(Hex.decode("04b65694ccccda83cbb1e56b31308acd08e993114c33f66a456b627c2c1c68bed605847b01ee22e2ee5014f547283a872d5ffb728802ec83e3761da32c4a01e705"));
 
         List<BtcECKey> genesisFederationPublicKeys = Lists.newArrayList(
                 federator0PublicKey, federator1PublicKey, federator2PublicKey,
@@ -49,7 +49,7 @@ public class BridgeMainNetConstants extends BridgeConstants {
         Instant genesisFederationAddressCreatedAt = Instant.ofEpochMilli(1514948400l);
 
         // Expected federation address is:
-        // 2NCJZnqZHjvTNn9CUR8WyB3253cB7tPYKUq
+        // 34zsQPsDRD3mzDWXy3ekBj2sDXPGdeLcL6
         genesisFederation = new Federation(
                 genesisFederationPublicKeys,
                 genesisFederationAddressCreatedAt,
