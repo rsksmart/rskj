1221f70bcc9ef3bd2579d48c0496565587f7e847
Ariel Mendelzon <amendelzon@rsk.co>
2018-01-23 13:51:25 +1300

    Removing unused imports

--- rskj-core/src/main/java/co/rsk/signing/ECDSACompositeSigner.java
+++ rskj-core/src/main/java/co/rsk/signing/ECDSACompositeSigner.java
@@ -1,9 +1,5 @@
 package co.rsk.signing;
 
-import org.apache.commons.collections4.CollectionUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
