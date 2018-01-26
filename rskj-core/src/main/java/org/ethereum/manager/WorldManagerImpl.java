037706fc34e5de03c10fd142fa4d6fca8b70ecd5
Diego López León <diegoll@rsk.co>
2018-01-26 18:00:08 -0300

    Add qualifier to CompositeEthereumListener bean

--- rskj-core/src/main/java/org/ethereum/manager/WorldManagerImpl.java
+++ rskj-core/src/main/java/org/ethereum/manager/WorldManagerImpl.java
@@ -35,6 +35,7 @@ import org.ethereum.net.server.ChannelManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PreDestroy;
@@ -69,7 +70,7 @@ public class WorldManagerImpl implements WorldManager {
                             HashRateCalculator hashRateCalculator,
                             ConfigCapabilities configCapabilities,
                             ChannelManager channelManager,
-                            EthereumListener listener,
+                            @Qualifier("compositeEthereumListener") EthereumListener listener,
                             BlockProcessor nodeBlockProcessor) {
         this.blockchain = blockchain;
         this.blockStore = blockStore;
