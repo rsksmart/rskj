d9b4dd10f109c5852589e24f25d8affb8f9ec088
Ariel Mendelzon <amendelzon@rsk.co>
2018-02-09 09:44:34 +1300

    Changes as per PR comments

--- rskj-core/src/main/java/org/ethereum/config/blockchain/testnet/TestNetFirstForkConfig.java
+++ rskj-core/src/main/java/org/ethereum/config/blockchain/testnet/TestNetFirstForkConfig.java
@@ -1,5 +1,6 @@
 package org.ethereum.config.blockchain.testnet;
 
+// TODO: find a proper name for the "FirstFork"
 public class TestNetFirstForkConfig extends TestNetAfterBridgeSyncConfig {
     @Override
     public boolean isRfs55() {
