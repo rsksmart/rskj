9859a7baf91e3330da7884fcdf9bb26819cc4895
Diego López León <diegoll@rsk.co>
2018-02-15 12:28:46 -0300

    Add RFS-94 hard fork flags

--- rskj-core/src/main/java/org/ethereum/config/blockchain/RegTestConfig.java
+++ rskj-core/src/main/java/org/ethereum/config/blockchain/RegTestConfig.java
@@ -80,4 +80,9 @@ public class RegTestConfig extends GenesisConfig {
     public boolean isRfs90() {
         return true;
     }
+
+    @Override
+    public boolean isRfs94() {
+        return true;
+    }
 }
