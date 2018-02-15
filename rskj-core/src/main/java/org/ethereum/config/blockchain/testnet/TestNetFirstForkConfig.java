945bf60d9efd444e32de8f6ef6e00aadcaf55da7
Joaquin Diaz Trepat <joaquin@rsk.co>
2018-02-15 12:41:24 -0300

    Hard fork implementation for static_call

--- rskj-core/src/main/java/org/ethereum/config/blockchain/testnet/TestNetFirstForkConfig.java
+++ rskj-core/src/main/java/org/ethereum/config/blockchain/testnet/TestNetFirstForkConfig.java
@@ -21,4 +21,9 @@ public class TestNetFirstForkConfig extends TestNetAfterBridgeSyncConfig {
     public boolean isRfs94() {
         return true;
     }
+
+   @Override
+    public boolean isRcs230() {
+        return true;
+    }
 }
