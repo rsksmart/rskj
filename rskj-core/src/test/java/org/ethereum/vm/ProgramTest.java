9b1989eada85b34c335178c6b72fb4b49c0fc6c4
Diego López León <diegoll@rsk.co>
2018-02-16 17:55:33 -0300

    Add support for return EXTCODESIZE for precompiled contracts

--- rskj-core/src/test/java/org/ethereum/vm/ProgramTest.java
+++ rskj-core/src/test/java/org/ethereum/vm/ProgramTest.java
@@ -111,6 +111,13 @@ public class ProgramTest {
     }
 
     @Test
+    public void callPrecompiledContractMethodThroughStub() {
+        ProgramResult result = TestContract.returnBridgeTest().executeFunction("invokeGetFeePerKb", BigInteger.ZERO);
+        Assert.assertFalse(result.isRevert());
+        Assert.assertNull(result.getException());
+    }
+
+    @Test
     public void returnDataCopyTest() {
         TestContract contract = TestContract.returnDataTest();
         ProgramResult result = contract.executeFunction("testCopy", BigInteger.ZERO);
