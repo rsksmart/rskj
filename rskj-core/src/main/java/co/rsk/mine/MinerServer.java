f515c1a3c6118be086da0434fb2a67ede1d30e02
Martín Medina <tinnmedina@gmail.com>
2018-02-20 02:35:49 -0300

    Replace fully qualified with import

--- rskj-core/src/main/java/co/rsk/mine/MinerServer.java
+++ rskj-core/src/main/java/co/rsk/mine/MinerServer.java
@@ -18,6 +18,8 @@
 
 package co.rsk.mine;
 
+import co.rsk.bitcoinj.core.BtcBlock;
+import co.rsk.bitcoinj.core.BtcTransaction;
 import co.rsk.core.RskAddress;
 import org.ethereum.core.Block;
 
@@ -35,12 +37,12 @@ public interface MinerServer {
     boolean isRunning();
 
     SubmitBlockResult submitBitcoinSolution(String blockHashForMergedMining,
-                                            co.rsk.bitcoinj.core.BtcBlock blockWithOnlyHeader,
-                                            co.rsk.bitcoinj.core.BtcTransaction coinbase,
+                                            BtcBlock blockWithOnlyHeader,
+                                            BtcTransaction coinbase,
                                             List<String> merkleHashes,
                                             int blockTxnCount);
 
-    SubmitBlockResult submitBitcoinBlock(String blockHashForMergedMining, co.rsk.bitcoinj.core.BtcBlock bitcoinMergedMiningBlock);
+    SubmitBlockResult submitBitcoinBlock(String blockHashForMergedMining, BtcBlock bitcoinMergedMiningBlock);
 
     boolean generateFallbackBlock();
 
