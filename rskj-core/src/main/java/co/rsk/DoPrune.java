e306cb7a3a561693350394c97f45a70323b5f057
ajlopez <webmaster@ajlopez.com>
2018-02-26 14:29:44 -0300

    Copying REMASC trie storage to alternative storage

--- rskj-core/src/main/java/co/rsk/DoPrune.java
+++ rskj-core/src/main/java/co/rsk/DoPrune.java
@@ -22,7 +22,9 @@ import co.rsk.blocks.FileBlockPlayer;
 import co.rsk.blocks.FileBlockRecorder;
 import co.rsk.config.RskSystemProperties;
 import co.rsk.core.Rsk;
+import co.rsk.core.RskAddress;
 import co.rsk.core.RskImpl;
+import co.rsk.crypto.Keccak256;
 import co.rsk.mine.MinerClient;
 import co.rsk.mine.MinerServer;
 import co.rsk.mine.TxBuilder;
@@ -34,6 +36,10 @@ import co.rsk.net.Metrics;
 import co.rsk.net.discovery.UDPServer;
 import co.rsk.net.handler.TxHandler;
 import co.rsk.rpc.netty.Web3HttpServer;
+import co.rsk.trie.Trie;
+import co.rsk.trie.TrieImpl;
+import co.rsk.trie.TrieStore;
+import co.rsk.trie.TrieStoreImpl;
 import org.ethereum.cli.CLIInterface;
 import org.ethereum.config.DefaultConfig;
 import org.ethereum.core.*;
@@ -42,15 +48,21 @@ import org.ethereum.net.server.ChannelManager;
 import org.ethereum.rpc.Web3;
 import org.ethereum.sync.SyncPool;
 import org.ethereum.util.BuildInfo;
+import org.ethereum.vm.PrecompiledContracts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.spongycastle.util.encoders.Hex;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Nullable;
+import java.util.List;
 import java.util.stream.Collectors;
+
+import static org.ethereum.datasource.DataSourcePool.levelDbByName;
+import static org.ethereum.util.ByteUtil.toHexString;
 
 @Component
 public class DoPrune {
@@ -86,10 +98,48 @@ public class DoPrune {
         CLIInterface.call(rskSystemProperties, args);
         logger.info("Running {},  core version: {}-{}", rskSystemProperties.genesisInfo(), rskSystemProperties.projectVersion(), rskSystemProperties.projectVersionModifier());
         BuildInfo.printInfo();
+
+        long height = this.blockchain.getBestBlock().getNumber();
+
+        String dataSourceName = getDataSourceName();
+        logger.info("Datasource Name {}", dataSourceName);
+        logger.info("Blockchain height {}", height);
+
+        TrieImpl source = new TrieImpl(new TrieStoreImpl(levelDbByName(this.rskSystemProperties, dataSourceName)), true);
+        TrieStore target = new TrieStoreImpl(levelDbByName(this.rskSystemProperties, dataSourceName + "B"));
+        this.processBlocks(height - 100, source, PrecompiledContracts.REMASC_ADDR, target);
+    }
+
+    public void processBlocks(long from, TrieImpl source, RskAddress contractAddress, TrieStore target) {
+        long n = from;
+
+        while (true) {
+            List<Block> blocks = this.blockchain.getBlocksByNumber(n);
+
+            if (blocks.isEmpty())
+                break;
+
+            for (Block b: blocks) {
+                byte[] stateRoot = b.getStateRoot();
+
+                logger.info("Block height {} State root {}", b.getNumber(), Hex.toHexString(stateRoot));
+                Repository repo = this.blockchain.getRepository().getSnapshotTo(stateRoot);
+
+                AccountState accountState = repo.getAccountState(contractAddress);
+                Trie contractStorage = source.getSnapshotTo(new Keccak256(accountState.getStateRoot()));
+                contractStorage.copyTo(target);
+            }
+
+            n++;
+        }
     }
 
     public void stop() {
         logger.info("Shutting down RSK node");
         rsk.close();
+    }
+
+    private static String getDataSourceName() {
+        return "details-storage/" + PrecompiledContracts.REMASC_ADDR;
     }
 }
