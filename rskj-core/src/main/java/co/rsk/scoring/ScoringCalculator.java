779a2f902046ea6df213f77fa87716df35b3fb5d
Lisandro <lisandro@rsk.co>
2018-04-03 18:11:09 -0300

    Punishment and bad scoring disabled

--- rskj-core/src/main/java/co/rsk/scoring/ScoringCalculator.java
+++ rskj-core/src/main/java/co/rsk/scoring/ScoringCalculator.java
@@ -16,9 +16,12 @@ public class ScoringCalculator {
      * @return  <tt>true</tt> if the peer has good reputation
      */
     public boolean hasGoodReputation(PeerScoring scoring) {
+        return true;
+        /*
         return  scoring.getEventCounter(EventType.INVALID_BLOCK) < 1 &&
                 scoring.getEventCounter(EventType.INVALID_MESSAGE) < 1 &&
                 scoring.getEventCounter(EventType.INVALID_HEADER) < 1;
         //TODO: implement empty messages as responses so timeout can be handled as it should
+        */
     }
 }
