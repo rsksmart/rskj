42eb576b91f33afb14069d0450c5ad858cdc9860
Martín Coll <mc@rsk.co>
2018-04-19 15:51:43 -0300

    Extract FederationSupport in preparation for decoupling Remasc from BridgeSupport

--- rskj-core/src/main/java/co/rsk/peg/BridgeSupport.java
+++ rskj-core/src/main/java/co/rsk/peg/BridgeSupport.java
@@ -68,8 +68,6 @@ public class BridgeSupport {
     private static final Logger logger = LoggerFactory.getLogger("BridgeSupport");
     private static final PanicProcessor panicProcessor = new PanicProcessor();
 
-    private enum StorageFederationReference { NONE, NEW, OLD, GENESIS;}
-
     private final List<String> FEDERATION_CHANGE_FUNCTIONS = Collections.unmodifiableList(Arrays.asList(
             "create",
             "add",
@@ -81,6 +79,8 @@ public class BridgeSupport {
     private final Repository rskRepository;
     private final RskSystemProperties config;
     private final BridgeEventLogger eventLogger;
+
+    private final FederationSupport federationSupport;
 
     private Context btcContext;
     private BtcBlockstoreWithCache btcBlockStore;
@@ -187,6 +187,7 @@ public class BridgeSupport {
         this.config = config;
         this.bridgeConstants = bridgeConstants;
         this.eventLogger = eventLogger;
+        this.federationSupport = new FederationSupport(provider, bridgeConstants, executionBlock);
     }
 
     private RepositoryBlockStore buildRepositoryBlockStore() throws BlockStoreException, IOException {
@@ -198,7 +199,7 @@ public class BridgeSupport {
         );
         if (btcBlockStore.getChainHead().getHeader().getHash().equals(btcParams.getGenesisBlock().getHash())) {
             // We are building the blockstore for the first time, so we have not set the checkpoints yet.
-            long time = getActiveFederation().getCreationTime().toEpochMilli();
+            long time = federationSupport.getActiveFederation().getCreationTime().toEpochMilli();
             InputStream checkpoints = this.getCheckPoints();
             if (time > 0 && checkpoints != null) {
                 CheckpointManager.checkpoint(btcParams, checkpoints, btcBlockStore, time);
@@ -1087,100 +1088,12 @@ public class BridgeSupport {
     }
 
     /**
-     * Returns the currently active federation reference.
-     * Logic is as follows:
-     * When no "new" federation is recorded in the blockchain, then return GENESIS
-     * When a "new" federation is present and no "old" federation is present, then return NEW
-     * When both "new" and "old" federations are present, then
-     * 1) If the "new" federation is at least bridgeConstants::getFederationActivationAge() blocks old,
-     * return the NEW
-     * 2) Otherwise, return OLD
-     * @return a reference to where the currently active federation is stored.
-     */
-    private StorageFederationReference getActiveFederationReference() {
-        Federation newFederation = provider.getNewFederation();
-
-        // No new federation in place, then the active federation
-        // is the genesis federation
-        if (newFederation == null) {
-            return StorageFederationReference.GENESIS;
-        }
-
-        Federation oldFederation = provider.getOldFederation();
-
-        // No old federation in place, then the active federation
-        // is the new federation
-        if (oldFederation == null) {
-            return StorageFederationReference.NEW;
-        }
-
-        // Both new and old federations in place
-        // If the minimum age has gone by for the new federation's
-        // activation, then that federation is the currently active.
-        // Otherwise, the old federation is still the currently active.
-        if (shouldFederationBeActive(newFederation)) {
-            return StorageFederationReference.NEW;
-        }
-
-        return StorageFederationReference.OLD;
-    }
-
-    /**
-     * Returns the currently retiring federation reference.
-     * Logic is as follows:
-     * When no "new" or "old" federation is recorded in the blockchain, then return empty.
-     * When both "new" and "old" federations are present, then
-     * 1) If the "new" federation is at least bridgeConstants::getFederationActivationAge() blocks old,
-     * return OLD
-     * 2) Otherwise, return empty
-     * @return the retiring federation.
-     */
-    private StorageFederationReference getRetiringFederationReference() {
-        Federation newFederation = provider.getNewFederation();
-        Federation oldFederation = provider.getOldFederation();
-
-        if (oldFederation == null || newFederation == null) {
-            return StorageFederationReference.NONE;
-        }
-
-        // Both new and old federations in place
-        // If the minimum age has gone by for the new federation's
-        // activation, then the old federation is the currently retiring.
-        // Otherwise, there is no retiring federation.
-        if (shouldFederationBeActive(newFederation)) {
-            return StorageFederationReference.OLD;
-        }
-
-        return StorageFederationReference.NONE;
-    }
-
-    private boolean shouldFederationBeActive(Federation federation) {
-        long federationAge = rskExecutionBlock.getNumber() - federation.getCreationBlockNumber();
-        return federationAge >= bridgeConstants.getFederationActivationAge();
-    }
-
-    private boolean amAwaitingFederationActivation() {
-        Federation newFederation = provider.getNewFederation();
-        Federation oldFederation = provider.getOldFederation();
-
-        return newFederation != null && oldFederation != null && !shouldFederationBeActive(newFederation);
-    }
-
-    /**
      * Returns the currently active federation.
      * See getActiveFederationReference() for details.
      * @return the currently active federation.
      */
     public Federation getActiveFederation() {
-        switch (getActiveFederationReference()) {
-            case NEW:
-                return provider.getNewFederation();
-            case OLD:
-                return provider.getOldFederation();
-            case GENESIS:
-            default:
-                return bridgeConstants.getGenesisFederation();
-        }
+        return federationSupport.getActiveFederation();
     }
 
     /**
@@ -1190,34 +1103,15 @@ public class BridgeSupport {
      */
     @Nullable
     public Federation getRetiringFederation() {
-        switch (getRetiringFederationReference()) {
-            case OLD:
-                return provider.getOldFederation();
-            case NONE:
-            default:
-                return null;
-        }
+        return federationSupport.getRetiringFederation();
     }
 
     private List<UTXO> getActiveFederationBtcUTXOs() throws IOException {
-        switch (getActiveFederationReference()) {
-            case OLD:
-                return provider.getOldFederationBtcUTXOs();
-            case NEW:
-            case GENESIS:
-            default:
-                return provider.getNewFederationBtcUTXOs();
-        }
+        return federationSupport.getActiveFederationBtcUTXOs();
     }
 
     private List<UTXO> getRetiringFederationBtcUTXOs() throws IOException {
-        switch (getRetiringFederationReference()) {
-            case OLD:
-                return provider.getOldFederationBtcUTXOs();
-            case NONE:
-            default:
-                return Collections.emptyList();
-        }
+        return federationSupport.getRetiringFederationBtcUTXOs();
     }
 
     /**
@@ -1233,7 +1127,7 @@ public class BridgeSupport {
      * @return the federation size
      */
     public Integer getFederationSize() {
-        return getActiveFederation().getPublicKeys().size();
+        return federationSupport.getFederationSize();
     }
 
     /**
@@ -1250,13 +1144,7 @@ public class BridgeSupport {
      * @return the federator's public key
      */
     public byte[] getFederatorPublicKey(int index) {
-        List<BtcECKey> publicKeys = getActiveFederation().getPublicKeys();
-
-        if (index < 0 || index >= publicKeys.size()) {
-            throw new IndexOutOfBoundsException(String.format("Federator index must be between 0 and {}", publicKeys.size() - 1));
-        }
-
-        return publicKeys.get(index).getPubKey();
+        return federationSupport.getFederatorPublicKey(index);
     }
 
     /**
@@ -1396,7 +1284,7 @@ public class BridgeSupport {
             return -1;
         }
 
-        if (amAwaitingFederationActivation()) {
+        if (federationSupport.amAwaitingFederationActivation()) {
             return -2;
         }
 
