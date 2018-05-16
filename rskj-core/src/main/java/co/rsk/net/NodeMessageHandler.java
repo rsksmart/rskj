c03540906dc217bd06c92d14f5f94bc414fbe480
ajlopez <webmaster@ajlopez.com>
2018-05-16 15:05:46 -0300

    AltSyncProcessor, first naive implementation

--- rskj-core/src/main/java/co/rsk/net/NodeMessageHandler.java
+++ rskj-core/src/main/java/co/rsk/net/NodeMessageHandler.java
@@ -56,6 +56,7 @@ public class NodeMessageHandler implements MessageHandler, Runnable {
     private final RskSystemProperties config;
     private final BlockProcessor blockProcessor;
     private final SyncProcessor syncProcessor;
+    private final AltSyncProcessor altSyncProcessor;
     private final ChannelManager channelManager;
     private final TransactionGateway transactionGateway;
     private final PeerScoringManager peerScoringManager;
@@ -74,6 +75,7 @@ public class NodeMessageHandler implements MessageHandler, Runnable {
     public NodeMessageHandler(RskSystemProperties config,
                               @Nonnull final BlockProcessor blockProcessor,
                               final SyncProcessor syncProcessor,
+                              final AltSyncProcessor altSyncProcessor,
                               @Nullable final ChannelManager channelManager,
                               @Nullable final TransactionGateway transactionGateway,
                               @Nullable final PeerScoringManager peerScoringManager,
@@ -83,9 +85,21 @@ public class NodeMessageHandler implements MessageHandler, Runnable {
         this.blockProcessor = blockProcessor;
         this.syncProcessor = syncProcessor;
         this.transactionGateway = transactionGateway;
+        this.altSyncProcessor = altSyncProcessor;
         this.blockValidationRule = blockValidationRule;
         this.cleanMsgTimestamp = System.currentTimeMillis();
         this.peerScoringManager = peerScoringManager;
+    }
+
+    // Old version used in tests
+    public NodeMessageHandler(RskSystemProperties config,
+                              @Nonnull final BlockProcessor blockProcessor,
+                              final SyncProcessor syncProcessor,
+                              @Nullable final ChannelManager channelManager,
+                              @Nullable final TransactionGateway transactionGateway,
+                              @Nullable final PeerScoringManager peerScoringManager,
+                              @Nonnull BlockValidationRule blockValidationRule) {
+        this(config, blockProcessor, syncProcessor, null, channelManager, transactionGateway, peerScoringManager, blockValidationRule);
     }
 
     /**
@@ -349,7 +363,13 @@ public class NodeMessageHandler implements MessageHandler, Runnable {
     private void processStatusMessage(@Nonnull final MessageChannel sender, @Nonnull final StatusMessage message) {
         final Status status = message.getStatus();
         logger.trace("Process status {}", status.getBestBlockNumber());
-        this.syncProcessor.processStatus(sender, status);
+
+        if (this.altSyncProcessor != null) {
+            this.altSyncProcessor.processStatus(sender.getPeerNodeID(), status);
+        }
+        else {
+            this.syncProcessor.processStatus(sender, status);
+        }
     }
 
     private void processGetBlockMessage(@Nonnull final MessageChannel sender, @Nonnull final GetBlockMessage message) {
@@ -393,11 +413,21 @@ public class NodeMessageHandler implements MessageHandler, Runnable {
     }
 
     private void processSkeletonResponseMessage(@Nonnull final MessageChannel sender, @Nonnull final SkeletonResponseMessage message) {
-        this.syncProcessor.processSkeletonResponse(sender, message);
+        if (this.altSyncProcessor != null) {
+            this.altSyncProcessor.processSkeletonResponse(sender.getPeerNodeID(), message);
+        }
+        else {
+            this.syncProcessor.processSkeletonResponse(sender, message);
+        }
     }
 
     private void processBlockHeadersResponseMessage(@Nonnull final MessageChannel sender, @Nonnull final BlockHeadersResponseMessage message) {
-        this.syncProcessor.processBlockHeadersResponse(sender, message);
+        if (this.altSyncProcessor != null) {
+            this.altSyncProcessor.processBlockHeadersResponse(sender.getPeerNodeID(), message);
+        }
+        else {
+            this.syncProcessor.processBlockHeadersResponse(sender, message);
+        }
     }
 
     private void processBodyRequestMessage(@Nonnull final MessageChannel sender, @Nonnull final BodyRequestMessage message) {
