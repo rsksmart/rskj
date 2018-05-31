7a8a0301f8eaf68d48d383c836d06b0e57ecb325
Diego López León <diegoll@rsk.co>
2018-05-31 10:26:12 -0300

    Add test cases for underscore name migration

--- rskj-core/src/test/java/co/rsk/cli/config/MigratorTest.java
+++ rskj-core/src/test/java/co/rsk/cli/config/MigratorTest.java
@@ -22,9 +22,11 @@ public class MigratorTest {
                 "    config = 13\n" +
                 "  }\n" +
                 "}\n" +
+                "flat.config = \"0.0.0.0\"\n" +
                 "another.config=\"don't change\"");
         Properties migrationProperties = new Properties();
         migrationProperties.put("inline.config.name", "inline.config.new.name");
+        migrationProperties.put("flat.config", "flat_config");
         migrationProperties.put("nested.nested.config", "nested.nested.new.config");
         migrationProperties.put("unknown.config", "none");
         migrationProperties.put("[new]new.key", "new value");
@@ -34,6 +36,9 @@ public class MigratorTest {
         Config config = ConfigFactory.parseString(migratedConfiguration);
         assertThat(config.hasPath("inline.config.name"), is(false));
         assertThat(config.getString("inline.config.new.name"), is("value"));
+        assertThat(config.hasPath("flat.config"), is(false));
+        assertThat(config.hasPath("flat_config"), is(true));
+        assertThat(config.getString("flat_config"), is("0.0.0.0"));
         assertThat(config.hasPath("nested.nested.config"), is(false));
         assertThat(config.getInt("nested.nested.new.config"), is(13));
         assertThat(config.hasPath("unknown.config"), is(false));
