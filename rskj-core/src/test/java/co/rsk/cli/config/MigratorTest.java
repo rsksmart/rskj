be3e75faee06af49e27e58bc5545d7844e7f7c01
Diego López León <diegoll@rsk.co>
2018-05-31 10:25:18 -0300

    Remove unused import

--- rskj-core/src/test/java/co/rsk/cli/config/MigratorTest.java
+++ rskj-core/src/test/java/co/rsk/cli/config/MigratorTest.java
@@ -4,7 +4,6 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import org.junit.Test;
 
-import java.io.BufferedReader;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.Properties;
