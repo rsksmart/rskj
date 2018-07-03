6fc3a7c1cbd414d3f203d21c810e06bd3a42319d
pedro <pedro@rsk.co>
2018-07-03 09:10:57 -0300

    Use only 2 hsm feds to test

--- rskj-core/src/main/java/co/rsk/config/BridgeDevNetConstants.java
+++ rskj-core/src/main/java/co/rsk/config/BridgeDevNetConstants.java
@@ -38,13 +38,14 @@ public class BridgeDevNetConstants extends BridgeConstants {
     BridgeDevNetConstants() {
         btcParamsString = NetworkParameters.ID_TESTNET;
 
-        BtcECKey federator0PublicKey = BtcECKey.fromPublicOnly(Hex.decode("0234ab441aa5edb1c7341315e21408c3947cce345156c465b3336e8c6a5552f35f"));
-        BtcECKey federator1PublicKey = BtcECKey.fromPublicOnly(Hex.decode("03301f6c4422aa96d85f52a93612a0c6eeea3d04cfa32f97a7a764c67e062e992a"));
+        //BtcECKey federator0PublicKey = BtcECKey.fromPublicOnly(Hex.decode("0234ab441aa5edb1c7341315e21408c3947cce345156c465b3336e8c6a5552f35f"));
+        //BtcECKey federator1PublicKey = BtcECKey.fromPublicOnly(Hex.decode("03301f6c4422aa96d85f52a93612a0c6eeea3d04cfa32f97a7a764c67e062e992a"));
         //BtcECKey federator2PublicKey = BtcECKey.fromPublicOnly(Hex.decode("02d33a1f8f5cfa2f7be71b0002710f4c8f3ea44fef40056be7b89ed3ca0eb3431c"));
-        BtcECKey federator2PublicKey = BtcECKey.fromPublicOnly(Hex.decode("03019a43d8a829b3287baf1fea4ae86138691d14a4deddd07e6f3fe8507cda8743"));
+        BtcECKey federator0PublicKey = BtcECKey.fromPublicOnly(Hex.decode("03019a43d8a829b3287baf1fea4ae86138691d14a4deddd07e6f3fe8507cda8743"));
+        BtcECKey federator1PublicKey = BtcECKey.fromPublicOnly(Hex.decode("029c76c70309b6324e1c9f7d2db9aff4456000b016ce5d8ac3e0d05bf0137b9ca5"));
 
         List<BtcECKey> genesisFederationPublicKeys = Lists.newArrayList(
-                federator0PublicKey, federator1PublicKey, federator2PublicKey
+                federator0PublicKey, federator1PublicKey
         );
         
         // Currently set to:
