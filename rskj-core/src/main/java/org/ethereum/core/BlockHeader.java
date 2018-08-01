be07a9710efc83bba37718246615d49bfdcb7056
Martín Coll <mc@rsk.co>
2018-08-01 18:58:13 -0300

    Deserialize merkle proof to empty byte array

--- rskj-core/src/main/java/org/ethereum/core/BlockHeader.java
+++ rskj-core/src/main/java/org/ethereum/core/BlockHeader.java
@@ -172,6 +172,10 @@ public class BlockHeader {
         this.sealed = sealed;
     }
 
+    private byte[] orEmpty(byte[] rlpData) {
+        return rlpData == null ? new byte[] {} : rlpData;
+    }
+
     public BlockHeader(byte[] parentHash, byte[] unclesHash, byte[] coinbase,
                        byte[] logsBloom, byte[] difficulty, long number,
                        byte[] gasLimit, long gasUsed, long timestamp,
