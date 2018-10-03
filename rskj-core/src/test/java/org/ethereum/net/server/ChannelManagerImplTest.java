f92d8daafb0e89301cfab2a978e97a73ee266d52
Lisandro <lisandro@rsk.co>
2018-10-03 17:16:48 -0300

    Add test to check a channel can be rejected

--- rskj-core/src/test/java/org/ethereum/net/server/ChannelManagerImplTest.java
+++ rskj-core/src/test/java/org/ethereum/net/server/ChannelManagerImplTest.java
@@ -19,11 +19,23 @@
 
 package org.ethereum.net.server;
 
-import org.junit.Before;
+import co.rsk.config.TestSystemProperties;
+import co.rsk.net.NodeID;
+import org.ethereum.crypto.HashUtil;
+import org.ethereum.net.NodeManager;
+import org.ethereum.sync.SyncPool;
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Roman Mandeleil
@@ -32,15 +44,11 @@ import static org.junit.Assert.assertEquals;
 @Ignore
 public class ChannelManagerImplTest {
 
-    private ChannelManagerImpl channelManagerImpl;
-
-    @Before
-    public void setUp() {
-        channelManagerImpl = new ChannelManagerImpl(null, null);
-    }
 
     @Test
     public void getNumberOfPeersToSendStatusTo() {
+        ChannelManagerImpl channelManagerImpl = new ChannelManagerImpl(new TestSystemProperties(), null);;
+
         assertEquals(1, channelManagerImpl.getNumberOfPeersToSendStatusTo(1));
         assertEquals(2, channelManagerImpl.getNumberOfPeersToSendStatusTo(2));
         assertEquals(3, channelManagerImpl.getNumberOfPeersToSendStatusTo(3));
@@ -50,6 +58,40 @@ public class ChannelManagerImplTest {
         assertEquals(4, channelManagerImpl.getNumberOfPeersToSendStatusTo(20));
         assertEquals(5, channelManagerImpl.getNumberOfPeersToSendStatusTo(25));
         assertEquals(10, channelManagerImpl.getNumberOfPeersToSendStatusTo(1000));
+    }
+
+    @Test
+    public void blockAddressIsAvailable() throws UnknownHostException {
+        ChannelManagerImpl channelManagerImpl = new ChannelManagerImpl(new TestSystemProperties(), null);;
+        Assert.assertTrue(channelManagerImpl.isAddressBlockAvailable(InetAddress.getLocalHost()));
+    }
+
+    @Test
+    public void blockAddressIsNotAvailable() throws UnknownHostException {
+        TestSystemProperties config = mock(TestSystemProperties.class);
+        when(config.maxConnectionsPerAddressBlock()).thenReturn(1);
+        when(config.blockAddressCIDR()).thenReturn(32);
+
+        SyncPool syncPool = mock(SyncPool.class);
+        ChannelManagerImpl channelManager = new ChannelManagerImpl(config, syncPool);
+
+        String remoteId = "remoteId";
+        NodeManager nodeManager = new NodeManager(null, config);
+
+        Channel peer = spy(new Channel(null, null, nodeManager, null, null, remoteId));
+        peer.setInetSocketAddress(new InetSocketAddress("127.0.0.1",5554));
+        peer.setNode(new NodeID(HashUtil.randomPeerId()).getID());
+        when(peer.isProtocolsInitialized()).thenReturn(true);
+        when(peer.isActive()).thenReturn(true);
+        when(peer.isUsingNewProtocol()).thenReturn(true);
+
+        Channel otherPeer = new Channel(null, null, nodeManager, null, null, remoteId);
+        otherPeer.setInetSocketAddress(new InetSocketAddress("127.0.0.1",5554));
+
+        channelManager.add(peer);
+        channelManager.tryProcessNewPeers();
+
+        Assert.assertFalse(channelManager.isAddressBlockAvailable(otherPeer.getInetSocketAddress().getAddress()));
     }
 
 }
