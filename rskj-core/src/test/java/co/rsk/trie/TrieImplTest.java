736abc022360b643962e41795043463d079c76ab
SergioDemianLerner <SergioDemianLerner@users.noreply.github.com>
2018-10-23 22:48:38 -0300

    Check if unitrie trieconverter works

--- rskj-core/src/test/java/co/rsk/trie/TrieImplTest.java
+++ rskj-core/src/test/java/co/rsk/trie/TrieImplTest.java
@@ -19,7 +19,11 @@
 package co.rsk.trie;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.spongycastle.util.encoders.Hex;
+
+import java.util.Random;
 
 /**
  * Created by ajlopez on 22/08/2016.
@@ -28,6 +32,27 @@ public class TrieImplTest {
     @Test
     public void bytesToKey() {
         Assert.assertArrayEquals(new byte[] { 0x01, 0x00, 0x01, 0x00, 0x01, 0x00, 0x01, 0x00 }, TrieImpl.bytesToKey(new byte[] { (byte)0xaa }));
+    }
+
+    public static byte[] randomKey(Random random ){
+
+        byte[] randomKey = new byte[random.nextInt(20)+1];
+        random.nextBytes(randomKey);
+        return randomKey;
+    }
+
+    @Ignore
+    @Test
+    public void trieConverter() {
+        int max = 100;
+        Trie trie = new TrieImpl();
+        Random rand = new Random(0); // seed with zero
+
+        for (int k = 0; k < max; k++)
+            trie = trie.put(randomKey(rand), TrieImplValueTest.makeValue(rand.nextInt(64)+1));
+
+        //Assert.assertEquals(trie.getHash().toHexString(),"");
+        System.out.println(trie.getHash().toHexString());
     }
 }
 
