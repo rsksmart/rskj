a199743eda60143a7f41f19aefbc19b28e2c16a1
ajlopez <webmaster@ajlopez.com>
2018-11-06 14:30:35 -0300

    Using ContractStorageStoreFactory

--- rskj-core/src/main/java/co/rsk/db/ContractStorageStoreFactory.java
+++ rskj-core/src/main/java/co/rsk/db/ContractStorageStoreFactory.java
@@ -1,7 +1,27 @@
 package co.rsk.db;
 
+import co.rsk.trie.TrieStore;
+import static org.ethereum.util.ByteUtil.toHexString;
+
 /**
  * Created by Angel on 11/6/2018.
  */
 public class ContractStorageStoreFactory {
+    private TrieStore.Pool pool;
+
+    public ContractStorageStoreFactory(TrieStore.Pool pool) {
+        this.pool = pool;
+    }
+
+    public TrieStore getTrieStore(byte[] address) {
+        return this.pool.getInstanceFor(getStorageNameForAddress(address));
+    }
+
+    private static String getUnifiedStorageName() {
+        return "contracts-storage";
+    }
+
+    private static String getStorageNameForAddress(byte[] address) {
+        return "details-storage/" + toHexString(address);
+    }
 }
