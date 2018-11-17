5412c839df27412af5cc7d44852993b17523d43e
ajlopez <webmaster@ajlopez.com>
2018-11-17 14:36:33 -0300

    Setting LevelDB cache

--- rskj-core/src/main/java/org/ethereum/datasource/LevelDbDataSource.java
+++ rskj-core/src/main/java/org/ethereum/datasource/LevelDbDataSource.java
@@ -86,7 +86,7 @@ public class LevelDbDataSource implements KeyValueDataSource {
             options.compressionType(CompressionType.NONE);
             options.blockSize(10 * 1024 * 1024);
             options.writeBufferSize(10 * 1024 * 1024);
-            options.cacheSize(0);
+            options.cacheSize(1024 * 1024 * 1024);
             options.paranoidChecks(true);
             options.verifyChecksums(true);
 
