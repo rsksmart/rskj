5a3913fa4a39ca6e77b168eedab8b49eea9397e8
Ariel Mendelzon <amendelzon@rsk.co>
2018-12-19 16:53:23 +1100

    Removing unused imports

--- rskj-core/src/test/java/co/rsk/peg/BridgeStorageProviderTest.java
+++ rskj-core/src/test/java/co/rsk/peg/BridgeStorageProviderTest.java
@@ -41,14 +41,12 @@ import org.ethereum.util.RLP;
 import org.ethereum.vm.DataWord;
 import org.ethereum.vm.PrecompiledContracts;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.internal.util.reflection.Whitebox;
 import org.mockito.invocation.InvocationOnMock;
 import org.powermock.api.mockito.PowerMockito;
-import org.powermock.api.mockito.internal.PowerMockitoCore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
