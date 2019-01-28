ef66b32341bfb9cd439faa5ab3c295de26327d23
ajlopez <webmaster@ajlopez.com>
2019-01-28 19:36:16 -0300

    BloomFilter or

--- rskj-core/src/test/java/co/rsk/logfilter/BloomFilterTest.java
+++ rskj-core/src/test/java/co/rsk/logfilter/BloomFilterTest.java
@@ -13,4 +13,62 @@ public class BloomFilterTest {
 
         Assert.assertEquals(BloomFilter.BLOOM_BITS, bloomFilter.size());
     }
+
+    @Test
+    public void orEmptyBloomFiters() {
+        BloomFilter bloomFilter1 = new BloomFilter();
+        BloomFilter bloomFilter2 = new BloomFilter();
+
+        BloomFilter result = bloomFilter1.or(bloomFilter2);
+
+        Assert.assertNotNull(result);
+
+        for (int k = 0; k < BloomFilter.BLOOM_BITS; k++) {
+            Assert.assertFalse(result.get(k));
+        }
+    }
+
+    @Test
+    public void orBloomFitersWithSameBitOn() {
+        BloomFilter bloomFilter1 = new BloomFilter();
+        BloomFilter bloomFilter2 = new BloomFilter();
+
+        bloomFilter1.set(42);
+        bloomFilter2.set(42);
+
+        BloomFilter result = bloomFilter1.or(bloomFilter2);
+
+        Assert.assertNotNull(result);
+
+        for (int k = 0; k < BloomFilter.BLOOM_BITS; k++) {
+            if (k == 42) {
+                Assert.assertTrue(result.get(k));
+            }
+            else {
+                Assert.assertFalse(result.get(k));
+            }
+        }
+    }
+
+    @Test
+    public void orBloomFitersWithDifferentBitsOn() {
+        BloomFilter bloomFilter1 = new BloomFilter();
+        BloomFilter bloomFilter2 = new BloomFilter();
+
+        bloomFilter1.set(42);
+        bloomFilter2.set(3);
+
+        BloomFilter result = bloomFilter1.or(bloomFilter2);
+
+        Assert.assertNotNull(result);
+
+        for (int k = 0; k < BloomFilter.BLOOM_BITS; k++) {
+            if (k == 42 || k == 3) {
+                Assert.assertTrue(result.get(k));
+            }
+            else {
+                Assert.assertFalse(result.get(k));
+            }
+        }
+    }
 }
