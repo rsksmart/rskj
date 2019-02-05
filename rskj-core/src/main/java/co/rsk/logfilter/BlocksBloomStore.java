fd25053b3c546965cd95b32cf9cfe9f023b06fae
ajlopez <webmaster@ajlopez.com>
2019-02-05 15:51:24 -0300

    BlocksBloomStore initial implementation

---
+++ rskj-core/src/main/java/co/rsk/logfilter/BlocksBloomStore.java
@@ -0,0 +1,25 @@
+package co.rsk.logfilter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by ajlopez on 05/02/2019.
+ */
+public class BlocksBloomStore {
+    public static final int NO_BLOCKS = 64;
+
+    private final Map<Long, BlocksBloom> blocksBloom = new HashMap<>();
+
+    public BlocksBloom getBlocksBloomByNumber(long number) {
+        return this.blocksBloom.get(firstNumberInRange(number));
+    }
+
+    public static long firstNumberInRange(long number) {
+        return number - (number % NO_BLOCKS);
+    }
+
+    public static long lastNumberInRange(long number) {
+        return firstNumberInRange(number) + NO_BLOCKS - 1;
+    }
+}
