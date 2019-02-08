ab0899e7323c1fdf0d2f36a1e5e047e70a8a83c9
Ariel Mendelzon <amendelzon@rsk.co>
2019-02-08 14:19:28 +1300

    Updated PrecompiledContractAddressTests with the new BTOUtils contract

--- rskj-core/src/test/java/co/rsk/vm/PrecompiledContractAddressTests.java
+++ rskj-core/src/test/java/co/rsk/vm/PrecompiledContractAddressTests.java
@@ -20,10 +20,15 @@ package co.rsk.vm;
 
 import co.rsk.config.TestSystemProperties;
 import co.rsk.core.RskAddress;
+import org.ethereum.config.BlockchainConfig;
+import org.ethereum.core.Blockchain;
 import org.ethereum.vm.DataWord;
 import org.ethereum.vm.PrecompiledContracts;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Sergio Demian Lerner on 12/10/2018.
@@ -38,6 +43,7 @@ public class PrecompiledContractAddressTests {
     public static final String SAMPLE_ADDR_STR = "0000000000000000000000000000000001000005";
     public static final String BRIDGE_ADDR_STR = "0000000000000000000000000000000001000006";
     public static final String REMASC_ADDR_STR = "0000000000000000000000000000000001000008";
+    public static final String BTOUTILS_ADDR_STR = "0000000000000000000000000000000001000009";
 
     private final TestSystemProperties config = new TestSystemProperties();
 
@@ -52,17 +58,23 @@ public class PrecompiledContractAddressTests {
         //checkAddr(pcList,SAMPLE_ADDR_STR ,"SamplePrecompiledContract");
         checkAddr(pcList,BRIDGE_ADDR_STR ,"Bridge");
         checkAddr(pcList,REMASC_ADDR_STR ,"RemascContract");
+        checkAddr(pcList,BTOUTILS_ADDR_STR,"BTOUtils");
     }
 
     void checkAddr(PrecompiledContracts pcList,String addr,String className) {
+        BlockchainConfig bcConfig = mock(BlockchainConfig.class);
+
+        // Enabling necessary RSKIPs for every precompiled contract to be available
+        when(bcConfig.isRskip106()).thenReturn(true);
+
         RskAddress a;
         a = new RskAddress(addr);
-        PrecompiledContracts.PrecompiledContract pc = pcList.getContractForAddress(null,new DataWord(a.getBytes()));
+        PrecompiledContracts.PrecompiledContract pc = pcList.getContractForAddress(bcConfig,new DataWord(a.getBytes()));
         Assert.assertEquals(className,pc.getClass().getSimpleName());
         // Do not get precompiles if the 12-byte prefix is non-zero.
         DataWord dw = new DataWord(a.getBytes());
         dw.getData()[0] = (byte) 0xff; // MSB
-        PrecompiledContracts.PrecompiledContract pc2 = pcList.getContractForAddress(null,dw);
+        PrecompiledContracts.PrecompiledContract pc2 = pcList.getContractForAddress(bcConfig,dw);
         Assert.assertEquals(null,pc2);
 
     }
