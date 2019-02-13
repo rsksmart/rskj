b6d5dac497d86782a4b1f53df0b10c6c71a3c46e
Ariel Mendelzon <amendelzon@rsk.co>
2019-02-13 13:56:03 +1300

    Refactor performance tests
    - Bugfixes
    - Extracting RepositoryTrackWithBenchmarking statistics section to BenchmarkedRepository interface
    - Extract common precompiled contract behavior from BridgePerformanceTest and BridgePerformanceTestCase into common framework
    - New PrecompiledContractPerfomanceTest class
    - New PrecompiledContractPerformanceTestCase class
    - BridgePerformanceTest now extends PrecompiledContractPerformanceTest
    - BridgePerformanceTestCase now extends PrecompiledContractPerformanceTestCase
    - Identity precompiled contract test case and test suite
    - Add the possibility of executing a callback with the precompiled method execution result
    - New class CombinedExecutionStats
    - Enhanced Mean capabilities

--- rskj-core/src/test/java/co/rsk/peg/performance/UpdateCollectionsTest.java
+++ rskj-core/src/test/java/co/rsk/peg/performance/UpdateCollectionsTest.java
@@ -61,7 +61,7 @@ public class UpdateCollectionsTest extends BridgePerformanceTestCase {
                 numCases,
                 abiEncoder,
                 storageInitializer,
-                Helper.getZeroValueRandomSenderTxBuilder(),
+                Helper.getZeroValueTxBuilder(Helper.getRandomFederatorECKey()),
                 Helper.getRandomHeightProvider(10), stats
         );
     }
@@ -120,7 +120,7 @@ public class UpdateCollectionsTest extends BridgePerformanceTestCase {
                 numCases,
                 abiEncoder,
                 storageInitializer,
-                Helper.getZeroValueRandomSenderTxBuilder(),
+                Helper.getZeroValueTxBuilder(Helper.getRandomFederatorECKey()),
                 Helper.getRandomHeightProvider(10),
                 stats
         );
@@ -187,7 +187,7 @@ public class UpdateCollectionsTest extends BridgePerformanceTestCase {
                 numCases,
                 abiEncoder,
                 storageInitializer,
-                Helper.getZeroValueRandomSenderTxBuilder(),
+                Helper.getZeroValueTxBuilder(Helper.getRandomFederatorECKey()),
                 heightProvider,
                 stats
         );
