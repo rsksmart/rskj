fef1b92c05b492ef6e214c6e65e693ff40fc8c18
ajlopez <webmaster@ajlopez.com>
2019-02-18 16:06:08 -0300

    BlocksBloomStore with noBlocks

--- rskj-core/src/test/java/co/rsk/logfilter/BlocksBloomStoreTest.java
+++ rskj-core/src/test/java/co/rsk/logfilter/BlocksBloomStoreTest.java
@@ -10,28 +10,32 @@ import org.junit.Test;
 public class BlocksBloomStoreTest {
     @Test
     public void getFirstNumberInRange() {
-        Assert.assertEquals(0, BlocksBloomStore.firstNumberInRange(0));
-        Assert.assertEquals(0, BlocksBloomStore.firstNumberInRange(1));
-        Assert.assertEquals(BlocksBloomStore.NO_BLOCKS, BlocksBloomStore.firstNumberInRange(BlocksBloomStore.NO_BLOCKS));
-        Assert.assertEquals(BlocksBloomStore.NO_BLOCKS, BlocksBloomStore.firstNumberInRange(BlocksBloomStore.NO_BLOCKS * 2 - 1));
+        BlocksBloomStore blocksBloomStore = new BlocksBloomStore(64);
+
+        Assert.assertEquals(0, blocksBloomStore.firstNumberInRange(0));
+        Assert.assertEquals(0, blocksBloomStore.firstNumberInRange(1));
+        Assert.assertEquals(blocksBloomStore.getNoBlocks(), blocksBloomStore.firstNumberInRange(blocksBloomStore.getNoBlocks()));
+        Assert.assertEquals(blocksBloomStore.getNoBlocks(), blocksBloomStore.firstNumberInRange(blocksBloomStore.getNoBlocks() * 2 - 1));
     }
 
     @Test
     public void getLastNumberInRange() {
-        Assert.assertEquals(BlocksBloomStore.NO_BLOCKS - 1, BlocksBloomStore.lastNumberInRange(0));
-        Assert.assertEquals(BlocksBloomStore.NO_BLOCKS - 1, BlocksBloomStore.lastNumberInRange(1));
-        Assert.assertEquals(BlocksBloomStore.NO_BLOCKS * 2 - 1, BlocksBloomStore.lastNumberInRange(BlocksBloomStore.NO_BLOCKS));
-        Assert.assertEquals(BlocksBloomStore.NO_BLOCKS * 2 - 1, BlocksBloomStore.lastNumberInRange(BlocksBloomStore.NO_BLOCKS * 2 - 1));
+        BlocksBloomStore blocksBloomStore = new BlocksBloomStore(64);
+
+        Assert.assertEquals(blocksBloomStore.getNoBlocks() - 1, blocksBloomStore.lastNumberInRange(0));
+        Assert.assertEquals(blocksBloomStore.getNoBlocks() - 1, blocksBloomStore.lastNumberInRange(1));
+        Assert.assertEquals(blocksBloomStore.getNoBlocks() * 2 - 1, blocksBloomStore.lastNumberInRange(blocksBloomStore.getNoBlocks()));
+        Assert.assertEquals(blocksBloomStore.getNoBlocks() * 2 - 1, blocksBloomStore.lastNumberInRange(blocksBloomStore.getNoBlocks() * 2 - 1));
     }
 
     @Test
     public void noBlocksBloom() {
-        BlocksBloomStore blocksBloomStore = new BlocksBloomStore();
+        BlocksBloomStore blocksBloomStore = new BlocksBloomStore(64);
 
         Assert.assertNull(blocksBloomStore.getBlocksBloomByNumber(0));
         Assert.assertNull(blocksBloomStore.getBlocksBloomByNumber(1));
-        Assert.assertNull(blocksBloomStore.getBlocksBloomByNumber(BlocksBloomStore.NO_BLOCKS - 1));
-        Assert.assertNull(blocksBloomStore.getBlocksBloomByNumber(BlocksBloomStore.NO_BLOCKS));
+        Assert.assertNull(blocksBloomStore.getBlocksBloomByNumber(blocksBloomStore.getNoBlocks() - 1));
+        Assert.assertNull(blocksBloomStore.getBlocksBloomByNumber(blocksBloomStore.getNoBlocks()));
     }
 
     @Test
@@ -45,13 +49,13 @@ public class BlocksBloomStoreTest {
         Bloom bloom1 = new Bloom(bytes1);
         Bloom bloom2 = new Bloom(bytes2);
 
-        blocksBloom.addBlockBloom(BlocksBloomStore.NO_BLOCKS, bloom1);
-        blocksBloom.addBlockBloom(BlocksBloomStore.NO_BLOCKS + 1, bloom2);
+        BlocksBloomStore blocksBloomStore = new BlocksBloomStore(64);
 
-        BlocksBloomStore blocksBloomStore = new BlocksBloomStore();
+        blocksBloom.addBlockBloom(blocksBloomStore.getNoBlocks(), bloom1);
+        blocksBloom.addBlockBloom(blocksBloomStore.getNoBlocks() + 1, bloom2);
 
         blocksBloomStore.setBlocksBloom(blocksBloom);
 
-        Assert.assertSame(blocksBloom, blocksBloomStore.getBlocksBloomByNumber(BlocksBloomStore.NO_BLOCKS));
+        Assert.assertSame(blocksBloom, blocksBloomStore.getBlocksBloomByNumber(blocksBloomStore.getNoBlocks()));
     }
 }
