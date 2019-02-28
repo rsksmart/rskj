350bf3225367bf6f0e08c951a5b00309146470ae
Ariel Mendelzon <amendelzon@rsk.co>
2019-03-01 12:51:55 +1300

    Fixing rebasing issues

--- rskj-core/src/test/java/co/rsk/peg/BridgeTest.java
+++ rskj-core/src/test/java/co/rsk/peg/BridgeTest.java
@@ -50,6 +50,7 @@ import org.ethereum.core.CallTransaction;
 import org.ethereum.core.Repository;
 import org.ethereum.core.Transaction;
 import org.ethereum.crypto.ECKey;
+import org.ethereum.datasource.HashMapDB;
 import org.ethereum.rpc.TypeConverter;
 import org.ethereum.util.RskTestFactory;
 import org.ethereum.vm.PrecompiledContracts;
