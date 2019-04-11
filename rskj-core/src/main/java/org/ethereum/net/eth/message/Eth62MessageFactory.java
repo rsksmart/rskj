d424025f5cfc1761950fa3606c6cd02a61a17745
Lisandro <lisandro@rsk.co>
2019-04-11 15:11:09 -0300

    Removed GetBlockBodiesMessage

--- rskj-core/src/main/java/org/ethereum/net/eth/message/Eth62MessageFactory.java
+++ rskj-core/src/main/java/org/ethereum/net/eth/message/Eth62MessageFactory.java
@@ -38,8 +38,6 @@ public class Eth62MessageFactory implements MessageFactory {
         switch (receivedCommand) {
             case STATUS:
                 return new StatusMessage(encoded);
-            case GET_BLOCK_BODIES:
-                return new GetBlockBodiesMessage(encoded);
             case BLOCK_BODIES:
                 return new BlockBodiesMessage(encoded);
             case NEW_BLOCK:
