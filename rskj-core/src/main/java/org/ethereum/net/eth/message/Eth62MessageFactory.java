7d3487f4e9a26081fdf998d99cbba81d61d818a5
Lisandro <lisandro@rsk.co>
2019-04-11 14:43:25 -0300

    Removed NewBlockHashesMessage

--- rskj-core/src/main/java/org/ethereum/net/eth/message/Eth62MessageFactory.java
+++ rskj-core/src/main/java/org/ethereum/net/eth/message/Eth62MessageFactory.java
@@ -38,8 +38,6 @@ public class Eth62MessageFactory implements MessageFactory {
         switch (receivedCommand) {
             case STATUS:
                 return new StatusMessage(encoded);
-            case NEW_BLOCK_HASHES:
-                return new NewBlockHashesMessage(encoded);
             case TRANSACTIONS:
                 return new TransactionsMessage(encoded);
             case GET_BLOCK_HEADERS:
