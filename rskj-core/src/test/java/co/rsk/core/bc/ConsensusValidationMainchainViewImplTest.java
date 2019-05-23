08206ada4b6ad3bbe5cadaed26432f6fa0c56d27
Martín Medina <tinnmedina@gmail.com>
2019-05-23 17:37:10 -0300

    Add MainchainView for consensus validation

---
+++ rskj-core/src/test/java/co/rsk/core/bc/ConsensusValidationMainchainViewImplTest.java
@@ -0,0 +1,223 @@
+/*
+ * This file is part of RskJ
+ * Copyright (C) 2019 RSK Labs Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package co.rsk.core.bc;
+
+import co.rsk.crypto.Keccak256;
+import org.ethereum.core.Block;
+import org.ethereum.core.BlockHeader;
+import org.ethereum.db.BlockStore;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class ConsensusValidationMainchainViewImplTest {
+
+    @Test
+    public void getWithHeightZeroReturnsEmpty() {
+        BlockStore blockStore = mock(BlockStore.class);
+        ConsensusValidationMainchainView view = new ConsensusValidationMainchainViewImpl(blockStore);
+
+        List<BlockHeader> result = view.get(new Keccak256(getRandomHash()), 0);
+
+        assertNotNull(result);
+        assertThat(result.size(), is(0));
+    }
+
+    @Test
+    public void getThatFindsAllBlocksOnBlockStore() {
+        BlockStore blockStore = createBlockStore(10);
+        ConsensusValidationMainchainView view = new ConsensusValidationMainchainViewImpl(blockStore);
+
+        Block bestBlock = blockStore.getBestBlock();
+        List<BlockHeader> result = view.get(bestBlock.getHash(), 5);
+
+        assertNotNull(result);
+        assertThat(result.size(), is(5));
+
+        assertThat(result.get(0).getHash(), is(bestBlock.getHash()));
+
+        byte[] bestBlockParentHash = bestBlock.getParentHash().getBytes();
+        assertThat(result.get(1).getHash(), is(blockStore.getBlockByHash(bestBlockParentHash).getHash()));
+
+        byte[] blockOneParentHash = result.get(1).getParentHash().getBytes();
+        assertThat(result.get(2).getHash(), is(blockStore.getBlockByHash(blockOneParentHash).getHash()));
+
+        byte[] blockTwoParentHash = result.get(2).getParentHash().getBytes();
+        assertThat(result.get(3).getHash(), is(blockStore.getBlockByHash(blockTwoParentHash).getHash()));
+
+        byte[] blockThreeParentHash = result.get(3).getParentHash().getBytes();
+        assertThat(result.get(4).getHash(), is(blockStore.getBlockByHash(blockThreeParentHash).getHash()));
+    }
+
+    @Test
+    public void getAllBlocksOnBlockStoreAreAllRequestedBlocks() {
+        BlockStore blockStore = createBlockStore(5);
+        ConsensusValidationMainchainView view = new ConsensusValidationMainchainViewImpl(blockStore);
+
+        Block bestBlock = blockStore.getBestBlock();
+        List<BlockHeader> result = view.get(bestBlock.getHash(), 5);
+
+        assertNotNull(result);
+        assertThat(result.size(), is(5));
+
+        assertThat(result.get(0).getHash(), is(bestBlock.getHash()));
+
+        byte[] bestBlockParentHash = bestBlock.getParentHash().getBytes();
+        assertThat(result.get(1).getHash(), is(blockStore.getBlockByHash(bestBlockParentHash).getHash()));
+
+        byte[] blockOneParentHash = result.get(1).getParentHash().getBytes();
+        assertThat(result.get(2).getHash(), is(blockStore.getBlockByHash(blockOneParentHash).getHash()));
+
+        byte[] blockTwoParentHash = result.get(2).getParentHash().getBytes();
+        assertThat(result.get(3).getHash(), is(blockStore.getBlockByHash(blockTwoParentHash).getHash()));
+
+        byte[] blockThreeParentHash = result.get(3).getParentHash().getBytes();
+        assertThat(result.get(4).getHash(), is(blockStore.getBlockByHash(blockThreeParentHash).getHash()));
+    }
+
+    @Test
+    public void getWithMissingBlockInTheMiddleOfChain() {
+        BlockStore blockStore = createBlockStore(10);
+        ConsensusValidationMainchainView view = new ConsensusValidationMainchainViewImpl(blockStore);
+
+        Block bestBlock = blockStore.getBestBlock();
+
+        byte[] bestBlockParentHash = bestBlock.getParentHash().getBytes();
+        when(blockStore.getBlockByHash(bestBlockParentHash)).thenReturn(null);
+
+        Map<Keccak256, BlockHeader> pendingHeadersByHash = new ConcurrentHashMap<>();
+        view.setPendingHeaders(pendingHeadersByHash);
+
+        List<BlockHeader> result = view.get(bestBlock.getHash(), 5);
+
+        assertNotNull(result);
+        assertThat(result.size(), is(0));
+    }
+
+    @Test
+    public void getAsksForMoreBlocksThanExist() {
+        BlockStore blockStore = createBlockStore(10);
+        ConsensusValidationMainchainView view = new ConsensusValidationMainchainViewImpl(blockStore);
+
+        Block bestBlock = blockStore.getBestBlock();
+
+        byte[] bestBlockParentHash = bestBlock.getParentHash().getBytes();
+        when(blockStore.getBlockByHash(bestBlockParentHash)).thenReturn(null);
+
+        Map<Keccak256, BlockHeader> pendingHeadersByHash = new ConcurrentHashMap<>();
+        view.setPendingHeaders(pendingHeadersByHash);
+
+        List<BlockHeader> result = view.get(bestBlock.getHash(), 42);
+
+        assertNotNull(result);
+        assertThat(result.size(), is(0));
+    }
+
+    @Test
+    public void getThatFindsBlocksOnBlockStoreAndPendingHeaders() {
+        BlockStore blockStore = createBlockStore(10);
+        ConsensusValidationMainchainView view = new ConsensusValidationMainchainViewImpl(blockStore);
+
+        Block bestBlock = blockStore.getBestBlock();
+
+        Map<Keccak256, BlockHeader> pendingHeadersByHash = new ConcurrentHashMap<>();
+        BlockHeader headerOnTopOfBestBlock = mock(BlockHeader.class);
+        when(headerOnTopOfBestBlock.getHash()).thenReturn(new Keccak256(getRandomHash()));
+        Keccak256 bestBlockHash = bestBlock.getHash();
+        when(headerOnTopOfBestBlock.getParentHash()).thenReturn(bestBlockHash);
+        pendingHeadersByHash.put(headerOnTopOfBestBlock.getHash(), headerOnTopOfBestBlock);
+
+        BlockHeader headerOnTopOfHeader = mock(BlockHeader.class);
+        when(headerOnTopOfHeader.getHash()).thenReturn(new Keccak256(getRandomHash()));
+        Keccak256 headerOnTopOfBestBlockHash = headerOnTopOfBestBlock.getHash();
+        when(headerOnTopOfHeader.getParentHash()).thenReturn(headerOnTopOfBestBlockHash);
+        pendingHeadersByHash.put(headerOnTopOfHeader.getHash(), headerOnTopOfHeader);
+
+        view.setPendingHeaders(pendingHeadersByHash);
+
+        List<BlockHeader> result = view.get(headerOnTopOfHeader.getHash(), 5);
+
+        assertNotNull(result);
+        assertThat(result.size(), is(5));
+
+        assertThat(result.get(0).getHash(), is(headerOnTopOfHeader.getHash()));
+
+        assertThat(result.get(1).getHash(), is(headerOnTopOfBestBlock.getHash()));
+
+        assertThat(result.get(2).getHash(), is(bestBlockHash));
+
+        byte[] bestBlockParentHash = bestBlock.getParentHash().getBytes();
+        assertThat(result.get(3).getHash(), is(blockStore.getBlockByHash(bestBlockParentHash).getHash()));
+
+        byte[] blockOneParentHash = result.get(3).getParentHash().getBytes();
+        assertThat(result.get(4).getHash(), is(blockStore.getBlockByHash(blockOneParentHash).getHash()));
+    }
+
+    private BlockStore createBlockStore(int numberOfBlocks) {
+        BlockStore blockstore = mock(BlockStore.class);
+
+        Block previousBlock = createBlock(420, new Keccak256(getRandomHash()));
+        when(blockstore.getBlockByHash(previousBlock.getHash().getBytes())).thenReturn(previousBlock);
+
+        for(long i = 421; i < 420 + numberOfBlocks; i++) {
+            Block block = createBlock(i, previousBlock.getHash());
+            when(blockstore.getBlockByHash(block.getHash().getBytes())).thenReturn(block);
+
+            if(i == 420 + numberOfBlocks - 1) {
+                when(blockstore.getBestBlock()).thenReturn(block);
+            }
+
+            previousBlock = block;
+        }
+
+        return blockstore;
+    }
+
+    private Block createBlock(long number, Keccak256 parentHash){
+        Block block = mock(Block.class);
+        when(block.getNumber()).thenReturn(number);
+        byte[] rawBlockHash = getRandomHash();
+        Keccak256 blockHash = new Keccak256(rawBlockHash);
+        when(block.getHash()).thenReturn(blockHash);
+        when(block.getParentHash()).thenReturn(parentHash);
+        BlockHeader header = mock(BlockHeader.class);
+        when(header.getHash()).thenReturn(blockHash);
+        when(header.getParentHash()).thenReturn(parentHash);
+        when(block.getHeader()).thenReturn(header);
+
+        return block;
+    }
+
+    private byte[] getRandomHash() {
+        byte[] byteArray = new byte[32];
+        new Random().nextBytes(byteArray);
+
+        return byteArray;
+    }
+}
