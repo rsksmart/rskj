9d5f438d79aaae4b342a12646ca5f48551055a46
Martín Medina <tinnmedina@gmail.com>
2019-05-23 17:37:10 -0300

    Add MainchainView for consensus validation

---
+++ rskj-core/src/main/java/co/rsk/core/bc/ConsensusValidationMainchainView.java
@@ -0,0 +1,32 @@
+/*
+ * This file is part of RskJ
+ * Copyright (C) 2019 RSK Labs Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package co.rsk.core.bc;
+
+import co.rsk.crypto.Keccak256;
+import org.ethereum.core.BlockHeader;
+
+import java.util.List;
+import java.util.Map;
+
+public abstract class ConsensusValidationMainchainView implements MainchainView {
+
+    public abstract List<BlockHeader> get(Keccak256 startingHashToGetMainchainFrom, int height);
+
+    public abstract void setPendingHeaders(Map<Keccak256, BlockHeader> pendingHeadersByHash);
+}
