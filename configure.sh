23488c9b76d08df052134eebe99bce6215fd864d
Lucas Gabriel Vuotto <lucas@rsk.co>
2019-08-07 20:18:56 +0000

    Use mktemp instead of rolling our own
    - Remove redundants rm
    
    - Use trap to always remove temporary files
    
      Useful in case that DOWNLOAD_HASH1 check fails, as it won't leave a
      leftover DOWNLOAD_FILE2 afterwards.
    
    - SHA256SUM Hash for configure.sh

--- configure.sh
+++ configure.sh
@@ -4,37 +4,35 @@ GRADLE_WITNESS="1b8eaa3a788aac37ee78fa65579246d1ad268e3c8cf42cd2caeffc50b3e50056
 GRADLE_WRAPPER="0f49043be582d7a39b671f924c66bd9337b92fa88ff5951225acc60560053067"
 DOWNLOADED_HASH1=''
 DOWNLOADED_HASH2=''
-DOWNLOAD_FILE1=$(dd if=/dev/urandom bs=64 count=1 2>/dev/null| od -t x8 -A none  | tr -d ' '\\n)
-DOWNLOAD_FILE2=$(dd if=/dev/urandom bs=64 count=1 2>/dev/null| od -t x8 -A none  | tr -d ' '\\n)
+DOWNLOAD_FILE1=$(mktemp)
+DOWNLOAD_FILE2=$(mktemp)
 unamestr=`uname`
+
+trap 'rm -f "$DOWNLOAD_FILE1" "$DOWNLOAD_FILE2"' EXIT INT QUIT TERM
 
 downloadJar() {
 	platform
 	if [ ! -d ./rskj-core/libs ]; then
 		mkdir ./rskj-core/libs
 	fi
-	curl https://deps.rsklabs.io/rsk-gradle-witness.jar -o ~/$DOWNLOAD_FILE1
-	curl https://deps.rsklabs.io/gradle-wrapper.jar -o ~/$DOWNLOAD_FILE2
+	curl https://deps.rsklabs.io/rsk-gradle-witness.jar -o "$DOWNLOAD_FILE1"
+	curl https://deps.rsklabs.io/gradle-wrapper.jar -o "$DOWNLOAD_FILE2"
 	if [ x"$PLATFORM" = x'linux' ] || [ x"$PLATFORM" = x'windows' ]; then
-		DOWNLOADED_HASH1=$(sha256sum ~/${DOWNLOAD_FILE1} | cut -d' ' -f1)
-		DOWNLOADED_HASH2=$(sha256sum ~/${DOWNLOAD_FILE2} | cut -d' ' -f1)
+		DOWNLOADED_HASH1=$(sha256sum "$DOWNLOAD_FILE1" | cut -d' ' -f1)
+		DOWNLOADED_HASH2=$(sha256sum "$DOWNLOAD_FILE2" | cut -d' ' -f1)
 	elif [ x"$PLATFORM" = x'mac' ]; then
-		DOWNLOADED_HASH1=$(shasum -a 256 ~/${DOWNLOAD_FILE1} | cut -d' ' -f1)
-		DOWNLOADED_HASH2=$(shasum -a 256 ~/${DOWNLOAD_FILE2} | cut -d' ' -f1)
+		DOWNLOADED_HASH1=$(shasum -a 256 "$DOWNLOAD_FILE1" | cut -d' ' -f1)
+		DOWNLOADED_HASH2=$(shasum -a 256 "$DOWNLOAD_FILE2" | cut -d' ' -f1)
 	fi
 	if [ "$GRADLE_WITNESS" != "$DOWNLOADED_HASH1" ]; then
-		rm -f ~/$DOWNLOAD_FILE1
 		exit 1
 	else
-		mv ~/${DOWNLOAD_FILE1} ./rskj-core/libs/rsk-gradle-witness.jar
-		rm -f ~/${DOWNLOAD_FILE1}
+		mv "$DOWNLOAD_FILE1" ./rskj-core/libs/rsk-gradle-witness.jar
 	fi
 	if [ "$GRADLE_WRAPPER" != "$DOWNLOADED_HASH2" ]; then
-		rm -f ~/${DOWNLOAD_FILE2}
 		exit 1
 	else
-		mv ~/${DOWNLOAD_FILE2} ./gradle/wrapper/gradle-wrapper.jar
-		rm -f ~/${DOWNLOAD_FILE2}
+		mv "$DOWNLOAD_FILE2" ./gradle/wrapper/gradle-wrapper.jar
 	fi
 }
 
