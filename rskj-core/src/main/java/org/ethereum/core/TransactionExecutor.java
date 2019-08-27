bb6b3e90f7cabc7470c805f36ec83d46f68f4313
SergioDemianLerner <SergioDemianLerner@users.noreply.github.com>
2019-08-27 13:58:19 -0300

    fix all tests not using program object in executor

--- rskj-core/src/main/java/org/ethereum/core/TransactionExecutor.java
+++ rskj-core/src/main/java/org/ethereum/core/TransactionExecutor.java
@@ -496,9 +496,10 @@ public class TransactionExecutor {
         long gasRefund = Math.min(result.getFutureRefund(), result.getGasUsed() / 2);
         result.addDeductedRefund(gasRefund);
 
-        if (program.getCallWithValuePerformed())
-            result.markCallWithValuePerformed();
-
+        if (program != null) {
+            if (program.getCallWithValuePerformed())
+                result.markCallWithValuePerformed();
+        }
         mEndGas = mEndGas.add(BigInteger.valueOf(gasRefund));
 
         summaryBuilder
