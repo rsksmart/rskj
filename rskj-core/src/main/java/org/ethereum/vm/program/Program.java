efc23492629ec7b58b1511053b7c3f79bfa4a647
Joaquin L. Pereyra <joaquinlpereyra@gmail.com>
2019-08-27 14:50:51 -0300

    reset return data buffer if call frame not created
    This is to make us EIP-211 compatible.
    Issue is #921 on Github has a bit of discussion.

--- rskj-core/src/main/java/org/ethereum/vm/program/Program.java
+++ rskj-core/src/main/java/org/ethereum/vm/program/Program.java
@@ -740,6 +740,9 @@ public class Program {
         Coin endowment = new Coin(msg.getEndowment().getData());
         Coin senderBalance = track.getBalance(senderAddress);
         if (isNotCovers(senderBalance, endowment)) {
+            // reset return data buffer when call did not create a new call frame
+            // *TODO*: set check to activate only after a certain release
+            returnDataBuffer = null;
             stackPushZero();
             refundGas(msg.getGas().longValue(), "refund gas from message call");
             return;
@@ -776,6 +779,9 @@ public class Program {
             track.commit();
             callResult = true;
             refundGas(msg.getGas().longValue(), "remaining gas from the internal call");
+            // *TODO*: set check to activate only after a certain release
+            // reset return data buffer after calling non existent contract
+            returnDataBuffer = null;
         }
 
         // 4. THE FLAG OF SUCCESS IS ONE PUSHED INTO THE STACK
@@ -841,6 +847,7 @@ public class Program {
             // 4. THE FLAG OF SUCCESS IS ONE PUSHED INTO THE STACK
             track.commit();
         }
+
 
 
         // 3. APPLY RESULTS: childResult.getHReturn() into out_memory allocated
