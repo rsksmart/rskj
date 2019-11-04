d6e28094f3e6f760a7c672b6f34dc8570df6eb32
Julian <julianlen@gmail.com>
2019-11-04 16:37:30 -0300

    CodeResponseMessage added

--- rskj-core/src/main/java/co/rsk/net/messages/CodeResponseMessage.java
+++ rskj-core/src/main/java/co/rsk/net/messages/CodeResponseMessage.java
@@ -20,49 +20,37 @@ package co.rsk.net.messages;
 
 import org.ethereum.util.RLP;
 
-public class CodeRequestMessage extends MessageWithId {
+public class CodeResponseMessage extends MessageWithId{
     private long id;
-    private byte[] blockHash;
-    private byte[] address;
+    private byte[] codeHash;
 
-    public CodeRequestMessage(long id, byte[] blockHash, byte[] address) {
+    public CodeResponseMessage(long id, byte[] codeHash) {
         this.id = id;
-        this.blockHash = blockHash.clone();
-        this.address = address.clone();
+        this.codeHash = codeHash.clone();
     }
 
     @Override
     public long getId() {
-        return this.id;
-    }
-
-    public byte[] getBlockHash() {
-        return blockHash.clone();
-    }
-
-    public byte[] getAddress() {
-        return address.clone();
+        return id;
     }
 
     @Override
     protected byte[] getEncodedMessageWithoutId() {
-        byte[] rlpBlockHash = RLP.encodeElement(this.blockHash);
-        byte[] rlpCodeHash = RLP.encodeElement(this.address);
-        return RLP.encodeList(rlpBlockHash, rlpCodeHash);
+        byte[] rlpBlockHash = RLP.encodeElement(this.codeHash);
+        return RLP.encodeList(rlpBlockHash);
     }
 
     @Override
     public MessageType getMessageType() {
-        return MessageType.CODE_REQUEST_MESSAGE;
-    }
-
-    @Override
-    public MessageType getResponseMessageType() {
         return MessageType.CODE_RESPONSE_MESSAGE;
     }
 
     @Override
     public void accept(MessageVisitor v) {
         v.apply(this);
+    }
+
+    public byte[] getCodeHash() {
+        return codeHash.clone();
     }
 }
