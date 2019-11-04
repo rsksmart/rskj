ce3b66f57ee37d56f7e073cc57dd0dc01b6c29ed
Julian <julianlen@gmail.com>
2019-11-04 15:07:23 -0300

    blockstore and locatorRepository added in lightprocessor

--- rskj-core/src/main/java/co/rsk/RskContext.java
+++ rskj-core/src/main/java/co/rsk/RskContext.java
@@ -1577,7 +1577,7 @@ public class RskContext implements NodeBootstrapper {
 
     private LightProcessor getLightProcessor() {
         if (lightProcessor == null) {
-            lightProcessor = new LightProcessor(getBlockchain(), getBlockSyncService());
+            lightProcessor = new LightProcessor(getBlockchain(), getBlockStore(), getRepositoryLocator());
         }
         return lightProcessor;
     }
