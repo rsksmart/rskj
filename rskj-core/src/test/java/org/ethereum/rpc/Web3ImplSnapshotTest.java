57a41bdd06678820207d53e1932766fa7ce91fb6
Martín Medina <tinnmedina@gmail.com>
2019-12-05 02:34:27 -0300

    Add extra data population protocol version 1

--- rskj-core/src/test/java/org/ethereum/rpc/Web3ImplSnapshotTest.java
+++ rskj-core/src/test/java/org/ethereum/rpc/Web3ImplSnapshotTest.java
@@ -42,6 +42,7 @@ import org.ethereum.core.Block;
 import org.ethereum.core.BlockFactory;
 import org.ethereum.core.Blockchain;
 import org.ethereum.rpc.Simples.SimpleEthereum;
+import org.ethereum.util.BuildInfo;
 import org.ethereum.util.RskTestFactory;
 import org.junit.Assert;
 import org.junit.Before;
@@ -235,6 +236,7 @@ public class Web3ImplSnapshotTest {
                 ),
                 clock,
                 blockFactory,
+                new BuildInfo("cb7f28e", "master"),
                 miningConfig
         );
     }
