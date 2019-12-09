502df8f6b1362047f57e484c505bb830b4d5b378
José Dahlquist <jose.dahlquist@gmail.com>
2019-12-09 18:01:01 -0300

    add BtcLockSenderProvider
    - Integrates P2shP2wpkhBtcLockSender instantiation
    - Integrates P2pkhBtcLockSender instantiation

---
+++ rskj-core/src/main/java/co/rsk/peg/btcLockSender/BtcLockSenderProvider.java
@@ -0,0 +1,27 @@
+package co.rsk.peg.btcLockSender;
+
+import co.rsk.bitcoinj.core.BtcTransaction;
+
+import java.util.Optional;
+
+public class BtcLockSenderProvider {
+
+    public Optional<BtcLockSender> tryGetBtcLockSender(BtcTransaction tx) {
+        if (tx == null) {
+            return Optional.empty();
+        }
+
+        try {
+            return Optional.of(new P2pkhBtcLockSender(tx));
+        } catch(BtcLockSenderParseException e) {
+            // Nothing to do...
+        }
+        try {
+            return Optional.of(new P2shP2wpkhBtcLockSender(tx));
+        } catch(BtcLockSenderParseException e) {
+            // Nothing to do...
+        }
+
+        return Optional.empty();
+    }
+}
