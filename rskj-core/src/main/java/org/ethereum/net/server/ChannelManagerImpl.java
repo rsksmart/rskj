90db4c9800450cb896ef658853815bcf9b9ade94
ajlopez <webmaster@ajlopez.com>
2020-01-05 14:41:22 -0300

    Removing unused imports; Coalesce code

--- rskj-core/src/main/java/org/ethereum/net/server/ChannelManagerImpl.java
+++ rskj-core/src/main/java/org/ethereum/net/server/ChannelManagerImpl.java
@@ -186,34 +186,10 @@ public class ChannelManagerImpl implements ChannelManager {
      */
     @Nonnull
     public Set<NodeID> broadcastBlock(@Nonnull final Block block) {
-        final Set<NodeID> nodesIdsBroadcastedTo = new HashSet<>();
-
-        final BlockIdentifier bi = new BlockIdentifier(block.getHash().getBytes(), block.getNumber());
-        final Message newBlock = new BlockMessage(block);
-        final Message newBlockHashes = new NewBlockHashesMessage(Arrays.asList(bi));
         synchronized (activePeersLock) {
-            // Get a randomized list with all the peers that don't have the block yet.
-            activePeers.values().forEach(c -> logger.trace("RSK activePeers: {}", c));
-            List<Channel> peers = new ArrayList<>(activePeers.values());
-            Collections.shuffle(peers);
-
-            int sqrt = (int) Math.floor(Math.sqrt(peers.size()));
-
-            for (int i = 0; i < sqrt; i++) {
-                Channel peer = peers.get(i);
-                nodesIdsBroadcastedTo.add(peer.getNodeId());
-                logger.trace("RSK propagate: {}", peer);
-                peer.sendMessage(newBlock);
-            }
-
-            for (int i = sqrt; i < peers.size(); i++) {
-                Channel peer = peers.get(i);
-                logger.trace("RSK announce: {}", peer);
-                peer.sendMessage(newBlockHashes);
-            }
+            Set<NodeID> peerIds = activePeers.values().stream().map(Peer::getPeerNodeID).collect(Collectors.toSet());
+            return this.broadcastBlock(block, peerIds);
         }
-
-        return nodesIdsBroadcastedTo;
     }
 
     /**
