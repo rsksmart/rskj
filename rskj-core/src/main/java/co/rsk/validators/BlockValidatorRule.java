e054d22a5da1f2304d4e6bf541f0bd4d70403c2d
Joaquin Pereyra <joaquinp@iovlabs.org>
2020-01-15 16:09:43 -0300

    create the BlockValidatorRule

---
+++ rskj-core/src/main/java/co/rsk/validators/BlockValidatorRule.java
@@ -0,0 +1,38 @@
+package co.rsk.validators;
+
+import org.ethereum.core.Block;
+
+/**
+ * BlockValidatorRule performs all the validations needed for a block to be considered
+ * a reasonable continuation of the chain.
+ * This class performs **all** validations, in contrast with SyncBlockValidatorRule.
+ */
+public class BlockValidatorRule implements BlockValidationRule {
+    private final BlockCompositeRule blockCompositeRule;
+
+    public BlockValidatorRule(TxsMinGasPriceRule txsMinGasPriceRule,
+                              BlockUnclesValidationRule blockUnclesValidationRule,
+                              BlockRootValidationRule blockRootValidationRule,
+                              ProofOfWorkRule proofOfWorkRule,
+                              RemascValidationRule remascValidationRule,
+                              BlockTimeStampValidationRule blockTimeStampValidationRule,
+                              GasLimitRule gasLimitRule,
+                              ExtraDataRule extraDataRule,
+                              ForkDetectionDataRule forkDetectionDataRule ) {
+        blockCompositeRule = new BlockCompositeRule(txsMinGasPriceRule,
+                blockUnclesValidationRule,
+                blockRootValidationRule,
+                proofOfWorkRule,
+                remascValidationRule,
+                blockTimeStampValidationRule,
+                gasLimitRule,
+                extraDataRule,
+                forkDetectionDataRule
+        );
+    }
+
+    public boolean isValid(Block block) {
+        return blockCompositeRule.isValid(block);
+    }
+
+}
