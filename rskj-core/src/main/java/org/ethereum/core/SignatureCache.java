ec52bf08ce7eb29d0795a59b609962b454035c9f
Julian <julianlen@gmail.com>
2020-01-29 14:55:23 -0300

    signatures from TTransactionPool are cached by SignatureCache

--- rskj-core/src/main/java/org/ethereum/core/SignatureCache.java
+++ rskj-core/src/main/java/org/ethereum/core/SignatureCache.java
@@ -1,17 +1,24 @@
 package org.ethereum.core;
 
 import co.rsk.core.RskAddress;
+import co.rsk.metrics.profilers.Metric;
+import co.rsk.metrics.profilers.Profiler;
+import co.rsk.metrics.profilers.ProfilerFactory;
 import co.rsk.panic.PanicProcessor;
+import co.rsk.remasc.RemascTransaction;
 import co.rsk.util.MaxSizeHashMap;
+import org.ethereum.crypto.ECKey;
 import org.ethereum.db.ByteArrayWrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.security.SignatureException;
 import java.util.Map;
 
 public class SignatureCache {
     private static final Logger logger = LoggerFactory.getLogger(SignatureCache.class);
     private static final PanicProcessor panicProcessor = new PanicProcessor();
+    private static final Profiler profiler = ProfilerFactory.getInstance();
 
     private static final int MAX_BROADCAST_TX_SIZE = 6000;
     private static final int TX_IN_THREE_BLOCKS = 900;
@@ -25,6 +32,25 @@ public class SignatureCache {
     }
 
     public RskAddress getSender(Transaction transaction) {
-        return transaction.getSender();
+
+        if (transaction instanceof RemascTransaction) {
+            return RemascTransaction.REMASC_ADDRESS;
+        }
+
+        RskAddress sender = sendersByBroadcastTx.computeIfAbsent(transaction, this::extractSenderFromSignature);
+        return sender;
+    }
+
+    private RskAddress extractSenderFromSignature(Transaction tx) {
+        Metric metric = profiler.start(Profiler.PROFILING_TYPE.KEY_RECOV_FROM_SIG);
+        try {
+            ECKey key = ECKey.signatureToKey(tx.getRawHash().getBytes(), tx.getSignature());
+            return new RskAddress(key.getAddress());
+        } catch (SignatureException e) {
+            logger.error("Unable to extract signature for tx {}", tx.getHash(), e);
+            return RskAddress.nullAddress();
+        } finally {
+            profiler.stop(metric);
+        }
     }
 }
