d0afa7374b7cbd481b2dc4151ad0f54555c47e63
Julian <julianlen@gmail.com>
2020-02-27 17:04:08 -0300

    light client added as option in MessageCodesResolver

---
+++ rskj-core/src/main/java/co/rsk/net/lc/LightClientMessageCodes.java
@@ -0,0 +1,27 @@
+package co.rsk.net.lc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum LightClientMessageCodes {
+
+    TEST(0x00);
+
+    private final int cmd;
+
+    private static final Map<Integer, LightClientMessageCodes> intToTypeMap = new HashMap<>();
+
+    static {
+        for (LightClientMessageCodes type : LightClientMessageCodes.values()) {
+            intToTypeMap.put(type.cmd, type);
+        }
+    }
+
+    LightClientMessageCodes(int cmd) {
+        this.cmd = cmd;
+    }
+
+    public byte asByte() {
+        return (byte) (cmd);
+    }
+}
