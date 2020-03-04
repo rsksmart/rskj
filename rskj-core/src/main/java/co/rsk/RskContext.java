18df78212aad00d094664119700dbfa2443a0c61
Julian <julianlen@gmail.com>
2020-03-04 16:55:20 -0300

    light client added as part of the handshake, test has been modified too

--- rskj-core/src/main/java/co/rsk/RskContext.java
+++ rskj-core/src/main/java/co/rsk/RskContext.java
@@ -45,6 +45,7 @@ import co.rsk.net.discovery.table.KademliaOptions;
 import co.rsk.net.discovery.table.NodeDistanceTable;
 import co.rsk.net.eth.*;
 import co.rsk.net.light.LightProcessor;
+import co.rsk.net.rlpx.LCMessageFactory;
 import co.rsk.net.sync.PeersInformation;
 import co.rsk.net.sync.SyncConfiguration;
 import co.rsk.peg.BridgeSupportFactory;
@@ -217,6 +218,7 @@ public class RskContext implements NodeBootstrapper {
     private RskWireProtocol.Factory rskWireProtocolFactory;
     private LightClientHandler lightClientHandler;
     private Eth62MessageFactory eth62MessageFactory;
+    private LCMessageFactory lcMessageFactory;
     private GasLimitCalculator gasLimitCalculator;
     private ReversibleTransactionExecutor reversibleTransactionExecutor;
     private TransactionExecutorFactory transactionExecutorFactory;
@@ -1143,12 +1145,22 @@ public class RskContext implements NodeBootstrapper {
                     getNodeManager(),
                     getRskWireProtocolFactory(),
                     getEth62MessageFactory(),
+                    getLightClientHandler(),
+                    getLCMessageFactory(),
                     getStaticMessages(),
                     getPeerScoringManager()
             );
         }
 
         return ethereumChannelInitializerFactory;
+    }
+
+    private LCMessageFactory getLCMessageFactory() {
+        if (lcMessageFactory == null) {
+            lcMessageFactory = new LCMessageFactory();
+        }
+
+        return lcMessageFactory;
     }
 
     private Eth62MessageFactory getEth62MessageFactory() {
