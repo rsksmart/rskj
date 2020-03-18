5b527135c8653c57c0c49345814cc0c0487eaec5
julianlen <julianlen@gmail.com>
2020-03-18 12:43:44 -0300

    LightClient Receipts messages (#1194)
    * GetBlockReceipts and BlockReceipts messages added and tested
    
    * delete blockreceipts(response/request) unused classes
    
    * messages added in both lightClientHandler and lightProcessor
    
    * initialize code instance variables in BlockReceipt y GetBlockReceipts

--- rskj-core/src/main/java/co/rsk/net/rlpx/LCMessageFactory.java
+++ rskj-core/src/main/java/co/rsk/net/rlpx/LCMessageFactory.java
@@ -1,6 +1,8 @@
 package co.rsk.net.rlpx;
 
 import co.rsk.net.light.LightClientMessageCodes;
+import co.rsk.net.light.message.BlockReceiptsMessage;
+import co.rsk.net.light.message.GetBlockReceiptsMessage;
 import co.rsk.net.light.message.TestMessage;
 import org.ethereum.net.message.Message;
 
@@ -11,6 +13,10 @@ public class LCMessageFactory {
         switch (receivedCommand) {
             case TEST:
                 return new TestMessage(encoded);
+            case GET_BLOCK_RECEIPTS:
+                return new GetBlockReceiptsMessage(encoded);
+            case BLOCK_RECEIPTS:
+                return new BlockReceiptsMessage(encoded);
             default:
                 throw new IllegalArgumentException("No such message");
         }
--- rskj-core/src/test/java/co/rsk/net/light/message/BlockReceiptsMessageTest.java
+++ rskj-core/src/test/java/co/rsk/net/light/message/BlockReceiptsMessageTest.java
@@ -1,5 +1,7 @@
-package co.rsk.net.messages;
+package co.rsk.net.light.message;
 
+import co.rsk.net.light.LightClientMessageCodes;
+import co.rsk.net.rlpx.LCMessageFactory;
 import org.bouncycastle.util.encoders.Hex;
 import org.ethereum.core.Bloom;
 import org.ethereum.core.Transaction;
@@ -12,33 +14,50 @@ import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
 
-public class BlockReceiptsResponseMessageTest {
-//    @Test
-//    public void createMessage() {
-//        List<TransactionReceipt> receipts = new LinkedList<>();
-//        receipts.add(createReceipt());
-//        receipts.add(createReceipt());
-//
-//        BlockReceiptsResponseMessage message = new BlockReceiptsResponseMessage(1, receipts);
-//        assertEquals(1, message.getId());
-//        assertEquals(receipts, message.getBlockReceipts());
-//        assertEquals(MessageType.BLOCK_RECEIPTS_RESPONSE_MESSAGE, message.getMessageType());
-//    }
-//
-//    @Test
-//    public void accept() {
-//
-//        List<TransactionReceipt> receipts = new LinkedList<>();
-//        BlockReceiptsResponseMessage message = new BlockReceiptsResponseMessage(1, receipts);
-//
-//        MessageVisitor visitor = mock(MessageVisitor.class);
-//
-//        message.accept(visitor);
-//
-//        verify(visitor, times(1)).apply(message);
-//    }
+public class BlockReceiptsMessageTest {
+
+    @Test
+    public void createMessage() {
+        List<TransactionReceipt> receipts = new LinkedList<>();
+        receipts.add(createReceipt());
+        receipts.add(createReceipt());
+
+        BlockReceiptsMessage message = new BlockReceiptsMessage(1, receipts);
+
+        assertEquals(LightClientMessageCodes.BLOCK_RECEIPTS, message.getCommand());
+        assertEquals(1, message.getId());
+        assertEquals(receipts, message.getBlockReceipts());
+        assertNull(message.getAnswerMessage());
+    }
+
+    @Test
+    public void messageEncodeDecodeShouldBeCorrect() {
+        List<TransactionReceipt> receipts = new LinkedList<>();
+        receipts.add(createReceipt());
+        receipts.add(createReceipt());
+        int requestId = 1;
+
+        BlockReceiptsMessage testMessage = new BlockReceiptsMessage(requestId, receipts);
+        byte[] encoded = testMessage.getEncoded();
+
+        LCMessageFactory lcMessageFactory = new LCMessageFactory();
+        BlockReceiptsMessage message = (BlockReceiptsMessage) lcMessageFactory.create((byte) 2, encoded);
+
+        List<TransactionReceipt> testBlockReceipts = testMessage.getBlockReceipts();
+        List<TransactionReceipt> blockReceipts = message.getBlockReceipts();
+        assertEquals(testBlockReceipts.size(), blockReceipts.size());
+
+        for (int i = 0; i < testBlockReceipts.size(); i++) {
+            assertArrayEquals(testBlockReceipts.get(i).getEncoded(), blockReceipts.get(i).getEncoded());
+        }
+
+        assertEquals(testMessage.getId(), message.getId());
+        assertEquals(testMessage.getCommand(), message.getCommand());
+        assertEquals(testMessage.getAnswerMessage(), message.getAnswerMessage());
+        assertArrayEquals(encoded, message.getEncoded());
+    }
+
 
     private static TransactionReceipt createReceipt() {
         byte[] stateRoot = Hex.decode("f5ff3fbd159773816a7c707a9b8cb6bb778b934a8f6466c7830ed970498f4b68");
@@ -57,9 +76,8 @@ public class BlockReceiptsResponseMessageTest {
         // TODO calculate cumulative gas
         TransactionReceipt receipt = new TransactionReceipt(stateRoot, gasUsed, gasUsed, bloom, logs, new byte[]{0x01});
 
-        receipt.setTransaction(new Transaction((byte[]) null, null, null, null, null, null));
+        receipt.setTransaction(new Transaction( null, null, null, null, null, null));
 
         return receipt;
     }
-
 }
