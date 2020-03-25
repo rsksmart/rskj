f51fdb96d9a44c938e38d09ac5b3e83385181519
julianlen <julianlen@gmail.com>
2020-03-25 11:05:59 -0300

    Code and GetCode messages (#1197)
    * GetCode and Code messages added
    
    * Code(request/response) message deleted from unused classes
    
    * CodeMessage and GetCodeMessage added in LightClientHandler and LightProcessor, also in its tests

--- rskj-core/src/main/java/co/rsk/net/eth/LightClientHandler.java
+++ rskj-core/src/main/java/co/rsk/net/eth/LightClientHandler.java
@@ -1,10 +1,25 @@
+/*
+ * This file is part of RskJ
+ * Copyright (C) 2020 RSK Labs Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package co.rsk.net.eth;
 
 import co.rsk.net.light.LightProcessor;
-import co.rsk.net.light.message.BlockReceiptsMessage;
-import co.rsk.net.light.message.GetBlockReceiptsMessage;
-import co.rsk.net.light.message.LightClientMessage;
-import co.rsk.net.light.message.TestMessage;
+import co.rsk.net.light.message.*;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import org.ethereum.net.MessageQueue;
@@ -43,6 +58,16 @@ public class LightClientHandler extends SimpleChannelInboundHandler<LightClientM
                 logger.debug("Read message: {} BLOCK_RECEIPTS. Sending receipts response", msg);
                 BlockReceiptsMessage blockReceiptsMsg = (BlockReceiptsMessage) msg;
                 lightProcessor.processBlockReceiptsMessage(blockReceiptsMsg.getId(), blockReceiptsMsg.getBlockReceipts(), msgQueue);
+                break;
+            case GET_CODE:
+                logger.debug("Read message: {} GET_CODE. Sending code request", msg);
+                GetCodeMessage getCodeMsg = (GetCodeMessage) msg;
+                lightProcessor.processGetCodeMessage(getCodeMsg.getId(), getCodeMsg.getBlockHash(), getCodeMsg.getAddress(), msgQueue);
+                break;
+            case CODE:
+                logger.debug("Read message: {} CODE. Sending code response", msg);
+                CodeMessage codeMsg = (CodeMessage) msg;
+                lightProcessor.processCodeMessage(codeMsg.getId(), codeMsg.getCodeHash(), msgQueue);
                 break;
             default:
                 break;
