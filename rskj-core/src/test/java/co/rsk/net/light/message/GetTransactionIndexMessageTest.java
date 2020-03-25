691ca80a4883085229a6da43f227498f211f9fb1
Sebastian Sicardi <smcsicardi@gmail.com>
2020-03-25 16:00:23 -0300

    GetTransactionIndex and TransactionIndex Messages (#1198)
    * init transaction index
    
    * WIP transactions
    
    * finished messages
    
    * added txidx to handler
    
    * fixed answer message return
    
    * hooked up to lightprocessor
    
    * len's changes
    
    * deleted old messages
    
    * rename classes
    
    * init tests
    
    * added tests for txindex
    
    * finish tests txindex
    
    * finish tests
    
    * fix sonarcould issues
    
    * code review

---
+++ rskj-core/src/test/java/co/rsk/net/light/message/GetTransactionIndexMessageTest.java
@@ -0,0 +1,65 @@
+/*
+ * This file is part of RskJ
+ * Copyright (C) 2020 RSK Labs Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package co.rsk.net.light.message;
+
+import co.rsk.net.light.LightClientMessageCodes;
+import co.rsk.net.rlpx.LCMessageFactory;
+import org.ethereum.crypto.HashUtil;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class GetTransactionIndexMessageTest {
+
+    private byte[] txHash;
+    private int id;
+
+    @Before
+    public void setUp() {
+        txHash = HashUtil.randomHash();
+        id = 1;
+    }
+
+    @Test
+    public void messageCreationShouldBeCorrect() {
+        GetTransactionIndexMessage testMessage = new GetTransactionIndexMessage(id, txHash);
+        assertEquals(LightClientMessageCodes.GET_TRANSACTION_INDEX, testMessage.getCommand());
+        assertArrayEquals(testMessage.getTxHash(), txHash);
+        assertEquals(testMessage.getId(), id);
+    }
+
+    @Test
+    public void messageEncodeDecodeShouldBeCorrect() {
+
+        GetTransactionIndexMessage testMessage = new GetTransactionIndexMessage(id, txHash);
+        byte[] encoded = testMessage.getEncoded();
+        LCMessageFactory lcMessageFactory = new LCMessageFactory();
+        byte code = LightClientMessageCodes.GET_TRANSACTION_INDEX.asByte();
+        GetTransactionIndexMessage message = (GetTransactionIndexMessage) lcMessageFactory.create(code, encoded);
+
+        assertArrayEquals(testMessage.getTxHash(), message.getTxHash());
+        assertEquals(testMessage.getId(), message.getId());
+        assertEquals(testMessage.getCommand(), message.getCommand());
+        assertEquals(testMessage.getAnswerMessage(), message.getAnswerMessage());
+        assertArrayEquals(encoded, message.getEncoded());
+    }
+
+}
---
+++ rskj-core/src/test/java/co/rsk/net/light/message/TransactionIndexMessageTest.java
@@ -0,0 +1,76 @@
+/*
+ * This file is part of RskJ
+ * Copyright (C) 2020 RSK Labs Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package co.rsk.net.light.message;
+
+import co.rsk.net.light.LightClientMessageCodes;
+import co.rsk.net.rlpx.LCMessageFactory;
+import org.ethereum.crypto.HashUtil;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TransactionIndexMessageTest {
+
+    private long id;
+    private long blockNumber;
+    private byte[] blockHash;
+    private long txIndex;
+
+    @Before
+    public void setUp() {
+        id = 1L;
+        blockNumber = 1000L;
+        blockHash = HashUtil.randomHash();
+        txIndex = 1234L;
+    }
+
+    @Test
+    public void createMessage() {
+        TransactionIndexMessage message = new TransactionIndexMessage(id, blockNumber, blockHash, txIndex);
+
+        assertEquals(LightClientMessageCodes.TRANSACTION_INDEX, message.getCommand());
+        assertEquals(id, message.getId());
+        assertEquals(blockNumber, message.getBlockNumber());
+        assertArrayEquals(blockHash, message.getBlockHash());
+        assertEquals(txIndex, message.getTransactionIndex());
+        assertNull(message.getAnswerMessage());
+    }
+
+    @Test
+    public void messageEncodeDecodeShouldBeCorrect() {
+        TransactionIndexMessage testMessage = new TransactionIndexMessage(id, blockNumber, blockHash, txIndex);
+        byte[] encoded = testMessage.getEncoded();
+
+        byte code = LightClientMessageCodes.TRANSACTION_INDEX.asByte();
+
+        LCMessageFactory lcMessageFactory = new LCMessageFactory();
+        TransactionIndexMessage message = (TransactionIndexMessage) lcMessageFactory.create(code, encoded);
+
+        assertEquals(testMessage.getCommand(), message.getCommand());
+        assertEquals(testMessage.getAnswerMessage(), message.getAnswerMessage());
+        assertArrayEquals(encoded, message.getEncoded());
+
+        assertEquals(testMessage.getId(), message.getId());
+        assertEquals(testMessage.getBlockNumber(), message.getBlockNumber());
+        assertArrayEquals(testMessage.getBlockHash(), message.getBlockHash());
+        assertEquals(testMessage.getTransactionIndex(), message.getTransactionIndex());
+
+    }
+}
