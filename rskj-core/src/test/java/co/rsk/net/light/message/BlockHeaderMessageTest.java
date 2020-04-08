c65e0c7a4532a0e601e387904377402eff277cc5
julianlen <julianlen@gmail.com>
2020-04-08 11:54:43 -0300

    GetBlockHeader and BlockHeader messages (#1204)
    * BlockHeader and GetBlockHeader messages added
    
    * Added getBlockHeader and blockHeader in lightProcessor and lightClientHandler, everything tested

---
+++ rskj-core/src/test/java/co/rsk/net/light/message/BlockHeaderMessageTest.java
@@ -0,0 +1,75 @@
+/*
+ * This file is part of RskJ
+ * Copyright (C) 2020 RSK Labs Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package co.rsk.net.light.message;
+
+import co.rsk.net.rlpx.LCMessageFactory;
+import org.ethereum.core.BlockFactory;
+import org.ethereum.core.BlockHeader;
+import org.junit.Before;
+import org.junit.Test;
+
+import static co.rsk.net.light.LightClientMessageCodes.BLOCK_HEADER;
+import static org.ethereum.crypto.HashUtil.randomHash;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+
+public class BlockHeaderMessageTest {
+
+    private long id;
+    private byte[] blockHeaderHash;
+    private BlockHeader blockHeader;
+    private BlockHeaderMessage testMessage;
+    private LCMessageFactory messageFactory;
+
+    @Before
+    public void setUp() {
+        id = 1;
+        blockHeaderHash = randomHash();
+        byte[] fullBlockHeaderHash = randomHash();
+        BlockFactory blockFactory = mock(BlockFactory.class);
+        blockHeader = mock(BlockHeader.class);
+        messageFactory = new LCMessageFactory(blockFactory);
+        testMessage = new BlockHeaderMessage(id, blockHeader);
+        when(blockHeader.getEncoded()).thenReturn(blockHeaderHash);
+        when(blockHeader.getFullEncoded()).thenReturn(fullBlockHeaderHash);
+        when(blockFactory.decodeHeader(fullBlockHeaderHash)).thenReturn(blockHeader);
+    }
+
+    @Test
+    public void messageCreationShouldBeCorrect() {
+        assertEquals(id, testMessage.getId());
+        assertArrayEquals(blockHeaderHash, testMessage.getBlockHeader().getEncoded());
+        assertNull(testMessage.getAnswerMessage());
+        assertEquals(BLOCK_HEADER, testMessage.getCommand());
+    }
+
+    @Test
+    public void messageEncodeDecodeShouldBeCorrect() {
+        byte[] encoded = testMessage.getEncoded();
+
+        BlockHeaderMessage blockHeaderMessage = (BlockHeaderMessage) messageFactory.create(BLOCK_HEADER.asByte(), encoded);
+
+        assertEquals(id, blockHeaderMessage.getId());
+        assertArrayEquals(blockHeader.getEncoded(), blockHeaderMessage.getBlockHeader().getEncoded());
+        assertEquals(BLOCK_HEADER, blockHeaderMessage.getCommand());
+        assertEquals(testMessage.getAnswerMessage(), blockHeaderMessage.getAnswerMessage());
+        assertArrayEquals(encoded, blockHeaderMessage.getEncoded());
+    }
+}
