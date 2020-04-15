b98515c22fde0a498b8d2c49ba71cd3856f5b473
julianlen <julianlen@gmail.com>
2020-04-15 11:54:27 -0300

    Status message (#1200)
    * change test message for status message adding the latest
    
    * create and test StatusMessage, also addd it to lightProcessor and lightClientHandler, processStatusMessage is not implemented
    
    * added LightSyncProcessoras manager for sync messaged, tested processStatus
    
    * lightPeer added as channel wrapper
    
    * Create Status as class
    
    * processing blockheader after status. Also tested

--- rskj-core/src/test/java/org/ethereum/net/rlpx/MessageCodecTest.java
+++ rskj-core/src/test/java/org/ethereum/net/rlpx/MessageCodecTest.java
@@ -1,6 +1,27 @@
+/*
+ * This file is part of RskJ
+ * Copyright (C) 2020 RSK Labs Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.ethereum.net.rlpx;
 
-import co.rsk.net.light.message.TestMessage;
+import co.rsk.core.BlockDifficulty;
+import co.rsk.crypto.Keccak256;
+import co.rsk.net.light.LightStatus;
+import co.rsk.net.light.message.StatusMessage;
 import co.rsk.net.rlpx.LCMessageFactory;
 import io.netty.channel.ChannelHandlerContext;
 import org.bouncycastle.util.encoders.Hex;
@@ -12,7 +33,6 @@ import org.ethereum.net.client.Capability;
 import org.ethereum.net.eth.EthVersion;
 import org.ethereum.net.eth.message.Eth62MessageFactory;
 import org.ethereum.net.eth.message.EthMessage;
-import org.ethereum.net.eth.message.StatusMessage;
 import org.ethereum.net.message.StaticMessages;
 import org.ethereum.net.p2p.P2pHandler;
 import org.ethereum.net.p2p.P2pMessage;
@@ -21,10 +41,12 @@ import org.ethereum.net.server.Channel;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigInteger;
 import java.net.SocketAddress;
 import java.util.LinkedList;
 import java.util.List;
 
+import static org.ethereum.crypto.HashUtil.randomHash;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -92,7 +114,7 @@ public class MessageCodecTest {
     public void encodeDecodeETHFrameAndShouldReturnETHMessage() {
 
         byte[] payload = Hex.decode("f84927808425c60144a0832056d3c93ff2739ace7199952e5365aa29f18805be05634c4db125c5340216a0955f36d073ccb026b78ab3424c15cf966a7563aa270413859f78702b9e8e22cb");
-        EthMessage ethTestMessage = new StatusMessage(payload);
+        EthMessage ethTestMessage = new org.ethereum.net.eth.message.StatusMessage(payload);
         BlockFactory blockFactory = mock(BlockFactory.class);
         List<Capability> cap = new LinkedList<>();
         cap.add(new Capability(Capability.RSK, EthVersion.UPPER));
@@ -125,7 +147,17 @@ public class MessageCodecTest {
     @Test
     public void encodeDecodeLCFrameAndShouldReturnLCMessage() {
 
-        TestMessage lcTestMessage = new TestMessage();
+        long id = 1L;
+        byte protocolVersion = (byte) 2;
+        int networkId = 2;
+        Keccak256 bestHash = new Keccak256(randomHash());
+        long bestNumber = 10L;
+        byte[] genesisHash = randomHash();
+        BlockDifficulty totalDifficulty = new BlockDifficulty(BigInteger.ONE);
+        LightStatus status = new LightStatus(protocolVersion, networkId,
+                totalDifficulty, bestHash.getBytes(), bestNumber, genesisHash);
+        StatusMessage lcStatusMessage = new StatusMessage(id, status);
+
         List<Capability> cap = new LinkedList<>();
         cap.add(new Capability(Capability.LC, (byte) 0));
         MessageCodec messageCodec = new MessageCodec(ethereumListener, config);
@@ -137,15 +169,15 @@ public class MessageCodecTest {
         List<Object> outMsg = new LinkedList<>();
 
         try {
-            messageCodec.encode(ctx, lcTestMessage, outFrame);
+            messageCodec.encode(ctx, lcStatusMessage, outFrame);
             assertEquals(1, outFrame.size());
 
             FrameCodec.Frame ethFrame = (FrameCodec.Frame) outFrame.get(0);
             messageCodec.decode(ctx, ethFrame, outMsg);
             assertEquals(1, outMsg.size());
 
-            TestMessage lcMessage =  (TestMessage) outMsg.get(0);
-            assertArrayEquals(lcTestMessage.getEncoded(), lcMessage.getEncoded());
+            StatusMessage lcMessage =  (StatusMessage) outMsg.get(0);
+            assertArrayEquals(lcStatusMessage.getEncoded(), lcMessage.getEncoded());
         } catch (Exception e) {
             e.printStackTrace();
         }
