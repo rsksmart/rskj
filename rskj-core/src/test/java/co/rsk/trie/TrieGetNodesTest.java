aa94a56b15bffc07ca4465886020b1777f9d4868
pete <petecarkeek@gmail.com>
2020-04-16 05:05:04 +0000

    more test notes

--- rskj-core/src/test/java/co/rsk/trie/TrieGetNodesTest.java
+++ rskj-core/src/test/java/co/rsk/trie/TrieGetNodesTest.java
@@ -58,7 +58,8 @@ public class TrieGetNodesTest {
 
         trie = trie.put("foo", "bar".getBytes());
         trie = trie.put("bar", "foo".getBytes());
-
+        
+        
         List<Trie> nodes = trie.getNodes("foo");
 
         Assert.assertNotNull(nodes);
@@ -66,7 +67,45 @@ public class TrieGetNodesTest {
         Assert.assertEquals(2, nodes.size());
 
         Assert.assertArrayEquals("bar".getBytes(StandardCharsets.UTF_8), nodes.get(0).getValue());
-        Assert.assertNull(nodes.get(1).getValue());
+        Assert.assertNull(nodes.get(1).getValue());  //the null subkey of "foo" i.e. ""
+    }
+    // mish understanding how getNodes() works.. with subkeys.
+    // so find() and findNodes() probably work the same way?
+    @Test
+    public void putKeyAndSubkeyAndGetNodes() {
+        Trie trie = new Trie();
+
+        //order of trie puts() does not matter for getNodes()
+        trie = trie.put("foo", "bar".getBytes()); // "foo": main key of interest 
+        trie = trie.put("f", "shortSubKeyVal".getBytes()); //short subkey "f", even empty "" works
+        trie = trie.put("baz", "notSubkeyValue".getBytes()); // not a subkey
+        trie = trie.put("fo", "longSubKeyVal".getBytes()); //a longer subkey
+        // adding "super" keys is irrelevant for getNodes()
+        // Cos we walk up the path, towards root node, not down.
+        // Unrelated keys are fine, as they can be linked to upstream branches.  
+        trie = trie.put("food", "superKeyVal".getBytes()); //a super key, should be neglected. 
+        trie = trie.put("foodies", "superPlusKeyVal".getBytes()); //a larger super key
+        
+        // add another unrelated key (different keypath from "foo")
+        trie = trie.put("diffKey", "diffVal".getBytes());
+        
+        // getNodes() ..
+        // nodes with subkeys are added to the list first, along with their values.
+        // startign with the given key, and walking up the keypath to shorter and shorter subkeys
+        // nodes with different keypaths are added as well, but their values are not (null)
+        // nodes that are further down the given keypath (subtrees) are neglected entirely. 
+        List<Trie> nodes = trie.getNodes("foo"); //foo, then fo, then f
+
+        Assert.assertNotNull(nodes);
+        Assert.assertFalse(nodes.isEmpty());
+        Assert.assertEquals(5, nodes.size());  
+
+        Assert.assertArrayEquals("bar".getBytes(StandardCharsets.UTF_8), nodes.get(0).getValue());
+        Assert.assertArrayEquals("longSubKeyVal".getBytes(StandardCharsets.UTF_8), nodes.get(1).getValue());
+        //Assert.assertNotNull(nodes.get(1).getValue()); //subkey's value is getable
+        Assert.assertNotNull(nodes.get(2).getValue()); // subkey values are getable
+        Assert.assertNull(nodes.get(3).getValue()); //not a subkey value is null
+
     }
 
     @Test
@@ -76,7 +115,7 @@ public class TrieGetNodesTest {
         Trie trie2 = trie1.put("foo", "bar2".getBytes());
 
         List<Trie> nodes = trie2.getNodes("foo");
-
+    
         Assert.assertNotNull(nodes);
         Assert.assertFalse(nodes.isEmpty());
         Assert.assertEquals(1, nodes.size());
