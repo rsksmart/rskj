0a4928e838780a7c90c96eb2163feb9eaa9cf46a
ajlopez <webmaster@ajlopez.com>
2020-04-16 15:36:19 -0300

    Checking counter value in recursive contracts tests

--- rskj-core/src/test/java/co/rsk/vm/VmDslTest.java
+++ rskj-core/src/test/java/co/rsk/vm/VmDslTest.java
@@ -23,11 +23,14 @@ import co.rsk.test.dsl.DslParser;
 import co.rsk.test.dsl.DslProcessorException;
 import co.rsk.test.dsl.WorldDslProcessor;
 import org.ethereum.core.Block;
+import org.ethereum.core.Transaction;
 import org.ethereum.core.TransactionReceipt;
+import org.ethereum.vm.DataWord;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import javax.xml.crypto.Data;
 import java.io.FileNotFoundException;
 
 /**
@@ -48,6 +51,18 @@ public class VmDslTest {
         Assert.assertNotNull(block);
         Assert.assertEquals(1, block.getTransactionsList().size());
 
+        Transaction creationTransaction = world.getTransactionByName("tx01");
+
+        Assert.assertNotNull(creationTransaction);
+
+        DataWord counterValue = world
+                .getRepositoryLocator()
+                .snapshotAt(block.getHeader())
+                .getStorageValue(creationTransaction.getContractAddress(), DataWord.ZERO);
+
+        Assert.assertNotNull(counterValue);
+        Assert.assertEquals(200, counterValue.intValue());
+
         TransactionReceipt transactionReceipt = world.getTransactionReceiptByName("tx02");
 
         Assert.assertNotNull(transactionReceipt);
@@ -67,6 +82,22 @@ public class VmDslTest {
         WorldDslProcessor processor = new WorldDslProcessor(world);
 
         processor.processCommands(parser);
+
+        Block block = world.getBlockByName("b02");
+
+        Assert.assertNotNull(block);
+        Assert.assertEquals(1, block.getTransactionsList().size());
+
+        Transaction creationTransaction = world.getTransactionByName("tx01");
+
+        Assert.assertNotNull(creationTransaction);
+
+        DataWord counterValue = world
+                .getRepositoryLocator()
+                .snapshotAt(block.getHeader())
+                .getStorageValue(creationTransaction.getContractAddress(), DataWord.ZERO);
+
+        Assert.assertNull(counterValue);
 
         TransactionReceipt transactionReceipt = world.getTransactionReceiptByName("tx02");
 
