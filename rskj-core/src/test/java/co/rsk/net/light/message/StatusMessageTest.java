f9fdc8d0da98c92ea1553f84087d437115a54a2c
Julian <julianlen@gmail.com>
2020-04-20 16:39:27 -0300

    test with zeros in StatusMessageTest

--- rskj-core/src/test/java/co/rsk/net/light/message/StatusMessageTest.java
+++ rskj-core/src/test/java/co/rsk/net/light/message/StatusMessageTest.java
@@ -37,24 +37,27 @@ import static org.mockito.Mockito.mock;
 
 public class StatusMessageTest {
 
-    private LightStatus lightStatus;
+    private Keccak256 bestHash;
+    private byte[] genesisHash;
 
     @Before
     public void setUp() {
-        byte protocolVersion = (byte) 2;
-        int networkId = 2;
-        Keccak256 bestHash = new Keccak256(randomHash());
-        long bestNumber = 10L;
-        byte[] genesisHash = randomHash();
-        BlockDifficulty totalDifficulty = new BlockDifficulty(BigInteger.ONE);
-        lightStatus = new LightStatus(protocolVersion, networkId,
-                totalDifficulty, bestHash.getBytes(), bestNumber, genesisHash);
+        bestHash = new Keccak256(randomHash());
+        genesisHash = randomHash();
     }
 
     @Test
     public void messageCreationShouldBeCorrect() {
+        BlockDifficulty totalDifficulty = new BlockDifficulty(BigInteger.ONE);
+        byte protocolVersion = (byte) 2;
+        int networkId = 2;
+        long bestNumber = 10L;
+        LightStatus lightStatus = new LightStatus(protocolVersion, networkId,
+                totalDifficulty, bestHash.getBytes(), bestNumber, genesisHash);
+
         long id = 1L;
         StatusMessage testMessage = new StatusMessage(id, lightStatus);
+
         assertEquals(STATUS, testMessage.getCommand());
         assertEquals(id, testMessage.getId());
         assertArrayEquals(lightStatus.getEncoded(), testMessage.getStatus().getEncoded());
@@ -63,16 +66,28 @@ public class StatusMessageTest {
     @Test
     public void messageEncodeDecodeShouldBeCorrect() {
         long id = 1L;
-        createMessageAndAssertEncodeDecode(id);
+        BlockDifficulty totalDifficulty = new BlockDifficulty(BigInteger.ONE);
+        byte protocolVersion = (byte) 2;
+        int networkId = 2;
+        long bestNumber = 10L;
+
+        createMessageAndAssertEncodeDecode(id, protocolVersion, networkId, totalDifficulty, bestNumber);
     }
 
     @Test
-    public void messageEncodeDecodeShouldBeCorrectWithZeroId() {
+    public void messageEncodeDecodeShouldBeCorrectWithZeroParameters() {
         long id = 0;
-        createMessageAndAssertEncodeDecode(id);
+        BlockDifficulty totalDifficulty = new BlockDifficulty(BigInteger.ZERO);
+        byte protocolVersion = (byte) 0;
+        int networkId = 0;
+        long bestNumber = 0;
+
+        createMessageAndAssertEncodeDecode(id, protocolVersion, networkId, totalDifficulty, bestNumber);
     }
 
-    private void createMessageAndAssertEncodeDecode(long id) {
+    private void createMessageAndAssertEncodeDecode(long id, byte protocolVersion, int networkId, BlockDifficulty totalDifficulty, long bestNumber) {
+        LightStatus lightStatus = new LightStatus(protocolVersion, networkId,
+                totalDifficulty, bestHash.getBytes(), bestNumber, genesisHash);
         LCMessageFactory lcMessageFactory = new LCMessageFactory(mock(BlockFactory.class));
         StatusMessage testMessage = new StatusMessage(id, lightStatus);
         StatusMessage decodedMessage = (StatusMessage) lcMessageFactory.create(STATUS.asByte(), testMessage.getEncoded());
