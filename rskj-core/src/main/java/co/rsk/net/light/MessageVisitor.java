4f2dd7b674f0122afaf92b67992ae284ce96f7c4
Sebastian Sicardi <smcsicardi@gmail.com>
2020-04-21 13:15:38 -0300

    header

--- rskj-core/src/main/java/co/rsk/net/light/MessageVisitor.java
+++ rskj-core/src/main/java/co/rsk/net/light/MessageVisitor.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of RskJ
+ * Copyright (C) 2019 RSK Labs Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package co.rsk.net.light;
 
 import co.rsk.net.eth.LightClientHandler;
@@ -6,6 +24,9 @@ import io.netty.channel.ChannelHandlerContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Created by Julian Len and Sebastian Sicardi on 21/04/20.
+ */
 public class MessageVisitor {
     private static final Logger logger = LoggerFactory.getLogger("lightnet");
     private final LightPeer lightPeer;
