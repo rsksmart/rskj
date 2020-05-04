e23c6c549ed0527139d387a1422eb584e676dd0d
Patricio <patricio@iovlabs.org>
2020-05-04 18:13:27 -0300

    Vlad comment possible fix. Sonar fixes

--- rskj-core/src/test/java/org/ethereum/net/rlpx/EIP8HandshakeTest.java
+++ rskj-core/src/test/java/org/ethereum/net/rlpx/EIP8HandshakeTest.java
@@ -76,7 +76,7 @@ public class EIP8HandshakeTest {
         // encode (on A side)
 
         AuthInitiateMessageV4 msg1 = handshakerA.createAuthInitiateV4(keyA);
-        msg1.signature = signatureA;
+        msg1.setSignature(signatureA);
         msg1.nonce = nonceA;
         msg1.version = 4;
 
@@ -88,17 +88,17 @@ public class EIP8HandshakeTest {
 
         assertEquals(4, msg2.version);
         assertArrayEquals(nonceA, msg2.nonce);
-        assertEquals(signatureA.getR(), msg2.signature.getR());
-        assertEquals(signatureA.getS(), msg2.signature.getS());
-        assertEquals(signatureA.getV(), msg2.signature.getV());
+        assertEquals(signatureA.getR(), msg2.getSignature().getR());
+        assertEquals(signatureA.getS(), msg2.getSignature().getS());
+        assertEquals(signatureA.getV(), msg2.getSignature().getV());
 
         AuthInitiateMessageV4 msg3 = handshakerB.decryptAuthInitiateV4(authMessageData, keyB);
 
         assertEquals(4, msg3.version);
         assertArrayEquals(nonceA, msg3.nonce);
-        assertEquals(signatureA.getR(), msg3.signature.getR());
-        assertEquals(signatureA.getS(), msg3.signature.getS());
-        assertEquals(signatureA.getV(), msg3.signature.getV());
+        assertEquals(signatureA.getR(), msg3.getSignature().getR());
+        assertEquals(signatureA.getS(), msg3.getSignature().getS());
+        assertEquals(signatureA.getV(), msg3.getSignature().getV());
     }
 
     // AuthInitiate EIP-8 format with version 56 and 3 additional list elements (sent from A to B)
