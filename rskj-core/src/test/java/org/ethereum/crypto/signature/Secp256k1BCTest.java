f1ede2b1d0fb465fd00645c567f28d93c1a4da82
Patricio <patricio@iovlabs.org>
2020-05-06 11:17:18 -0300

    Refactor names and adding tests.

--- rskj-core/src/test/java/org/ethereum/crypto/signature/Secp256k1BCTest.java
+++
@@ -1,8 +0,0 @@
-package org.ethereum.crypto.signature;
-
-public class Secp256k1BCTest extends AbstractSecp256k1Test {
-
-    public Secp256k1BCTest() {
-        super(new Secp256k1BC());
-    }
-}
--- rskj-core/src/test/java/org/ethereum/crypto/signature/Secp256k1NativeTest.java
+++
@@ -1,8 +0,0 @@
-package org.ethereum.crypto.signature;
-
-public class Secp256k1NativeTest extends AbstractSecp256k1Test {
-
-    public Secp256k1NativeTest() {
-        super(new Secp256k1Native());
-    }
-}
