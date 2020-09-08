66901af5dbc45c855849d7c53fdb34bb9e9b973d
Guido Medina <guido@iovlabs.org>
2020-09-08 16:10:17 -0300

    Add unit test for PeginInstructions classes and update BtcTransactionFormatUtilsTest as it now contains the method extractOpReturnData

--- rskj-core/src/test/java/co/rsk/peg/pegininstructions/PeginInstructionsProviderTest.java
+++ rskj-core/src/test/java/co/rsk/peg/pegininstructions/PeginInstructionsProviderTest.java
@@ -1,4 +1,111 @@
 package co.rsk.peg.pegininstructions;
 
+import co.rsk.bitcoinj.core.BtcTransaction;
+import co.rsk.bitcoinj.core.Coin;
+import co.rsk.bitcoinj.core.NetworkParameters;
+import co.rsk.bitcoinj.script.Script;
+import co.rsk.bitcoinj.script.ScriptOpCodes;
+import co.rsk.config.BridgeRegTestConstants;
+import co.rsk.core.RskAddress;
+import org.bouncycastle.util.encoders.Hex;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PeginInstructionsProviderTest {
+    private final NetworkParameters params = BridgeRegTestConstants.getInstance().getBtcParams();
+
+    @Test(expected = PeginInstructionsException.class)
+    public void peginInstructionsProvider_null_op_return_data() throws Exception {
+        BtcTransaction btcTransaction = new BtcTransaction(params);
+
+        // Add OP_RETURN output with empty data
+        btcTransaction.addOutput(Coin.ZERO, new Script(new byte[] { ScriptOpCodes.OP_RETURN }));
+
+        PeginInstructionsProvider peginInstructionsProvider = new PeginInstructionsProvider();
+        peginInstructionsProvider.buildPeginInstructions(btcTransaction);
+    }
+
+    @Test(expected = PeginInstructionsException.class)
+    public void peginInstructionsProvider_invalid_protocol_version() throws Exception {
+        String rawTx = "0200000001956f33bf742ae944ee92b9f7838c81b27c00d01c51eddf815583d71022f75a50000000006a473044022022" +
+                "279ce3060a1595e61a6d04b89d59b6b8917542bc1fe9073ce2e2fd6c8e24c802205348bd58180fa89f995ff80e01e8cb60100d" +
+                "f0affbc0312a83d291b073f0a4700121036ba47a8665e02bf265f849f41ce9985626fff1a4290dc87cc63c3ab0399f5765ffff" +
+                "ffff0328cdf5050000000017a914603a20f8bdace9fd117feaccd9161f3497662c348700e1f5050000000017a91419d7e0ee9b" +
+                "f6bd70d1d046b066d1c2726e1accc1870000000000000000186a1609990e537aad84447a2c2a7590d5f2665ef5cf9b667a0000" +
+                "0000";
+
+        BtcTransaction btcTransaction = new BtcTransaction(params, Hex.decode(rawTx));
+
+        PeginInstructionsProvider peginInstructionsProvider = new PeginInstructionsProvider();
+        peginInstructionsProvider.buildPeginInstructions(btcTransaction);
+    }
+
+    @Test(expected = PeginInstructionsParseException.class)
+    public void peginInstructionsProvider_parse_data_length_smaller_than_expected_pegin_instructions_v1() throws
+            Exception {
+        String rawTx = "02000000000101df517677f84ebd82a4719f532ef660f10dc6df2963fec596d47a559638f37b2f0000000017160014" +
+                "5ab27e716e6277a99df7267292353b626c36bf16ffffffff03b03024180100000017a914c1a47cc7236ba6bf7081f3ec6440b3" +
+                "6f5c8a41408700e1f505000000001976a91429d3b0d878547000449bef07e44a387761f1578c88ac0000000000000000166a14" +
+                "00010e537aad84447a2c2a7590d5f2665ef5cf9b0247304402207e8e20ae25731ee1a4df4f1ee8b9a0da14f4157bbe7e116408" +
+                "3492631aca3443022072312aabc1d8e95b98a48edbce7d9ce2247ba6b76194d4bc797a2ce38e2ee3330121024212698b17ae06e" +
+                "5abc6520def8ab960adb7afb3f9d6000a4834bd3f5abc146e00000000";
+
+        BtcTransaction btcTransaction = new BtcTransaction(params, Hex.decode(rawTx));
+
+        PeginInstructionsProvider peginInstructionsProvider = new PeginInstructionsProvider();
+        peginInstructionsProvider.buildPeginInstructions(btcTransaction);
+    }
+
+    @Test(expected = PeginInstructionsParseException.class)
+    public void peginInstructionsProvider_parse_data_length_different_than_supported_pegin_instructions_v1() throws
+            Exception {
+        String rawTx = "0200000001d891ab0f1365c7272b4a4932647208886ac42f6a8375a0af2839c89dc58084fb000000006a473044022" +
+                "06dffd165bc596eebf09fb9d61faeeb433bcb4cd498fb701ebed6e5c71afc4a8f02201439e21528214876eb3e37b610dc41a" +
+                "9daa116560f164d3903da762aecd1506d01210337005aa37799aeaa510ac681102c7fcbdac18aa155476332dd8a866e3caaa3" +
+                "35ffffffff0300e1f505000000001976a91429d3b0d878547000449bef07e44a387761f1578c88acc4ccf5050000000017a9" +
+                "14a0251d6d4eb50f44da49b8dce5a78fd46e9dc1a88700000000000000001b6a1900010e537aad84447a2c2a7590d5f2665e" +
+                "f5cf9b667a98909100000000";
+
+        BtcTransaction btcTransaction = new BtcTransaction(params, Hex.decode(rawTx));
+
+        PeginInstructionsProvider peginInstructionsProvider = new PeginInstructionsProvider();
+        peginInstructionsProvider.buildPeginInstructions(btcTransaction);
+    }
+
+    @Test
+    public void peginInstructionsProvider_return_pegin_instructions_v1() throws Exception {
+        String rawTx = "0200000001bf584649795f54f4578f5351bcb072d7f28d97894d8db9733f385edd37d2c64c000000006a47304402203" +
+                "a3103a1d10d814f5cf2143c7408e491648ce5775769b1728e2b51366b641dda02207449de7736549886db203ee49bfd15d59d" +
+                "079002255abda9da2410763321662c01210211f2a6a136195c327ec67df3e6e1ec06e0e5e107f7b518d12d13d2f7263e4ce3" +
+                "ffffffff0380969800000000001976a9148bc874b7d6cf12e920d227e28c994daa5b5cc4ab88ac148ee400000000001976a91" +
+                "44f4c767a2d308eebb3f0f1247f9163c896e0b7d288ac0000000000000000186a1600010e537aad84447a2c2a7590d5f2665e" +
+                "f5cf9b667a00000000";
+
+        BtcTransaction btcTransaction = new BtcTransaction(params, Hex.decode(rawTx));
+
+        PeginInstructionsProvider peginInstructionsProvider = new PeginInstructionsProvider();
+        PeginInstructionsVersion1 peginInstructionsVersion1 =
+                (PeginInstructionsVersion1) peginInstructionsProvider.buildPeginInstructions(btcTransaction);
+
+        Assert.assertEquals(1, peginInstructionsVersion1.getProtocolVersion());
+    }
+
+    @Test
+    public void peginInstructionsProvider_return_rsk_destination_address_from_pegin_instructions_v1() throws Exception {
+        String rawTx = "0200000001bf584649795f54f4578f5351bcb072d7f28d97894d8db9733f385edd37d2c64c000000006a47304402203" +
+                "a3103a1d10d814f5cf2143c7408e491648ce5775769b1728e2b51366b641dda02207449de7736549886db203ee49bfd15d59d" +
+                "079002255abda9da2410763321662c01210211f2a6a136195c327ec67df3e6e1ec06e0e5e107f7b518d12d13d2f7263e4ce3" +
+                "ffffffff0380969800000000001976a9148bc874b7d6cf12e920d227e28c994daa5b5cc4ab88ac148ee400000000001976a91" +
+                "44f4c767a2d308eebb3f0f1247f9163c896e0b7d288ac0000000000000000186a1600010e537aad84447a2c2a7590d5f2665e" +
+                "f5cf9b667a00000000";
+
+        BtcTransaction btcTransaction = new BtcTransaction(params, Hex.decode(rawTx));
+
+        PeginInstructionsProvider peginInstructionsProvider = new PeginInstructionsProvider();
+        PeginInstructionsVersion1 peginInstructionsVersion1 =
+                (PeginInstructionsVersion1) peginInstructionsProvider.buildPeginInstructions(btcTransaction);
+
+        RskAddress expectedRskDestinationAddress = new RskAddress("0x0e537aad84447a2c2a7590d5f2665ef5cf9b667a");
+        Assert.assertEquals(expectedRskDestinationAddress, peginInstructionsVersion1.getRskDestinationAddress());
+    }
 }
--- rskj-core/src/test/java/co/rsk/peg/utils/BtcTransactionFormatUtilsTest.java
+++ rskj-core/src/test/java/co/rsk/peg/utils/BtcTransactionFormatUtilsTest.java
@@ -20,19 +20,22 @@ package co.rsk.peg.utils;
 
 import co.rsk.bitcoinj.core.*;
 import co.rsk.bitcoinj.script.Script;
+import co.rsk.bitcoinj.script.ScriptOpCodes;
 import co.rsk.config.BridgeRegTestConstants;
+import co.rsk.peg.InvalidOpReturnOutputException;
+import co.rsk.peg.NoOpReturnException;
+import org.bouncycastle.util.encoders.Hex;
 import org.junit.Assert;
 import org.junit.Test;
-import org.bouncycastle.util.encoders.Hex;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
 
 public class BtcTransactionFormatUtilsTest {
+    private final NetworkParameters params = BridgeRegTestConstants.getInstance().getBtcParams();
 
     @Test
     public void calculateBtcTxHash() {
-        BtcTransaction btcTransaction = new BtcTransaction(NetworkParameters.fromID(NetworkParameters.ID_REGTEST));
+        BtcTransaction btcTransaction = new BtcTransaction(params);
         Assert.assertThat(BtcTransactionFormatUtils.calculateBtcTxHash(btcTransaction.bitcoinSerialize()), is(btcTransaction.getHash()));
 
         byte[] rawBtcTransaction = Hex.decode("020000000418bc858998739dbb7e7676435178dba5e71157b1537d415518d5c1fce6349018000000006a47304402204317903e40f8736858f87758e6" +
@@ -53,7 +56,7 @@ public class BtcTransactionFormatUtilsTest {
 
     @Test
     public void getInputsCount() {
-        BtcTransaction btcTransaction = new BtcTransaction(NetworkParameters.fromID(NetworkParameters.ID_REGTEST));
+        BtcTransaction btcTransaction = new BtcTransaction(params);
         Assert.assertThat(BtcTransactionFormatUtils.getInputsCount(btcTransaction.bitcoinSerialize()), is(0L));
 
         byte[] rawBtcTransaction = Hex.decode("020000000418bc858998739dbb7e7676435178dba5e71157b1537d415518d5c1fce6349018000000006a47304402204317903e40f8736858f87758e6" +
@@ -79,7 +82,7 @@ public class BtcTransactionFormatUtilsTest {
                 "1fb5d8839b3fae6c74f652c9ffc9291008f4ea39f9565980220354c734511a0560367b300eecb1a7472317a995462622e06ee91" +
                 "cbe0517c17e1012102e87cd90f3cb0d64eeba797fbb8f8ceaadc09e0128afbaefb0ee9535875ea395400000000";
 
-        BtcTransaction tx = new BtcTransaction(BridgeRegTestConstants.getInstance().getBtcParams(), Hex.decode(rawTx));
+        BtcTransaction tx = new BtcTransaction(params, Hex.decode(rawTx));
         Assert.assertThat(tx.getInputs().size(), is(1));
         Assert.assertThat(BtcTransactionFormatUtils.getInputsCountForSegwit(Hex.decode(rawTx)), is(1L));
     }
@@ -92,16 +95,60 @@ public class BtcTransactionFormatUtilsTest {
                 "1fb5d8839b3fae6c74f652c9ffc9291008f4ea39f9565980220354c734511a0560367b300eecb1a7472317a995462622e06ee91" +
                 "cbe0517c17e1012102e87cd90f3cb0d64eeba797fbb8f8ceaadc09e0128afbaefb0ee9535875ea395400000000";
 
-        BtcTransaction otherTx = new BtcTransaction(BridgeRegTestConstants.getInstance().getBtcParams());
+        BtcTransaction otherTx = new BtcTransaction(params);
         otherTx.addInput(Sha256Hash.ZERO_HASH, 0, new Script(new byte[]{}));
         TransactionWitness txWit = new TransactionWitness(1);
         txWit.setPush(0, new byte[]{});
         otherTx.setWitness(0, txWit);
-        otherTx.addOutput(Coin.COIN, Address.fromBase58(BridgeRegTestConstants.getInstance().getBtcParams(), "mvbnrCX3bg1cDRUu8pkecrvP6vQkSLDSou"));
+        otherTx.addOutput(Coin.COIN, Address.fromBase58(params, "mvbnrCX3bg1cDRUu8pkecrvP6vQkSLDSou"));
 
-        BtcTransaction tx = new BtcTransaction(BridgeRegTestConstants.getInstance().getBtcParams(), Hex.decode(rawTx));
+        BtcTransaction tx = new BtcTransaction(params, Hex.decode(rawTx));
 
         Assert.assertThat(tx.getInputs().size(), is(1));
         Assert.assertThat(BtcTransactionFormatUtils.getInputsCountForSegwit(otherTx.bitcoinSerialize()), is(1L));
+    }
+
+    @Test(expected = NoOpReturnException.class)
+    public void getOpReturnOutput_no_op_return() throws NoOpReturnException, InvalidOpReturnOutputException {
+        BtcTransaction btcTransaction = new BtcTransaction(params);
+        BtcTransactionFormatUtils.extractOpReturnData(btcTransaction);
+    }
+
+    @Test(expected = InvalidOpReturnOutputException.class)
+    public void getOpReturnOutput_two_op_return() throws NoOpReturnException, InvalidOpReturnOutputException {
+        String rawTx = "020000000180dadf166442396b8e9b31215169d7d52c052b43fa4c3421c6324a29b80a7c05000000006a47304402205" +
+                "5fbcfd50d0133a09476770c5961662fe9cd9be2fb012dcef1d5f47a87fe418702200b2e2d5a68bd21ac78d84a5b4e3dd7bc852" +
+                "b81664457bbce3a86eb9a6b6ed3e1012103bfba43a10d19431f8288ccd8237ed68cc76279fe705160f2684d27f18fbddfc3fff" +
+                "fffff0400e1f5050000000017a91419d7e0ee9bf6bd70d1d046b066d1c2726e1accc18700000000000000002d6a2b00010e537" +
+                "aad84447a2c2a7590d5f2665ef5cf9b667a0219d7e0ee9bf6bd70d1d046b066d1c2726e1accc100000000000000002d6a2b000" +
+                "10e537aad84447a2c2a7590d5f2665ef5cf9b667a0219d7e0ee9bf6bd70d1d046b066d1c2726e1accc14cc7f5050000000017a" +
+                "9141d0e9e273c2cd3a18a769ae43a92eba8b54039e78700000000";
+
+        BtcTransaction btcTransaction = new BtcTransaction(params, Hex.decode(rawTx));
+        BtcTransactionFormatUtils.extractOpReturnData(btcTransaction);
+    }
+
+    @Test
+    public void getOpReturnOutput_one_op_return() throws NoOpReturnException, InvalidOpReturnOutputException {
+        String rawTx = "0200000001bf584649795f54f4578f5351bcb072d7f28d97894d8db9733f385edd37d2c64c000000006a47304402203a" +
+                "3103a1d10d814f5cf2143c7408e491648ce5775769b1728e2b51366b641dda02207449de7736549886db203ee49bfd15d59d079" +
+                "002255abda9da2410763321662c01210211f2a6a136195c327ec67df3e6e1ec06e0e5e107f7b518d12d13d2f7263e4ce3ffffff" +
+                "ff0380969800000000001976a9148bc874b7d6cf12e920d227e28c994daa5b5cc4ab88ac148ee400000000001976a9144f4c767" +
+                "a2d308eebb3f0f1247f9163c896e0b7d288ac0000000000000000186a1600010e537aad84447a2c2a7590d5f2665ef5cf9b667a" +
+                "00000000";
+
+        BtcTransaction btcTransaction = new BtcTransaction(params, Hex.decode(rawTx));
+        byte[] data = BtcTransactionFormatUtils.extractOpReturnData(btcTransaction);
+
+        Assert.assertEquals("00010e537aad84447a2c2a7590d5f2665ef5cf9b667a", Hex.toHexString(data));
+    }
+
+    @Test
+    public void getOpReturnOutput_empty_data_op_return() throws NoOpReturnException, InvalidOpReturnOutputException {
+        BtcTransaction btcTransaction = new BtcTransaction(params);
+
+        // Add OP_RETURN output with empty data
+        btcTransaction.addOutput(Coin.ZERO, new Script(new byte[] { ScriptOpCodes.OP_RETURN }));
+        Assert.assertNull(BtcTransactionFormatUtils.extractOpReturnData(btcTransaction));
     }
 }
