c51dc6bdeb9e05b0b2fa06e28c88df0575ba32b9
Guido Medina <guido@iovlabs.org>
2020-09-09 16:03:58 -0300

    Applying improvements in PeginInstructions code - Reorder PeginInstructionsBase class methods - Move extractOpReturnData to PeginInstructionsProvider class - Make getBtcRefundAddressFromData method protected - Add override annotation to getRskDestinationAddress and getProtocolVersion methods on base class - Add op_return data validation on PegInstructionsProvider - Fix some test and add validations - Make buildPeginInstructions Optional - Rename getBtcRefundAddress to parseBtcRefundAddress and make it void - Move NoOpReturnException class to pegininstructions package - Add some debug info - Remove unnecessary data validation length from provider - Move getBtcRefundAddress logic to parseAdditionalData on PeginInstructionsV1

--- rskj-core/src/main/java/co/rsk/peg/InvalidOpReturnOutputException.java
+++
@@ -1,7 +0,0 @@
-package co.rsk.peg;
-
-public class InvalidOpReturnOutputException extends Exception {
-    public InvalidOpReturnOutputException(String message) {
-        super(message);
-    }
-}
--- rskj-core/src/test/java/co/rsk/peg/pegininstructions/PeginInstructionsBaseTest.java
+++ rskj-core/src/test/java/co/rsk/peg/pegininstructions/PeginInstructionsBaseTest.java
@@ -13,4 +13,12 @@ public class PeginInstructionsBaseTest {
 
         Assert.assertEquals(1, protocolVersion);
     }
+
+    @Test
+    public void extractProtocolVersion_version_bigger_than_9() {
+        int protocolVersion = PeginInstructionsBase.extractProtocolVersion(
+            Hex.decode("000a0e537aad84447a2c2a7590d5f2665ef5cf9b667a"));
+
+        Assert.assertEquals(10, protocolVersion);
+    }
 }
