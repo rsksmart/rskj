00ac30eca34ae4efeaa6adf358ef97bf5e9ceed5
Guido Medina <guido@iovlabs.org>
2020-09-15 10:40:16 -0300

    Remove data length validation from provider Validate data on extractProtocolVersion

--- rskj-core/src/test/java/co/rsk/peg/pegininstructions/PeginInstructionsBaseTest.java
+++ rskj-core/src/test/java/co/rsk/peg/pegininstructions/PeginInstructionsBaseTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class PeginInstructionsBaseTest {
 
     @Test
-    public void extractProtocolVersion() {
+    public void extractProtocolVersion() throws PeginInstructionsParseException {
         int protocolVersion = PeginInstructionsBase.extractProtocolVersion(
                 Hex.decode("00010e537aad84447a2c2a7590d5f2665ef5cf9b667a"));
 
@@ -15,10 +15,20 @@ public class PeginInstructionsBaseTest {
     }
 
     @Test
-    public void extractProtocolVersion_version_bigger_than_9() {
+    public void extractProtocolVersion_version_bigger_than_9() throws PeginInstructionsParseException {
         int protocolVersion = PeginInstructionsBase.extractProtocolVersion(
             Hex.decode("000a0e537aad84447a2c2a7590d5f2665ef5cf9b667a"));
 
         Assert.assertEquals(10, protocolVersion);
+    }
+
+    @Test(expected = PeginInstructionsParseException.class)
+    public void extractProtocolVersion_null_data() throws PeginInstructionsParseException {
+        PeginInstructionsBase.extractProtocolVersion(null);
+    }
+
+    @Test(expected = PeginInstructionsParseException.class)
+    public void extractProtocolVersion_one_byte_data() throws PeginInstructionsParseException {
+        PeginInstructionsBase.extractProtocolVersion(new byte[]{1});
     }
 }
