fcb850c5af69ccff1ba3de25de7bb68495b590f3
Volodymyr Kravets <volodymyr.kravets@outlook.com>
2020-09-17 09:50:04 +0300

    Updated unit tests

--- rskj-core/src/test/java/co/rsk/net/handler/txvalidator/TxValidatorNonceRangeValidatorTest.java
+++ rskj-core/src/test/java/co/rsk/net/handler/txvalidator/TxValidatorNonceRangeValidatorTest.java
@@ -48,23 +48,23 @@ public class TxValidatorNonceRangeValidatorTest {
 
     @Test
     public void fiveSlotsRange() {
-        Transaction tx1 = Mockito.mock(Transaction.class);
-        Transaction tx2 = Mockito.mock(Transaction.class);
-        Transaction tx3 = Mockito.mock(Transaction.class);
-        Transaction tx4 = Mockito.mock(Transaction.class);
-        AccountState as = Mockito.mock(AccountState.class);
+        Transaction[] txs = new Transaction[7];
+        for (int i = 0; i < 7; i++) {
+            txs[i] = Mockito.mock(Transaction.class);
+            Mockito.when(txs[i].getNonceAsInteger()).thenReturn(BigInteger.valueOf(i));
+        }
 
-        Mockito.when(tx1.getNonceAsInteger()).thenReturn(BigInteger.valueOf(0));
-        Mockito.when(tx2.getNonceAsInteger()).thenReturn(BigInteger.valueOf(1));
-        Mockito.when(tx3.getNonceAsInteger()).thenReturn(BigInteger.valueOf(5));
-        Mockito.when(tx4.getNonceAsInteger()).thenReturn(BigInteger.valueOf(6));
+        AccountState as = Mockito.mock(AccountState.class);
         Mockito.when(as.getNonce()).thenReturn(BigInteger.valueOf(1));
 
         TxValidatorNonceRangeValidator tvnrv = new TxValidatorNonceRangeValidator(5);
-        Assert.assertFalse(tvnrv.validate(tx1, as, null, null, Long.MAX_VALUE, false).transactionIsValid());
-        Assert.assertTrue(tvnrv.validate(tx2, as, null, null, Long.MAX_VALUE, false).transactionIsValid());
-        Assert.assertTrue(tvnrv.validate(tx3, as, null, null, Long.MAX_VALUE, false).transactionIsValid());
-        Assert.assertFalse(tvnrv.validate(tx4, as, null, null, Long.MAX_VALUE, false).transactionIsValid());
+
+        for (int i = 0; i < 7; i++) {
+            Transaction tx = txs[i];
+            long txNonce = tx.getNonceAsInteger().longValue();
+            boolean isValid = tvnrv.validate(tx, as, null, null, Long.MAX_VALUE, false).transactionIsValid();
+            Assert.assertEquals(isValid, txNonce >= 1 && txNonce <= 5); // only valid if tx nonce is in the range [1; 5]
+        }
     }
 
     @Test(expected = IllegalArgumentException.class)
