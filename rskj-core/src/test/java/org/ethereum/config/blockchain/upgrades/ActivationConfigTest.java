cdbf1c48cdd4bdf7afca846a76356ce6c277a0bf
Guido Medina <guido@iovlabs.org>
2020-09-23 10:57:57 -0300

    Create RSKIP 176 code

--- rskj-core/src/test/java/org/ethereum/config/blockchain/upgrades/ActivationConfigTest.java
+++ rskj-core/src/test/java/org/ethereum/config/blockchain/upgrades/ActivationConfigTest.java
@@ -78,6 +78,7 @@ public class ActivationConfigTest {
             "    rskip171: iris300",
             "    rskip172: iris300",
             "    rskip174: iris300",
+            "    rskip176: iris300",
             "    rskip179: iris300",
             "    rskip180: iris300",
             "    rskip181: iris300",
