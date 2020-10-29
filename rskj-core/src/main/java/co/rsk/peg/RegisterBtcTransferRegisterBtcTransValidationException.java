a963d710cc14b9a01dbd69124f5fd9122baa06b0
Pamela Gonzalez Perilli <pamelagonzalezperilli@MacBook-Pro-de-Pamela.local>
2020-10-29 10:39:39 -0300

    Create pre-validations for the RegisterBtcTransfer function
    This commit is only based on creating the previous validations for this function. It does not include the logic of the functionality itself.

---
+++ rskj-core/src/main/java/co/rsk/peg/RegisterBtcTransferRegisterBtcTransValidationException.java
@@ -0,0 +1,7 @@
+package co.rsk.peg;
+
+public class RegisterBtcTransferRegisterBtcTransValidationException extends RegisterBtcTransferException{
+    public RegisterBtcTransferRegisterBtcTransValidationException(String message) {
+        super(message);
+    }
+}
