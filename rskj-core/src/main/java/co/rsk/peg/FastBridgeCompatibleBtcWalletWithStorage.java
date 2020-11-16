379c5d58da2fee736674db7d148ec1a042c0144a
Guido Medina <guido@iovlabs.org>
2020-11-16 15:35:27 -0300

    - Create abstract class FastBridgeCompatibleBtcWallet and classes FastBridgeCompatibleBtcWalletWithSingleScript & FastBridgeCompatibleBtcWalletWithStorage that extends from it. This change allows creating wallet for generating rejection release for fast bridge federations.

--- rskj-core/src/main/java/co/rsk/peg/FastBridgeCompatibleBtcWalletWithStorage.java
+++ rskj-core/src/main/java/co/rsk/peg/FastBridgeCompatibleBtcWalletWithStorage.java
@@ -1,18 +1,14 @@
 package co.rsk.peg;
 
 import co.rsk.bitcoinj.core.Context;
-import co.rsk.bitcoinj.script.RedeemScriptParser;
-import co.rsk.bitcoinj.script.Script;
-import co.rsk.bitcoinj.wallet.RedeemData;
 import co.rsk.peg.fastbridge.FastBridgeFederationInformation;
 import java.util.List;
 import java.util.Optional;
-import javax.annotation.Nullable;
 
-public class FastBridgeCompatibleBtcWallet extends BridgeBtcWallet {
+public class FastBridgeCompatibleBtcWalletWithStorage extends FastBridgeCompatibleBtcWallet {
     private final BridgeStorageProvider bridgeStorageProvider;
 
-    public FastBridgeCompatibleBtcWallet(
+    public FastBridgeCompatibleBtcWalletWithStorage(
         Context btcContext,
         List<Federation> federations,
         BridgeStorageProvider bridgeStorageProvider
@@ -21,33 +17,9 @@ public class FastBridgeCompatibleBtcWallet extends BridgeBtcWallet {
         this.bridgeStorageProvider = bridgeStorageProvider;
     }
 
-    @Nullable
     @Override
-    public RedeemData findRedeemDataFromScriptHash(byte[] payToScriptHash) {
-        Optional<FastBridgeFederationInformation> fastBridgeFederationInformation =
-            this.bridgeStorageProvider.getFastBridgeFederationInformation(payToScriptHash);
-
-        if (fastBridgeFederationInformation.isPresent()) {
-            FastBridgeFederationInformation fastBridgeFederationInformationInstance =
-                fastBridgeFederationInformation.get();
-
-            Optional<Federation> destinationFederation = getDestinationFederation(
-                fastBridgeFederationInformationInstance.getFederationScriptHash()
-            );
-
-            if (!destinationFederation.isPresent()) {
-                return null;
-            }
-
-            Federation destinationFederationInstance = destinationFederation.get();
-            Script fedRedeemScript = destinationFederationInstance.getRedeemScript();
-            Script fastBridgeRedeemScript = RedeemScriptParser
-                .createMultiSigFastBridgeRedeemScript(fedRedeemScript,
-                    fastBridgeFederationInformationInstance.getDerivationHash());
-
-            return RedeemData.of(destinationFederationInstance.getBtcPublicKeys(), fastBridgeRedeemScript);
-        }
-
-        return super.findRedeemDataFromScriptHash(payToScriptHash);
+    protected Optional<FastBridgeFederationInformation> getFastBridgeFederationInformation(
+        byte[] payToScriptHash) {
+        return this.bridgeStorageProvider.getFastBridgeFederationInformation(payToScriptHash);
     }
 }
