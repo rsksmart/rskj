4739c3f7fb17d1e826686bef8befc93ec9fd9e6f
Pamela Gonzalez Perilli <pamelagonzalezperilli@MacBook-Pro-de-Pamela.local>
2020-11-19 09:30:09 -0300

    Save data of Fast Bridge in Storage

--- rskj-core/src/test/java/co/rsk/peg/BridgeSupportTest.java
+++ rskj-core/src/test/java/co/rsk/peg/BridgeSupportTest.java
@@ -6083,6 +6083,9 @@ public class BridgeSupportTest {
             activations
         );
 
+        FederationSupport federationSupportMock = mock(FederationSupport.class);
+        doReturn(provider.getNewFederationBtcUTXOs()).when(federationSupportMock).getActiveFederationBtcUTXOs();
+
         BridgeSupport bridgeSupport = spy(new BridgeSupport(
             bridgeConstants,
             provider,
@@ -6139,6 +6142,10 @@ public class BridgeSupportTest {
             preCallLbcAddressBalance.add(co.rsk.core.Coin.fromBitcoin(Coin.COIN)),
             postCallLbcAddressBalance
         );
+
+        bridgeSupport.save();
+        Assert.assertTrue(provider.isFastBridgeFederationDerivationHashUsed(tx.getHash(), Sha256Hash.ZERO_HASH));
+        Assert.assertEquals(1, provider.getNewFederationBtcUTXOs().size());
     }
 
     @Test
@@ -6155,6 +6162,7 @@ public class BridgeSupportTest {
             mock(BridgeStorageProvider.class),
             mock(BridgeEventLogger.class),
             new BtcLockSenderProvider(),
+            new PeginInstructionsProvider(),
             mock(Repository.class),
             mock(Block.class),
             mock(Context.class),
@@ -6369,8 +6377,48 @@ public class BridgeSupportTest {
         tx.addOutput(amount, btcAddress);
         BtcECKey srcKey = new BtcECKey();
         tx.addInput(PegTestUtils.createHash(1),
-            0, ScriptBuilder.createInputScript(null, srcKey));
+                0, ScriptBuilder.createInputScript(null, srcKey));
         return tx;
+    }
+
+    @Test
+    public void saveFastBridgeDataInStorage_Ok() throws IOException {
+        Repository repository = createRepository();
+        BridgeStorageProvider provider = new BridgeStorageProvider(
+            repository,
+            PrecompiledContracts.BRIDGE_ADDR,
+            bridgeConstants,
+            activationsAfterForks);
+        BridgeSupport bridgeSupport = getBridgeSupport(bridgeConstants, provider, activationsAfterForks);
+
+        Sha256Hash btcTxHash = PegTestUtils.createHash(1);
+        Sha256Hash derivationHash = PegTestUtils.createHash(1);
+
+        byte[] fastBridgeScriptHash = new byte[]{0x1};
+        FastBridgeFederationInformation fastBridgeFederationInformation = new FastBridgeFederationInformation(
+            PegTestUtils.createHash(2),
+            new byte[]{0x1},
+            fastBridgeScriptHash
+        );
+
+        List<UTXO> utxos = new ArrayList<>();
+        Sha256Hash utxoHash = PegTestUtils.createHash(1);
+        UTXO utxo = new UTXO(utxoHash, 0, Coin.COIN.multiply(2), 0, false, new Script(new byte[]{}));
+        utxos.add(utxo);
+
+        Assert.assertEquals(0, provider.getNewFederationBtcUTXOs().size());
+        bridgeSupport.saveFastBridgeDataInStorage(btcTxHash, derivationHash, fastBridgeFederationInformation,  utxos);
+
+        bridgeSupport.save();
+
+        Assert.assertEquals(1, provider.getNewFederationBtcUTXOs().size());
+        assertEquals(utxo, provider.getNewFederationBtcUTXOs().get(0));
+        Assert.assertTrue(provider.isFastBridgeFederationDerivationHashUsed(btcTxHash, derivationHash));
+        Optional<FastBridgeFederationInformation> optionalFastBridgeFederationInformation = provider.getFastBridgeFederationInformation(fastBridgeScriptHash);
+        Assert.assertTrue(optionalFastBridgeFederationInformation.isPresent());
+        FastBridgeFederationInformation obtainedFastBridgeFederationInformation = optionalFastBridgeFederationInformation.get();
+        Assert.assertEquals(fastBridgeFederationInformation.getDerivationHash(), obtainedFastBridgeFederationInformation.getDerivationHash() );
+        Assert.assertArrayEquals(fastBridgeFederationInformation.getFederationScriptHash(), obtainedFastBridgeFederationInformation.getFederationScriptHash() );
     }
 
     private void assertRefundInProcessPegInVersion1(
