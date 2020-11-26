64a65bdfc84703f0f533ab613347ae6ab3124cf8
Volodymyr Kravets <volodymyr.kravets@outlook.com>
2020-11-26 14:11:50 +0200

    Consider MGP in eth_getPrice

--- rskj-core/src/main/java/org/ethereum/listener/GasPriceTracker.java
+++ rskj-core/src/main/java/org/ethereum/listener/GasPriceTracker.java
@@ -27,8 +27,10 @@ import org.ethereum.core.TransactionReceipt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Calculates a 'reasonable' Gas price based on statistics of the latest transaction's Gas prices
@@ -39,17 +41,29 @@ import java.util.List;
  * Created by Anton Nashatyrev on 22.09.2015.
  */
 public class GasPriceTracker extends EthereumListenerAdapter {
+
     private static final Logger logger = LoggerFactory.getLogger("gaspricetracker");
 
-    private final Coin[] window = new Coin[512];
+    private static final int WINDOW_SIZE = 512;
+
+    private static final BigInteger BI_10 = BigInteger.valueOf(10);
+    private static final BigInteger BI_11 = BigInteger.valueOf(11);
+
+    private final Coin[] window = new Coin[WINDOW_SIZE];
+    private final AtomicReference<Coin> bestBlockPriceRef = new AtomicReference<>();
+
     private Coin defaultPrice = Coin.valueOf(20_000_000_000L);
-    private int idx = window.length - 1;
-    private boolean filled = false;
+    private int idx = WINDOW_SIZE - 1;
 
     private Coin lastVal;
 
     @Override
-    public void onBlock(Block block, List<TransactionReceipt> receipts) {
+    public void onBestBlock(Block block, List<TransactionReceipt> receipts) {
+        bestBlockPriceRef.set(block.getMinimumGasPrice());
+    }
+
+    @Override
+    public synchronized void onBlock(Block block, List<TransactionReceipt> receipts) {
         logger.trace("Start onBlock");
 
         defaultPrice = block.getMinimumGasPrice();
@@ -61,30 +75,39 @@ public class GasPriceTracker extends EthereumListenerAdapter {
         logger.trace("End onBlock");
     }
 
-    public void onTransaction(Transaction tx) {
+    private void onTransaction(Transaction tx) {
         if (tx instanceof RemascTransaction) {
             return;
         }
 
         if (idx == -1) {
-            idx = window.length - 1;
-            filled = true;
+            idx = WINDOW_SIZE - 1;
             lastVal = null;  // recalculate only 'sometimes'
         }
 
         window[idx--] = tx.getGasPrice();
     }
 
-    public Coin getGasPrice() {
-        if (!filled) {
+    public synchronized Coin getGasPrice() {
+        if (window[0] == null) { // not filled yet
             return defaultPrice;
         } else {
             if (lastVal == null) {
-                Coin[] values = Arrays.copyOf(window, window.length);
+                Coin[] values = Arrays.copyOf(window, WINDOW_SIZE);
                 Arrays.sort(values);
                 lastVal = values[values.length / 4];  // 25% percentile
             }
-            return lastVal;
+
+            Coin bestBlockPrice = bestBlockPriceRef.get();
+            if (bestBlockPrice == null) {
+                return lastVal;
+            } else {
+                return max(lastVal, bestBlockPrice.multiply(BI_11).divide(BI_10));
+            }
         }
+    }
+
+    private static Coin max(Coin a, Coin b) {
+        return a.compareTo(b) > 0 ? a : b;
     }
 }
