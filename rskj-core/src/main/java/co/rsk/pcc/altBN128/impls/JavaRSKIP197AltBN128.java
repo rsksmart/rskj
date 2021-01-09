c6b534d69d2733c25521e5ccba2c225f472ae01a
Patricio <patricio@iovlabs.org>
2021-01-08 21:46:17 -0300

    fix sonar

--- rskj-core/src/main/java/co/rsk/pcc/altBN128/impls/JavaRSKIP197AltBN128.java
+++ rskj-core/src/main/java/co/rsk/pcc/altBN128/impls/JavaRSKIP197AltBN128.java
@@ -1,12 +1,5 @@
 package co.rsk.pcc.altBN128.impls;
 
-import co.rsk.crypto.altbn128java.*;
-import org.ethereum.util.BIUtil;
-import org.ethereum.vm.DataWord;
-
-import static co.rsk.pcc.altBN128.BN128Pairing.PAIR_SIZE;
-import static org.ethereum.util.ByteUtil.*;
-
 public class JavaRSKIP197AltBN128 extends JavaAltBN128 {
 
     @Override
