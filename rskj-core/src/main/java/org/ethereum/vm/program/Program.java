5f2b20b26fbf1e51d4bd80c895a9e134e2ce2531
José Dahlquist <jose.dahlquist@gmail.com>
2021-01-11 15:36:30 -0300

    Change Program for precompiled contracts
    Add log support for precompiled contracts when executing internal transaction

--- rskj-core/src/main/java/org/ethereum/vm/program/Program.java
+++ rskj-core/src/main/java/org/ethereum/vm/program/Program.java
@@ -1420,7 +1420,7 @@ public class Program {
                     Collections.emptyList()
             );
 
-            contract.init(internalTx, executionBlock, track, this.invoke.getBlockStore(), null, null);
+            contract.init(internalTx, executionBlock, track, this.invoke.getBlockStore(), null, result.getLogInfoList());
         }
 
         long requiredGas = contract.getGasForData(data);
