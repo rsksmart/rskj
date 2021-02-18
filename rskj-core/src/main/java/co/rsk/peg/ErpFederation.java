d763058bdb03c82f9b1660cd66847ca17477ac17
Guido Medina <guido@iovlabs.org>
2021-02-18 10:28:45 -0300

    Create ERP Federation class
    - Make Federation class inheritable and create ERP Federation class
    - Update PendingFederation class to create an ERP federation from RSKIP 199 activation

---
+++ rskj-core/src/main/java/co/rsk/peg/ErpFederation.java
@@ -0,0 +1,57 @@
+package co.rsk.peg;
+
+import co.rsk.bitcoinj.core.BtcECKey;
+import co.rsk.bitcoinj.core.NetworkParameters;
+import co.rsk.bitcoinj.script.ErpFederationRedeemScriptParser;
+import co.rsk.bitcoinj.script.Script;
+import co.rsk.bitcoinj.script.ScriptBuilder;
+import java.time.Instant;
+import java.util.List;
+
+public class ErpFederation extends Federation {
+    private final List<BtcECKey> erpPubKeys;
+    private final long activationDelay;
+
+    public ErpFederation(
+        List<FederationMember> members,
+        Instant creationTime,
+        long creationBlockNumber,
+        NetworkParameters btcParams,
+        List<BtcECKey> erpPubKeys,
+        long activationDelay
+    ) {
+        super(members, creationTime, creationBlockNumber, btcParams);
+        this.erpPubKeys = erpPubKeys;
+        this.activationDelay = activationDelay;
+    }
+
+    public List<BtcECKey> getErpPubKeys() {
+        return erpPubKeys;
+    }
+
+    public long getActivationDelay() {
+        return activationDelay;
+    }
+
+    @Override
+    public Script getRedeemScript() {
+        if (redeemScript == null) {
+            redeemScript = ErpFederationRedeemScriptParser.createErpRedeemScript(
+                ScriptBuilder.createRedeemScript(getNumberOfSignaturesRequired(), getBtcPublicKeys()),
+                ScriptBuilder.createRedeemScript(2, erpPubKeys),
+                activationDelay
+            );
+        }
+
+        return redeemScript;
+    }
+
+    @Override
+    public Script getP2SHScript() {
+        if (p2shScript == null) {
+            p2shScript = ScriptBuilder.createP2SHOutputScript(redeemScript);
+        }
+
+        return p2shScript;
+    }
+}
