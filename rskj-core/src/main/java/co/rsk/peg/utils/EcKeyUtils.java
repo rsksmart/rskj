b48b2864151fa397d52ce96193e7f965e4a89143
Guido Medina <guido@iovlabs.org>
2021-02-22 16:25:28 -0300

    Create ECKey Util class
    - Create method to compress public keys
    - Add unit tests

---
+++ rskj-core/src/main/java/co/rsk/peg/utils/EcKeyUtils.java
@@ -0,0 +1,21 @@
+package co.rsk.peg.utils;
+
+import co.rsk.bitcoinj.core.BtcECKey;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EcKeyUtils {
+    public static List<BtcECKey> getCompressedPubKeysList(List<BtcECKey> pubKeys) {
+        List<BtcECKey> compressedPubKeysList = new ArrayList<>();
+
+        for (BtcECKey key: pubKeys) {
+            compressedPubKeysList.add(
+                BtcECKey.fromPublicOnly(
+                    key.getPubKeyPoint().getEncoded(true)
+                )
+            );
+        }
+
+        return compressedPubKeysList;
+    }
+}
