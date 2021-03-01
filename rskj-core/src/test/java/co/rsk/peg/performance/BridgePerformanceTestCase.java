568105b90d8a9a214e94930c0d8f57b4f64c4d2d
Pamela Gonzalez Perilli <pamgonzalezperilli@gmail.com>
2021-03-01 14:15:43 -0300

    Include new case to refund case in ReleaseBtc function

--- rskj-core/src/test/java/co/rsk/peg/performance/BridgePerformanceTestCase.java
+++ rskj-core/src/test/java/co/rsk/peg/performance/BridgePerformanceTestCase.java
@@ -210,6 +210,7 @@ public abstract class BridgePerformanceTestCase extends PrecompiledContractPerfo
         EnvironmentBuilder environmentBuilder = new EnvironmentBuilder() {
             private Bridge bridge;
             private RepositoryTrackWithBenchmarking benchmarkerTrack;
+            private BridgeStorageProvider storageProvider;
 
             private TrieStore createTrieStore() {
                 return new TrieStoreImpl(new HashMapDB());
@@ -221,8 +222,18 @@ public abstract class BridgePerformanceTestCase extends PrecompiledContractPerfo
                 Trie trie = new Trie(trieStore);
                 benchmarkerTrack = new RepositoryTrackWithBenchmarking(trieStore,  trie);
                 Repository repository = benchmarkerTrack.startTracking();
-                BridgeStorageProvider storageProvider = new BridgeStorageProvider(repository, PrecompiledContracts.BRIDGE_ADDR, bridgeConstants, activationConfig.forBlock((long) executionIndex));
-                BtcBlockStore btcBlockStore = btcBlockStoreFactory.newInstance(repository, bridgeConstants, storageProvider, activationConfig.forBlock((long) executionIndex));
+                storageProvider = new BridgeStorageProvider(
+                        repository,
+                        PrecompiledContracts.BRIDGE_ADDR,
+                        bridgeConstants,
+                        activationConfig.forBlock((long) executionIndex)
+                );
+                BtcBlockStore btcBlockStore = btcBlockStoreFactory.newInstance(
+                        repository,
+                        bridgeConstants,
+                        storageProvider,
+                        activationConfig.forBlock((long) executionIndex)
+                );
                 storageInitializer.initialize(storageProvider, repository, executionIndex, btcBlockStore);
                 repository.addBalance(PrecompiledContracts.BRIDGE_ADDR, co.rsk.core.Coin.fromBitcoin(Coin.COIN.multiply(21_000_000L)));
 
@@ -280,6 +291,11 @@ public abstract class BridgePerformanceTestCase extends PrecompiledContractPerfo
                     @Override
                     public BenchmarkedRepository getBenchmarkedRepository() {
                         return benchmarkerTrack;
+                    }
+
+                    @Override
+                    public BridgeStorageProvider getStorageProvider() {
+                        return storageProvider;
                     }
 
                     @Override
