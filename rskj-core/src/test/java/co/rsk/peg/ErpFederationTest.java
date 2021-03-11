4bc250c4c3ad73e693849a9a88b0a214cd4b5bf4
Guido Medina <guido@iovlabs.org>
2021-03-11 11:45:40 -0300

    Update ERP regtest constants

--- rskj-core/src/test/java/co/rsk/peg/ErpFederationTest.java
+++ rskj-core/src/test/java/co/rsk/peg/ErpFederationTest.java
@@ -20,6 +20,8 @@ public class ErpFederationTest {
         "03b9fc46657cf72a1afa007ecf431de1cd27ff5cc8829fa625b66ca47b967e6b24",
         "029cecea902067992d52c38b28bf0bb2345bda9b21eca76b16a17c477a64e43301",
         "03284178e5fbcc63c54c3b38e3ef88adf2da6c526313650041b0ef955763634ebd",
+        "03776b1fd8f86da3c1db3d69699e8250a15877d286734ea9a6da8e9d8ad25d16c1",
+        "03ab0e2cd7ed158687fc13b88019990860cdb72b1f5777b58513312550ea1584bc"
     }).map(hex -> BtcECKey.fromPublicOnly(Hex.decode(hex))).collect(Collectors.toList());
 
     private static final long ACTIVATION_DELAY_VALUE = 5063;
@@ -49,18 +51,19 @@ public class ErpFederationTest {
     @Test
     public void getRedeemScript() {
         Script redeemScript = federation.getRedeemScript();
-        Assert.assertEquals(17, redeemScript.getChunks().size());
+        Assert.assertEquals(19, redeemScript.getChunks().size());
 
         // First element: OP_0 - Belonging to the standard of BTC
         // M elements OP_0 - Belonging to M/N amount of signatures
         // OP_0 - Belonging to ERP
         // Last element: Program of redeem script
-        String expectedProgram = "64522102ed3bace23c5e17652e174c835fb72bf53ee306b3406a26890221b4"
-            + "cef7500f88210385a7b790fc9d962493788317e4874a4ab07f1e9c78c773c47f2f6c96df756f05210"
-            + "3cd5a3be41717d65683fe7a9de8ae5b4b8feced69f26a8b55eeefbcc2e74b75fb53670213c7b27552"
-            + "21029cecea902067992d52c38b28bf0bb2345bda9b21eca76b16a17c477a64e433012103284178e5f"
-            + "bcc63c54c3b38e3ef88adf2da6c526313650041b0ef955763634ebd2103b9fc46657cf72a1afa007e"
-            + "cf431de1cd27ff5cc8829fa625b66ca47b967e6b245368ae";
+        String expectedProgram = "64522102ed3bace23c5e17652e174c835fb72bf53ee306b3406a26890221b4cef"
+            + "7500f88210385a7b790fc9d962493788317e4874a4ab07f1e9c78c773c47f2f6c96df756f052103cd5a3"
+            + "be41717d65683fe7a9de8ae5b4b8feced69f26a8b55eeefbcc2e74b75fb53670213c7b2755221029cece"
+            + "a902067992d52c38b28bf0bb2345bda9b21eca76b16a17c477a64e433012103284178e5fbcc63c54c3b3"
+            + "8e3ef88adf2da6c526313650041b0ef955763634ebd2103776b1fd8f86da3c1db3d69699e8250a15877d"
+            + "286734ea9a6da8e9d8ad25d16c12103ab0e2cd7ed158687fc13b88019990860cdb72b1f5777b58513312"
+            + "550ea1584bc2103b9fc46657cf72a1afa007ecf431de1cd27ff5cc8829fa625b66ca47b967e6b245568ae";
 
         Assert.assertEquals(expectedProgram, Hex.toHexString(redeemScript.getProgram()));
     }
@@ -68,7 +71,7 @@ public class ErpFederationTest {
     @Test
     public void getP2SHScript() {
         Script p2shs = federation.getP2SHScript();
-        String expectedProgram = "a914faa3554b3ced8b80db8c09e26d4027db56e04ae987";
+        String expectedProgram = "a914a5bf4a2c554eef184fb19abfb673dcf330748b1187";
 
         Assert.assertEquals(expectedProgram, Hex.toHexString(p2shs.getProgram()));
         Assert.assertEquals(3, p2shs.getChunks().size());
@@ -81,7 +84,7 @@ public class ErpFederationTest {
     @Test
     public void getAddress() {
         String fedAddress = federation.getAddress().toBase58();
-        String expectedAddress = "2NG6Uaq5jFGs2xSn3mjDysR8BDTCfyTJgjb";
+        String expectedAddress = "2N8Mcdyo7oERBZf3AfUirvXEpthTevCaFao";
 
         Assert.assertEquals(expectedAddress, fedAddress);
     }
@@ -101,11 +104,15 @@ public class ErpFederationTest {
                 + "dfae714467c15649fbdb61c70e367fb43f326dc807691923cd16698af99e",
             "04284178e5fbcc63c54c3b38e3ef88adf2da6c526313650041b0ef955763634ebd4076b8bb"
                 + "c11b4a3f559c8041b03a903d7d7efacc4dd3796a27df324c7aa3bc5d",
+            "04776b1fd8f86da3c1db3d69699e8250a15877d286734ea9a6da8e9d8ad25d16c118424627ece3cba0" 
+                + "028fcbd4a0372485641a02383f4cdcee932542efd60d1029",
+            "04ab0e2cd7ed158687fc13b88019990860cdb72b1f5777b58513312550ea1584bc08b4554783b4960c6a"
+                + "bb761979d24d76a08ac38e775d72b960cd5644e1a54f01"
         }).map(hex -> BtcECKey.fromPublicOnly(Hex.decode(hex))).collect(Collectors.toList());
 
         // Recreate federation
         federation = new ErpFederation(
-            FederationTestUtils.getFederationMembersFromPks(100, 200, 300),
+            FederationTestUtils.getFederationMembersFromPks(100, 200, 300, 400, 500),
             ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
             0L,
             NetworkParameters.fromID(NetworkParameters.ID_REGTEST),
