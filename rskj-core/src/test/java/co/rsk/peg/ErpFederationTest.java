379c0840b53f3ea5d21cec824d58f35cdc6a6d3d
Guido Medina <guido@iovlabs.org>
2021-03-11 15:01:26 -0300

    Update ERP multisig to 3/5

--- rskj-core/src/test/java/co/rsk/peg/ErpFederationTest.java
+++ rskj-core/src/test/java/co/rsk/peg/ErpFederationTest.java
@@ -57,13 +57,14 @@ public class ErpFederationTest {
         // M elements OP_0 - Belonging to M/N amount of signatures
         // OP_0 - Belonging to ERP
         // Last element: Program of redeem script
-        String expectedProgram = "64522102ed3bace23c5e17652e174c835fb72bf53ee306b3406a26890221b4cef"
-            + "7500f88210385a7b790fc9d962493788317e4874a4ab07f1e9c78c773c47f2f6c96df756f052103cd5a3"
-            + "be41717d65683fe7a9de8ae5b4b8feced69f26a8b55eeefbcc2e74b75fb53670213c7b2755221029cece"
-            + "a902067992d52c38b28bf0bb2345bda9b21eca76b16a17c477a64e433012103284178e5fbcc63c54c3b3"
-            + "8e3ef88adf2da6c526313650041b0ef955763634ebd2103776b1fd8f86da3c1db3d69699e8250a15877d"
-            + "286734ea9a6da8e9d8ad25d16c12103ab0e2cd7ed158687fc13b88019990860cdb72b1f5777b58513312"
-            + "550ea1584bc2103b9fc46657cf72a1afa007ecf431de1cd27ff5cc8829fa625b66ca47b967e6b245568ae";
+        String expectedProgram = "64522102ed3bace23c5e17652e174c835fb72bf53ee306b3406a26890221b4ce"
+            + "f7500f88210385a7b790fc9d962493788317e4874a4ab07f1e9c78c773c47f2f6c96df756f052103cd5"
+            + "a3be41717d65683fe7a9de8ae5b4b8feced69f26a8b55eeefbcc2e74b75fb53670213c7b2755321029c"
+            + "ecea902067992d52c38b28bf0bb2345bda9b21eca76b16a17c477a64e433012103284178e5fbcc63c54"
+            + "c3b38e3ef88adf2da6c526313650041b0ef955763634ebd2103776b1fd8f86da3c1db3d69699e8250a1"
+            + "5877d286734ea9a6da8e9d8ad25d16c12103ab0e2cd7ed158687fc13b88019990860cdb72b1f5777b58"
+            + "513312550ea1584bc2103b9fc46657cf72a1afa007ecf431de1cd27ff5cc8829fa625b66ca47b967e6b"
+            + "245568ae";
 
         Assert.assertEquals(expectedProgram, Hex.toHexString(redeemScript.getProgram()));
     }
@@ -71,7 +72,7 @@ public class ErpFederationTest {
     @Test
     public void getP2SHScript() {
         Script p2shs = federation.getP2SHScript();
-        String expectedProgram = "a914a5bf4a2c554eef184fb19abfb673dcf330748b1187";
+        String expectedProgram = "a914bbb7b7942d0fb850bd619b399e96d8b8b36ff89187";
 
         Assert.assertEquals(expectedProgram, Hex.toHexString(p2shs.getProgram()));
         Assert.assertEquals(3, p2shs.getChunks().size());
@@ -84,7 +85,7 @@ public class ErpFederationTest {
     @Test
     public void getAddress() {
         String fedAddress = federation.getAddress().toBase58();
-        String expectedAddress = "2N8Mcdyo7oERBZf3AfUirvXEpthTevCaFao";
+        String expectedAddress = "2NAMnS3XpcWw1KrYkszRw7gWHFkxuMYrU2Z";
 
         Assert.assertEquals(expectedAddress, fedAddress);
     }
