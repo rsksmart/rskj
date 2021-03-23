1542b97b34db59c964cdfdd9150661580796feef
ajlopez <webmaster@ajlopez.com>
2021-03-23 18:16:34 -0300

    Bridge new method getBestBlockHeader simple test

--- rskj-core/src/test/java/co/rsk/peg/BridgeNewMethodsTest.java
+++ rskj-core/src/test/java/co/rsk/peg/BridgeNewMethodsTest.java
@@ -7,38 +7,64 @@ import org.ethereum.config.blockchain.upgrades.ActivationConfig;
 import org.ethereum.config.blockchain.upgrades.ActivationConfigsForTest;
 import org.ethereum.core.Block;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Random;
 
 import static org.mockito.Mockito.*;
 
 public class BridgeNewMethodsTest {
-    @Test
-    public void getBestBlockNumber() throws IOException, BlockStoreException {
-        TestSystemProperties config = spy(new TestSystemProperties());
-        Constants constants = Constants.regtest();
+    private static Random random = new Random();
+
+    private TestSystemProperties config;
+    private Constants constants;
+    private ActivationConfig activationConfig;
+    private BridgeSupport bridgeSupport;
+    private BridgeSupportFactory bridgeSupportFactory;
+    private Block rskExecutionBlock;
+    private Bridge bridge;
+
+    @Before
+    public void beforeEach() {
+        config = spy(new TestSystemProperties());
+        constants = Constants.regtest();
         when(config.getNetworkConstants()).thenReturn(constants);
-        ActivationConfig activationConfig = spy(ActivationConfigsForTest.genesis());
+        activationConfig = spy(ActivationConfigsForTest.genesis());
         when(config.getActivationConfig()).thenReturn(activationConfig);
-        BridgeSupport bridgeSupport = mock(BridgeSupport.class);
-        when(bridgeSupport.getBtcBlockchainBestChainHeight()).thenReturn(42);
-        BridgeSupportFactory bridgeSupportFactory = mock(BridgeSupportFactory.class);
+        bridgeSupport = mock(BridgeSupport.class);
+        bridgeSupportFactory = mock(BridgeSupportFactory.class);
         when(bridgeSupportFactory.newInstance(any(), any(), any(), any())).thenReturn(bridgeSupport);
-        Block rskExecutionBlock = mock(Block.class);
+        rskExecutionBlock = mock(Block.class);
         when(rskExecutionBlock.getNumber()).thenReturn(42L);
 
-        Bridge bridge = new Bridge(
-            null,
-            constants,
-            activationConfig,
-            bridgeSupportFactory
+        bridge = new Bridge(
+                null,
+                constants,
+                activationConfig,
+                bridgeSupportFactory
         );
 
         bridge.init(null, rskExecutionBlock, null, null, null, null);
+    }
+
+    @Test
+    public void getBestBlockNumber() throws IOException, BlockStoreException {
+        when(bridgeSupport.getBtcBlockchainBestChainHeight()).thenReturn(42);
 
         long result = bridge.getBestBlockNumber(new Object[0]);
 
         Assert.assertEquals(42, result);
+    }
+
+    @Test
+    public void getBestBlockHeader() throws IOException, BlockStoreException {
+        byte[] header = new byte[80];
+        random.nextBytes(header);
+        when(bridgeSupport.getBtcBlockchainSerializedBestBlockHeader()).thenReturn(header);
+        byte[] result = bridge.getBestBlockHeader(new Object[0]);
+
+        Assert.assertArrayEquals(header, result);
     }
 }
