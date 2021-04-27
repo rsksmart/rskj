49630ef1c174ceeb654c9ff539de6e08ef998282
elmol <eliasmolini@gmail.com>
2021-04-27 17:38:45 -0300

    eip-1898 implementation - eth_getTransactionCount - get block by { "blockNumber": "0x0" }

--- rskj-core/src/test/java/org/ethereum/rpc/Web3ImplTest.java
+++ rskj-core/src/test/java/org/ethereum/rpc/Web3ImplTest.java
@@ -793,6 +793,33 @@ public class Web3ImplTest {
     }
 
     @Test
+    //[ "0x<address>", { "blockNumber": "0x0" } -> return tx count at given address in genesis block
+    public void getTransactionCountByBlockNumber() {
+        World world = new World();
+
+
+        Account acc1 = new AccountBuilder(world).name("acc1").balance(Coin.valueOf(100000000)).build();
+        Account acc2 = new AccountBuilder().name("acc2").build();
+        Transaction tx = new TransactionBuilder().sender(acc1).receiver(acc2).value(BigInteger.valueOf(1000000)).build();
+        List<Transaction> txs = new ArrayList<>();
+        txs.add(tx);
+        Block genesis = world.getBlockChain().getBestBlock();
+        Block block1 = new BlockBuilder(world.getBlockChain(), world.getBridgeSupportFactory(),
+                world.getBlockStore()).trieStore(world.getTrieStore()).parent(genesis).transactions(txs).build();
+        assertEquals(ImportResult.IMPORTED_BEST, world.getBlockChain().tryToConnect(block1));
+
+
+        String accountAddress = ByteUtil.toHexString(acc1.getAddress().getBytes());
+        Map<String, String> blockRef = new HashMap<String, String>() {
+            {
+                put("blockNumber", "0x1");
+            }
+        };
+        Web3Impl web3 = createWeb3(world);
+        assertEquals("0x1", web3.eth_getTransactionCount(accountAddress, blockRef));
+    }
+
+    @Test
     public void getBlockByNumber() {
         World world = new World();
 
