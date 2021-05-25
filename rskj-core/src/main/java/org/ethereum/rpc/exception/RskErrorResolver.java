2b0b2280bed29ca0c4dac08fee2ca77da2ad8907
Nazaret Garcia <nazaret@iovlabs.org>
2021-05-25 17:12:53 -0300

    Adding error handler

--- rskj-core/src/main/java/org/ethereum/rpc/exception/RskErrorResolver.java
+++ rskj-core/src/main/java/org/ethereum/rpc/exception/RskErrorResolver.java
@@ -2,11 +2,13 @@ package org.ethereum.rpc.exception;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import com.googlecode.jsonrpc4j.ErrorResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -23,10 +25,21 @@ public class RskErrorResolver implements ErrorResolver {
             error =  new JsonError(((RskJsonRpcRequestException) t).getCode(), t.getMessage(), null);
         } else if (t instanceof InvalidFormatException) {
             error = new JsonError(-32603, "Internal server error, probably due to invalid parameter type", null);
+        } else if (t instanceof UnrecognizedPropertyException) {
+            String message = getExceptionMessage((UnrecognizedPropertyException) t);
+            logger.error("JsonRPC error {} for method {} with arguments {}", message, method, arguments);
+            error = new JsonError(-32603, message, null);
         } else {
             logger.error("JsonRPC error when for method {} with arguments {}", method, arguments, t);
             error = new JsonError(-32603, "Internal server error", null);
         }
         return error;
+    }
+
+    private String getExceptionMessage(UnrecognizedPropertyException ex) {
+        if (ex.getMessage() == null) return "Invalid parameters";
+        return Arrays.stream(ex.getMessage().split("\\n at "))
+                .findFirst()
+                .orElse("Invalid parameters");
     }
 }
