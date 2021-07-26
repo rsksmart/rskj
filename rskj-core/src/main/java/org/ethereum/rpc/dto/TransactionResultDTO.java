040ff81311f6ad04e1f974c4137be4ed1065e3d2
fedejinich <federico@iovlabs.org>
2021-07-26 13:52:48 -0300

    removed unnecesary setters

--- rskj-core/src/main/java/org/ethereum/rpc/dto/TransactionResultDTO.java
+++ rskj-core/src/main/java/org/ethereum/rpc/dto/TransactionResultDTO.java
@@ -82,112 +82,56 @@ public class TransactionResultDTO {
         return hash;
     }
 
-    public void setHash(String hash) {
-        this.hash = hash;
-    }
-
     public String getNonce() {
         return nonce;
-    }
-
-    public void setNonce(String nonce) {
-        this.nonce = nonce;
     }
 
     public String getBlockHash() {
         return blockHash;
     }
 
-    public void setBlockHash(String blockHash) {
-        this.blockHash = blockHash;
-    }
-
     public String getBlockNumber() {
         return blockNumber;
-    }
-
-    public void setBlockNumber(String blockNumber) {
-        this.blockNumber = blockNumber;
     }
 
     public String getTransactionIndex() {
         return transactionIndex;
     }
 
-    public void setTransactionIndex(String transactionIndex) {
-        this.transactionIndex = transactionIndex;
-    }
-
     public String getFrom() {
         return from;
-    }
-
-    public void setFrom(String from) {
-        this.from = from;
     }
 
     public String getTo() {
         return to;
     }
 
-    public void setTo(String to) {
-        this.to = to;
-    }
-
     public String getGas() {
         return gas;
-    }
-
-    public void setGas(String gas) {
-        this.gas = gas;
     }
 
     public String getGasPrice() {
         return gasPrice;
     }
 
-    public void setGasPrice(String gasPrice) {
-        this.gasPrice = gasPrice;
-    }
-
     public String getValue() {
         return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
     }
 
     public String getInput() {
         return input;
     }
 
-    public void setInput(String input) {
-        this.input = input;
-    }
-
     public String getV() {
         return v;
-    }
-
-    public void setV(String v) {
-        this.v = v;
     }
 
     public String getR() {
         return r;
     }
 
-    public void setR(String r) {
-        this.r = r;
-    }
-
     public String getS() {
         return s;
-    }
-
-    public void setS(String s) {
-        this.s = s;
     }
 
 }
