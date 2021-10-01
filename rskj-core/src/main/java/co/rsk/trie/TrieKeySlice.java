651f661f38572df40bc29d51c8593adf0958fe8a
SergioDemianLerner <SergioDemianLerner@users.noreply.github.com>
2021-10-01 00:20:14 -0300

    test

--- rskj-core/src/main/java/co/rsk/trie/TrieKeySlice.java
+++ rskj-core/src/main/java/co/rsk/trie/TrieKeySlice.java
@@ -17,6 +17,10 @@
  */
 package co.rsk.trie;
 
+import com.google.common.annotations.VisibleForTesting;
+import org.ethereum.db.ByteArrayWrapper;
+import org.ethereum.util.ByteUtil;
+
 import java.util.Arrays;
 
 /**
@@ -28,10 +32,16 @@ public class TrieKeySlice {
     private final int offset;
     private final int limit;
 
-    private TrieKeySlice(byte[] expandedKey, int offset, int limit) {
+    static private final int maxLength = Short.MAX_VALUE;
+
+    private  TrieKeySlice(byte[] expandedKey, int offset, int limit) {
         this.expandedKey = expandedKey;
         this.offset = offset;
         this.limit = limit;
+    }
+
+    public TrieKeySlice clone() {
+        return new TrieKeySlice(Arrays.copyOfRange(expandedKey, offset, limit),0,length());
     }
 
     public int length() {
@@ -48,6 +58,11 @@ public class TrieKeySlice {
     }
 
     public TrieKeySlice slice(int from, int to) {
+
+       // Return empty if bounds match. This is a huge performance gain
+        if (from==to) {
+            return empty();
+        }
         if (from < 0) {
             throw new IllegalArgumentException("The start position must not be lower than 0");
         }
@@ -56,12 +71,12 @@ public class TrieKeySlice {
             throw new IllegalArgumentException("The start position must not be greater than the end position");
         }
 
-        int newOffset = offset + from;
+        int newOffset = (offset + from);
         if (newOffset > limit) {
             throw new IllegalArgumentException("The start position must not exceed the key length");
         }
 
-        int newLimit = offset + to;
+        int newLimit = (offset + to);
         if (newLimit > limit) {
             throw new IllegalArgumentException("The end position must not exceed the key length");
         }
@@ -69,6 +84,7 @@ public class TrieKeySlice {
         return new TrieKeySlice(expandedKey, newOffset, newLimit);
     }
 
+    // This operation is frequent when traversing the trie. It must be very fast.
     public TrieKeySlice commonPath(TrieKeySlice other) {
         int maxCommonLengthPossible = Math.min(length(), other.length());
         for (int i = 0; i < maxCommonLengthPossible; i++) {
@@ -80,13 +96,56 @@ public class TrieKeySlice {
         return slice(0, maxCommonLengthPossible);
     }
 
+    public TrieKeySlice appendBit(byte implicitByte) {
+        int length = length();
+        int newLength = length + 1;
+        byte[] newExpandedKey = new byte[newLength];
+        System.arraycopy(expandedKey, offset, newExpandedKey, 0, length);
+        newExpandedKey[length] = implicitByte;
+        return new TrieKeySlice(newExpandedKey,0,newLength);
+    }
+
+    public TrieKeySlice append(TrieKeySlice childSharedPath) {
+        int childSharedPathLength = childSharedPath.length();
+        if (childSharedPathLength==0) return this;
+        int length = length();
+        int newLength = length + childSharedPathLength;
+        if (newLength>maxLength)
+            throw new IllegalArgumentException("Concatenation leads to a too large key");
+
+        byte[] newExpandedKey = new byte[newLength];
+        System.arraycopy(expandedKey, offset, newExpandedKey, 0, length);
+
+        for(int i=0;i<childSharedPathLength;i++)
+            newExpandedKey[length+i]=childSharedPath.get(i);
+        return new TrieKeySlice(newExpandedKey, 0, newLength);
+    }
+
+    public boolean equals(Object other) {
+        if (!(other instanceof TrieKeySlice)) {
+            return false;
+        }
+        TrieKeySlice otherKey = (TrieKeySlice) other;
+        if (this.length()!=otherKey .length()) {
+            return false;
+        }
+        for(int i=0;i<length();i++)
+            if (get(i)!=otherKey.get(i))
+                return false;
+        return true;
+    }
+
     /**
      * Rebuild a shared path as [...this, implicitByte, ...childSharedPath]
+     * This operation is common when modifying the trie. It must not be slow.
      */
     public TrieKeySlice rebuildSharedPath(byte implicitByte, TrieKeySlice childSharedPath) {
         int length = length();
         int childSharedPathLength = childSharedPath.length();
         int newLength = length + 1 + childSharedPathLength;
+        if (newLength>maxLength)
+            throw new IllegalArgumentException("Concatenation leads to a too large key");
+
         byte[] newExpandedKey = Arrays.copyOfRange(expandedKey, offset, offset + newLength);
         newExpandedKey[length] = implicitByte;
         System.arraycopy(
@@ -101,24 +160,74 @@ public class TrieKeySlice {
             return this;
         }
         int currentLength = length();
-        byte[] paddedExpandedKey = new byte[currentLength + paddingLength];
+        int newLength = currentLength + paddingLength;
+        if (newLength>maxLength)
+            throw new IllegalArgumentException("Concatenation leads to a too large key");
+
+        byte[] paddedExpandedKey = new byte[newLength];
         System.arraycopy(expandedKey, offset, paddedExpandedKey, paddingLength, currentLength);
         return new TrieKeySlice(paddedExpandedKey, 0, paddedExpandedKey.length);
     }
 
+    // Creates a TrieKeySlice from a key (a bit-compressed array, also called encoded key).
+    // The number of bits in this key is always multiple of 8.
     public static TrieKeySlice fromKey(byte[] key) {
         byte[] expandedKey = PathEncoder.decode(key, key.length * 8);
         return new TrieKeySlice(expandedKey, 0, expandedKey.length);
     }
 
+    // Creates a TrieKeySlice from a an encoded key (src).
+    // It receives a certain byte offset to start from the encoded key src,
+    // but the resulting TrieKeySlice can have any bit length, because keyLength bit granularity.
+    // encodedLength is a byte length of the encoded src.
+    // Precondition: keyLength <= (encodedLength-offset)*8
     public static TrieKeySlice fromEncoded(byte[] src, int offset, int keyLength, int encodedLength) {
+        if (keyLength <= (encodedLength-offset)*8)
+            throw new IllegalArgumentException("Illegal keyLength");
+
+        if (keyLength>TrieKeySlice.maxLength)
+            throw new IllegalArgumentException("Key too long");
+
         // TODO(mc) avoid copying by passing the indices to PathEncoder.decode
         byte[] encodedKey = Arrays.copyOfRange(src, offset, offset + encodedLength);
         byte[] expandedKey = PathEncoder.decode(encodedKey, keyLength);
         return new TrieKeySlice(expandedKey, 0, expandedKey.length);
     }
 
+    @VisibleForTesting
+    // This constructor is ONLY for testing the object
+    // size in memory. It should never be used in production code.
+    public static TrieKeySlice nullFilledObject() {
+        return new TrieKeySlice(null,0,0);
+    }
+
+        @VisibleForTesting
+    // This method is marked as public because any application that uses the rskj jar
+    // as a library to access the RSK state needs to instantiate TrieKeySlices.
+    public static TrieKeySlice fromExpanded(byte[] expandedKey, int offset, int limit) {
+        if (limit > expandedKey.length)
+            throw new IllegalArgumentException("Invalid limit");
+
+        if ((offset > expandedKey.length) || (offset>limit))
+            throw new IllegalArgumentException("Invalid offset");
+
+        int keyLength = limit-offset;
+        if (keyLength>TrieKeySlice.maxLength)
+            throw new IllegalArgumentException("Key too long");
+
+        return new TrieKeySlice(expandedKey, offset, limit);
+    }
+
+    static TrieKeySlice emptyTrie = new TrieKeySlice(new byte[0], 0, 0);
+
     public static TrieKeySlice empty() {
-        return new TrieKeySlice(new byte[0], 0, 0);
+        return emptyTrie;
+    }
+
+    // Start aways with a vector with enough capacity to append keys
+    public static TrieKeySlice emptyWithCapacity() {
+        // This is the maximum size of a trie key in the current RSK Unitrie
+        int maxSize = (1+30+1+42)*8;
+        return new TrieKeySlice(new byte[maxSize], 0, 0);
     }
 }
--- rskj-core/src/test/java/co/rsk/trie/TrieKeySliceTest.java
+++ rskj-core/src/test/java/co/rsk/trie/TrieKeySliceTest.java
@@ -18,11 +18,97 @@
 package co.rsk.trie;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
 
 public class TrieKeySliceTest {
+
+    @Test
+    //@Ignore
+    // This is a test to perform only once.
+    // It checks that, on a 64 bit-system, the size of a KeyTrieSlice object
+    // is 24 bytes.
+    public void sizeTest() {
+        int maxObjects = 1000_000;
+        TrieKeySlice[] myObjArray = new TrieKeySlice[maxObjects];
+        long beforeUsedMem=Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
+        for(int i=0;i<myObjArray.length;i++) {
+            myObjArray[i] = TrieKeySlice.nullFilledObject();
+        }
+        long afterUsedMem=Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
+        long objectsize = (afterUsedMem-beforeUsedMem)/ myObjArray.length;
+        System.out.println("Object size: "+(afterUsedMem-beforeUsedMem)*1.0/ myObjArray.length);
+        Assert.assertTrue((objectsize>=23) && (objectsize<27));
+        myObjArray[0] =null; // avoid garbage collection
+    }
+
+    @Test
+    public void appendShortOverflows() {
+        // While in no place in the RSKj code keys lengths and offset are longer
+        // than 592 bits, which fits nicely in a short field, we test here what happens
+        // if we try to create such a key.
+
+
+    }
+    @Test
+    public void equalsAndCloneTest() {
+        TrieKeySlice initialKey = TrieKeySlice.fromExpanded(
+                new byte[]{(byte) 1,1,1,1,0,0,0,0, 1,1,1,1}, // 12 bits,
+                0,12);
+        TrieKeySlice clonedKey = initialKey.clone();
+        Assert.assertTrue(initialKey.equals(clonedKey));
+        TrieKeySlice modifiedKey = initialKey.appendBit((byte)1);
+        Assert.assertFalse(modifiedKey.equals(clonedKey));
+        Assert.assertFalse(modifiedKey.equals(initialKey));
+
+        // Now test equals after a slice
+        TrieKeySlice slicedKey0 = initialKey.slice(1,5);
+        TrieKeySlice lastKey = TrieKeySlice.fromExpanded(
+                new byte[]{(byte)  1,1,1,0}, // 4 bits
+                0,4);
+        Assert.assertTrue(slicedKey0.equals(lastKey));
+
+
+
+
+    }
+    @Test
+    public void appendTest() {
+        // Choose bits not multiple of 8 to cover those cases.
+        TrieKeySlice initialKey = TrieKeySlice.fromExpanded(
+                new byte[]{(byte) 1,1,1,1,0,0,0,0, 1,1,1,1}, // 12 bits,
+                0,12);
+
+        TrieKeySlice appendToKey;
+        appendToKey = TrieKeySlice.fromExpanded(
+                new byte[]{(byte) 1,0,1,0,1,0,1,0, 1}, // 9 bits,
+                0,9);
+
+        TrieKeySlice appendedKey = initialKey.append(appendToKey);
+
+        TrieKeySlice result1 =TrieKeySlice.fromExpanded(
+                new byte[]{(byte) 1,1,1,1,0,0,0,0, 1,1,1,1, 1,0,1,0,1,0,1,0, 1},0,21);
+
+        Assert.assertTrue(appendedKey.equals(result1));
+
+        // Now test appendign a single bit
+        TrieKeySlice appendedKeyPlusBit0 = appendToKey.appendBit((byte)1);
+        TrieKeySlice result2 =TrieKeySlice.fromExpanded(
+                new byte[]{(byte) 1,0,1,0,1,0,1,0, 1,1},0,10);
+
+        Assert.assertTrue(appendedKeyPlusBit0.equals(result2));
+
+        // Now append the other bit
+        // Now test appendign a single bit
+        TrieKeySlice appendedKeyPlusBit1 = appendToKey.appendBit((byte)0);
+        TrieKeySlice result3 =TrieKeySlice.fromExpanded(
+                new byte[]{(byte) 1,0,1,0,1,0,1,0, 1,0},0,10);
+
+        Assert.assertTrue(appendedKeyPlusBit1.equals(result3));
+    }
+
     @Test
     public void bytesToKey() {
         Assert.assertArrayEquals(
