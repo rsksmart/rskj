9ddbea2dad36e6bf1c1020b9ff123c076ef45ac9
Marcos <marcos@iovlabs.org>
2021-10-29 21:14:21 -0300

    Move old version of method used to deserialize addresses to BridgeUtilsLegacy

---
+++ rskj-core/src/main/java/co/rsk/peg/BridgeUtilsLegacy.java
@@ -0,0 +1,36 @@
+package co.rsk.peg;
+
+import co.rsk.bitcoinj.core.Address;
+import co.rsk.bitcoinj.core.NetworkParameters;
+import org.ethereum.config.blockchain.upgrades.ActivationConfig;
+import org.ethereum.config.blockchain.upgrades.ConsensusRule;
+
+/**
+ * Methods included in this class are to be used only prior to the latest HF activation
+ */
+@Deprecated
+public class BridgeUtilsLegacy {
+
+    @Deprecated
+    protected static Address deserializeBtcAddressWithVersionLegacy(
+        NetworkParameters networkParameters,
+        ActivationConfig.ForBlock activations,
+        byte[] addressBytes) throws BridgeIllegalArgumentException {
+
+        if (activations.isActive(ConsensusRule.RSKIP284)) {
+            throw new DeprecatedMethodCallException(
+                "Calling BridgeUtils.deserializeBtcAddressWithVersionLegacy method after RSKIP284 activation"
+            );
+        }
+
+        if (addressBytes == null || addressBytes.length == 0) {
+            throw new BridgeIllegalArgumentException("Can't get an address version if the bytes are empty");
+        }
+
+        int version = addressBytes[0];
+        byte[] hashBytes = new byte[20];
+        System.arraycopy(addressBytes, 1, hashBytes, 0, 20);
+
+        return new Address(networkParameters, version, hashBytes);
+    }
+}
