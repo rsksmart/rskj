6ff97836e8c27fee57fbc2f77391884ab26ec534
Kelvin Isievwore <kelvinator4leo@gmail.com>
2021-11-02 14:40:37 +0100

    Created new Bridge method: getQueuedPegoutsCount

--- rskj-core/src/main/java/co/rsk/peg/BridgeSupport.java
+++ rskj-core/src/main/java/co/rsk/peg/BridgeSupport.java
@@ -2523,6 +2523,13 @@ public class BridgeSupport {
         return activations.isActive(RSKIP271) ? provider.getNextPegoutHeight().orElse(0L) : 0L;
     }
 
+    public int getQueuedPegoutsCount() throws IOException {
+        if (activations.isActive(RSKIP271)) {
+            return provider.getReleaseRequestQueueSize();
+        }
+        return 0;
+    }
+
     public BigInteger registerFastBridgeBtcTransaction(
         Transaction rskTx,
         byte[] btcTxSerialized,
