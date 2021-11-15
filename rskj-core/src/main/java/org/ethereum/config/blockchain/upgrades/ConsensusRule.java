7fdc750e77339f048b1a4691cf09de4a169fa177
fedejinich <federico@iovlabs.org>
2021-11-15 19:48:56 -0300

    add rskip240 (storage rent)

--- rskj-core/src/main/java/org/ethereum/config/blockchain/upgrades/ConsensusRule.java
+++ rskj-core/src/main/java/org/ethereum/config/blockchain/upgrades/ConsensusRule.java
@@ -69,7 +69,8 @@ public enum ConsensusRule {
     RSKIP201("rskip201"),
     RSKIP218("rskip218"), // New rewards fee adddress
     RSKIP219("rskip219"),
-    RSKIP220("rskip220");
+    RSKIP220("rskip220"),
+    RSKIP240("rskip240"); // Storage rent
 
     private String configKey;
 
