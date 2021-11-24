f7a3efe78c8a853ea1899fa4d4af95c49187cb36
Marcos <marcos@iovlabs.org>
2021-11-24 19:19:22 -0300

    Hardcode testnet erp fed redeem script before HF activation

--- rskj-core/src/main/java/co/rsk/peg/ErpFederation.java
+++ rskj-core/src/main/java/co/rsk/peg/ErpFederation.java
@@ -11,10 +11,14 @@ import java.time.Instant;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import org.bouncycastle.util.encoders.Hex;
+import org.ethereum.config.blockchain.upgrades.ActivationConfig;
+import org.ethereum.config.blockchain.upgrades.ConsensusRule;
 
 public class ErpFederation extends Federation {
     private final List<BtcECKey> erpPubKeys;
     private final long activationDelay;
+    private final ActivationConfig.ForBlock activations;
 
     private Script standardRedeemScript;
     private Script standardP2SHScript;
@@ -25,11 +29,13 @@ public class ErpFederation extends Federation {
         long creationBlockNumber,
         NetworkParameters btcParams,
         List<BtcECKey> erpPubKeys,
-        long activationDelay
+        long activationDelay,
+        ActivationConfig.ForBlock activations
     ) {
         super(members, creationTime, creationBlockNumber, btcParams);
         this.erpPubKeys = EcKeyUtils.getCompressedPubKeysList(erpPubKeys);
         this.activationDelay = activationDelay;
+        this.activations = activations;
 
         // Try getting the redeem script in order to validate it can be built
         // using the given public keys and csv value
@@ -46,6 +52,12 @@ public class ErpFederation extends Federation {
 
     @Override
     public final Script getRedeemScript() {
+        if (!activations.isActive(ConsensusRule.RSKIP284) &&
+            !btcParams.getId().equals(NetworkParameters.ID_MAINNET)) {
+            final byte[] ERP_TESTNET_REDEEM_SCRIPT_BYTES = Hex.decode("6453210208f40073a9e43b3e9103acec79767a6de9b0409749884e989960fee578012fce210225e892391625854128c5c4ea4340de0c2a70570f33db53426fc9c746597a03f42102afc230c2d355b1a577682b07bc2646041b5d0177af0f98395a46018da699b6da210344a3c38cd59afcba3edcebe143e025574594b001700dec41e59409bdbd0f2a0921039a060badbeb24bee49eb2063f616c0f0f0765d4ca646b20a88ce828f259fcdb9556702cd50b27552210216c23b2ea8e4f11c3f9e22711addb1d16a93964796913830856b568cc3ea21d321034db69f2112f4fb1bb6141bf6e2bd6631f0484d0bd95b16767902c9fe219d4a6f210275562901dd8faae20de0a4166362a4f82188db77dbed4ca887422ea1ec185f145368ae");
+            return new Script(ERP_TESTNET_REDEEM_SCRIPT_BYTES);
+        }
+
         if (redeemScript == null) {
             redeemScript = ErpFederationRedeemScriptParser.createErpRedeemScript(
                 ScriptBuilder.createRedeemScript(getNumberOfSignaturesRequired(), getBtcPublicKeys()),
