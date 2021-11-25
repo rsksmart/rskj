0754204ec1f1d0a916a82df4efd5c60be741d928
Marcos <marcos@iovlabs.org>
2021-11-25 18:32:05 -0300

    Add getRedeemScript tests for ErpFederation pre and post RSKIP284

--- rskj-core/src/test/java/co/rsk/peg/ErpFederationTest.java
+++ rskj-core/src/test/java/co/rsk/peg/ErpFederationTest.java
@@ -1,5 +1,8 @@
 package co.rsk.peg;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import co.rsk.bitcoinj.core.BtcECKey;
 import co.rsk.bitcoinj.core.NetworkParameters;
 import co.rsk.bitcoinj.core.VerificationException;
@@ -10,12 +13,15 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.bouncycastle.util.encoders.Hex;
+import org.ethereum.config.blockchain.upgrades.ActivationConfig;
+import org.ethereum.config.blockchain.upgrades.ConsensusRule;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ErpFederationTest {
     private ErpFederation federation;
+    private final byte[] ERP_TESTNET_REDEEM_SCRIPT_BYTES = Hex.decode("6453210208f40073a9e43b3e9103acec79767a6de9b0409749884e989960fee578012fce210225e892391625854128c5c4ea4340de0c2a70570f33db53426fc9c746597a03f42102afc230c2d355b1a577682b07bc2646041b5d0177af0f98395a46018da699b6da210344a3c38cd59afcba3edcebe143e025574594b001700dec41e59409bdbd0f2a0921039a060badbeb24bee49eb2063f616c0f0f0765d4ca646b20a88ce828f259fcdb9556702cd50b27552210216c23b2ea8e4f11c3f9e22711addb1d16a93964796913830856b568cc3ea21d321034db69f2112f4fb1bb6141bf6e2bd6631f0484d0bd95b16767902c9fe219d4a6f210275562901dd8faae20de0a4166362a4f82188db77dbed4ca887422ea1ec185f145368ae");
 
     // ERP federation keys
     private static final List<BtcECKey> ERP_FED_KEYS = Arrays.stream(new String[]{
@@ -30,13 +36,17 @@ public class ErpFederationTest {
 
     @Before
     public void createErpFederation() {
+        ActivationConfig.ForBlock activations = mock(ActivationConfig.ForBlock.class);
+        when(activations.isActive(ConsensusRule.RSKIP284)).thenReturn(true);
+
         federation = new ErpFederation(
             FederationTestUtils.getFederationMembersFromPks(100, 200, 300),
             ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
             0L,
             NetworkParameters.fromID(NetworkParameters.ID_REGTEST),
             ERP_FED_KEYS,
-            ACTIVATION_DELAY_VALUE
+            ACTIVATION_DELAY_VALUE,
+            activations
         );
     }
 
@@ -107,7 +117,7 @@ public class ErpFederationTest {
                 + "dfae714467c15649fbdb61c70e367fb43f326dc807691923cd16698af99e",
             "04284178e5fbcc63c54c3b38e3ef88adf2da6c526313650041b0ef955763634ebd4076b8bb"
                 + "c11b4a3f559c8041b03a903d7d7efacc4dd3796a27df324c7aa3bc5d",
-            "04776b1fd8f86da3c1db3d69699e8250a15877d286734ea9a6da8e9d8ad25d16c118424627ece3cba0" 
+            "04776b1fd8f86da3c1db3d69699e8250a15877d286734ea9a6da8e9d8ad25d16c118424627ece3cba0"
                 + "028fcbd4a0372485641a02383f4cdcee932542efd60d1029",
             "04ab0e2cd7ed158687fc13b88019990860cdb72b1f5777b58513312550ea1584bc08b4554783b4960c6a"
                 + "bb761979d24d76a08ac38e775d72b960cd5644e1a54f01"
@@ -120,7 +130,8 @@ public class ErpFederationTest {
             0L,
             NetworkParameters.fromID(NetworkParameters.ID_REGTEST),
             erpPubKeysList,
-            ACTIVATION_DELAY_VALUE
+            ACTIVATION_DELAY_VALUE,
+            mock(ActivationConfig.ForBlock.class)
         );
 
         Assert.assertEquals(ERP_FED_KEYS, federation.getErpPubKeys());
@@ -128,25 +139,109 @@ public class ErpFederationTest {
 
     @Test(expected = VerificationException.class)
     public void createInvalidErpFederation_negativeCsvValue() {
+        ActivationConfig.ForBlock activations = mock(ActivationConfig.ForBlock.class);
+        when(activations.isActive(ConsensusRule.RSKIP284)).thenReturn(true);
+
         new ErpFederation(
             FederationTestUtils.getFederationMembersFromPks(100, 200, 300),
             ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
             0L,
             NetworkParameters.fromID(NetworkParameters.ID_REGTEST),
             ERP_FED_KEYS,
-            -100L
+            -100L,
+            activations
         );
     }
 
     @Test(expected = VerificationException.class)
     public void createInvalidErpFederation_csvValueAboveMax() {
+        ActivationConfig.ForBlock activations = mock(ActivationConfig.ForBlock.class);
+        when(activations.isActive(ConsensusRule.RSKIP284)).thenReturn(true);
+
         new ErpFederation(
             FederationTestUtils.getFederationMembersFromPks(100, 200, 300),
             ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
             0L,
             NetworkParameters.fromID(NetworkParameters.ID_REGTEST),
             ERP_FED_KEYS,
-            ErpFederationRedeemScriptParser.MAX_CSV_VALUE + 1
+            ErpFederationRedeemScriptParser.MAX_CSV_VALUE + 1,
+            activations
         );
+    }
+
+    @Test
+    public void getRedeemScript_before_RSKIP_284_testnet() {
+        ActivationConfig.ForBlock activations = mock(ActivationConfig.ForBlock.class);
+        when(activations.isActive(ConsensusRule.RSKIP284)).thenReturn(false);
+
+        Federation erpFederation = new ErpFederation(
+            FederationTestUtils.getFederationMembersFromPks(100, 200, 300),
+            ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
+            0L,
+            NetworkParameters.fromID(NetworkParameters.ID_TESTNET),
+            ERP_FED_KEYS,
+            ACTIVATION_DELAY_VALUE,
+            activations
+        );
+
+        Script testnetRedeemScript = new Script(ERP_TESTNET_REDEEM_SCRIPT_BYTES);
+        Assert.assertEquals(testnetRedeemScript, erpFederation.getRedeemScript());
+    }
+
+    @Test
+    public void getRedeemScript_before_RSKIP_284_mainnet() {
+        ActivationConfig.ForBlock activations = mock(ActivationConfig.ForBlock.class);
+        when(activations.isActive(ConsensusRule.RSKIP284)).thenReturn(false);
+
+        Federation erpFederation = new ErpFederation(
+            FederationTestUtils.getFederationMembersFromPks(100, 200, 300),
+            ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
+            0L,
+            NetworkParameters.fromID(NetworkParameters.ID_MAINNET),
+            ERP_FED_KEYS,
+            ACTIVATION_DELAY_VALUE,
+            activations
+        );
+
+        Script testnetRedeemScript = new Script(ERP_TESTNET_REDEEM_SCRIPT_BYTES);
+        Assert.assertNotEquals(testnetRedeemScript, erpFederation.getRedeemScript());
+    }
+
+    @Test
+    public void getRedeemScript_after_RSKIP_284_testnet() {
+        ActivationConfig.ForBlock activations = mock(ActivationConfig.ForBlock.class);
+        when(activations.isActive(ConsensusRule.RSKIP284)).thenReturn(true);
+
+        Federation erpFederation = new ErpFederation(
+            FederationTestUtils.getFederationMembersFromPks(100, 200, 300),
+            ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
+            0L,
+            NetworkParameters.fromID(NetworkParameters.ID_TESTNET),
+            ERP_FED_KEYS,
+            ACTIVATION_DELAY_VALUE,
+            activations
+        );
+
+        Script testnetRedeemScript = new Script(ERP_TESTNET_REDEEM_SCRIPT_BYTES);
+        Assert.assertNotEquals(testnetRedeemScript, erpFederation.getRedeemScript());
+    }
+
+    @Test
+    public void getRedeemScript_after_RSKIP_284_mainnet() {
+        ActivationConfig.ForBlock activations = mock(ActivationConfig.ForBlock.class);
+        when(activations.isActive(ConsensusRule.RSKIP284)).thenReturn(true);
+
+        Federation erpFederation = new ErpFederation(
+            FederationTestUtils.getFederationMembersFromPks(100, 200, 300),
+            ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
+            0L,
+            NetworkParameters.fromID(NetworkParameters.ID_MAINNET),
+            ERP_FED_KEYS,
+            ACTIVATION_DELAY_VALUE,
+            activations
+        );
+
+        Script testnetRedeemScript = new Script(ERP_TESTNET_REDEEM_SCRIPT_BYTES);
+        Assert.assertNotEquals(testnetRedeemScript, erpFederation.getRedeemScript());
     }
 }
