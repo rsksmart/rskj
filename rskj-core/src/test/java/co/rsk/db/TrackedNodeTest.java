aa92be093be33ac59149309def78465393c8027c
fedejinich <federico@iovlabs.org>
2022-03-21 15:12:23 -0300

    introduce TrackedNode class, useful to track used trie-nodes from repositories

---
+++ rskj-core/src/test/java/co/rsk/db/TrackedNodeTest.java
@@ -0,0 +1,56 @@
+package co.rsk.db;
+
+import org.ethereum.crypto.Keccak256Helper;
+import org.ethereum.db.ByteArrayWrapper;
+import org.ethereum.db.OperationType;
+import org.ethereum.db.TrackedNode;
+import org.junit.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.ethereum.db.OperationType.*;
+import static org.junit.Assert.assertEquals;
+
+public class TrackedNodeTest {
+
+    public static final ByteArrayWrapper VALID_KEY = new ByteArrayWrapper(Keccak256Helper.keccak256("something".getBytes(StandardCharsets.UTF_8)));
+    private static final String TRANSACTION_HASH = "this can be any non-empty string";
+
+    @Test
+    public void createValidTrackedNodes() {
+        List<TrackedNode> validElements = Arrays.asList(
+            new TrackedNode(VALID_KEY, READ_OPERATION,  TRANSACTION_HASH, true, false),
+            new TrackedNode(VALID_KEY, READ_OPERATION,  TRANSACTION_HASH, false, false),
+            new TrackedNode(VALID_KEY, WRITE_OPERATION, TRANSACTION_HASH, true, false),
+            new TrackedNode(VALID_KEY, WRITE_OPERATION, TRANSACTION_HASH, true, true)
+        );
+
+        assertEquals(4, validElements.size());
+    }
+
+    @Test
+    public void invalidTrackedNode() {
+        try {
+            new TrackedNode(VALID_KEY, WRITE_OPERATION, TRANSACTION_HASH, false, true);
+        } catch (IllegalArgumentException e) {
+            assertEquals("a WRITE_OPERATION should always have a true result", e.getMessage());
+        }
+
+        try {
+            new TrackedNode(VALID_KEY, READ_OPERATION, TRANSACTION_HASH, false, true);
+        } catch (IllegalArgumentException e) {
+            assertEquals("a READ_OPERATION shouldn't have an isDelete", e.getMessage());
+        }
+
+        OperationType[] values = OperationType.values();
+        for(int i = 0; i < values.length; i++) {
+            try {
+                new TrackedNode(VALID_KEY, values[i], "", true, false);
+            } catch (IllegalArgumentException e) {
+                assertEquals("transaction hash can not be empty", e.getMessage());
+            }
+        }
+    }
+}
