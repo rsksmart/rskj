4c5905e04a19ded45aeef1bb19ab93526506e24e
fedejinich <federico@iovlabs.org>
2022-03-23 14:25:24 -0300

    World supports custom time between blocks & saves used transaction executors

--- rskj-core/src/main/java/co/rsk/core/bc/BlockExecutor.java
+++ rskj-core/src/main/java/co/rsk/core/bc/BlockExecutor.java
@@ -295,7 +295,7 @@ public class BlockExecutor {
                     vmTrace,
                     vmTraceOptions,
                     deletedAccounts);
-            boolean transactionExecuted = txExecutor.executeTransaction();
+            boolean transactionExecuted = executeTransaction(txExecutor);
 
             if (!acceptInvalidTransactions && !transactionExecuted) {
                 if (discardInvalidTxs) {
@@ -377,6 +377,13 @@ public class BlockExecutor {
         profiler.stop(metric);
         logger.trace("End executeInternal.");
         return result;
+    }
+
+    /**
+     * This method is then overridden for testing purposes at BlockExecutorDSL
+     * */
+    protected boolean executeTransaction(TransactionExecutor txExecutor) {
+        return txExecutor.executeTransaction();
     }
 
     /**
