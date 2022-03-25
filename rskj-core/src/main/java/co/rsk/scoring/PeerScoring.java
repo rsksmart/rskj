69c9de0b272313ba64c17b911f8aba8745aff40f
Iago Lluque <iago.lluque@iovlabs.org>
2022-03-25 13:30:05 +0100

    TIMEOUT_MESSAGE no longer counts positive for scoring

--- rskj-core/src/main/java/co/rsk/scoring/PeerScoring.java
+++ rskj-core/src/main/java/co/rsk/scoring/PeerScoring.java
@@ -55,8 +55,6 @@ public class PeerScoring {
                 case INVALID_TRANSACTION:
                 case INVALID_MESSAGE:
                 case INVALID_HEADER:
-                    // TODO(lsebrie): review how to handle timeouts properly
-                    //case TIMEOUT_MESSAGE:
                     if (score > 0) {
                         score = 0;
                     }
@@ -66,6 +64,7 @@ public class PeerScoring {
                 case FAILED_HANDSHAKE:
                 case SUCCESSFUL_HANDSHAKE:
                 case REPEATED_MESSAGE:
+                case TIMEOUT_MESSAGE:
                     break;
 
                 default:
