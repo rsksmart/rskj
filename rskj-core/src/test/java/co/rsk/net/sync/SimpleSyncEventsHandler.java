48577100858855cb06e9f2a42231ba7fc8d81f06
Iago Lluque <iago.lluque@iovlabs.org>
2022-03-31 18:53:07 +0200

    Add license to modified classes

--- rskj-core/src/test/java/co/rsk/net/sync/SimpleSyncEventsHandler.java
+++ rskj-core/src/test/java/co/rsk/net/sync/SimpleSyncEventsHandler.java
@@ -1,6 +1,23 @@
+/*
+ * This file is part of RskJ
+ * Copyright (C) 2022 RSK Labs Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package co.rsk.net.sync;
 
-import co.rsk.net.NodeID;
 import co.rsk.net.Peer;
 import co.rsk.scoring.EventType;
 import org.ethereum.core.Block;
@@ -8,7 +25,6 @@ import org.ethereum.core.BlockHeader;
 import org.ethereum.core.BlockIdentifier;
 
 import javax.annotation.Nullable;
-import java.net.InetAddress;
 import java.util.Deque;
 import java.util.List;
 import java.util.Map;
