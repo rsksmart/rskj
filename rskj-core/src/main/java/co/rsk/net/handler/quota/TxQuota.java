db78d45a017308d20a826140c6fe2f37e6e6062d
Iago Lluque <iago.lluque@iovlabs.org>
2022-04-06 13:10:33 +0200

    Increased MAX_QUOTAS_SIZE and removed address field from TxQuota to reduce its size and, as a consequence, map size

--- rskj-core/src/main/java/co/rsk/net/handler/quota/TxQuota.java
+++ rskj-core/src/main/java/co/rsk/net/handler/quota/TxQuota.java
@@ -18,6 +18,8 @@
 
 package co.rsk.net.handler.quota;
 
+import co.rsk.core.RskAddress;
+import co.rsk.crypto.Keccak256;
 import co.rsk.util.TimeProvider;
 import org.ethereum.core.Transaction;
 import org.slf4j.Logger;
@@ -30,13 +32,11 @@ public class TxQuota {
 
     private static final Logger logger = LoggerFactory.getLogger(TxQuota.class);
 
-    private final String addressHex;
     private final TimeProvider timeProvider;
     private long timestamp;
     private double availableVirtualGas;
 
     private TxQuota(String addressHex, String txHash, long availableVirtualGas, TimeProvider timeProvider) {
-        this.addressHex = addressHex;
         this.timeProvider = timeProvider;
         this.timestamp = this.timeProvider.currentTimeMillis();
         this.availableVirtualGas = availableVirtualGas;
@@ -46,30 +46,31 @@ public class TxQuota {
 
     /**
      * Creates a new instance of TxQuota
-     * @param addressHex Hex of the address sourcing the tx
-     * @param txHash Hash of the tx after which the quota is being created, for logging purposes
+     *
+     * @param address      Address sourcing the tx, for logging purposes
+     * @param txHash       Hash of the tx after which the quota is being created, for logging purposes
      * @param initialQuota The initialQuota to provide
      * @param timeProvider The time provider to calculate inactivity periods, etc.
      * @return a new instance of TxQuota
      */
-    public static TxQuota createNew(String addressHex, String txHash, long initialQuota, TimeProvider timeProvider) {
-        return new TxQuota(addressHex, txHash, initialQuota, timeProvider);
+    public static TxQuota createNew(RskAddress address, Keccak256 txHash, long initialQuota, TimeProvider timeProvider) {
+        return new TxQuota(address.toHexString(), txHash.toHexString(), initialQuota, timeProvider);
     }
 
     /**
      * Checks whether the accumulated virtual gas is greater than the received gas to consume. If enough, received gas is discounted from accumulated.
      *
      * @param virtualGasToConsume Gas to be consumed
-     * @param tx Tx being executed, used for logging purposes only
+     * @param tx                  Tx being executed, used for logging purposes only
      * @return True if there was enough accumulated gas
      */
     public synchronized boolean acceptVirtualGasConsumption(double virtualGasToConsume, Transaction tx) {
         if (this.availableVirtualGas < virtualGasToConsume) {
-            logger.warn("NOT enough virtualGas for address [{}] and tx [{}]: availableVirtualGas=[{}], virtualGasToConsume=[{}]", this.addressHex, tx, this.availableVirtualGas, virtualGasToConsume);
+            logger.warn("NOT enough virtualGas for tx [{}]: availableVirtualGas=[{}], virtualGasToConsume=[{}]", tx, this.availableVirtualGas, virtualGasToConsume);
             return false;
         }
 
-        logger.trace("Enough virtualGas for address [{}] and tx [{}]: availableVirtualGas=[{}], virtualGasToConsume=[{}]", this.addressHex, tx, this.availableVirtualGas, virtualGasToConsume);
+        logger.trace("Enough virtualGas for tx [{}]: availableVirtualGas=[{}], virtualGasToConsume=[{}]", tx, this.availableVirtualGas, virtualGasToConsume);
         this.availableVirtualGas -= virtualGasToConsume;
         return true;
     }
@@ -77,11 +78,12 @@ public class TxQuota {
     /**
      * Refreshes availableVirtualGas according to inactivity time
      *
+     * @param address         Address sourcing the tx, for logging purposes
      * @param maxGasPerSecond Gas to accumulate for each second of inactivity
      * @param maxQuota        Max virtual gas to provide even if accumulated gas is greater
      * @return The new accumulated virtual gas
      */
-    public synchronized double refresh(long maxGasPerSecond, long maxQuota) {
+    public synchronized double refresh(RskAddress address, long maxGasPerSecond, long maxQuota) {
         long currentTimestamp = this.timeProvider.currentTimeMillis();
 
         double timeDiffSeconds = (currentTimestamp - this.timestamp) / 1000d;
@@ -89,7 +91,7 @@ public class TxQuota {
         this.timestamp = currentTimestamp;
         this.availableVirtualGas = Math.min(maxQuota, this.availableVirtualGas + addToQuota);
 
-        logger.trace("Quota refreshed for account [{}], new value [{}] (addToQuota [{}])", this.addressHex, this.availableVirtualGas, addToQuota);
+        logger.trace("Quota refreshed for account [{}], new value [{}] (addToQuota [{}])", address.toHexString(), this.availableVirtualGas, addToQuota);
 
         return this.availableVirtualGas;
     }
@@ -97,8 +99,7 @@ public class TxQuota {
     @Override
     public String toString() {
         return "TxQuota{" +
-                "addressHex='" + addressHex + '\'' +
-                ", timestamp=" + timestamp +
+                "timestamp=" + timestamp +
                 ", availableVirtualGas=" + availableVirtualGas +
                 '}';
     }
