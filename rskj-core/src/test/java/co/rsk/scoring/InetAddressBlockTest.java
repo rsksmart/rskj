ba8512654a9aa1e6519643bb7c6fb48a47c065ef
Iago Lluque <iago.lluque@iovlabs.org>
2022-04-19 16:59:34 +0200

    For PeerScoring ban/unban, treat AddressBlock as a subnet

--- rskj-core/src/test/java/co/rsk/scoring/InetAddressBlockTest.java
+++ rskj-core/src/test/java/co/rsk/scoring/InetAddressBlockTest.java
@@ -10,6 +10,7 @@ import java.util.Random;
 /**
  * Created by ajlopez on 11/07/2017.
  */
+// TODO when InetAddressBlock==subnet gets clarified, update this test-base accordingly, probably some tests will no longer be needed
 public class InetAddressBlockTest {
     private static Random random = new Random();
 
@@ -122,30 +123,6 @@ public class InetAddressBlockTest {
     }
 
     @Test
-    public void usingIPV4With32BitsMask() throws UnknownHostException {
-        InetAddress address = generateIPAddressV4();
-        InetAddress address2 = InetAddress.getByAddress(address.getAddress());
-        InetAddress address3 = InetAddress.getByAddress(generateIPAddressV4().getAddress());
-
-        InetAddressBlock mask = new InetAddressBlock(address, 32);
-
-        Assert.assertTrue(mask.contains(address2));
-        Assert.assertFalse(mask.contains(address3));
-    }
-
-    @Test
-    public void usingIPV6With32BitsMask() throws UnknownHostException {
-        InetAddress address = generateIPAddressV6();
-        InetAddress address2 = InetAddress.getByAddress(address.getAddress());
-        InetAddress address3 = InetAddress.getByAddress(generateIPAddressV6().getAddress());
-
-        InetAddressBlock mask = new InetAddressBlock(address, 32);
-
-        Assert.assertTrue(mask.contains(address2));
-        Assert.assertFalse(mask.contains(address3));
-    }
-
-    @Test
     public void doesNotContainIPV4() throws UnknownHostException {
         InetAddress address = generateIPAddressV6();
         InetAddress address2 = generateIPAddressV4();
@@ -208,5 +185,169 @@ public class InetAddressBlockTest {
         bytes[nbyte]++;
 
         return InetAddress.getByAddress(bytes);
+    }
+
+    @Test
+    public void subnetWithCidr8IPV4() throws UnknownHostException {
+        InetAddress previousAddressOut = InetAddress.getByName("191.255.255.255");
+        InetAddress firstAddressIn = InetAddress.getByName("192.0.0.1");
+        InetAddress middleAddressIn = InetAddress.getByName("192.122.122.122");
+        InetAddress lastAddressIn = InetAddress.getByName("192.255.255.255");
+        InetAddress nextAddressOut = InetAddress.getByName("193.0.0.1");
+
+        InetAddressBlock mask = new InetAddressBlock(firstAddressIn, 8);
+
+        Assert.assertFalse(mask.subnetContains(previousAddressOut));
+        Assert.assertTrue(mask.subnetContains(firstAddressIn));
+        Assert.assertTrue(mask.subnetContains(middleAddressIn));
+        Assert.assertTrue(mask.subnetContains(lastAddressIn));
+        Assert.assertFalse(mask.subnetContains(nextAddressOut));
+    }
+
+    @Test
+    public void subnetWithCidr16IPV4() throws UnknownHostException {
+        InetAddress previousAddressOut = InetAddress.getByName("192.167.255.255");
+        InetAddress firstAddressIn = InetAddress.getByName("192.168.0.1");
+        InetAddress middleAddressIn = InetAddress.getByName("192.168.122.122");
+        InetAddress lastAddressIn = InetAddress.getByName("192.168.255.255");
+        InetAddress nextAddressOut = InetAddress.getByName("192.169.0.1");
+
+        InetAddressBlock mask = new InetAddressBlock(firstAddressIn, 16);
+
+        Assert.assertFalse(mask.subnetContains(previousAddressOut));
+        Assert.assertTrue(mask.subnetContains(firstAddressIn));
+        Assert.assertTrue(mask.subnetContains(middleAddressIn));
+        Assert.assertTrue(mask.subnetContains(lastAddressIn));
+        Assert.assertFalse(mask.subnetContains(nextAddressOut));
+    }
+
+    @Test
+    public void subnetWithCidr24IPV4() throws UnknownHostException {
+        InetAddress previousAddressOut = InetAddress.getByName("192.167.255.255");
+        InetAddress firstAddressIn = InetAddress.getByName("192.168.0.1");
+        InetAddress middleAddressIn = InetAddress.getByName("192.168.0.100");
+        InetAddress lastAddressIn = InetAddress.getByName("192.168.0.255");
+        InetAddress nextAddressOut = InetAddress.getByName("192.168.1.1");
+
+        InetAddressBlock mask = new InetAddressBlock(firstAddressIn, 24);
+
+        Assert.assertFalse(mask.subnetContains(previousAddressOut));
+        Assert.assertTrue(mask.subnetContains(firstAddressIn));
+        Assert.assertTrue(mask.subnetContains(middleAddressIn));
+        Assert.assertTrue(mask.subnetContains(lastAddressIn));
+        Assert.assertFalse(mask.subnetContains(nextAddressOut));
+    }
+
+    @Test
+    public void subnetWithCidr32IPV4() throws UnknownHostException {
+        InetAddress uniqueAddress = InetAddress.getByName("192.168.0.47");
+        InetAddress nextAddressOut = InetAddress.getByName("192.168.0.48");
+        InetAddress previousAddressOut = InetAddress.getByName("192.168.1.47");
+
+        InetAddressBlock mask = new InetAddressBlock(uniqueAddress, 32);
+
+        Assert.assertFalse(mask.subnetContains(previousAddressOut));
+        Assert.assertTrue(mask.subnetContains(uniqueAddress));
+        Assert.assertFalse(mask.subnetContains(nextAddressOut));
+    }
+
+    @Test
+    public void subnetWithCidr8IPV6() throws UnknownHostException {
+        InetAddress previousAddressOut = InetAddress.getByName("1fff:ffff:ffff:ffff:ffff:ffff:ffff:ffff");
+        InetAddress firstAddressIn = InetAddress.getByName("2000:0000:0000:0000:0000:0000:0000:0000");
+        InetAddress middleAddressIn = InetAddress.getByName("2001:0db8:85a3:0000:0000:8a2e:0370:7335");
+        InetAddress lastAddressIn = InetAddress.getByName("20ff:ffff:ffff:ffff:ffff:ffff:ffff:ffff");
+        InetAddress nextAddressOut = InetAddress.getByName("2100:0000:0000:0000:0000:0000:0000:0000");
+
+        InetAddressBlock mask = new InetAddressBlock(firstAddressIn, 8);
+
+        Assert.assertFalse(mask.subnetContains(previousAddressOut));
+        Assert.assertTrue(mask.subnetContains(firstAddressIn));
+        Assert.assertTrue(mask.subnetContains(middleAddressIn));
+        Assert.assertTrue(mask.subnetContains(lastAddressIn));
+        Assert.assertFalse(mask.subnetContains(nextAddressOut));
+    }
+
+    @Test
+    public void subnetWithCidr16IPV6() throws UnknownHostException {
+        InetAddress previousAddressOut = InetAddress.getByName("2000:ffff:ffff:ffff:ffff:ffff:ffff:ffff");
+        InetAddress firstAddressIn = InetAddress.getByName("2001:0000:0000:0000:0000:0000:0000:0000");
+        InetAddress middleAddressIn = InetAddress.getByName("2001:0db8:85a3:0000:0000:8a2e:0370:7335");
+        InetAddress lastAddressIn = InetAddress.getByName("2001:ffff:ffff:ffff:ffff:ffff:ffff:ffff");
+        InetAddress nextAddressOut = InetAddress.getByName("2002:0000:0000:0000:0000:0000:0000:0000");
+
+        InetAddressBlock mask = new InetAddressBlock(firstAddressIn, 16);
+
+        Assert.assertFalse(mask.subnetContains(previousAddressOut));
+        Assert.assertTrue(mask.subnetContains(firstAddressIn));
+        Assert.assertTrue(mask.subnetContains(middleAddressIn));
+        Assert.assertTrue(mask.subnetContains(lastAddressIn));
+        Assert.assertFalse(mask.subnetContains(nextAddressOut));
+    }
+
+    @Test
+    public void subnetWithCidr24IPV6() throws UnknownHostException {
+        InetAddress previousAddressOut = InetAddress.getByName("2001:0cff:ffff:ffff:ffff:ffff:ffff:ffff");
+        InetAddress firstAddressIn = InetAddress.getByName("2001:0d00:0000:0000:0000:0000:0000:0000");
+        InetAddress middleAddressIn = InetAddress.getByName("2001:0db8:85a3:0000:0000:8a2e:0370:7335");
+        InetAddress lastAddressIn = InetAddress.getByName("2001:0dff:ffff:ffff:ffff:ffff:ffff:ffff");
+
+        InetAddress nextAddressOut = InetAddress.getByName("2001:0e00:0000:0000:0000:0000:0000:0000");
+
+        InetAddressBlock mask = new InetAddressBlock(firstAddressIn, 24);
+
+        Assert.assertFalse(mask.subnetContains(previousAddressOut));
+        Assert.assertTrue(mask.subnetContains(firstAddressIn));
+        Assert.assertTrue(mask.subnetContains(middleAddressIn));
+        Assert.assertTrue(mask.subnetContains(lastAddressIn));
+        Assert.assertFalse(mask.subnetContains(nextAddressOut));
+    }
+
+    @Test
+    public void subnetWithCidr32IPV6() throws UnknownHostException {
+        InetAddress previousAddressOut = InetAddress.getByName("2001:0db7:ffff:ffff:ffff:ffff:ffff:ffff");
+        InetAddress firstAddressIn = InetAddress.getByName("2001:0db8:0000:0000:0000:0000:0000:0000");
+        InetAddress middleAddressIn = InetAddress.getByName("2001:0db8:85a3:0000:0000:8a2e:0370:7335");
+        InetAddress lastAddressIn = InetAddress.getByName("2001:0db8:ffff:ffff:ffff:ffff:ffff:ffff");
+        InetAddress nextAddressOut = InetAddress.getByName("2001:0db9:0000:0000:0000:0000:0000:0000");
+
+        InetAddressBlock mask = new InetAddressBlock(firstAddressIn, 32);
+
+        Assert.assertFalse(mask.subnetContains(previousAddressOut));
+        Assert.assertTrue(mask.subnetContains(firstAddressIn));
+        Assert.assertTrue(mask.subnetContains(middleAddressIn));
+        Assert.assertTrue(mask.subnetContains(lastAddressIn));
+        Assert.assertFalse(mask.subnetContains(nextAddressOut));
+    }
+
+    @Test
+    public void subnetWithCidr64IPV6() throws UnknownHostException {
+        InetAddress previousAddressOut = InetAddress.getByName("2001:0db8:85a2:ffff:ffff:ffff:ffff:ffff");
+        InetAddress firstAddressIn = InetAddress.getByName("2001:0db8:85a3:0000:0000:0000:0000:0000");
+        InetAddress middleAddressIn = InetAddress.getByName("2001:0db8:85a3:0000:0000:8a2e:0370:7335");
+        InetAddress lastAddressIn = InetAddress.getByName("2001:0db8:85a3:0000:ffff:ffff:ffff:ffff");
+        InetAddress nextAddressOut = InetAddress.getByName("2001:0db8:85a3:0001:0000:0000:0000:0000");
+
+        InetAddressBlock mask = new InetAddressBlock(firstAddressIn, 64);
+
+        Assert.assertFalse(mask.subnetContains(previousAddressOut));
+        Assert.assertTrue(mask.subnetContains(firstAddressIn));
+        Assert.assertTrue(mask.subnetContains(middleAddressIn));
+        Assert.assertTrue(mask.subnetContains(lastAddressIn));
+        Assert.assertFalse(mask.subnetContains(nextAddressOut));
+    }
+
+    @Test
+    public void subnetWithCidr128IPV6() throws UnknownHostException {
+        InetAddress uniqueAddress = InetAddress.getByName("2001:0db8:85a3:0000:0000:8a2e:0370:7334");
+
+        InetAddress previousAddressOut = InetAddress.getByName("2001:0db8:85a3:0000:0000:8a2e:0370:7333");
+        InetAddress nextAddressOut = InetAddress.getByName("2001:0db8:85a3:0000:0000:8a2e:0370:7335");
+
+        InetAddressBlock mask = new InetAddressBlock(uniqueAddress, 128);
+
+        Assert.assertFalse(mask.subnetContains(previousAddressOut));
+        Assert.assertTrue(mask.subnetContains(uniqueAddress));
+        Assert.assertFalse(mask.subnetContains(nextAddressOut));
     }
 }
