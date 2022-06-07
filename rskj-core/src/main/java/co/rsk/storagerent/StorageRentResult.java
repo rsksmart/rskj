7d910092f2e7d064263946bc38b9b3f8914c85ce
fedejinich <federico@iovlabs.org>
2022-06-07 18:28:48 -0300

    StorageRentManager as stateless class

---
+++ rskj-core/src/main/java/co/rsk/storagerent/StorageRentResult.java
@@ -0,0 +1,45 @@
+package co.rsk.storagerent;
+
+import java.util.List;
+import java.util.Set;
+
+public class StorageRentResult {
+
+    private final Set<RentedNode> rentedNodes;
+    private final List<RentedNode> rollbackNodes;
+    private final long rollbacksRent;
+    private final long payableRent;
+    private final long gasAfterPayingRent;
+
+    public StorageRentResult(Set<RentedNode> rentedNodes, List<RentedNode> rollbackNodes, long payableRent, long rollbacksRent, long gasAfterPayingRent) {
+        this.rentedNodes = rentedNodes;
+        this.rollbackNodes = rollbackNodes;
+        this.payableRent = payableRent;
+        this.rollbacksRent = rollbacksRent;
+        this.gasAfterPayingRent = gasAfterPayingRent;
+    }
+
+    public long paidRent() {
+        return this.payableRent + this.rollbacksRent;
+    }
+
+    public Set<RentedNode> getRentedNodes() {
+        return rentedNodes;
+    }
+
+    public List<RentedNode> getRollbackNodes() {
+        return rollbackNodes;
+    }
+
+    public long getRollbacksRent() {
+        return rollbacksRent;
+    }
+
+    public long getPayableRent() {
+        return payableRent;
+    }
+
+    public long getGasAfterPayingRent() {
+        return gasAfterPayingRent;
+    }
+}
