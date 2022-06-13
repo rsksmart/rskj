04e7328f119dee2dde903aa37faecb38664ccae5
Ilan <36084092+ilanolkies@users.noreply.github.com>
2022-06-13 15:01:05 -0300

    Update TraceModuleImpl.java

--- rskj-core/src/main/java/co/rsk/rpc/modules/trace/TraceModuleImpl.java
+++ rskj-core/src/main/java/co/rsk/rpc/modules/trace/TraceModuleImpl.java
@@ -15,9 +15,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package co.rsk.rpc.modules.trace;
-
 
 import java.math.BigInteger;
 import java.util.ArrayList;
