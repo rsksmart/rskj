49aa3496358fa26438a87cc7c496db4e9fcd53fd
julianlen <julianlen@gmail.com>
2022-06-15 13:28:47 -0300

    Research/parallel-tx/header generation from master (#1792)
    * Added the execution plan for the transaction to the block header as a transactionEdgeList.
    
    * Added ParallelizeTransactionHandler that builds the different buckets that are going to be executed by different threads. If the sequential bucket doesn't have enough gas for the new transaction, the transaction isn't added to the block. In addition, once all the transactions are processed, the order of the transactions in the buckets is added to the receipt. The REMASC transaction is always added to the SequentialBucket. When a transaction is properly added to a bucket, it returns the gas used in that bucket.
    
    * Added a tracker to the MutableRepository (MR). If the MR is used for any reason but builds the block, the tracker used is a DummyTracker one. 
    
    * Review processed
    
    * Handler's been refactorized (#1760)
    
    * Added tests.
    
    * Research/parallel-tx/splitting executions (#1799)
    
    * changed method names, deleted executeInternal from blockExecutor, and changed execute for executeParallel in some places. fixed BlockExecutorTest since from now on when the block is executed with executeAndFill and its blockNumber is over the rskip144 then the transactionEdgeList is built by the miner
    
    * Three changes so test pass (i) Changed the block hash in Web3ImplLogsTest due to the new field in the header for txEdges, (ii) Many tests used a really high gas, then we decided not to split the gas limit by two for parallel and sequential bucket, and (iii) addRemascFee wasn't present in the oldSequentialExecute and now is added.

---
+++ rskj-core/src/main/java/co/rsk/core/bc/ReadWrittenKeysTracker.java
@@ -0,0 +1,64 @@
+/*
+ * This file is part of RskJ
+ * Copyright (C) 2017 RSK Labs Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package co.rsk.core.bc;
+
+import org.ethereum.db.ByteArrayWrapper;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//TODO(JULI):
+// * Next step should be to check whether a key is written in the cache but also deleted in the same transaction. This key shouldn't be considered as a written key.
+
+public class ReadWrittenKeysTracker implements IReadWrittenKeysTracker {
+    private Set<ByteArrayWrapper> temporalReadKeys;
+    private Set<ByteArrayWrapper> temporalWrittenKeys;
+
+    public ReadWrittenKeysTracker() {
+        this.temporalReadKeys = new HashSet<>();
+        this.temporalWrittenKeys = new HashSet<>();
+    }
+
+    @Override
+    public Set<ByteArrayWrapper> getTemporalReadKeys(){
+        return this.temporalReadKeys;
+    }
+
+    @Override
+    public Set<ByteArrayWrapper> getTemporalWrittenKeys(){
+        return this.temporalWrittenKeys;
+    }
+
+    @Override
+    public void addNewReadKey(ByteArrayWrapper key) {
+        temporalReadKeys.add(key);
+    }
+
+    @Override
+    public void addNewWrittenKey(ByteArrayWrapper key) {
+        temporalWrittenKeys.add(key);
+    }
+
+    @Override
+    public void clear() {
+        this.temporalReadKeys = new HashSet<>();
+        this.temporalWrittenKeys = new HashSet<>();
+
+    }
+}
