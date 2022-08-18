e85da9c381848ba993b7279758de802c5e71de44
fedejinich <federico@iovlabs.org>
2022-08-18 18:04:58 +0300

    simplify thresholds

--- rskj-core/src/main/java/org/ethereum/db/OperationType.java
+++ rskj-core/src/main/java/org/ethereum/db/OperationType.java
@@ -4,5 +4,5 @@ public enum OperationType {
     WRITE_OPERATION,
     READ_OPERATION,
     DELETE_OPERATION,
-    READ_CONTRACT_CODE_OPERATION
+    READ_CONTRACT_CODE_OPERATION // todo(fedejinich) does this still make sense? (there are no special treatments for this operation type)
 }
