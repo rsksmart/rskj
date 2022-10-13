5b4fe7b3ece42785908b8341b258f99909d9117b
Marcos <marcos@iovlabs.org>
2022-10-13 19:03:37 -0300

    Refactor ErpFederation tests to use mainnet erp constant values

--- rskj-core/src/test/java/co/rsk/peg/ErpFederationTest.java
+++ rskj-core/src/test/java/co/rsk/peg/ErpFederationTest.java
@@ -46,28 +46,24 @@ import org.junit.Test;
 
 public class ErpFederationTest {
     private ErpFederation federation;
+    private List<BtcECKey> defaultKeys;
+    private List<BtcECKey> emergencyKeys;
+    private long activationDelayValue;
     private ActivationConfig.ForBlock activations;
-
-    // Default federation multisig keys
-    private static final List<BtcECKey> DEFAULT_FED_KEYS = Arrays.stream(new String[]{
-        "03b9fc46657cf72a1afa007ecf431de1cd27ff5cc8829fa625b66ca47b967e6b24",
-        "029cecea902067992d52c38b28bf0bb2345bda9b21eca76b16a17c477a64e43301",
-        "03284178e5fbcc63c54c3b38e3ef88adf2da6c526313650041b0ef955763634ebd",
-        "03776b1fd8f86da3c1db3d69699e8250a15877d286734ea9a6da8e9d8ad25d16c1",
-        "03ab0e2cd7ed158687fc13b88019990860cdb72b1f5777b58513312550ea1584bc"
-    }).map(hex -> BtcECKey.fromPublicOnly(Hex.decode(hex))).sorted(BtcECKey.PUBKEY_COMPARATOR).collect(Collectors.toList());
-
-    // Emergency multisig keys
-    private static final List<BtcECKey> ERP_KEYS = Arrays.stream(new String[]{
-        "02ed3bace23c5e17652e174c835fb72bf53ee306b3406a26890221b4cef7500f88",
-        "0385a7b790fc9d962493788317e4874a4ab07f1e9c78c773c47f2f6c96df756f05",
-        "03cd5a3be41717d65683fe7a9de8ae5b4b8feced69f26a8b55eeefbcc2e74b75fb"
-    }).map(hex -> BtcECKey.fromPublicOnly(Hex.decode(hex))).sorted(BtcECKey.PUBKEY_COMPARATOR).collect(Collectors.toList());
-
-    private static final long ACTIVATION_DELAY_VALUE = 5063;
 
     @Before
     public void setup() {
+        defaultKeys = Arrays.stream(new String[]{
+            "03b9fc46657cf72a1afa007ecf431de1cd27ff5cc8829fa625b66ca47b967e6b24",
+            "029cecea902067992d52c38b28bf0bb2345bda9b21eca76b16a17c477a64e43301",
+            "03284178e5fbcc63c54c3b38e3ef88adf2da6c526313650041b0ef955763634ebd",
+            "03776b1fd8f86da3c1db3d69699e8250a15877d286734ea9a6da8e9d8ad25d16c1",
+            "03ab0e2cd7ed158687fc13b88019990860cdb72b1f5777b58513312550ea1584bc"
+        }).map(hex -> BtcECKey.fromPublicOnly(Hex.decode(hex))).sorted(BtcECKey.PUBKEY_COMPARATOR).collect(Collectors.toList());
+        emergencyKeys = BridgeMainNetConstants.getInstance().getErpFedPubKeysList();
+        emergencyKeys.sort(BtcECKey.PUBKEY_COMPARATOR);
+        activationDelayValue = BridgeMainNetConstants.getInstance().getErpFedActivationDelay();
+
         activations = mock(ActivationConfig.ForBlock.class);
         when(activations.isActive(ConsensusRule.RSKIP284)).thenReturn(true);
 
@@ -76,12 +72,12 @@ public class ErpFederationTest {
 
     @Test
     public void getErpPubKeys() {
-        Assert.assertEquals(ERP_KEYS, federation.getErpPubKeys());
+        Assert.assertEquals(emergencyKeys, federation.getErpPubKeys());
     }
 
     @Test
     public void getActivationDelay() {
-        Assert.assertEquals(ACTIVATION_DELAY_VALUE, federation.getActivationDelay());
+        Assert.assertEquals(activationDelayValue, federation.getActivationDelay());
     }
 
     @Test
@@ -89,7 +85,7 @@ public class ErpFederationTest {
         Script redeemScript = federation.getRedeemScript();
         validateErpRedeemScript(
             redeemScript,
-            ACTIVATION_DELAY_VALUE,
+            activationDelayValue,
             false
         );
     }
@@ -102,7 +98,7 @@ public class ErpFederationTest {
 
         validateErpRedeemScript(
             redeemScript,
-            ACTIVATION_DELAY_VALUE,
+            activationDelayValue,
             true
         );
     }
@@ -121,7 +117,7 @@ public class ErpFederationTest {
     @Test
     public void getP2SHScript() {
         Script p2shs = federation.getP2SHScript();
-        String expectedProgram = "a914867df89b0837b2fd86eb98751d14b6cef52d342487";
+        String expectedProgram = "a9142f7126864a9794495ca5fd3e210708ecdb6944f087";
 
         Assert.assertEquals(expectedProgram, Hex.toHexString(p2shs.getProgram()));
         Assert.assertEquals(3, p2shs.getChunks().size());
@@ -134,36 +130,36 @@ public class ErpFederationTest {
     @Test
     public void getAddress() {
         String fedAddress = federation.getAddress().toBase58();
-        String expectedAddress = "2N5WMScfkbBVWMByrGw6GZDFF3m4tb3qqP8";
+        String expectedAddress = "2Mwa5J8hkVjCtDZ13pDWDuGGACfsLK6XrKr";
 
         Assert.assertEquals(expectedAddress, fedAddress);
     }
 
     @Test
     public void getErpPubKeys_compressed_public_keys() {
-        Assert.assertEquals(ERP_KEYS, federation.getErpPubKeys());
+        Assert.assertEquals(emergencyKeys, federation.getErpPubKeys());
     }
 
     @Test
     public void getErpPubKeys_uncompressed_public_keys() {
         // Public keys used for creating federation, but uncompressed format now
-        List<BtcECKey> uncompressedErpKeys = ERP_KEYS
+        List<BtcECKey> uncompressedErpKeys = emergencyKeys
             .stream()
             .map(BtcECKey::decompress)
             .collect(Collectors.toList());
 
         // Recreate federation
         ErpFederation federationWithUncompressedKeys = new ErpFederation(
-            FederationTestUtils.getFederationMembersWithBtcKeys(DEFAULT_FED_KEYS),
+            FederationTestUtils.getFederationMembersWithBtcKeys(defaultKeys),
             ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
             0L,
             NetworkParameters.fromID(NetworkParameters.ID_REGTEST),
             uncompressedErpKeys,
-            ACTIVATION_DELAY_VALUE,
+            activationDelayValue,
             mock(ActivationConfig.ForBlock.class)
         );
 
-        Assert.assertEquals(ERP_KEYS, federationWithUncompressedKeys.getErpPubKeys());
+        Assert.assertEquals(emergencyKeys, federationWithUncompressedKeys.getErpPubKeys());
     }
 
     @Test
@@ -223,7 +219,7 @@ public class ErpFederationTest {
             ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
             0L,
             NetworkParameters.fromID(NetworkParameters.ID_REGTEST),
-            ERP_KEYS,
+            emergencyKeys,
             -100L,
             activations
         );
@@ -236,7 +232,7 @@ public class ErpFederationTest {
             ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
             0L,
             NetworkParameters.fromID(NetworkParameters.ID_REGTEST),
-            ERP_KEYS,
+            emergencyKeys,
             -100,
             activations
         );
@@ -249,7 +245,7 @@ public class ErpFederationTest {
             ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
             0L,
             NetworkParameters.fromID(NetworkParameters.ID_REGTEST),
-            ERP_KEYS,
+            emergencyKeys,
             0,
             activations
         );
@@ -262,7 +258,7 @@ public class ErpFederationTest {
             ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
             0L,
             NetworkParameters.fromID(NetworkParameters.ID_REGTEST),
-            ERP_KEYS,
+            emergencyKeys,
             ErpFederationRedeemScriptParser.MAX_CSV_VALUE + 1,
             activations
         );
@@ -278,8 +274,8 @@ public class ErpFederationTest {
             ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
             0L,
             NetworkParameters.fromID(NetworkParameters.ID_TESTNET),
-            ERP_KEYS,
-            ACTIVATION_DELAY_VALUE,
+            emergencyKeys,
+            activationDelayValue,
             activations
         );
 
@@ -292,19 +288,19 @@ public class ErpFederationTest {
         when(activations.isActive(ConsensusRule.RSKIP284)).thenReturn(false);
 
         Federation erpFederation = new ErpFederation(
-            FederationTestUtils.getFederationMembersWithBtcKeys(DEFAULT_FED_KEYS),
+            FederationTestUtils.getFederationMembersWithBtcKeys(defaultKeys),
             ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
             0L,
             NetworkParameters.fromID(NetworkParameters.ID_MAINNET),
-            ERP_KEYS,
-            ACTIVATION_DELAY_VALUE,
+            emergencyKeys,
+            activationDelayValue,
             activations
         );
 
         Assert.assertNotEquals(TestConstants.ERP_TESTNET_REDEEM_SCRIPT, erpFederation.getRedeemScript());
         validateErpRedeemScript(
             erpFederation.getRedeemScript(),
-            ACTIVATION_DELAY_VALUE,
+            activationDelayValue,
             false
         );
     }
@@ -312,19 +308,19 @@ public class ErpFederationTest {
     @Test
     public void getRedeemScript_after_RSKIP_284_testnet() {
         Federation erpFederation = new ErpFederation(
-            FederationTestUtils.getFederationMembersWithBtcKeys(DEFAULT_FED_KEYS),
+            FederationTestUtils.getFederationMembersWithBtcKeys(defaultKeys),
             ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
             0L,
             NetworkParameters.fromID(NetworkParameters.ID_TESTNET),
-            ERP_KEYS,
-            ACTIVATION_DELAY_VALUE,
+            emergencyKeys,
+            activationDelayValue,
             activations
         );
 
         Assert.assertNotEquals(TestConstants.ERP_TESTNET_REDEEM_SCRIPT, erpFederation.getRedeemScript());
         validateErpRedeemScript(
             erpFederation.getRedeemScript(),
-            ACTIVATION_DELAY_VALUE,
+            activationDelayValue,
             false
         );
     }
@@ -332,19 +328,19 @@ public class ErpFederationTest {
     @Test
     public void getRedeemScript_after_RSKIP_284_mainnet() {
         Federation erpFederation = new ErpFederation(
-            FederationTestUtils.getFederationMembersWithBtcKeys(DEFAULT_FED_KEYS),
+            FederationTestUtils.getFederationMembersWithBtcKeys(defaultKeys),
             ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
             0L,
             NetworkParameters.fromID(NetworkParameters.ID_MAINNET),
-            ERP_KEYS,
-            ACTIVATION_DELAY_VALUE,
+            emergencyKeys,
+            activationDelayValue,
             activations
         );
 
         Assert.assertNotEquals(TestConstants.ERP_TESTNET_REDEEM_SCRIPT, erpFederation.getRedeemScript());
         validateErpRedeemScript(
             erpFederation.getRedeemScript(),
-            ACTIVATION_DELAY_VALUE,
+            activationDelayValue,
             false
         );
     }
@@ -462,8 +458,8 @@ public class ErpFederationTest {
             ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
             0L,
             NetworkParameters.fromID(NetworkParameters.ID_TESTNET),
-            ERP_KEYS,
-            ACTIVATION_DELAY_VALUE,
+            emergencyKeys,
+            activationDelayValue,
             activationsPre
         );
 
@@ -472,8 +468,8 @@ public class ErpFederationTest {
             ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
             0L,
             NetworkParameters.fromID(NetworkParameters.ID_TESTNET),
-            ERP_KEYS,
-            ACTIVATION_DELAY_VALUE,
+            emergencyKeys,
+            activationDelayValue,
             activationsPre
         );
 
@@ -485,8 +481,8 @@ public class ErpFederationTest {
             ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
             0L,
             NetworkParameters.fromID(NetworkParameters.ID_TESTNET),
-            ERP_KEYS,
-            ACTIVATION_DELAY_VALUE,
+            emergencyKeys,
+            activationDelayValue,
             activationsPost
         );
 
@@ -498,8 +494,8 @@ public class ErpFederationTest {
             ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
             0L,
             NetworkParameters.fromID(NetworkParameters.ID_TESTNET),
-            ERP_KEYS,
-            ACTIVATION_DELAY_VALUE,
+            emergencyKeys,
+            activationDelayValue,
             activationsPost
         );
 
@@ -788,7 +784,7 @@ public class ErpFederationTest {
         when(activations.isActive(ConsensusRule.RSKIP293)).thenReturn(isRskip293Active);
 
         Federation erpFederation = new ErpFederation(
-            FederationTestUtils.getFederationMembersWithBtcKeys(DEFAULT_FED_KEYS),
+            FederationTestUtils.getFederationMembersWithBtcKeys(defaultKeys),
             ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
             0L,
             constants.getBtcParams(),
@@ -803,7 +799,7 @@ public class ErpFederationTest {
             .collect(Collectors.toList());
         validateErpRedeemScript(
             erpFederation.getRedeemScript(),
-            DEFAULT_FED_KEYS,
+            defaultKeys,
             sortedErpKeys,
             constants.getErpFedActivationDelay(),
             isRskip293Active
@@ -843,12 +839,12 @@ public class ErpFederationTest {
 
     private ErpFederation createDefaultErpFederation() {
         return new ErpFederation(
-            FederationTestUtils.getFederationMembersWithBtcKeys(DEFAULT_FED_KEYS),
+            FederationTestUtils.getFederationMembersWithBtcKeys(defaultKeys),
             ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
             0L,
             NetworkParameters.fromID(NetworkParameters.ID_REGTEST),
-            ERP_KEYS,
-            ACTIVATION_DELAY_VALUE,
+            emergencyKeys,
+            activationDelayValue,
             activations
         );
     }
@@ -860,8 +856,8 @@ public class ErpFederationTest {
 
         validateErpRedeemScript(
             erpRedeemScript,
-            DEFAULT_FED_KEYS,
-            ERP_KEYS,
+            defaultKeys,
+            emergencyKeys,
             csvValue,
             isRskip293Active
         );
