477443f7f4d39f94d1b09f674dd1eb293b5903d4
Marcos <marcos@iovlabs.org>
2022-10-14 11:24:05 -0300

    Add P2shErpFederation tests

---
+++ rskj-core/src/test/java/co/rsk/peg/P2shErpFederationTest.java
@@ -0,0 +1,130 @@
+package co.rsk.peg;
+
+import static org.mockito.Mockito.mock;
+
+import co.rsk.bitcoinj.core.BtcECKey;
+import co.rsk.bitcoinj.core.Utils;
+import co.rsk.bitcoinj.script.Script;
+import co.rsk.bitcoinj.script.ScriptOpCodes;
+import co.rsk.config.BridgeConstants;
+import co.rsk.config.BridgeMainNetConstants;
+import co.rsk.config.BridgeTestNetConstants;
+import java.time.ZonedDateTime;
+import java.util.List;
+import org.ethereum.config.blockchain.upgrades.ActivationConfig;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class P2shErpFederationTest {
+
+    @Test
+    public void getRedeemScript_testnet() {
+        test_getRedeemScript(BridgeTestNetConstants.getInstance());
+    }
+
+    @Test
+    public void getRedeemScript_mainnet() {
+        test_getRedeemScript(BridgeMainNetConstants.getInstance());
+    }
+
+    private void test_getRedeemScript(BridgeConstants bridgeConstants) {
+        List<BtcECKey> defaultKeys = bridgeConstants.getGenesisFederation().getBtcPublicKeys();
+        List<BtcECKey> emergencyKeys = bridgeConstants.getErpFedPubKeysList();
+        long activationDelay = bridgeConstants.getErpFedActivationDelay();
+
+        ActivationConfig.ForBlock activations = mock(ActivationConfig.ForBlock.class);
+
+        Federation p2shErpFederation = new P2shErpFederation(
+            FederationTestUtils.getFederationMembersWithBtcKeys(defaultKeys),
+            ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
+            0L,
+            bridgeConstants.getBtcParams(),
+            emergencyKeys,
+            activationDelay,
+            activations
+        );
+
+        validateP2shErpRedeemScript(
+            p2shErpFederation.getRedeemScript(),
+            defaultKeys,
+            emergencyKeys,
+            activationDelay
+        );
+    }
+
+    private void validateP2shErpRedeemScript(
+        Script erpRedeemScript,
+        List<BtcECKey> defaultMultisigKeys,
+        List<BtcECKey> emergencyMultisigKeys,
+        Long csvValue) {
+
+        // Keys are sorted when added to the redeem script, so we need them sorted in order to validate
+        defaultMultisigKeys.sort(BtcECKey.PUBKEY_COMPARATOR);
+        emergencyMultisigKeys.sort(BtcECKey.PUBKEY_COMPARATOR);
+
+        byte[] serializedCsvValue = Utils.signedLongToByteArrayLE(csvValue);
+
+        byte[] script = erpRedeemScript.getProgram();
+        Assert.assertTrue(script.length > 0);
+
+        int index = 0;
+
+        // First byte should equal OP_NOTIF
+        Assert.assertEquals(ScriptOpCodes.OP_NOTIF, script[index++]);
+
+        // Next byte should equal M, from an M/N multisig
+        int m = defaultMultisigKeys.size() / 2 + 1;
+        Assert.assertEquals(ScriptOpCodes.getOpCode(String.valueOf(m)), script[index++]);
+
+        // Assert public keys
+        for (BtcECKey key: defaultMultisigKeys) {
+            byte[] pubkey = key.getPubKey();
+            Assert.assertEquals(pubkey.length, script[index++]);
+            for (byte b : pubkey) {
+                Assert.assertEquals(b, script[index++]);
+            }
+        }
+
+        // Next byte should equal N, from an M/N multisig
+        int n = defaultMultisigKeys.size();
+        Assert.assertEquals(ScriptOpCodes.getOpCode(String.valueOf(n)), script[index++]);
+
+        // Next byte should equal OP_CHECKMULTISIG
+        Assert.assertEquals(Integer.valueOf(ScriptOpCodes.OP_CHECKMULTISIG).byteValue(), script[index++]);
+
+        // Next byte should equal OP_ELSE
+        Assert.assertEquals(ScriptOpCodes.OP_ELSE, script[index++]);
+
+        // Next byte should equal csv value length
+        Assert.assertEquals(serializedCsvValue.length, script[index++]);
+
+        // Next bytes should equal the csv value in bytes
+        for (int i = 0; i < serializedCsvValue.length; i++) {
+            Assert.assertEquals(serializedCsvValue[i], script[index++]);
+        }
+
+        Assert.assertEquals(Integer.valueOf(ScriptOpCodes.OP_CHECKSEQUENCEVERIFY).byteValue(), script[index++]);
+        Assert.assertEquals(ScriptOpCodes.OP_DROP, script[index++]);
+
+        // Next byte should equal M, from an M/N multisig
+        m = emergencyMultisigKeys.size() / 2 + 1;
+        Assert.assertEquals(ScriptOpCodes.getOpCode(String.valueOf(m)), script[index++]);
+
+        for (BtcECKey key: emergencyMultisigKeys) {
+            byte[] pubkey = key.getPubKey();
+            Assert.assertEquals(Integer.valueOf(pubkey.length).byteValue(), script[index++]);
+            for (byte b : pubkey) {
+                Assert.assertEquals(b, script[index++]);
+            }
+        }
+
+        // Next byte should equal N, from an M/N multisig
+        n = emergencyMultisigKeys.size();
+        Assert.assertEquals(ScriptOpCodes.getOpCode(String.valueOf(n)), script[index++]);
+
+        // Next byte should equal OP_CHECKMULTISIG
+        Assert.assertEquals(Integer.valueOf(ScriptOpCodes.OP_CHECKMULTISIG).byteValue(), script[index++]);
+
+        Assert.assertEquals(ScriptOpCodes.OP_ENDIF, script[index++]);
+    }
+}
