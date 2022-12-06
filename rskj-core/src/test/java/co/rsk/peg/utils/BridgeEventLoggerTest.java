5f2c157437ab179d075ccf6c2a992c24436b6364
jeremy-then <jeremy.then@iovlabs.org>
2022-12-06 10:39:18 -0400

    Solves sonarcloud tests code smells by removing 'public' keyword on test methods

--- rskj-core/src/test/java/co/rsk/peg/utils/BridgeEventLoggerTest.java
+++ rskj-core/src/test/java/co/rsk/peg/utils/BridgeEventLoggerTest.java
@@ -1,9 +1,6 @@
 package co.rsk.peg.utils;
 
-import co.rsk.bitcoinj.core.Address;
-import co.rsk.bitcoinj.core.BtcTransaction;
-import co.rsk.bitcoinj.core.Coin;
-import co.rsk.bitcoinj.core.NetworkParameters;
+import co.rsk.bitcoinj.core.*;
 import co.rsk.core.RskAddress;
 import co.rsk.crypto.Keccak256;
 import co.rsk.peg.PegTestUtils;
@@ -23,49 +20,50 @@ public class BridgeEventLoggerTest {
     private Keccak256 rskTxHash;
 
     @BeforeEach
-    public void setup() {
+    void setup() {
         eventLogger = spy(BridgeEventLogger.class);
         btcTxMock = mock(BtcTransaction.class);
         rskTxHash = PegTestUtils.createHash3(1);
     }
 
     @Test
-    public void testLogLockBtc() {
+    void testLogLockBtc() {
         assertThrows(UnsupportedOperationException.class, () -> {
             eventLogger.logLockBtc(mock(RskAddress.class), btcTxMock, mock(Address.class), Coin.SATOSHI);
         });
     }
 
     @Test
-    public void testLogPeginBtc() {
+    void testLogPeginBtc() {
         assertThrows(UnsupportedOperationException.class, () -> {
             eventLogger.logPeginBtc(mock(RskAddress.class), btcTxMock, Coin.SATOSHI, 1);
         });
     }
 
     @Test
-    public void testLogReleaseBtcRequested() {
+    void testLogReleaseBtcRequested() {
+        byte[] rskTxHashBytes = rskTxHash.getBytes();
         assertThrows(UnsupportedOperationException.class, () -> {
-            eventLogger.logReleaseBtcRequested(rskTxHash.getBytes(), btcTxMock, Coin.SATOSHI);
+            eventLogger.logReleaseBtcRequested(rskTxHashBytes, btcTxMock, Coin.SATOSHI);
         });
     }
 
     @Test
-    public void testLogRejectedPegin() {
+    void testLogRejectedPegin() {
         assertThrows(UnsupportedOperationException.class, () -> {
             eventLogger.logRejectedPegin(btcTxMock, RejectedPeginReason.PEGIN_CAP_SURPASSED);
         });
     }
 
     @Test
-    public void testLogUnrefundablePegin() {
+    void testLogUnrefundablePegin() {
         assertThrows(UnsupportedOperationException.class, () -> {
             eventLogger.logUnrefundablePegin(btcTxMock, UnrefundablePeginReason.LEGACY_PEGIN_UNDETERMINED_SENDER);
         });
     }
 
     @Test
-    public void testLogReleaseBtcRequestReceived() {
+    void testLogReleaseBtcRequestReceived() {
         String sender = "0x00000000000000000000000000000000000000";
         String base58Address = "mipcBbFg9gMiCh81Kj8tqqdgoZub1ZJRfn";
         Address btcDestinationAddress = Address.fromBase58(NetworkParameters.fromID(NetworkParameters.ID_REGTEST), base58Address);
@@ -76,7 +74,7 @@ public class BridgeEventLoggerTest {
     }
 
     @Test
-    public void testLogReleaseBtcRequestRejected() {
+    void testLogReleaseBtcRequestRejected() {
         String sender = "0x00000000000000000000000000000000000000";
         Coin amount = Coin.COIN;
         RejectedPegoutReason reason = RejectedPegoutReason.LOW_AMOUNT;
@@ -86,16 +84,18 @@ public class BridgeEventLoggerTest {
     }
 
     @Test
-    public void logBatchPegoutCreated() {
+    void logBatchPegoutCreated() {
+        Sha256Hash btcTxHash = btcTxMock.getHash();
         assertThrows(UnsupportedOperationException.class, () -> {
-            eventLogger.logBatchPegoutCreated(btcTxMock.getHash(), new ArrayList<>());
+            eventLogger.logBatchPegoutCreated(btcTxHash, new ArrayList<>());
         });
     }
 
     @Test
-    public void logPegoutConfirmed() {
+    void logPegoutConfirmed() {
+        Sha256Hash btcTxHash = btcTxMock.getHash();
         assertThrows(UnsupportedOperationException.class, () -> {
-            eventLogger.logPegoutConfirmed(btcTxMock.getHash(), 5);
+            eventLogger.logPegoutConfirmed(btcTxHash, 5);
         });
     }
 
