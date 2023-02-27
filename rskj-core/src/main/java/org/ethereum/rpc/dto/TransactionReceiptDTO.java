e914688b889180dd9882094212b883974d60fc04
maximo santoro <maximo.santoro@iovlabs.org>
2023-02-27 14:29:55 -0300

    fix type to one digit

--- rskj-core/src/main/java/org/ethereum/rpc/dto/TransactionReceiptDTO.java
+++ rskj-core/src/main/java/org/ethereum/rpc/dto/TransactionReceiptDTO.java
@@ -72,7 +72,7 @@ public class TransactionReceiptDTO {
         transactionHash = receipt.getTransaction().getHash().toJsonString();
         transactionIndex = HexUtils.toQuantityJsonHex(txInfo.getIndex());
         logsBloom = HexUtils.toUnformattedJsonHex(txInfo.getReceipt().getBloomFilter().getData());
-        type = "0x00";
+        type = "0x0";
     }
 
     public String getTransactionHash() {
