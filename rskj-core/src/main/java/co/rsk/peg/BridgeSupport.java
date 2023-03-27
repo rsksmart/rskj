809aed31ba1fdcd547bd1b90a7f02b5dee6692aa
Nathaniel <nathaniel.calderon>
2023-03-27 10:10:13 -0400

    Remove unused import

--- rskj-core/src/main/java/co/rsk/peg/BridgeSupport.java
+++ rskj-core/src/main/java/co/rsk/peg/BridgeSupport.java
@@ -76,7 +76,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 import javax.annotation.Nullable;
 
