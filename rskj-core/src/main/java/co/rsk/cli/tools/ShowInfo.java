ab42489639cd4c9d7c11c2896900d88719b9d08b
Reynold Morel <reynold.morel@iovlabs.org>
2023-04-03 11:00:26 -0400

    Address comments

--- rskj-core/src/main/java/co/rsk/cli/tools/ShowInfo.java
+++ rskj-core/src/main/java/co/rsk/cli/tools/ShowInfo.java
@@ -17,15 +17,16 @@
  */
 package co.rsk.cli.tools;
 
-import co.rsk.RskContext;
-import co.rsk.cli.CliToolRskContextAware;
+import co.rsk.cli.PicoCliToolRskContextAware;
 import co.rsk.trie.Trie;
 import co.rsk.trie.TrieStore;
 import org.ethereum.core.Block;
 import org.ethereum.db.BlockStore;
 import org.spongycastle.util.encoders.Hex;
+import picocli.CommandLine;
 
 import javax.annotation.Nonnull;
+import java.io.IOException;
 import java.lang.invoke.MethodHandles;
 import java.util.Arrays;
 import java.util.Locale;
@@ -33,28 +34,21 @@ import java.util.Objects;
 import java.util.Optional;
 
 /**
- * The entry point for export state CLI tool
- * This is an experimental/unsupported tool
- *
- * This tool can be interrupted and re-started and it will continue the migration from
- * the point where it left.
- *
- * Required cli args:
- *
- * - args[0] - command "stateroot" / blocknumber / dbFormat / dbPath /triestorePath
- *
- * STATEROOT:
- * - args[1] - block number
-
- *
+ * The entry point for show info tool.
  */
-public class ShowInfo extends CliToolRskContextAware  {
-    private static final int COMMAND_IDX = 0;
-    private static final int BLOCK_NUMBER_IDX = 1;
+@CommandLine.Command(name = "showinfo", mixinStandardHelpOptions = true, version = "showinfo 1.0",
+        description = "The entry point for show info tool")
+public class ShowInfo extends PicoCliToolRskContextAware {
+    @CommandLine.Option(names = {"-cmd", "--command"}, description = "Commands:\n\n" +
+            "STATEROOT - shows state root info.\n" +
+            "TRIESTORE - shows trie store info. \n" +
+            "STATESIZES - shows state sizes info. \n" +
+            "DB - shows db info. \n", required = true)
+    private String command;
 
-    public static void main(String[] args) {
-        create(MethodHandles.lookup().lookupClass()).execute(args);
-    }
+    @CommandLine.Option(names = {"-b", "--block"}, description = "Block number")
+    private Long blockNumber;
+
     enum Command {
         STATEROOT("STATEROOT"),
         TRIESTORE("TRIESTORE"),
@@ -75,18 +69,24 @@ public class ShowInfo extends CliToolRskContextAware  {
         }
     }
 
+    public static void main(String[] args) {
+        create(MethodHandles.lookup().lookupClass()).execute(args);
+    }
+
     @Override
-    protected void onExecute(@Nonnull String[] args, @Nonnull RskContext ctx) throws Exception {
-        Command command = Command.ofName(args[COMMAND_IDX].toUpperCase(Locale.ROOT));
+    public Integer call() throws IOException {
+        Command command = Command.ofName(this.command.toUpperCase(Locale.ROOT));
 
         BlockStore blockStore = ctx.getBlockStore();
-        TrieStore trieStore = null;
+        TrieStore trieStore;
         long blockNumber = -1;
-        Block block = null;
-        if (command== Command.STATESIZES) {
+        Block block;
+
+        if (command == Command.STATESIZES) {
             trieStore = ctx.getTrieStore();
             System.out.println("block number,size");
-            for (blockNumber = 1_600_000;blockNumber<=4_300_000;blockNumber +=50_000) {
+
+            for (blockNumber = 1_600_000; blockNumber <= 4_300_000; blockNumber += 50_000) {
 
                 block = blockStore.getChainBlockByNumber(blockNumber);
                 Optional<Trie> otrie = trieStore.retrieve(block.getStateRoot());
@@ -97,44 +97,43 @@ public class ShowInfo extends CliToolRskContextAware  {
                 }
 
                 Trie trie = otrie.get();
-                System.out.println(blockNumber+","+trie.getChildrenSize().value);
+                System.out.println(blockNumber + "," + trie.getChildrenSize().value);
 
             }
-            return;
+
+            return 0;
         }
-        if (command== Command.STATEROOT) {
-            blockNumber = Long.parseLong(args[BLOCK_NUMBER_IDX]);
+
+        if (command == Command.STATEROOT) {
             block = blockStore.getChainBlockByNumber(blockNumber);
-            System.out.println("block "+blockNumber+" state root: "+
+            System.out.println("block " + blockNumber + " state root: " +
                     Hex.toHexString(block.getStateRoot()));
             trieStore = ctx.getTrieStore();
-            showState( block.getStateRoot(), trieStore);
-            return;
+            showState(block.getStateRoot(), trieStore);
+            return 0;
         }
 
-        byte[] root = null;
+        byte[] root;
 
-
-        if (command== Command.DB) {
-            System.out.println("DB Path: "+ctx.getRskSystemProperties().databaseDir());
-            System.out.println("DB Kind: "+ctx.getRskSystemProperties().databaseKind().name());
-
-        } else if (command== Command.TRIESTORE) {
+        if (command == Command.DB) {
+            System.out.println("DB Path: " + ctx.getRskSystemProperties().databaseDir());
+            System.out.println("DB Kind: " + ctx.getRskSystemProperties().databaseKind().name());
+        } else if (command == Command.TRIESTORE) {
             block = blockStore.getBestBlock();
             root = block.getStateRoot();
             trieStore = ctx.getTrieStore();
-            System.out.println("best block "+blockNumber);
-            System.out.println("State root: "+ Hex.toHexString(root));
+            System.out.println("best block " + blockNumber);
+            System.out.println("State root: " + Hex.toHexString(root));
             showState(root, trieStore);
-        }  else {
-            System.exit(1);
+        } else {
+            return -1;
         }
 
+        return 0;
     }
 
 
-    private boolean showState( byte[] root , TrieStore trieStore) {
-
+    private boolean showState(byte[] root, TrieStore trieStore) {
         Optional<Trie> otrie = trieStore.retrieve(root);
 
         if (!otrie.isPresent()) {
