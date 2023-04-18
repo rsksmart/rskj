700c1552113557161db425160f1b556cce024e39
maximo santoro <maximo.santoro@iovlabs.org>
2023-04-18 17:17:28 -0300

    add --version and --help options

--- rskj-core/src/main/java/co/rsk/cli/RskjCli.java
+++ rskj-core/src/main/java/co/rsk/cli/RskjCli.java
@@ -9,5 +9,18 @@ import picocli.CommandLine;
 @CommandLine.Command(name = "rskj", mixinStandardHelpOptions = true, version = "RSKJ Node 4.5.0",
         description = "RSKJ blockchain node implementation in Java")
 public class RskjCli {
+    public boolean isVersionRequested() {
+        return versionRequested;
+    }
+
+    public boolean isUsageRequested() {
+        return usageRequested;
+    }
+
+    @CommandLine.Option(names = {"-v", "--version"}, versionHelp = true, description = "Display version info")
+    private boolean versionRequested;
+
+    @CommandLine.Option(names = {"-h", "--help"}, usageHelp = true, description = "Display this help message")
+    private boolean usageRequested;
 
 }
