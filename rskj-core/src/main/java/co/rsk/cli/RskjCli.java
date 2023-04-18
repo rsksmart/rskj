c94e1b4d3cc11ed6aa54a4678fd17fb1b5826ca7
maximo santoro <maximo.santoro@iovlabs.org>
2023-04-18 17:16:45 -0300

    add new parser class using PicoCli

---
+++ rskj-core/src/main/java/co/rsk/cli/RskjCli.java
@@ -0,0 +1,13 @@
+package co.rsk.cli;
+
+import picocli.CommandLine;
+
+import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "rskj", mixinStandardHelpOptions = true, version = "RSKJ Node 4.5.0",
+        description = "RSKJ blockchain node implementation in Java")
+public class RskjCli {
+
+}
