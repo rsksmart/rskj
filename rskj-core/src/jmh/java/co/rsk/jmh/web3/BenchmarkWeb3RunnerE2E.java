848a195b810a8d408894ad196ea005a794bc36c6
Iago Lluque <iago.lluque@iovlabs.org>
2023-04-19 13:52:15 +0200

    Implemented Gradle task handling OOTB jmh parameters and/or custom subset

--- rskj-core/src/jmh/java/co/rsk/jmh/web3/BenchmarkWeb3RunnerE2E.java
+++ rskj-core/src/jmh/java/co/rsk/jmh/web3/BenchmarkWeb3RunnerE2E.java
@@ -22,15 +22,20 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class BenchmarkWeb3RunnerE2E {
 
-    // TODO(iago) think if it is better to encapsulate here the parameters and call jar for concrete class
     public static void main(String[] args) throws Exception {
+        Path resultDir = Paths.get(System.getProperty("user.dir"), "build", "reports", "jmh");
+        resultDir.toFile().mkdirs();
+
         Options opt = new OptionsBuilder()
                 .threads(1)
                 .param("suite", "e2e")
-                .param("host", "http://localhost:4444") // TODO(iago)
-                .result("/Users/iago.lluque/tmp/jmh_test_result.txt") // TODO(iago)
+                .param("host", "http://localhost:4444")
+                .result(resultDir + "/result_test.txt")
                 .shouldFailOnError(true)
                 .forks(1)
                 .build();
