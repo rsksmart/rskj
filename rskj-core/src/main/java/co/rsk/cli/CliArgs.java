7125262340165982af5036203d405863154c03f5
maximo santoro <maximo.santoro@iovlabs.org>
2023-05-17 19:04:06 -0300

    add of constructor to CliArgs for RskCli

--- rskj-core/src/main/java/co/rsk/cli/CliArgs.java
+++ rskj-core/src/main/java/co/rsk/cli/CliArgs.java
@@ -29,6 +29,9 @@ public class CliArgs<O, F> {
     private final Set<F> flags;
     private final Map<String, String> paramValueMap;
 
+    public static <O, F> CliArgs<O, F> of(Map<O, String> options, Set<F> flags, Map<String, String> paramValueMap) {
+        return new CliArgs<>(options, flags, paramValueMap);
+    }
     private CliArgs(Map<O, String> options, Set<F> flags, Map<String, String> paramValueMap) {
         this.options = Collections.unmodifiableMap(options);
         this.flags = Collections.unmodifiableSet(flags);
@@ -66,140 +69,141 @@ public class CliArgs<O, F> {
      * That is, options must be prefixed with "{@code -}", and must specify a value,
      * and flags must be prefixed with "{@code --}", and may not specify a value.
      */
-//    public static class Parser<O extends Enum<O> & OptionalizableCliArg, F extends Enum<F> & CliArg> {
-//
-//        private final EnumSet<O> options;
-//        private final EnumSet<F> flags;
-//        private final boolean ignoreUnmatchedArgs;
-//
-//        public Parser(Class<O> optionsClass, Class<F> flagsClass) {
-//            this(optionsClass, flagsClass, false);
-//        }
-//
-//        public Parser(Class<O> optionsClass, Class<F> flagsClass, boolean ignoreUnmatchedArgs) {
-//            this.options = EnumSet.allOf(optionsClass);
-//            this.flags = EnumSet.allOf(flagsClass);
-//            this.ignoreUnmatchedArgs = ignoreUnmatchedArgs;
-//        }
-//
-//        public CliArgs<O, F> parse(String[] args) {
-//            List<String> arguments = new LinkedList<>();
-//            Map<O, String> options = new HashMap<>();
-//            Set<F> flags = new HashSet<>();
-//            Map<String, String> paramValueMap = new HashMap<>();
-//
-//            for (int i = 0; i < args.length; i++) {
-//                switch (args[i].charAt(0)) {
-//                    case '-':
-//                        this.parseArguments(args, i, options, flags, paramValueMap);
-//                        break;
-//                    default:
-//                        arguments.add(args[i]);
-//                        break;
-//                }
-//            }
-//
-//            Set<O> missingOptions = this.options.stream()
-//                    .filter(arg -> !arg.isOptional())
-//                    .collect(Collectors.toSet());
-//            missingOptions.removeAll(options.keySet());
-//            if (!missingOptions.isEmpty()) {
-//                throw new IllegalArgumentException(
-//                        String.format("Missing configuration options: %s", missingOptions)
-//                );
-//            }
-//
-//            return new CliArgs<>(arguments, options, flags, paramValueMap);
-//        }
-//
-//        private void parseArguments(String[] args, int i, Map<O, String> options, Set<F> flags, Map<String, String> paramValueMap) {
-//            if (args[i].length() < 2) {
-//                throw new IllegalArgumentException("You must provide an option name, e.g. -d");
-//            }
-//            char currentChar = Character.toLowerCase(args[i].charAt(1));
-//            if (currentChar == '-') {
-//                if (args[i].length() < 3) {
-//                    throw new IllegalArgumentException("You must provide a flag name, e.g. --quiet");
-//                }
-//
-//                F flag = getFlagByName(args[i].substring(2, args[i].length()));
-//
-//                if (flag != null) {
-//                    flags.add(flag);
-//                }
-//            } else if (currentChar == 'x') {
-//                String arg = args[i].substring(2);
-//                paramValueMap.putAll(parseArgToMap(arg));
-//            } else {
-//                if (args.length - 1 == i) {
-//                    throw new IllegalArgumentException(
-//                            String.format("A value must be provided after the option -%s", args[i])
-//                    );
-//                }
-//
-//                O option = getOptionByName(args[i].substring(1, args[i].length()));
-//
-//                if (option != null) {
-//                    options.put(option, args[i + 1]);
-//                }
-//
-//                i++;
-//            }
-//        }
-//
-//        private F getFlagByName(String flagName) {
-//            F flagFound = flags.stream()
-//                    .filter(flag -> flag.getName().equals(flagName))
-//                    .findFirst()
-//                    .orElse(null);
-//
-//            if (flagFound == null && !this.ignoreUnmatchedArgs) {
-//                throw new NoSuchElementException(String.format("--%s is not a valid flag", flagName));
-//            }
-//
-//            return flagFound;
-//        }
-//
-//        private O getOptionByName(String optionName) {
-//            O option = options.stream()
-//                    .filter(opt -> opt.getName().equals(optionName))
-//                    .findFirst()
-//                    .orElse(null);
-//
-//            if (option == null && !this.ignoreUnmatchedArgs) {
-//                throw new NoSuchElementException(String.format("-%s is not a valid option", optionName));
-//            }
-//
-//            return option;
-//        }
-//
-//        /**
-//         * Parses a string argument in the format e.g <i>database.dir=/home/rsk/core<i/> to a map in the following
-//         * structure:
-//         * <blockquote>
-//         *     {
-//         *         "database": {
-//         *             "dir": "/home/rsk/core"
-//         *         }
-//         *     }
-//         * </blockquote>
-//         * @param arg to parse
-//         * @return a string arg parsed to an equivalent map having the same structure system properties would have.
-//         */
-//        private Map<String, String> parseArgToMap(String arg) {
-//            String[] paramValue = arg.split("=", 2);
-//
-//            if (paramValue.length != 2) {
-//                throw new IllegalArgumentException("You must provide a valid arg, e.g. -Xparam.part1.part2=value");
-//            }
-//
-//            String param = paramValue[0];
-//            String value = paramValue[1];
-//
-//            Map<String, String> paramValueMap = new HashMap<>();
-//            paramValueMap.put(param, value);
-//
-//            return paramValueMap;
-//        }
-//    }
+    // TODO: kill this class
+    public static class Parser<O extends Enum<O> & OptionalizableCliArg, F extends Enum<F> & CliArg> {
+
+        private final EnumSet<O> options;
+        private final EnumSet<F> flags;
+        private final boolean ignoreUnmatchedArgs;
+
+        public Parser(Class<O> optionsClass, Class<F> flagsClass) {
+            this(optionsClass, flagsClass, false);
+        }
+
+        public Parser(Class<O> optionsClass, Class<F> flagsClass, boolean ignoreUnmatchedArgs) {
+            this.options = EnumSet.allOf(optionsClass);
+            this.flags = EnumSet.allOf(flagsClass);
+            this.ignoreUnmatchedArgs = ignoreUnmatchedArgs;
+        }
+
+        public CliArgs<O, F> parse(String[] args) {
+            List<String> arguments = new LinkedList<>();
+            Map<O, String> options = new HashMap<>();
+            Set<F> flags = new HashSet<>();
+            Map<String, String> paramValueMap = new HashMap<>();
+
+            for (int i = 0; i < args.length; i++) {
+                switch (args[i].charAt(0)) {
+                    case '-':
+                        this.parseArguments(args, i, options, flags, paramValueMap);
+                        break;
+                    default:
+                        arguments.add(args[i]);
+                        break;
+                }
+            }
+
+            Set<O> missingOptions = this.options.stream()
+                    .filter(arg -> !arg.isOptional())
+                    .collect(Collectors.toSet());
+            missingOptions.removeAll(options.keySet());
+            if (!missingOptions.isEmpty()) {
+                throw new IllegalArgumentException(
+                        String.format("Missing configuration options: %s", missingOptions)
+                );
+            }
+
+            return new CliArgs<>(options, flags, paramValueMap);
+        }
+
+        private void parseArguments(String[] args, int i, Map<O, String> options, Set<F> flags, Map<String, String> paramValueMap) {
+            if (args[i].length() < 2) {
+                throw new IllegalArgumentException("You must provide an option name, e.g. -d");
+            }
+            char currentChar = Character.toLowerCase(args[i].charAt(1));
+            if (currentChar == '-') {
+                if (args[i].length() < 3) {
+                    throw new IllegalArgumentException("You must provide a flag name, e.g. --quiet");
+                }
+
+                F flag = getFlagByName(args[i].substring(2, args[i].length()));
+
+                if (flag != null) {
+                    flags.add(flag);
+                }
+            } else if (currentChar == 'x') {
+                String arg = args[i].substring(2);
+                paramValueMap.putAll(parseArgToMap(arg));
+            } else {
+                if (args.length - 1 == i) {
+                    throw new IllegalArgumentException(
+                            String.format("A value must be provided after the option -%s", args[i])
+                    );
+                }
+
+                O option = getOptionByName(args[i].substring(1, args[i].length()));
+
+                if (option != null) {
+                    options.put(option, args[i + 1]);
+                }
+
+                i++;
+            }
+        }
+
+        private F getFlagByName(String flagName) {
+            F flagFound = flags.stream()
+                    .filter(flag -> flag.getName().equals(flagName))
+                    .findFirst()
+                    .orElse(null);
+
+            if (flagFound == null && !this.ignoreUnmatchedArgs) {
+                throw new NoSuchElementException(String.format("--%s is not a valid flag", flagName));
+            }
+
+            return flagFound;
+        }
+
+        private O getOptionByName(String optionName) {
+            O option = options.stream()
+                    .filter(opt -> opt.getName().equals(optionName))
+                    .findFirst()
+                    .orElse(null);
+
+            if (option == null && !this.ignoreUnmatchedArgs) {
+                throw new NoSuchElementException(String.format("-%s is not a valid option", optionName));
+            }
+
+            return option;
+        }
+
+        /**
+         * Parses a string argument in the format e.g <i>database.dir=/home/rsk/core<i/> to a map in the following
+         * structure:
+         * <blockquote>
+         *     {
+         *         "database": {
+         *             "dir": "/home/rsk/core"
+         *         }
+         *     }
+         * </blockquote>
+         * @param arg to parse
+         * @return a string arg parsed to an equivalent map having the same structure system properties would have.
+         */
+        private Map<String, String> parseArgToMap(String arg) {
+            String[] paramValue = arg.split("=", 2);
+
+            if (paramValue.length != 2) {
+                throw new IllegalArgumentException("You must provide a valid arg, e.g. -Xparam.part1.part2=value");
+            }
+
+            String param = paramValue[0];
+            String value = paramValue[1];
+
+            Map<String, String> paramValueMap = new HashMap<>();
+            paramValueMap.put(param, value);
+
+            return paramValueMap;
+        }
+    }
 }
