8c0eb724e11635c9972db46365ba28f74353c72d
Julian Len <julian@iovlabs.org>
2023-05-22 11:32:41 -0300

    StateChunk message request created with encode and decode

---
+++ rskj-core/src/main/java/co/rsk/net/messages/StateChunkRequestMessage.java
@@ -0,0 +1,39 @@
+package co.rsk.net.messages;
+
+import org.ethereum.util.RLP;
+
+public class StateChunkRequestMessage extends MessageWithId {
+
+    private final long id;
+    private byte[] hash;
+
+    public StateChunkRequestMessage(long id, byte[] hash) {
+        this.id = id;
+        this.hash = hash;
+    }
+
+    @Override
+    public MessageType getMessageType() {
+        return MessageType.STATE_CHUNK_REQUEST_MESSAGE;
+    }
+
+    @Override
+    public void accept(MessageVisitor v) {
+
+    }
+
+    @Override
+    public long getId() {
+        return this.id;
+    }
+
+    @Override
+    protected byte[] getEncodedMessageWithoutId() {
+        byte[] rlpHash = RLP.encodeElement(this.hash);
+        return RLP.encodeList(rlpHash);
+    }
+
+    public byte[] getHash() {
+        return this.hash.clone();
+    }
+}
