b74e585f4539b3bca9558a83826b36bfb893aee2
Reynold Morel <reynold.morel@iovlabs.org>
2023-06-19 11:00:10 -0400

    Updated eth call with tRIF transactions

--- rskj-core/src/jmh/java/co/rsk/jmh/web3/plan/EthCallPlan.java
+++ rskj-core/src/jmh/java/co/rsk/jmh/web3/plan/EthCallPlan.java
@@ -21,64 +21,45 @@ package co.rsk.jmh.web3.plan;
 import co.rsk.jmh.helpers.BenchmarkHelper;
 import co.rsk.jmh.web3.BenchmarkWeb3Exception;
 import co.rsk.jmh.web3.e2e.RskModuleWeb3j;
-import co.rsk.jmh.web3.factory.TransactionFactory;
 import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.infra.BenchmarkParams;
-import org.web3j.protocol.core.methods.request.Transaction;
-import org.web3j.protocol.core.methods.response.EthTransaction;
 
-import java.io.IOException;
 import java.math.BigInteger;
-import java.util.Iterator;
-import java.util.Optional;
 
 @State(Scope.Benchmark)
 public class EthCallPlan extends BasePlan {
 
-    private org.web3j.protocol.core.methods.response.Transaction contractCallTransaction;
     private BigInteger blockNumber;
     private RskModuleWeb3j.EthCallArguments ethCallArguments;
-    private Iterator<Transaction> transactionsContractCall;
 
     @Override
     @Setup(Level.Trial) // move to "Level.Iteration" in case we set a batch size at some point
     public void setUp(BenchmarkParams params) throws BenchmarkWeb3Exception {
         super.setUp(params);
 
-        String address = configuration.getString("sendTransaction.from");
-
-        long nonce = Optional.ofNullable(web3Connector.ethGetTransactionCount(address))
-                .map(BigInteger::longValue)
-                .orElseThrow(() -> new BenchmarkWeb3Exception("Could not get account nonce"));
-
-        long warmupIters = (long) params.getWarmup().getCount() * params.getWarmup().getBatchSize(); // in case we set a batch size at some point
-        long measurementIters = (long) params.getMeasurement().getCount() * params.getMeasurement().getBatchSize();  // in case we set a batch size at some point
-        long numOfTransactions = warmupIters + measurementIters;
-
-        transactionsContractCall = TransactionFactory.createTransactions(TransactionFactory.TransactionType.CONTRACT_CALL, configuration, nonce, numOfTransactions).listIterator();
-
         blockNumber = web3Connector.ethBlockNumber();
         ethCallArguments = buildEthCallArguments();
     }
 
-    private org.web3j.protocol.core.methods.response.Transaction setupContractCallTransaction() throws IOException {
-        Transaction txcc = transactionsContractCall.next();
+    private org.web3j.protocol.core.methods.response.Transaction setupTransaction() {
         org.web3j.protocol.core.methods.response.Transaction tx = new org.web3j.protocol.core.methods.response.Transaction();
 
-        tx.setHash(configuration.getString("eth.transactionContractCallHash"));
-
-        tx.setFrom(txcc.getFrom());
-        tx.setTo(txcc.getTo());
-        tx.setGas(txcc.getGas());
-        tx.setGasPrice(txcc.getGasPrice());
-        tx.setValue(txcc.getValue());
-        tx.setNonce(txcc.getNonce());
-        tx.setChainId(txcc.getChainId());
-        tx.setInput(txcc.getData());
+        tx.setHash(configuration.getString("eth_call.transaction.hash"));
+        tx.setNonce(configuration.getString("eth_call.transaction.nonce"));
+        tx.setBlockHash(configuration.getString("eth_call.transaction.blockHash"));
+        tx.setBlockNumber(configuration.getString("eth_call.transaction.blockNumber"));
+        tx.setTransactionIndex(configuration.getString("eth_call.transaction.transactionIndex"));
+        tx.setFrom(configuration.getString("eth_call.transaction.from"));
+        tx.setTo(configuration.getString("eth_call.transaction.to"));
+        tx.setGas(configuration.getString("eth_call.transaction.gas"));
+        tx.setGasPrice(configuration.getString("eth_call.transaction.gasPrice"));
+        tx.setValue(configuration.getString("eth_call.transaction.value"));
+        tx.setInput(configuration.getString("eth_call.transaction.input"));
+        tx.setType(configuration.getString("eth_call.transaction.type"));
 
         return tx;
     }
@@ -89,34 +70,25 @@ public class EthCallPlan extends BasePlan {
         BenchmarkHelper.waitForBlocks(configuration);
     }
 
-    public org.web3j.protocol.core.methods.response.Transaction getContractCallTransaction() {
-        try {
-            contractCallTransaction = setupContractCallTransaction();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return contractCallTransaction;
-    }
 
     private RskModuleWeb3j.EthCallArguments buildEthCallArguments() {
         RskModuleWeb3j.EthCallArguments args = new RskModuleWeb3j.EthCallArguments();
-        org.web3j.protocol.core.methods.response.Transaction contractCallTransaction = getContractCallTransaction();
+        org.web3j.protocol.core.methods.response.Transaction tx = setupTransaction();
 
-        args.setFrom(contractCallTransaction.getFrom());
-        args.setTo(contractCallTransaction.getTo());
-        args.setGas("0x" + contractCallTransaction.getGas().toString(16));
-        args.setGasPrice("0x" + contractCallTransaction.getGasPrice().toString(16));
-        args.setValue("0x" + contractCallTransaction.getValue().toString(16));
-        args.setNonce("0x" + contractCallTransaction.getNonce().toString(16));
-        args.setChainId("0x" + BigInteger.valueOf(contractCallTransaction.getChainId()).toString(16));
-        args.setData(contractCallTransaction.getInput());
+        args.setFrom(tx.getFrom());
+        args.setTo(tx.getTo());
+        args.setGas("0x" + tx.getGas().toString(16));
+        args.setGasPrice("0x" + tx.getGasPrice().toString(16));
+        args.setValue("0x" + tx.getValue().toString(16));
+        args.setNonce("0x" + tx.getNonce().toString(16));
+        args.setChainId("0x" + BigInteger.valueOf(tx.getChainId()).toString(16));
+        args.setData(tx.getInput());
 
         return args;
     }
 
     public BigInteger getBlockNumber() {
-        return blockNumber;
+        return new BigInteger(configuration.getString("eth_call.transaction.blockNumber").replace("0x", ""), 16);
     }
 
     public RskModuleWeb3j.EthCallArguments getEthCallArguments() {
