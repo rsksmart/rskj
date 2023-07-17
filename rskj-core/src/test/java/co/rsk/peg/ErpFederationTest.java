e140b7366b5d0cb50e7e312555b46f793624121b
Marcos <marcos@iovlabs.org>
2023-07-17 15:39:47 -0300

    Create spendFromErp util method in FederationTestUtils

--- rskj-core/src/test/java/co/rsk/peg/ErpFederationTest.java
+++ rskj-core/src/test/java/co/rsk/peg/ErpFederationTest.java
@@ -1,6 +1,5 @@
 package co.rsk.peg;
 
-import static co.rsk.peg.ReleaseTransactionBuilder.BTC_TX_VERSION_2;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
@@ -12,13 +11,10 @@ import co.rsk.bitcoinj.core.BtcTransaction;
 import co.rsk.bitcoinj.core.Coin;
 import co.rsk.bitcoinj.core.NetworkParameters;
 import co.rsk.bitcoinj.core.ScriptException;
-import co.rsk.bitcoinj.core.Sha256Hash;
 import co.rsk.bitcoinj.core.Utils;
 import co.rsk.bitcoinj.core.VerificationException;
-import co.rsk.bitcoinj.crypto.TransactionSignature;
 import co.rsk.bitcoinj.script.ErpFederationRedeemScriptParser;
 import co.rsk.bitcoinj.script.Script;
-import co.rsk.bitcoinj.script.ScriptBuilder;
 import co.rsk.bitcoinj.script.ScriptOpCodes;
 import co.rsk.config.BridgeConstants;
 import co.rsk.config.BridgeMainNetConstants;
@@ -29,6 +25,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.Instant;
@@ -39,7 +36,7 @@ import java.util.stream.Collectors;
 import org.bouncycastle.util.encoders.Hex;
 import org.ethereum.config.blockchain.upgrades.ActivationConfig;
 import org.ethereum.config.blockchain.upgrades.ConsensusRule;
-import org.ethereum.crypto.ECKey;
+import org.ethereum.crypto.HashUtil;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
@@ -704,113 +701,6 @@ class ErpFederationTest {
         ));
     }
 
-    private void spendFromErpFed(
-        NetworkParameters networkParameters,
-        long activationDelay,
-        boolean isRskip293Active,
-        boolean signWithEmergencyMultisig) {
-
-        // Created with GenNodeKeyId using seed 'fed1'
-        byte[] publicKeyBytes = Hex.decode("043267e382e076cbaa199d49ea7362535f95b135de181caf66b391f541bf39ab0e75b8577faac2183782cb0d76820cf9f356831d216e99d886f8a6bc47fe696939");
-        BtcECKey btcKey = BtcECKey.fromPublicOnly(publicKeyBytes);
-        ECKey rskKey = ECKey.fromPublicOnly(publicKeyBytes);
-        FederationMember fed1 = new FederationMember(btcKey, rskKey, rskKey);
-        BtcECKey fed1PrivKey = BtcECKey.fromPrivate(Hex.decode("529822842595a3a6b3b3e51e9cffa0db66452599f7beec542382a02b1e42be4b"));
-
-        // Created with GenNodeKeyId using seed 'fed3', used for fed2 to keep keys sorted
-        publicKeyBytes = Hex.decode("0443e106d90183e2eef7d5cb7538a634439bf1301d731787c6736922ff19e750ed39e74a76731fed620aeedbcd77e4de403fc4148efd3b5dbfc6cef550aa63c377");
-        btcKey = BtcECKey.fromPublicOnly(publicKeyBytes);
-        rskKey = ECKey.fromPublicOnly(publicKeyBytes);
-        FederationMember fed2 = new FederationMember(btcKey, rskKey, rskKey);
-        BtcECKey fed2PrivKey = BtcECKey.fromPrivate(Hex.decode("b2889610e66cd3f7de37c81c20c786b576349b80b3f844f8409e3a29d95c0c7c"));
-
-        // Created with GenNodeKeyId using seed 'fed2', used for fed3 to keep keys sorted
-        publicKeyBytes = Hex.decode("04bd5b51b1c5d799da190285c8078a2712b8e5dc6f73c799751e6256bb89a4bd04c6444b00289fc76ee853fcfa52b3083d66c42e84f8640f53a4cdf575e4d4a399");
-        btcKey = BtcECKey.fromPublicOnly(publicKeyBytes);
-        rskKey = ECKey.fromPublicOnly(publicKeyBytes);
-        FederationMember fed3 = new FederationMember(btcKey, rskKey, rskKey);
-        BtcECKey fed3PrivKey = BtcECKey.fromPrivate(Hex.decode("fa013890aa14dd269a0ca16003cabde1688021358b662d17b1e8c555f5cccc6e"));
-
-        // Created with GenNodeKeyId using seed 'erp1'
-        publicKeyBytes = Hex.decode("048f5a88b08d75765b36951254e68060759de5be7e559972c37c67fc8cedafeb2643a4a8a618125530e275fe310c72dbdd55fa662cdcf8e134012f8a8d4b7e8400");
-        BtcECKey erp1Key = BtcECKey.fromPublicOnly(publicKeyBytes);
-        BtcECKey erp1PrivKey = BtcECKey.fromPrivate(Hex.decode("1f28656deb5f108f8cdf14af34ac4ff7a5643a7ac3f77b8de826b9ad9775f0ca"));
-
-        // Created with GenNodeKeyId using seed 'erp2'
-        publicKeyBytes = Hex.decode("04deba35a96add157b6de58f48bb6e23bcb0a17037bed1beb8ba98de6b0a0d71d60f3ce246954b78243b41337cf8f93b38563c3bcd6a5329f1d68c057d0e5146e8");
-        BtcECKey erp2Key = BtcECKey.fromPublicOnly(publicKeyBytes);
-        BtcECKey erp2PrivKey = BtcECKey.fromPrivate(Hex.decode("4e58ebe9cd04ffea5ab81dd2aded3ab8a63e44f3b47aef334e369d895c351646"));
-
-        // Created with GenNodeKeyId using seed 'erp3'
-        publicKeyBytes = Hex.decode("04c34fcd05cef2733ea7337c37f50ae26245646aba124948c6ff8dcdf82128499808fc9148dfbc0e0ab510b4f4a78bf7a58f8b6574e03dae002533c5059973b61f");
-        BtcECKey erp3Key = BtcECKey.fromPublicOnly(publicKeyBytes);
-        BtcECKey erp3PrivKey = BtcECKey.fromPrivate(Hex.decode("57e8d2cd51c3b076ca96a1043c8c6d32c6c18447e411a6279cda29d70650977b"));
-
-        ActivationConfig.ForBlock activations = mock(ActivationConfig.ForBlock.class);
-        when(activations.isActive(ConsensusRule.RSKIP284)).thenReturn(true);
-        when(activations.isActive(ConsensusRule.RSKIP293)).thenReturn(isRskip293Active);
-        ErpFederation erpFed = new ErpFederation(
-            Arrays.asList(fed1, fed2, fed3),
-            ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
-            0L,
-            networkParameters,
-            Arrays.asList(erp1Key, erp2Key, erp3Key),
-            activationDelay,
-            activations
-        );
-
-        // Below code can be used to create a transaction spending from the emergency multisig in testnet or mainnet
-//        String RAW_FUND_TX = "";
-//        BtcTransaction pegInTx = new BtcTransaction(networkParameters, Hex.decode(RAW_FUND_TX));
-        int outputIndex = 0; // Remember to change this value accordingly in case of using an existing raw tx
-        BtcTransaction pegInTx = new BtcTransaction(networkParameters);
-        pegInTx.addOutput(Coin.valueOf(990_000), erpFed.getAddress());
-
-        Address destinationAddress = PegTestUtils.createRandomP2PKHBtcAddress(networkParameters);
-        BtcTransaction pegOutTx = new BtcTransaction(networkParameters);
-        pegOutTx.addInput(pegInTx.getOutput(outputIndex));
-        pegOutTx.addOutput(Coin.valueOf(900_000), destinationAddress);
-        pegOutTx.setVersion(BTC_TX_VERSION_2);
-        pegOutTx.getInput(0).setSequenceNumber(activationDelay);
-
-        // Create signatures
-        Sha256Hash sigHash = pegOutTx.hashForSignature(
-            0,
-            erpFed.getRedeemScript(),
-            BtcTransaction.SigHash.ALL,
-            false
-        );
-
-        BtcECKey.ECDSASignature signature1;
-        BtcECKey.ECDSASignature signature2;
-        BtcECKey.ECDSASignature signature3;
-        if (signWithEmergencyMultisig) {
-            signature1 = erp1PrivKey.sign(sigHash);
-            signature2 = erp2PrivKey.sign(sigHash);
-            signature3 = erp3PrivKey.sign(sigHash);
-        } else {
-            signature1 = fed1PrivKey.sign(sigHash);
-            signature2 = fed2PrivKey.sign(sigHash);
-            signature3 = fed3PrivKey.sign(sigHash);
-        }
-
-        // Try different signature permutations
-        Script inputScript = createInputScript(erpFed.getRedeemScript(), signature1, signature2, signWithEmergencyMultisig);
-        pegOutTx.getInput(0).setScriptSig(inputScript);
-        inputScript.correctlySpends(pegOutTx,0, pegInTx.getOutput(outputIndex).getScriptPubKey());
-
-        inputScript = createInputScript(erpFed.getRedeemScript(), signature1, signature3, signWithEmergencyMultisig);
-        pegOutTx.getInput(0).setScriptSig(inputScript);
-        inputScript.correctlySpends(pegOutTx,0, pegInTx.getOutput(outputIndex).getScriptPubKey());
-
-        inputScript = createInputScript(erpFed.getRedeemScript(), signature2, signature3, signWithEmergencyMultisig);
-        pegOutTx.getInput(0).setScriptSig(inputScript);
-        inputScript.correctlySpends(pegOutTx,0, pegInTx.getOutput(outputIndex).getScriptPubKey());
-
-        // Uncomment to print the raw tx in console and broadcast https://blockstream.info/testnet/tx/push
-//        System.out.println(Hex.toHexString(pegOutTx.bitcoinSerialize()));
-    }
-
     private void createErpFederation(BridgeConstants constants, boolean isRskip293Active) {
         when(activations.isActive(ConsensusRule.RSKIP293)).thenReturn(isRskip293Active);
 
@@ -833,35 +723,66 @@ class ErpFederationTest {
         );
     }
 
-    private Script createInputScript(
-        Script fedRedeemScript,
-        BtcECKey.ECDSASignature signature1,
-        BtcECKey.ECDSASignature signature2,
-        boolean signWithTheEmergencyMultisig) {
+    private void spendFromErpFed(
+        NetworkParameters networkParameters,
+        long activationDelay,
+        boolean isRskip293Active,
+        boolean signWithEmergencyMultisig) {
 
-        TransactionSignature txSignature1 = new TransactionSignature(
-            signature1,
-            BtcTransaction.SigHash.ALL,
-            false
+        List<BtcECKey> standardKeys = Arrays.stream(new String[]{
+            "fed1",
+            "fed2",
+            "fed3",
+            "fed4",
+            "fed5",
+            "fed6",
+            "fed7",
+            "fed8",
+            "fed9",
+            "fed10",
+        }).map(seed -> BtcECKey.fromPrivate(HashUtil.keccak256(seed.getBytes(StandardCharsets.UTF_8))))
+            .sorted(BtcECKey.PUBKEY_COMPARATOR)
+            .collect(Collectors.toList());
+
+        List<BtcECKey> emergencyKeys = Arrays.stream(new String[]{
+            "erp1",
+            "erp2",
+            "erp3",
+            "erp4"
+        }).map(seed -> BtcECKey.fromPrivate(HashUtil.keccak256(seed.getBytes(StandardCharsets.UTF_8))))
+            .sorted(BtcECKey.PUBKEY_COMPARATOR)
+            .collect(Collectors.toList());
+
+        ActivationConfig.ForBlock activations = mock(ActivationConfig.ForBlock.class);
+        when(activations.isActive(ConsensusRule.RSKIP284)).thenReturn(true);
+        when(activations.isActive(ConsensusRule.RSKIP293)).thenReturn(isRskip293Active);
+        ErpFederation erpFed = new ErpFederation(
+            FederationMember.getFederationMembersFromKeys(standardKeys),
+            ZonedDateTime.parse("2017-06-10T02:30:00Z").toInstant(),
+            0L,
+            networkParameters,
+            emergencyKeys,
+            activationDelay,
+            activations
         );
-        byte[] txSignature1Encoded = txSignature1.encodeToBitcoin();
 
-        TransactionSignature txSignature2 = new TransactionSignature(
-            signature2,
-            BtcTransaction.SigHash.ALL,
-            false
+        Coin value = Coin.valueOf(1_000_000);
+        Coin fee = Coin.valueOf(10_000);
+        BtcTransaction fundTx = new BtcTransaction(networkParameters);
+        fundTx.addOutput(value, erpFed.getAddress());
+
+        Address destinationAddress = PegTestUtils.createRandomP2PKHBtcAddress(networkParameters);
+
+        FederationTestUtils.spendFromErpFed(
+            networkParameters,
+            erpFed,
+            signWithEmergencyMultisig ? emergencyKeys : standardKeys,
+            fundTx.getHash(),
+            0,
+            destinationAddress,
+            value.minus(fee),
+            signWithEmergencyMultisig
         );
-        byte[] txSignature2Encoded = txSignature2.encodeToBitcoin();
-
-        int flowOpCode = signWithTheEmergencyMultisig ? 1 : 0;
-        ScriptBuilder scriptBuilder = new ScriptBuilder();
-        return scriptBuilder
-            .number(0)
-            .data(txSignature1Encoded)
-            .data(txSignature2Encoded)
-            .number(flowOpCode)
-            .data(fedRedeemScript.getProgram())
-            .build();
     }
 
     private ErpFederation createDefaultErpFederation() {
