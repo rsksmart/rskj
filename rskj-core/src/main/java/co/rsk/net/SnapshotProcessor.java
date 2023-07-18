edb4363162647b38e5b013f923e75c3a22ed23b8
Iago Lluque <iago.lluque@iovlabs.org>
2023-07-18 10:32:49 +0200

    feat: evaluate LZ4 performance

--- rskj-core/src/main/java/co/rsk/net/SnapshotProcessor.java
+++ rskj-core/src/main/java/co/rsk/net/SnapshotProcessor.java
@@ -7,6 +7,9 @@ import co.rsk.trie.IterationElement;
 import co.rsk.trie.Trie;
 import co.rsk.trie.TrieStore;
 import com.google.common.collect.Maps;
+import net.jpountz.lz4.LZ4Compressor;
+import net.jpountz.lz4.LZ4Factory;
+import net.jpountz.lz4.LZ4SafeDecompressor;
 import org.ethereum.core.Block;
 import org.ethereum.core.Blockchain;
 import org.ethereum.net.NodeHandler;
@@ -21,6 +24,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.math.BigInteger;
+import java.nio.ByteBuffer;
 import java.util.*;
 
 public class SnapshotProcessor implements InternalService {
@@ -89,6 +93,20 @@ public class SnapshotProcessor implements InternalService {
                 message.getChunkOfTrieKeyValue().length
         );
 
+        // TODO(iago) do whatever it's needed, reading just to check load
+        for (int i = 0; i < trieElements.size(); i++) {
+            final RLPList trieElement = (RLPList) trieElements.get(i);
+            final byte[] key = trieElement.get(0).getRLPData();
+            final int uncompressedSize = ByteUtil.byteArrayToInt(trieElement.get(2).getRLPData());
+            final byte[] value = decompressLz4(trieElement.get(1).getRLPData(), uncompressedSize);
+            final String keyString = ByteUtil.toHexString(key);
+            final String valueString = ByteUtil.toHexString(value);
+
+            if (logger.isTraceEnabled()) {
+                logger.trace("State chunk received - Key: {}, Value: {}", keyString, valueString);
+            }
+        }
+
         this.stateSize = this.stateSize.add(BigInteger.valueOf(trieElements.size()));
         this.stateChunkSize = this.stateChunkSize.add(BigInteger.valueOf(message.getChunkOfTrieKeyValue().length));
         logger.debug("State progress: {} chunks ({} bytes)", this.stateSize.toString(), this.stateChunkSize.toString());
@@ -130,7 +148,24 @@ public class SnapshotProcessor implements InternalService {
             logger.info("Single node read.");
             byte[] key = e.getNodeKey().encode();
             byte[] value = e.getNode().getValue();
-            final byte[] element = RLP.encodeList(RLP.encodeElement(key), RLP.encodeElement(value));
+            if (value == null) {
+                logger.debug("State value is null for key {}", ByteUtil.toHexString(key));
+                // TODO(iago) revisit this
+                continue;
+            }
+
+            byte[] compressedValue = compressLz4(value);
+
+            // TODO(iago) remove this
+            if (logger.isTraceEnabled()) {
+                if (Arrays.equals(decompressLz4(compressedValue, value.length), value)) {
+                    logger.trace("===== compressed value is equal to original value for key {}", ByteUtil.toHexString(key));
+                } else {
+                    logger.trace("===== compressed value is different from original value for key {}", ByteUtil.toHexString(key));
+                }
+            }
+
+            final byte[] element = RLP.encodeList(RLP.encodeElement(key), RLP.encodeElement(compressedValue), RLP.encodeInt(value.length));
             trieEncoded.add(element);
             logger.info("Single node calculated.");
             i = KBYTES.equals(this.chunkSizeType)? i + element.length : i+1;
@@ -140,6 +175,31 @@ public class SnapshotProcessor implements InternalService {
         StateChunkResponseMessage responseMessage = new StateChunkResponseMessage(request.getId(), chunkBytes, blockNumber, request.getFrom(), !it.hasNext());
         logger.debug("Sending state chunk of {} bytes to node {}", chunkBytes.length, sender.getPeerNodeID());
         sender.sendMessage(responseMessage);
+    }
+
+    private static byte[] compressLz4(byte[] src) {
+        // TODO(iago) share instances
+        LZ4Factory lz4Factory = LZ4Factory.safeInstance();
+        LZ4Compressor fastCompressor = lz4Factory.fastCompressor();
+        int maxCompressedLength = fastCompressor.maxCompressedLength(src.length);
+        byte[] dst = new byte[maxCompressedLength];
+        int compressedLength = fastCompressor.compress(src, 0, src.length, dst, 0, maxCompressedLength);
+        return Arrays.copyOf(dst, compressedLength);
+    }
+
+    private static byte[] decompressLz4(byte[] src, int originalSize) {
+        LZ4SafeDecompressor decompressor = LZ4Factory.safeInstance().safeDecompressor();
+
+        // TODO(iago) try to not use ByteBuffer
+        ByteBuffer decompressedBuffer = ByteBuffer.allocate(originalSize);
+        decompressor.decompress(ByteBuffer.wrap(src), decompressedBuffer);
+
+        // TODO(iago) revisit this
+        if (!decompressedBuffer.hasArray()) {
+            throw new IllegalStateException("Decompressed buffer does not have backing array");
+        }
+
+        return decompressedBuffer.array();
     }
 
     private void connect() {
