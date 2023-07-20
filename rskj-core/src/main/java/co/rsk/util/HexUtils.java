a6122731949b08dde223f8b00c6cf6b836cb930b
maximo santoro <maximo.santoro@iovlabs.org>
2023-07-20 19:50:31 -0300

    fix hex validation for uppercase hex strings

--- rskj-core/src/main/java/co/rsk/util/HexUtils.java
+++ rskj-core/src/main/java/co/rsk/util/HexUtils.java
@@ -282,7 +282,7 @@ public class HexUtils {
         for (int i = startAt; i < data.length(); i++) {
             char c = data.charAt(i);
 
-            if (!(c >= '0' && c <= '9' || c >= 'a' && c <= 'f')) {
+            if (!(c >= '0' && c <= '9' || c >= 'a' && c <= 'f' || c >= 'A' && c <= 'F')) {
                 return false;
             }
         }
