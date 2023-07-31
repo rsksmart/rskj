f71732d515b87bb2df3575cff382479d166172d2
nathanieliov <nathaniel.calderon@iovlabs.org>
2023-07-31 08:17:40 -0400

    - Deprecated PegTestUtils.CreateHash method. - Created a copy of PegTestUtils.CreateHash method into BitcoinTestUtils. - Replaced usage of PegTestUtils.CreateHash method to BitcoinTestUtils.CreateHash

--- rskj-core/src/test/java/co/rsk/peg/bitcoin/BitcoinTestUtils.java
+++ rskj-core/src/test/java/co/rsk/peg/bitcoin/BitcoinTestUtils.java
@@ -3,6 +3,7 @@ package co.rsk.peg.bitcoin;
 import co.rsk.bitcoinj.core.Address;
 import co.rsk.bitcoinj.core.BtcECKey;
 import co.rsk.bitcoinj.core.NetworkParameters;
+import co.rsk.bitcoinj.core.Sha256Hash;
 import co.rsk.bitcoinj.script.Script;
 import co.rsk.bitcoinj.script.ScriptBuilder;
 import java.nio.charset.StandardCharsets;
@@ -36,5 +37,15 @@ public class BitcoinTestUtils {
         Script outputScript = ScriptBuilder.createP2SHOutputScript(redeemScript);
 
         return Address.fromP2SHScript(networkParameters, outputScript);
+    }
+
+    public static Sha256Hash createHash(int nHash) {
+        byte[] bytes = new byte[32];
+        bytes[0] = (byte) (0xFF & nHash);
+        bytes[1] = (byte) (0xFF & nHash >> 8);
+        bytes[2] = (byte) (0xFF & nHash >> 16);
+        bytes[3] = (byte) (0xFF & nHash >> 24);
+
+        return Sha256Hash.wrap(bytes);
     }
 }
