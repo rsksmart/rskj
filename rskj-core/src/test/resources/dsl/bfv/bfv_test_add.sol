207289c4ef68cfbaa93d36169d38b3bc0828f5a5
fedejinich <federico@iovlabs.org>
2023-08-01 17:05:15 +0200

    call bfv.add from solidity

---
+++ rskj-core/src/test/resources/dsl/bfv/bfv_test_add.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.18;
+
+contract BFVAddTest {
+    function bfvAddTest(bytes memory data) public view returns (bytes memory) {
+        address addAddr = 0x0000000000000000000000000000000001000011;
+        return bfvOp(addAddr, data);
+    }
+
+    function bfvSubTest(bytes memory data) public view returns (bytes memory) {
+        address addAddr = 0x0000000000000000000000000000000001000012;
+        return bfvOp(addAddr, data);
+    }
+
+    function bfvMulTest(bytes memory data) public view returns (bytes memory) {
+        address addAddr = 0x0000000000000000000000000000000001000013;
+        return bfvOp(addAddr, data);
+    }
+
+    function bfvOp(address precAddress, bytes memory data)
+        public
+        view
+        returns (bytes memory)
+    {
+        //one of the default accounts of regtest
+        //address addr = 0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826;
+        bytes memory input = data;
+        uint256 inputLen = input.length;
+        uint256 outLen = 32768; // todo(fedejinich) this cannot be calcuated at compile time
+
+        bytes memory retval; // todo(fedejinich) this might be a bytes32 memory?
+
+        assembly {
+            // allocate output byte array
+            let res := mload(outLen)
+
+            // call precompile (STATICCALL returns success or failure)
+            retval := staticcall(
+               gas(),
+                precAddress,
+                input,
+                inputLen,
+                res,
+                outLen
+            )
+        }
+
+        return retval;
+    }
+}
+
