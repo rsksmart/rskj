71f1f4d51bcefdaae675b3e125d282744cad2a74
nathanieliov <nathaniel.calderon@iovlabs.org>
2023-08-15 16:07:42 -0400

    Removed unused import. Renamed variable.

--- rskj-core/src/test/java/co/rsk/peg/BridgeSupportTest.java
+++ rskj-core/src/test/java/co/rsk/peg/BridgeSupportTest.java
@@ -19,7 +19,6 @@
 package co.rsk.peg;
 
 import co.rsk.bitcoinj.core.*;
-import co.rsk.bitcoinj.crypto.TransactionSignature;
 import co.rsk.bitcoinj.script.Script;
 import co.rsk.bitcoinj.script.ScriptBuilder;
 import co.rsk.bitcoinj.store.BlockStoreException;
