879a092446cd70e14b721cf2e61cf6ad0c622009
julia zack <julia.zack@iovlabs.org>
2023-08-25 12:55:31 -0300

    Create ReleaseWitnessTransactionBuilder and ReleaseWitnessTransactionBuilderTest to create a correct witness transaction and calculate its correct size for fees

---
+++ rskj-core/src/main/java/co/rsk/peg/ReleaseWitnessTransactionBuilder.java
@@ -0,0 +1,229 @@
+/*
+ * This file is part of RskJ
+ * Copyright (C) 2017 RSK Labs Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package co.rsk.peg;
+
+import co.rsk.bitcoinj.core.*;
+import co.rsk.bitcoinj.crypto.TransactionSignature;
+import co.rsk.bitcoinj.script.Script;
+import co.rsk.bitcoinj.script.ScriptBuilder;
+import co.rsk.bitcoinj.wallet.SendRequest;
+import co.rsk.bitcoinj.wallet.Wallet;
+import org.ethereum.config.blockchain.upgrades.ActivationConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static co.rsk.bitcoinj.script.ScriptOpCodes.OP_0;
+
+public class ReleaseWitnessTransactionBuilder {
+
+    public static final int BTC_TX_VERSION_2 = 2;
+
+    public class BuildResult {
+        private final BtcTransaction btcTx;
+        private final List<UTXO> selectedUTXOs;
+        private final Response responseCode;
+
+        public BuildResult(BtcTransaction btcTx, List<UTXO> selectedUTXOs, Response responseCode) {
+            this.btcTx = btcTx;
+            this.selectedUTXOs = selectedUTXOs;
+            this.responseCode = responseCode;
+        }
+
+        public BtcTransaction getBtcTx() {
+            return btcTx;
+        }
+
+        public List<UTXO> getSelectedUTXOs() {
+            return selectedUTXOs;
+        }
+
+        public Response getResponseCode() {
+            return responseCode;
+        }
+    }
+
+    private interface SendRequestConfigurator {
+        void configure(SendRequest sr);
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger("ReleaseWitnessTransactionBuilder");
+
+    private final NetworkParameters params;
+    private final Wallet wallet;
+    private final Address changeAddress;
+    private final Script redeemScript;
+    private final List<BtcECKey> signers;
+    private final Coin feePerKb;
+    private final ActivationConfig.ForBlock activations;
+
+    public ReleaseWitnessTransactionBuilder(
+        NetworkParameters params,
+        Wallet wallet,
+        Address changeAddress,
+        Script redeemScript,
+        List<BtcECKey> signers,
+        Coin feePerKb,
+        ActivationConfig.ForBlock activations
+    ) {
+        this.params = params;
+        this.wallet = wallet;
+        this.changeAddress = changeAddress;
+        this.redeemScript = redeemScript;
+        this.signers = signers;
+        this.feePerKb = feePerKb;
+        this.activations = activations;
+    }
+
+    public Wallet getWallet() {
+        return wallet;
+    }
+
+    public Address getChangeAddress() {
+        return changeAddress;
+    }
+
+    public Coin getFeePerKb() {
+        return feePerKb;
+    }
+
+    public BuildResult buildAmountTo(Address to, Coin amount, boolean isSegwit) {
+        return buildWithConfiguration((SendRequest sr) -> {
+            sr.tx.addOutput(amount, to);
+            sr.changeAddress = changeAddress;
+            sr.isSegwit = isSegwit;
+        }, String.format("sending %s to %s", amount, to));
+    }
+
+    public BuildResult buildBatchedPegouts(List<ReleaseRequestQueue.Entry> entries) {
+        return buildWithConfiguration((SendRequest sr) -> {
+            for (ReleaseRequestQueue.Entry entry : entries) {
+                sr.tx.addOutput(entry.getAmount(), entry.getDestination());
+            }
+            sr.changeAddress = changeAddress;
+        }, String.format("batching %d pegouts", entries.size()));
+    }
+
+    public BuildResult buildEmptyWalletTo(Address to) {
+        return buildWithConfiguration((SendRequest sr) -> {
+            sr.tx.addOutput(Coin.ZERO, to);
+            sr.changeAddress = to;
+            sr.emptyWallet = true;
+        }, String.format("emptying wallet to %s", to));
+    }
+
+    private BuildResult buildWithConfiguration(
+        SendRequestConfigurator sendRequestConfigurator,
+        String operationDescription) {
+
+        // Build a tx and send request and configure it
+        BtcTransaction btcTx = new BtcTransaction(params);
+
+        btcTx.setVersion(BTC_TX_VERSION_2);
+
+        SendRequest sr = SendRequest.forTx(btcTx);
+        // Default settings
+        defaultSettingsConfigurator.configure(sr);
+        // Specific settings
+        sendRequestConfigurator.configure(sr);
+
+        try {
+            wallet.completeTx(sr);
+
+            // Disconnect input from output because we don't need the reference and it interferes serialization
+            for (TransactionInput transactionInput : btcTx.getInputs()) {
+                transactionInput.disconnect();
+            }
+
+            List<UTXO> selectedUTXOs = wallet
+                .getUTXOProvider().getOpenTransactionOutputs(wallet.getWatchedAddresses()).stream()
+                .filter(utxo ->
+                    btcTx.getInputs().stream().anyMatch(input ->
+                        input.getOutpoint().getHash().equals(utxo.getHash()) &&
+                            input.getOutpoint().getIndex() == utxo.getIndex()
+                    )
+                )
+                .collect(Collectors.toList());
+
+            byte[] redeemScriptHash = Sha256Hash.hash(redeemScript.getProgram());
+            Script scriptSig = new ScriptBuilder().number(OP_0).data(redeemScriptHash).build();
+            Script segwitScriptSig = new ScriptBuilder().data(scriptSig.getProgram()).build();
+            btcTx.getInput(0).setScriptSig(segwitScriptSig);
+
+            int requiredSignatures = signers.size() / 2 + 1;
+
+            List<TransactionSignature> txSignatures = Stream
+                .generate(TransactionSignature::dummy)
+                .limit(requiredSignatures)
+                .collect(Collectors.toList());
+
+            TransactionWitness txWitness = TransactionWitness.createWitnessScript(redeemScript, txSignatures);
+
+            int txInputsSize = btcTx.getInputs().size();
+            for (int i = 0; i < txInputsSize; i++) {
+                btcTx.getInput(i).setScriptSig(segwitScriptSig);
+                btcTx.setWitness(i, txWitness);
+            }
+
+            System.out.println(btcTx.hasWitness()); // true
+
+            return new BuildResult(btcTx, selectedUTXOs, Response.SUCCESS);
+        } catch (InsufficientMoneyException e) {
+            logger.warn(String.format("Not enough BTC in the wallet to complete %s", operationDescription), e);
+            // Comment out panic logging for now
+            // panicProcessor.panic("nomoney", "Not enough confirmed BTC in the federation wallet to complete " + rskTxHash + " " + btcTx);
+            return new BuildResult(null, null, Response.INSUFFICIENT_MONEY);
+        } catch (Wallet.CouldNotAdjustDownwards e) {
+            logger.warn(String.format("A user output could not be adjusted downwards to pay tx fees %s", operationDescription), e);
+            // Comment out panic logging for now
+            // panicProcessor.panic("couldnotadjustdownwards", "A user output could not be adjusted downwards to pay tx fees " + rskTxHash + " " + btcTx);
+            return new BuildResult(null, null, Response.COULD_NOT_ADJUST_DOWNWARDS);
+        } catch (Wallet.ExceededMaxTransactionSize e) {
+            logger.warn(String.format("Tx size too big %s", operationDescription), e);
+            // Comment out panic logging for now
+            // panicProcessor.panic("exceededmaxtransactionsize", "Tx size too big " + rskTxHash + " " + btcTx);
+            return new BuildResult(null, null, Response.EXCEED_MAX_TRANSACTION_SIZE);
+        } catch (UTXOProviderException e) {
+            logger.warn(String.format("UTXO provider exception sending %s", operationDescription), e);
+            // Comment out panic logging for now
+            // panicProcessor.panic("utxoprovider", "UTXO provider exception " + rskTxHash + " " + btcTx);
+            return new BuildResult(null, null, Response.UTXO_PROVIDER_EXCEPTION);
+        }
+    }
+
+    private final SendRequestConfigurator defaultSettingsConfigurator = (SendRequest sr) -> {
+        sr.missingSigsMode = Wallet.MissingSigsMode.USE_OP_ZERO;
+        sr.feePerKb = getFeePerKb();
+        sr.shuffleOutputs = false;
+        sr.recipientsPayFees = true;
+    };
+
+    protected enum Response {
+        SUCCESS,
+        INSUFFICIENT_MONEY,
+        COULD_NOT_ADJUST_DOWNWARDS,
+        EXCEED_MAX_TRANSACTION_SIZE,
+        UTXO_PROVIDER_EXCEPTION
+    }
+
+}
+
---
+++ rskj-core/src/test/java/co/rsk/peg/ReleaseWitnessTransactionBuilderTest.java
@@ -0,0 +1,141 @@
+package co.rsk.peg;
+
+import co.rsk.bitcoinj.core.*;
+import co.rsk.bitcoinj.script.P2shErpFederationRedeemScriptParser;
+import co.rsk.bitcoinj.script.Script;
+import co.rsk.bitcoinj.script.ScriptBuilder;
+import co.rsk.bitcoinj.wallet.Wallet;
+import co.rsk.config.BridgeConstants;
+import co.rsk.config.BridgeRegTestConstants;
+import co.rsk.config.BridgeTestNetConstants;
+import co.rsk.peg.bitcoin.BitcoinTestUtils;
+import org.ethereum.config.blockchain.upgrades.ActivationConfig;
+import org.ethereum.config.blockchain.upgrades.ConsensusRule;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.spongycastle.util.encoders.Hex;
+
+import java.math.BigInteger;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+public class ReleaseWitnessTransactionBuilderTest {
+    private Wallet wallet;
+    private Address changeAddress;
+    private ReleaseTransactionBuilder builder;
+    private ActivationConfig.ForBlock activations;
+    private Context btcContext;
+    private NetworkParameters networkParameters;
+    private BridgeConstants bridgeConstants;
+    private Federation federation;
+
+    @BeforeEach
+    void setup() {
+        wallet = mock(Wallet.class);
+        changeAddress = mockAddress(1000);
+        activations = mock(ActivationConfig.ForBlock.class);
+        bridgeConstants = BridgeRegTestConstants.getInstance();
+        networkParameters = bridgeConstants.getBtcParams();
+        btcContext = new Context(networkParameters);
+        federation = bridgeConstants.getGenesisFederation();
+        builder = new ReleaseTransactionBuilder(
+            networkParameters,
+            wallet,
+            changeAddress,
+            Coin.MILLICOIN.multiply(2),
+            activations
+        );
+    }
+
+    private Address mockAddress(int pk) {
+        return BtcECKey.fromPrivate(BigInteger.valueOf(pk)).toAddress(NetworkParameters.fromID(NetworkParameters.ID_REGTEST));
+    }
+
+    @Test
+    void fees_from_p2shp2wsh_erp_federation() {
+        ActivationConfig.ForBlock activations = mock(ActivationConfig.ForBlock.class);
+        when(activations.isActive(ConsensusRule.RSKIP284)).thenReturn(true);
+
+        BridgeConstants bridgeConstants = BridgeTestNetConstants.getInstance();
+
+        List<BtcECKey> standardKeys = BitcoinTestUtils.getBtcEcKeysFromSeeds(
+            new String[]{
+                "fed1", "fed2", "fed3", "fed4", "fed5"
+                //, "fed6", "fed7", "fed8", "fed9"
+            },
+            true
+        );
+
+        List<BtcECKey> emergencyKeys = bridgeConstants.getErpFedPubKeysList();
+        long activationDelay = bridgeConstants.getErpFedActivationDelay();
+
+        Script standardRedeem = new ScriptBuilder().createRedeemScript(standardKeys.size()/2+1, standardKeys);
+        Script emergencyRedeem = new ScriptBuilder().createRedeemScript(emergencyKeys.size()/2+1, emergencyKeys);
+        Script redeemScript = P2shErpFederationRedeemScriptParser.createP2shP2wshErpRedeemScript(standardRedeem, emergencyRedeem, activationDelay);
+
+        Script p2shP2wshOutputScript = ScriptBuilder.createP2SHP2WSHOutputScript(redeemScript);
+        Address segwitAddress = Address.fromP2SHScript(
+            networkParameters,
+            p2shP2wshOutputScript
+        );
+        System.out.println(segwitAddress);
+
+        Federation p2shErpFederation = new P2shErpFederation(
+            FederationMember.getFederationMembersFromKeys(standardKeys),
+            Instant.now(),
+            0,
+            bridgeConstants.getBtcParams(),
+            emergencyKeys,
+            activationDelay,
+            activations
+        );
+
+        List<UTXO> utxos = Arrays.asList(
+            new UTXO(
+                Sha256Hash.wrap("6dcf5ab88b689fa5faad3444098137ef2536a327877e815103a9194b99cde584"),
+                0,
+                Coin.valueOf(10_000),
+                0,
+                false,
+                p2shErpFederation.getP2SHScript()
+            )
+        );
+
+        Wallet thisWallet = BridgeUtils.getFederationSpendWallet(
+            new Context(bridgeConstants.getBtcParams()),
+            p2shErpFederation,
+            utxos,
+            false,
+            mock(BridgeStorageProvider.class)
+        );
+
+        ReleaseWitnessTransactionBuilder releaseWitnessTransactionBuilder = new ReleaseWitnessTransactionBuilder(
+            bridgeConstants.getBtcParams(),
+            thisWallet,
+            p2shErpFederation.getAddress(),
+            redeemScript,
+            standardKeys,
+            Coin.SATOSHI.multiply(1000),
+            activations
+        );
+
+        Address pegoutRecipient = Address.fromBase58(networkParameters,"msgc5Gtz2L9MVhXPDrFRCYPa16QgoZ2EjP");
+        Coin pegoutAmount = Coin.valueOf(9_500);
+
+        ReleaseWitnessTransactionBuilder.BuildResult result = releaseWitnessTransactionBuilder.buildAmountTo(
+            pegoutRecipient,
+            pegoutAmount,
+            true
+        );
+
+        System.out.println(Hex.toHexString(result.getBtcTx().bitcoinSerialize()));
+
+        Assertions.assertTrue(result.getBtcTx().hasWitness());
+        Assertions.assertEquals(ReleaseWitnessTransactionBuilder.Response.SUCCESS, result.getResponseCode());
+    }
+
+}
