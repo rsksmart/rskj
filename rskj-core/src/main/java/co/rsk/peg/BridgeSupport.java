7c9154bafc32097d3d39cd6aa654c124e3ad5445
nathanieliov <nathaniel.calderon@iovlabs.org>
2023-08-24 20:37:33 -0400

    Created and implemented savePegoutTxSigHash new method responsible for storing the sig hash at peg out creation

--- rskj-core/src/main/java/co/rsk/peg/BridgeSupport.java
+++ rskj-core/src/main/java/co/rsk/peg/BridgeSupport.java
@@ -48,6 +48,7 @@ import co.rsk.config.BridgeConstants;
 import co.rsk.core.RskAddress;
 import co.rsk.crypto.Keccak256;
 import co.rsk.panic.PanicProcessor;
+import co.rsk.peg.bitcoin.BitcoinUtils;
 import co.rsk.peg.bitcoin.CoinbaseInformation;
 import co.rsk.peg.bitcoin.MerkleBranch;
 import co.rsk.peg.bitcoin.RskAllowUnconfirmedCoinSelector;
@@ -1021,6 +1022,9 @@ public class BridgeSupport {
             pegoutsWaitingForConfirmations.add(btcTx, rskExecutionBlock.getNumber());
         }
 
+        // Store sigHash to be able to identify tx type
+        savePegoutTxSigHash(btcTx);
+
         // Mark UTXOs as spent
         availableUTXOs.removeIf(utxo -> selectedUTXOs.stream().anyMatch(selectedUtxo ->
             utxo.getHash().equals(selectedUtxo.getHash()) && utxo.getIndex() == selectedUtxo.getIndex()
@@ -1175,6 +1179,7 @@ public class BridgeSupport {
 
                 BtcTransaction generatedTransaction = result.getBtcTx();
                 addToPegoutsWaitingForConfirmations(generatedTransaction, pegoutsWaitingForConfirmations, rskTx.getHash(), totalPegoutValue);
+                savePegoutTxSigHash(generatedTransaction);
 
                 // Remove batched requests from the queue after successfully batching pegouts
                 pegoutRequests.removeEntries(pegoutEntries);
@@ -1197,6 +1202,17 @@ public class BridgeSupport {
                 logger.info("[processPegoutsInBatch] Next Pegout Height updated from {} to {}", currentBlockNumber, nextPegoutHeight);
             }
         }
+    }
+
+    private void savePegoutTxSigHash(BtcTransaction pegoutTx) {
+        if (!activations.isActive(ConsensusRule.RSKIP379)){
+            return;
+        }
+        Optional<Sha256Hash> pegoutTxSigHash = BitcoinUtils.getFirstInputSigHash(pegoutTx);
+        if (!pegoutTxSigHash.isPresent()){
+            throw new IllegalStateException(String.format("SigHash could not be obtained from btc tx %s", pegoutTx.getHash()));
+        }
+        provider.setPegoutTxSigHash(pegoutTxSigHash.get());
     }
 
     /**
