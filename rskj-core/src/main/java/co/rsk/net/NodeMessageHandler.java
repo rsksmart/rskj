00c8dee878aed1ccfb2c3e095999fa56e1215628
Julian Len <julian@iovlabs.org>
2023-08-28 12:03:50 +0200

    it prints messages once the node is synced

--- rskj-core/src/main/java/co/rsk/net/NodeMessageHandler.java
+++ rskj-core/src/main/java/co/rsk/net/NodeMessageHandler.java
@@ -23,10 +23,7 @@ import co.rsk.config.RskSystemProperties;
 import co.rsk.core.RskAddress;
 import co.rsk.core.bc.BlockUtils;
 import co.rsk.crypto.Keccak256;
-import co.rsk.net.messages.BlockMessage;
-import co.rsk.net.messages.Message;
-import co.rsk.net.messages.MessageType;
-import co.rsk.net.messages.MessageVisitor;
+import co.rsk.net.messages.*;
 import co.rsk.scoring.EventType;
 import co.rsk.scoring.PeerScoringManager;
 import co.rsk.util.ExecState;
@@ -232,6 +229,12 @@ public class NodeMessageHandler implements MessageHandler, InternalService, Runn
         // also, while queue implementation stays unbounded, offer() will never return false
         messageCounter.increment(sender);
         MessageTask messageTask = new MessageTask(sender, message, score, nodeMsgTraceInfo);
+
+        if (messageTask.getMessage().getMessageType() == MessageType.BLOCK_MESSAGE) {
+            BlockMessage blockMessage = (BlockMessage) messageTask.getMessage();
+            loggerSnapExperiment.debug("BlockMessage block arrived: [{}] from: [{}]", blockMessage.getBlock().getNumber(), messageTask.sender.getPeerNodeID());
+        }
+
         boolean messageAdded = this.queue.offer(messageTask);
         if (!messageAdded) {
             messageCounter.decrement(sender);
