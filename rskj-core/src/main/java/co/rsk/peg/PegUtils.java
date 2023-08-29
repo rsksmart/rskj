c729e29dd4b722a00cd71dacf9ded4727f348eb7
nathanieliov <nathaniel.calderon@iovlabs.org>
2023-08-29 17:11:19 -0400

    Added tests.

--- rskj-core/src/main/java/co/rsk/peg/PegUtils.java
+++ rskj-core/src/main/java/co/rsk/peg/PegUtils.java
@@ -12,29 +12,27 @@ import java.util.List;
 import java.util.Optional;
 
 public class PegUtils {
-    private PegUtils() {
-    }
+    private PegUtils() { }
 
     public static PegTxType getTransactionType(
         ActivationConfig.ForBlock activations,
-        BridgeStorageProvider bridgeStorageProvider,
+        BridgeStorageProvider provider,
         Wallet liveFederationsWallet,
         BtcTransaction btcTransaction
     ) throws RegisterBtcTransactionException {
         if (!activations.isActive(ConsensusRule.RSKIP379)){
-            throw new RegisterBtcTransactionException("Can't call PegUtil.getTransactionType before RSKIP379 activation");
+            throw new RegisterBtcTransactionException("Can't call PegUtil.getTransactionType before RSKIP379 activation.");
         }
 
         List<TransactionOutput> liveFederationOutputs = btcTransaction.getWalletOutputs(liveFederationsWallet);
-        if (liveFederationOutputs.isEmpty()){
-            return PegTxType.UNKNOWN;
-        }
 
         Optional<Sha256Hash> inputSigHash = BitcoinUtils.getFirstInputSigHash(btcTransaction);
-        if (inputSigHash.isPresent() && bridgeStorageProvider.hasPegoutTxSigHash(inputSigHash.get())){
+        if (inputSigHash.isPresent() && provider.hasPegoutTxSigHash(inputSigHash.get())){
             return PegTxType.PEGOUT_OR_MIGRATION;
-        } else {
+        } else if (!liveFederationOutputs.isEmpty()){
             return PegTxType.PEGIN;
+        } else {
+            return PegTxType.UNKNOWN;
         }
     }
 }
