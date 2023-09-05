56a7b42e2ad4c6291dfba8353429b05d41ac3ab1
nathanieliov <nathaniel.calderon@iovlabs.org>
2023-09-04 20:35:36 -0400

    Added new tests. Refactored helper methods.

--- rskj-core/src/test/java/co/rsk/peg/bitcoin/BitcoinTestUtils.java
+++ rskj-core/src/test/java/co/rsk/peg/bitcoin/BitcoinTestUtils.java
@@ -17,6 +17,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import co.rsk.bitcoinj.script.ScriptChunk;
+import org.bouncycastle.util.encoders.Hex;
 import org.ethereum.crypto.HashUtil;
 
 public class BitcoinTestUtils {
@@ -57,28 +58,32 @@ public class BitcoinTestUtils {
     }
 
     public static List<BtcECKey.ECDSASignature> extractSignaturesFromTxInput(TransactionInput txInput) {
+        String btcHexSignaturesPrefix = "30";
         Script scriptSig = txInput.getScriptSig();
         List<ScriptChunk> chunks = scriptSig.getChunks();
         Script redeemScript = new Script(chunks.get(chunks.size() - 1).data);
         RedeemScriptParser parser = RedeemScriptParserFactory.get(redeemScript.getChunks());
 
-        // This chunk may be 0 or 1 depending on if the federation is spending funds from standard multisig or erp
-        int flowOpCodeChunk;
+        // This is the last chunk after the signatures.
+        int lastSigChunk;
         RedeemScriptParser.MultiSigType multiSigType = parser.getMultiSigType();
 
         if (multiSigType == RedeemScriptParser.MultiSigType.STANDARD_MULTISIG
                 || multiSigType == RedeemScriptParser.MultiSigType.FAST_BRIDGE_MULTISIG
         ) {
-            flowOpCodeChunk = chunks.size() - 1;
+            lastSigChunk = chunks.size() - 1;
         } else {
-            flowOpCodeChunk = chunks.size() - 2;
+            lastSigChunk = chunks.size() - 2;
         }
 
         List<BtcECKey.ECDSASignature> signatures = new ArrayList<>();
-        for (int i = 1; i < flowOpCodeChunk; i++) {
+        for (int i = 1; i < lastSigChunk; i++) {
             ScriptChunk chunk = chunks.get(i);
-            if (chunk.isOpCode() || chunk.data.length > 0) {
-                signatures.add(TransactionSignature.decodeFromDER(chunk.data));
+            if (!chunk.isOpCode() && chunk.data.length > 0) {
+                // Bitcoin signatures always starts with the prefix ‘30’.
+                if (Hex.toHexString(chunk.data).startsWith(btcHexSignaturesPrefix)){
+                    signatures.add(TransactionSignature.decodeFromDER(chunk.data));
+                }
             }
         }
         return signatures;
