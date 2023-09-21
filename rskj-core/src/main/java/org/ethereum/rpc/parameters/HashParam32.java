0dd8a3e4b858c426316b7550c52e36edb227cab2
Reynold Morel <reynold.morel@iovlabs.org>
2023-09-21 08:23:38 -0400

    Add (C) header to new files

--- rskj-core/src/main/java/org/ethereum/rpc/parameters/HashParam32.java
+++ rskj-core/src/main/java/org/ethereum/rpc/parameters/HashParam32.java
@@ -37,7 +37,7 @@ public abstract class HashParam32 {
         try {
             hashBytes = HexUtils.stringHexToByteArray(hash);
         } catch (Exception e) {
-            throw RskJsonRpcRequestException.invalidParamError("Invalid " + hashType + " format. " + e.getMessage());
+            throw RskJsonRpcRequestException.invalidParamError("Invalid " + hashType + " format. " + e);
         }
 
         if (HASH_BYTE_LENGTH != hashBytes.length) {
--- rskj-core/src/test/java/org/ethereum/rpc/parameters/HexIndexParamTest.java
+++ rskj-core/src/test/java/org/ethereum/rpc/parameters/HexIndexParamTest.java
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.ethereum.rpc.parameters;
 
 import org.ethereum.rpc.exception.RskJsonRpcRequestException;
