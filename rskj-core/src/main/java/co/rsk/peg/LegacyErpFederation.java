f6dce43cde5cd1bfe72d90bb93f7de15d678cbb7
julia zack <julia.zack@iovlabs.org>
2023-10-04 12:41:24 -0300

    Fix sonar issues

--- rskj-core/src/main/java/co/rsk/peg/LegacyErpFederation.java
+++ rskj-core/src/main/java/co/rsk/peg/LegacyErpFederation.java
@@ -37,9 +37,7 @@ public class LegacyErpFederation extends ErpFederation {
 
         // Try getting the redeem script in order to validate it can be built
         // using the given public keys and csv value
-        getRedeemScript();
         validateRedeemScript();
-        getStandardRedeemScript();
     }
 
     @Override
