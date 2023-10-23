e30796b224a3c7f73e71b693b6778bc9a75b8bd6
nathanieliov <nathaniel.calderon@iovlabs.org>
2023-10-23 09:59:56 -0400

    - Removed no needed spy. - Fix typo.

--- rskj-core/src/test/java/co/rsk/peg/BridgeSupportTestIntegration.java
+++ rskj-core/src/test/java/co/rsk/peg/BridgeSupportTestIntegration.java
@@ -1571,7 +1571,6 @@ public class BridgeSupportTestIntegration {
             .withExecutionBlock(rskCurrentBlock)
             .withBtcBlockStoreFactory(mockFactory)
             .withProvider(bridgeStorageProvider).build();
-        bridgeSupport = Mockito.spy(bridgeSupport);
 
         // Test that transaction type is identified as a peg-out
         PegTxType transactionType = bridgeSupport.getTransactionType(releaseWithChangeTx);
@@ -1583,7 +1582,7 @@ public class BridgeSupportTestIntegration {
         assertThat(activeFederationUtxos, hasSize(0));
         // Assert utxo is registered to retiring fed
         assertThat(retiringFederationUtxos, hasSize(1));
-        // assert utxo registered is the expceted one
+        // assert utxo registered is the expected one
         UTXO changeUtxo = retiringFederationUtxos.get(0);
         assertThat(changeUtxo.getValue(), is(changeValue));
         assertThat(changeUtxo.getScript().getToAddress(params), is(retiringFederationAddress));
