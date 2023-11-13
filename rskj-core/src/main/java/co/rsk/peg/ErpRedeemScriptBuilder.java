8f28067e6506f2a618b4dec38f2d38becc8ee509
julia zack <julia.zack@iovlabs.org>
2023-11-13 17:48:05 -0300

    Create ErpRedeemScriptBuilders

---
+++ rskj-core/src/main/java/co/rsk/peg/ErpRedeemScriptBuilder.java
@@ -0,0 +1,14 @@
+package co.rsk.peg;
+
+import co.rsk.bitcoinj.core.BtcECKey;
+import co.rsk.bitcoinj.script.Script;
+
+import java.util.List;
+
+public interface ErpRedeemScriptBuilder {
+    long MAX_CSV_VALUE = 65535L;
+
+    Script createRedeemScript(List<BtcECKey> defaultPublicKeys,
+                                            List<BtcECKey> emergencyPublicKeys,
+                                            long csvValue);
+}
