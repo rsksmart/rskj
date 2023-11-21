d013bd57d7844154c7a71c5e8066fd8a9f7813d1
Marcos <marcos@iovlabs.org>
2023-11-21 17:47:51 -0300

    Move evaluatePegin method to PegUtils class

--- rskj-core/src/main/java/co/rsk/peg/PegUtils.java
+++ rskj-core/src/main/java/co/rsk/peg/PegUtils.java
@@ -1,5 +1,10 @@
 package co.rsk.peg;
 
+import static co.rsk.peg.pegin.RejectedPeginReason.INVALID_AMOUNT;
+import static co.rsk.peg.pegin.RejectedPeginReason.LEGACY_PEGIN_MULTISIG_SENDER;
+import static co.rsk.peg.pegin.RejectedPeginReason.LEGACY_PEGIN_UNDETERMINED_SENDER;
+import static co.rsk.peg.pegin.RejectedPeginReason.PEGIN_V1_INVALID_PAYLOAD;
+
 import co.rsk.bitcoinj.core.Address;
 import co.rsk.bitcoinj.core.BtcTransaction;
 import co.rsk.bitcoinj.core.Coin;
@@ -10,14 +15,22 @@ import co.rsk.bitcoinj.script.Script;
 import co.rsk.bitcoinj.wallet.Wallet;
 import co.rsk.config.BridgeConstants;
 import co.rsk.peg.bitcoin.BitcoinUtils;
-import org.ethereum.config.blockchain.upgrades.ActivationConfig;
-import org.ethereum.config.blockchain.upgrades.ConsensusRule;
-
+import co.rsk.peg.btcLockSender.BtcLockSender.TxSenderAddressType;
+import co.rsk.peg.pegin.PeginEvaluationResult;
+import co.rsk.peg.pegin.PeginProcessAction;
+import co.rsk.peg.pegin.RejectedPeginReason;
+import co.rsk.peg.pegininstructions.PeginInstructionsException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import org.ethereum.config.blockchain.upgrades.ActivationConfig;
+import org.ethereum.config.blockchain.upgrades.ConsensusRule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PegUtils {
+    private static final Logger logger = LoggerFactory.getLogger(PegUtils.class);
+
     private PegUtils() { }
 
     static PegTxType getTransactionTypeUsingPegoutIndex(
@@ -26,8 +39,8 @@ public class PegUtils {
         Wallet liveFederationsWallet,
         BtcTransaction btcTransaction
     ) {
-        if (!activations.isActive(ConsensusRule.RSKIP379)){
-            throw new IllegalStateException("Can't call this method before RSKIP379 activation.");
+        if (!activations.isActive(ConsensusRule.RSKIP379)) {
+            throw new IllegalStateException("Can't call this method before RSKIP379 activation");
         }
 
         List<TransactionOutput> liveFederationOutputs = btcTransaction.getWalletOutputs(liveFederationsWallet);
@@ -47,14 +60,18 @@ public class PegUtils {
      * If no utxo is sent to the <b>fedWallet </b>or there is at least 1 utxo sent to the <b>fedWallet</b> that is below minimum,
      * this method will return false. It will return true when all utxos sent to the <b>fedWallet</b> are above the minimum pegin value.
      *
-     * @param minimumPegInTxValue
      * @param btcTx
      * @param fedWallet
+     * @param minimumPegInTxValue
      * @param activations
      * @return
      */
-    public static boolean allUTXOsToFedAreAboveMinimumPeginValue(Coin minimumPegInTxValue, BtcTransaction btcTx, Wallet fedWallet, ActivationConfig.ForBlock activations) {
-
+    static boolean allUTXOsToFedAreAboveMinimumPeginValue(
+        BtcTransaction btcTx,
+        Wallet fedWallet,
+        Coin minimumPegInTxValue,
+        ActivationConfig.ForBlock activations
+    ) {
         if(!activations.isActive(ConsensusRule.RSKIP379)) {
             return !PegUtilsLegacy.isAnyUTXOAmountBelowMinimum(minimumPegInTxValue, btcTx, fedWallet);
         }
@@ -69,7 +86,7 @@ public class PegUtils {
         );
     }
 
-    public static PegTxType getTransactionType(
+    static PegTxType getTransactionType(
         ActivationConfig.ForBlock activations,
         BridgeStorageProvider provider,
         BridgeConstants bridgeConstants,
@@ -78,7 +95,6 @@ public class PegUtils {
         BtcTransaction btcTransaction,
         long btcTransactionHeight
     ) {
-
         List<Federation> liveFeds = new ArrayList<>();
         liveFeds.add(activeFederation);
         if (retiringFederation != null){
@@ -116,6 +132,74 @@ public class PegUtils {
                 minimumPeginTxValue,
                 liveFederationsWallet
             );
+        }
+    }
+
+    static PeginEvaluationResult evaluatePegin(
+        BtcTransaction btcTx,
+        PeginInformation peginInformation,
+        Coin minimumPeginTxValue,
+        Wallet fedWallet,
+        ActivationConfig.ForBlock activations
+    ) {
+        if(!activations.isActive(ConsensusRule.RSKIP379)) {
+            throw new IllegalStateException("Can't call this method before RSKIP379 activation");
+        }
+
+        if(!allUTXOsToFedAreAboveMinimumPeginValue(btcTx, fedWallet, minimumPeginTxValue, activations)) {
+            logger.debug("[evaluatePegin] Peg-in contains at least one utxo below the minimum value");
+            return new PeginEvaluationResult(PeginProcessAction.CANNOT_BE_PROCESSED, INVALID_AMOUNT);
+        }
+
+        try {
+            peginInformation.parse(btcTx);
+        } catch (PeginInstructionsException e) {
+            logger.warn("[evaluatePegin] Error while trying to parse peg-in information for tx {}. {}",
+                btcTx.getHash(),
+                e.getMessage()
+            );
+            return getPeginEvaluationResultForInvalidPegin(peginInformation);
+        }
+
+        int protocolVersion = peginInformation.getProtocolVersion();
+        switch (protocolVersion) {
+            case 0:
+                return evaluateLegacyPeginSender(peginInformation.getSenderBtcAddressType());
+            case 1:
+                return new PeginEvaluationResult(PeginProcessAction.CAN_BE_REGISTERED);
+            default:
+                // This flow should never be reached.
+                String message = String.format("Invalid state. Unexpected pegin protocol %d", protocolVersion);
+                logger.error("[evaluatePegin] {}", message);
+                throw new IllegalStateException(message);
+        }
+    }
+
+    private static PeginEvaluationResult getPeginEvaluationResultForInvalidPegin(PeginInformation peginInformation) {
+        boolean hasRefundAddress = peginInformation.getBtcRefundAddress() != null;
+        int protocolVersion = peginInformation.getProtocolVersion();
+
+        PeginProcessAction peginProcessAction = hasRefundAddress ?
+            PeginProcessAction.CAN_BE_REFUNDED :
+            PeginProcessAction.CANNOT_BE_PROCESSED;
+
+        RejectedPeginReason rejectedPeginReason = protocolVersion == 1 ?
+            PEGIN_V1_INVALID_PAYLOAD :
+            LEGACY_PEGIN_MULTISIG_SENDER;
+
+        return new PeginEvaluationResult(peginProcessAction, rejectedPeginReason);
+    }
+
+    private static PeginEvaluationResult evaluateLegacyPeginSender(TxSenderAddressType senderAddressType)  {
+        switch (senderAddressType) {
+            case P2PKH:
+            case P2SHP2WPKH:
+                return new PeginEvaluationResult(PeginProcessAction.CAN_BE_REGISTERED);
+            case P2SHMULTISIG:
+            case P2SHP2WSH:
+                return new PeginEvaluationResult(PeginProcessAction.CAN_BE_REFUNDED, LEGACY_PEGIN_MULTISIG_SENDER);
+            default:
+                return new PeginEvaluationResult(PeginProcessAction.CANNOT_BE_PROCESSED, LEGACY_PEGIN_UNDETERMINED_SENDER);
         }
     }
 }
