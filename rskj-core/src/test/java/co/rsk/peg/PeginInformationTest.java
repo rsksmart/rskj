053afd89162ff656a35e2207a9601cb145736397
nathanieliov <nathaniel.calderon@iovlabs.org>
2023-11-20 21:08:25 -0400

    Add tests for parsing bech32 tx

--- rskj-core/src/test/java/co/rsk/peg/PeginInformationTest.java
+++ rskj-core/src/test/java/co/rsk/peg/PeginInformationTest.java
@@ -9,6 +9,7 @@ import co.rsk.bitcoinj.core.BtcTransaction;
 import co.rsk.bitcoinj.core.NetworkParameters;
 import co.rsk.config.BridgeConstants;
 import co.rsk.config.BridgeRegTestConstants;
+import co.rsk.config.BridgeTestNetConstants;
 import co.rsk.core.RskAddress;
 import co.rsk.peg.btcLockSender.BtcLockSender;
 import co.rsk.peg.btcLockSender.BtcLockSender.TxSenderAddressType;
@@ -18,7 +19,10 @@ import co.rsk.peg.pegininstructions.PeginInstructionsException;
 import co.rsk.peg.pegininstructions.PeginInstructionsProvider;
 import co.rsk.peg.pegininstructions.PeginInstructionsVersion1;
 import java.util.Optional;
+
+import org.bouncycastle.util.encoders.Hex;
 import org.ethereum.config.blockchain.upgrades.ActivationConfig;
+import org.ethereum.config.blockchain.upgrades.ActivationConfigsForTest;
 import org.ethereum.config.blockchain.upgrades.ConsensusRule;
 import org.ethereum.crypto.ECKey;
 import org.junit.jupiter.api.Assertions;
@@ -417,6 +421,28 @@ class PeginInformationTest {
             activationsMock
         );
 
+        Assertions.assertThrows(PeginInstructionsException.class, () -> peginInformation.parse(btcTx));
+    }
+
+    @Test
+    void parse_bech32_withPeginInstructions_fingerroot() {
+        // Arrange
+        ActivationConfig.ForBlock activations = ActivationConfigsForTest.fingerroot500().forBlock(0);
+
+        String rawTx = "02000000000101cf8b3b2baa22df50b1959d83b2f279ef231fb7cf2009ebfa35644d9e1f0184930200000000fdffffff02706408000000000017a9146e4b5ae85d86e4db0e6e5db09f8c276328cdbf3f87ec5cd40500000000160014d7aa00421cd50c8f282dc7d32992a5e2932a92f3024730440220313d11b58bd2861e4a2f8b2d1b644250569e35c946fcba426de94ed28974f12102207b9074796eea9f823a2d56239f49674c4ec34d40f519b3babd5eba44f94495eb012102acbad9efed3a451f646b93b5fd37a796c1758875cc33eed1626d4ed673d00a9b5abd2600";
+        BtcTransaction btcTx = new BtcTransaction(BridgeTestNetConstants.getInstance().getBtcParams(), Hex.decode(rawTx));
+
+        BtcLockSenderProvider btcLockSenderProviderMock = new BtcLockSenderProvider();
+        PeginInstructionsProvider peginInstructionsProvider = new PeginInstructionsProvider();
+
+        // Act
+        PeginInformation peginInformation = new PeginInformation(
+            btcLockSenderProviderMock,
+            peginInstructionsProvider,
+            activations
+        );
+
+        // assert
         Assertions.assertThrows(PeginInstructionsException.class, () -> peginInformation.parse(btcTx));
     }
 }
