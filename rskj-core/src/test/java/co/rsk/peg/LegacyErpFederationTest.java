5dc82e94fbe34a382ef420d95511b0b6a450866e
nathanieliov <nathaniel.calderon@iovlabs.org>
2024-01-30 11:00:50 -0400

    - Change findFirst to findAny on getMemberByBtcPublicKey method - Improved naming convention for the unit tests added

--- rskj-core/src/test/java/co/rsk/peg/LegacyErpFederationTest.java
+++ rskj-core/src/test/java/co/rsk/peg/LegacyErpFederationTest.java
@@ -698,7 +698,7 @@ class LegacyErpFederationTest {
     }
 
     @Test
-    void test_getMemberByBtcPublicKey_passing_existing_btcPublicKey(){
+    void getMemberByBtcPublicKey_passing_existing_btcPublicKey_should_return_found_member(){
         BtcECKey existingMemberBtcPublicKey = standardKeys.get(0);
         Optional<FederationMember> foundMember = federation.getMemberByBtcPublicKey(existingMemberBtcPublicKey);
         Assertions.assertTrue(foundMember.isPresent());
@@ -706,14 +706,14 @@ class LegacyErpFederationTest {
     }
 
     @Test
-    void test_getMemberByBtcPublicKey_passing_non_existing_btcPublicKey(){
+    void getMemberByBtcPublicKey_passing_non_existing_btcPublicKey_should_return_empty(){
         BtcECKey noExistingBtcPublicKey = new BtcECKey();
         Optional<FederationMember> foundMember = federation.getMemberByBtcPublicKey(noExistingBtcPublicKey);
         Assertions.assertFalse(foundMember.isPresent());
     }
 
     @Test
-    void test_getMemberByBtcPublicKey_passing_null_btcPublicKey(){
+    void getMemberByBtcPublicKey_passing_null_btcPublicKey_should_return_empty(){
         Optional<FederationMember> foundMember = federation.getMemberByBtcPublicKey(null);
         Assertions.assertFalse(foundMember.isPresent());
     }
