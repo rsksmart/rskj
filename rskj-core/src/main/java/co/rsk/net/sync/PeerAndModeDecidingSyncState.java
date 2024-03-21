4abf4d0d10079e9704971e7c5a5cdb6f0e00bd5e
fmacleal <frederico.macielleal@iovlabs.org>
2024-03-21 14:40:04 +0100

    Removing unused import

--- rskj-core/src/main/java/co/rsk/net/sync/PeerAndModeDecidingSyncState.java
+++ rskj-core/src/main/java/co/rsk/net/sync/PeerAndModeDecidingSyncState.java
@@ -24,7 +24,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
-import java.util.List;
 import java.util.Optional;
 
 public class PeerAndModeDecidingSyncState extends BaseSyncState {
