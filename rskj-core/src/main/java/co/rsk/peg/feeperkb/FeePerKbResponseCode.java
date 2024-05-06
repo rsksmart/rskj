46c0153d1f1907b32d85039e070020f3cee5a6a4
julia-zack <julia.zack@rootstocklabs.com>
2024-05-06 11:33:23 -0300

    Rollback response codes change

--- rskj-core/src/main/java/co/rsk/peg/feeperkb/FeePerKbResponseCode.java
+++ rskj-core/src/main/java/co/rsk/peg/feeperkb/FeePerKbResponseCode.java
@@ -4,8 +4,8 @@ public enum FeePerKbResponseCode {
     SUCCESSFUL(1),
     UNSUCCESSFUL(-1),
     EXCESSIVE(-2),
-    NEGATIVE(-3),
-    UNAUTHORIZED(-4),
+    NEGATIVE(-1),
+    UNAUTHORIZED(-10),
     GENERIC(-10);
 
     private final int code;
