d61953f66b543af25cd340e1cb59a6ea4985fa5b
fmacleal <frederico.macielleal@iovlabs.org>
2024-05-09 18:56:33 +0200

    Fixing the failing test, due a deletion of resources

--- rskj-core/src/integrationTest/java/co/rsk/util/cli/RskjNodeCommandLineTest.java
+++ rskj-core/src/integrationTest/java/co/rsk/util/cli/RskjNodeCommandLineTest.java
@@ -1,5 +1,6 @@
 package co.rsk.util.cli;
 
+import co.rsk.util.FilesHelper;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -11,7 +12,7 @@ class RskjNodeCommandLineTest {
     @Test
     void whenExecuteCommandConnectBlocks_thenOutputWithSuccess() throws IOException, InterruptedException {
         //given
-        String filePath = getIntegrationTestResourcesFullPath() + "server_blocks.csv";
+        String filePath = FilesHelper.getAbsolutPathFromResourceFile(getClass(), "server_blocks.csv");
         //when
         RskjCommandLineBase rskjCommandLineBase = new ConnectBlocksCommandLine(filePath);
         Process cliProcess = rskjCommandLineBase.executeCommand();
@@ -19,10 +20,5 @@ class RskjNodeCommandLineTest {
         //then
         assertTrue(output.contains("ConnectBlocks finished"));
         cliProcess.destroyForcibly();
-    }
-
-    private static String getIntegrationTestResourcesFullPath() {
-        String projectPath = System.getProperty("user.dir");
-        return String.format("%s/src/integrationTest/resources/", projectPath);
     }
 }
