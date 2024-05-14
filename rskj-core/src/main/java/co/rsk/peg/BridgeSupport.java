2ea2e36f1d5acd225ddfe87f5485672312c35302
julia-zack <julia.zack@rootstocklabs.com>
2024-05-14 13:39:32 -0300

    Add missing call to getCode

--- rskj-core/src/main/java/co/rsk/peg/BridgeSupport.java
+++ rskj-core/src/main/java/co/rsk/peg/BridgeSupport.java
@@ -2335,7 +2335,7 @@ public class BridgeSupport {
                 break;
             default:
                 // Fail by default
-                result = new ABICallVoteResult(false, FederationChangeResponseCode.GENERIC_ERROR);
+                result = new ABICallVoteResult(false, FederationChangeResponseCode.GENERIC_ERROR.getCode());
         }
 
         return result;
