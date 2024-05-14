c0e537a58da03a50db048acff7d607b7cbb2d2d5
nathanieliov <nathaniel.calderon@iovlabs.org>
2024-05-14 01:33:11 -0400

    Added lowell700 hardfork

--- rskj-core/src/test/java/org/ethereum/config/blockchain/upgrades/ActivationConfigsForTest.java
+++ rskj-core/src/test/java/org/ethereum/config/blockchain/upgrades/ActivationConfigsForTest.java
@@ -191,6 +191,11 @@ public class ActivationConfigsForTest {
         return rskips;
     }
 
+    private static List<ConsensusRule> getLovell700Rskips() {
+        List<ConsensusRule> rskips = new ArrayList<>();
+        return rskips;
+    }
+
     public static ActivationConfig genesis() {
         return only();
     }
@@ -332,6 +337,30 @@ public class ActivationConfigsForTest {
 
         return enableTheseDisableThose(rskips, except);
     }
+
+    public static ActivationConfig lovell700(List<ConsensusRule> except) {
+        List<ConsensusRule> rskips = new ArrayList<>();
+        rskips.addAll(getPaidBridgeTxsRskip());
+        rskips.addAll(getOrchidRskips());
+        rskips.addAll(getOrchid060Rskips());
+        rskips.addAll(getWasabi100Rskips());
+        rskips.addAll(getBahamasRskips());
+        rskips.addAll(getTwoToThreeRskips());
+        rskips.addAll(getPapyrus200Rskips());
+        rskips.addAll(getIris300Rskips());
+        rskips.addAll(getHop400Rskips());
+        rskips.addAll(getHop401Rskips());
+        rskips.addAll(getFingerroot500Rskips());
+        rskips.addAll(getArrowhead600Rskips());
+        rskips.addAll(getLovell700Rskips());
+
+        return enableTheseDisableThose(rskips, except);
+    }
+
+    public static ActivationConfig lovell700() {
+        return lovell700(Collections.emptyList());
+    }
+
 
     public static ActivationConfig regtest() {
         return REGTEST;
