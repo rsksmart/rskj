681193814f8a215f8d1dbb150ebc0b92b12f16c5
nathanieliov <nathaniel.calderon@iovlabs.org>
2024-05-16 08:53:44 -0400

    Apply format to all the changed files.

--- rskj-core/src/main/java/co/rsk/peg/bitcoin/UtxoUtils.java
+++ rskj-core/src/main/java/co/rsk/peg/bitcoin/UtxoUtils.java
@@ -9,27 +9,30 @@ import java.util.List;
 import org.spongycastle.util.encoders.Hex;
 
 public class UtxoUtils {
-    private UtxoUtils() {}
+
+    private UtxoUtils() {
+    }
 
     /**
      * Decode a {@code byte[]} of encoded outpoint values.
      *
      * @param encodedOutpointValues
-     * @return {@code List<Coin>} the list of outpoint values decoded in the same ordered they were before encoding.
-     * Or an {@code Collections.EMPTY_LIST} when {@code encodedOutpointValues} is {@code null} or {@code empty byte[]}.
+     * @return {@code List<Coin>} the list of outpoint values decoded in the same ordered they were
+     * before encoding. Or an {@code Collections.EMPTY_LIST} when {@code encodedOutpointValues} is
+     * {@code null} or {@code empty byte[]}.
      */
-    public static List<Coin> decodeOutpointValues(byte[] encodedOutpointValues){
+    public static List<Coin> decodeOutpointValues(byte[] encodedOutpointValues) {
         if (encodedOutpointValues == null || encodedOutpointValues.length == 0) {
             return Collections.emptyList();
         }
         int offset = 0;
         List<Coin> outpointValues = new ArrayList<>();
 
-        while (encodedOutpointValues.length > offset){
+        while (encodedOutpointValues.length > offset) {
             VarInt valueAsVarInt;
             try {
                 valueAsVarInt = new VarInt(encodedOutpointValues, offset);
-            } catch (Exception ex){
+            } catch (Exception ex) {
                 throw new InvalidOutpointValueException(
                     String.format("Invalid value with invalid VarInt format: %s",
                         Hex.toHexString(encodedOutpointValues).toUpperCase()
@@ -52,8 +55,9 @@ public class UtxoUtils {
      * Encode a {@code List<Coin} of outpoint values.
      *
      * @param outpointValues
-     * @return {@code byte[]} the list of outpoint values encoded preserving the order of the entries.
-     * Or an {@code empty byte[]} when {@code outpointValues} is {@code null} or {@code empty}.
+     * @return {@code byte[]} the list of outpoint values encoded preserving the order of the
+     * entries. Or an {@code empty byte[]} when {@code outpointValues} is {@code null} or
+     * {@code empty}.
      */
     public static byte[] encodeOutpointValues(List<Coin> outpointValues) {
         if (outpointValues == null || outpointValues.isEmpty()) {
@@ -66,12 +70,13 @@ public class UtxoUtils {
             VarInt varIntOutpointValue = new VarInt(outpointValue.getValue());
             encodedOutpointValues.add(varIntOutpointValue.encode());
         }
-        return Bytes.concat(encodedOutpointValues.toArray(new byte[][]{{}}));
+        return Bytes.concat(encodedOutpointValues.toArray(new byte[][]{}));
     }
 
     private static void assertValidOutpointValue(Coin outpointValue) {
-        if (outpointValue == null || outpointValue.isNegative()){
-            throw new InvalidOutpointValueException(String.format("Invalid outpoint value: %s. Negative and null values are not allowed.",
+        if (outpointValue == null || outpointValue.isNegative()) {
+            throw new InvalidOutpointValueException(String.format(
+                "Invalid outpoint value: %s. Negative and null values are not allowed.",
                 outpointValue));
         }
     }
