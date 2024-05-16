101ce38c6be89ebaea0185c33585b3a5dc43f706
julia-zack <julia.zack@rootstocklabs.com>
2024-05-16 13:45:09 -0300

    Make buildFederation method receive FederationConstants instead of BridgeConstants

--- rskj-core/src/main/java/co/rsk/peg/BridgeSupport.java
+++ rskj-core/src/main/java/co/rsk/peg/BridgeSupport.java
@@ -2198,7 +2198,7 @@ public class BridgeSupport {
             currentPendingFederation.buildFederation(
                 creationTime,
                 rskExecutionBlock.getNumber(),
-                bridgeConstants,
+                bridgeConstants.getFederationConstants(),
                 activations
             )
         );
