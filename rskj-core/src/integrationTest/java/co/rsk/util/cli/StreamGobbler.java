1ba81719b3b558fb710b034652bf2366a4ae43c3
fmacleal <frederico.macielleal@iovlabs.org>
2024-05-17 15:13:50 +0200

    Adding some classes to help with Integration Tests
    - Some refactors made on the Integration Tests classes
    - Some classes to easily spin up an rskj node on the integration test,
    with possibility to check the output from it
    - Adding README.md to the integrationTest module, in order to be clear
    with examples and how to use the integration tests classes created with
    examples.
    
    Apply suggestions from code review
    
    Applying suggestions from review.
    
    Co-authored-by: Vovchyk <volodymyr.kravets@outlook.com>

---
+++ rskj-core/src/integrationTest/java/co/rsk/util/cli/StreamGobbler.java
@@ -0,0 +1,40 @@
+/*
+ * This file is part of RskJ
+ * Copyright (C) 2024 RSK Labs Ltd.
+ * (derived from ethereumJ library, Copyright (c) 2016 <ether.camp>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package co.rsk.util.cli;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.function.Consumer;
+
+public class StreamGobbler implements Runnable {
+    private final InputStream inputStream;
+    private final Consumer<String> consumer;
+
+    public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
+        this.inputStream = inputStream;
+        this.consumer = consumer;
+    }
+
+    @Override
+    public void run() {
+        new BufferedReader(new InputStreamReader(inputStream)).lines()
+                .forEach(consumer);
+    }
+}
