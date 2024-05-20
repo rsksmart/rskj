890b5866aa2af0f172d21f99a49751a400204e97
julia-zack <julia.zack@iovlabs.org>
2024-05-20 12:32:01 -0300

    Remove unnecessary thrown exception declaration from saveToRepository method

--- rskj-core/src/main/java/co/rsk/peg/storage/StorageAccessor.java
+++ rskj-core/src/main/java/co/rsk/peg/storage/StorageAccessor.java
@@ -9,7 +9,7 @@ public interface StorageAccessor {
 
     <T> void safeSaveToRepository(DataWord addressKey, T object, RepositorySerializer<T> serializer);
 
-    void saveToRepository(DataWord addressKey, byte[] data) throws IOException;
+    void saveToRepository(DataWord addressKey, byte[] data);
 
     interface RepositoryDeserializer<T> {
         T deserialize(byte[] data) throws IOException;
