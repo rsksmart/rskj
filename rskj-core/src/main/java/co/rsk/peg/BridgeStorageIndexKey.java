2ad1ff7d55a8309ab25069176908847f76e365a8
julia-zack <julia.zack@iovlabs.org>
2024-05-20 16:50:58 -0300

    Create FederationStorageIndexKey enum to have federation-related keys from BridgeStorageIndexKey

--- rskj-core/src/main/java/co/rsk/peg/BridgeStorageIndexKey.java
+++ rskj-core/src/main/java/co/rsk/peg/BridgeStorageIndexKey.java
@@ -3,32 +3,18 @@ package co.rsk.peg;
 import org.ethereum.vm.DataWord;
 
 public enum BridgeStorageIndexKey {
-    NEW_FEDERATION_BTC_UTXOS_KEY("newFederationBtcUTXOs"),
-    NEW_FEDERATION_BTC_UTXOS_KEY_FOR_TESTNET_PRE_HOP("newFederationBtcUTXOsForTestnet"),
-    NEW_FEDERATION_BTC_UTXOS_KEY_FOR_TESTNET_POST_HOP("newFedBtcUTXOsForTestnetPostHop"),
-    OLD_FEDERATION_BTC_UTXOS_KEY("oldFederationBtcUTXOs"),
+
     BTC_TX_HASHES_ALREADY_PROCESSED_KEY("btcTxHashesAP"),
     RELEASE_REQUEST_QUEUE("releaseRequestQueue"),
     PEGOUTS_WAITING_FOR_CONFIRMATIONS("releaseTransactionSet"),
     PEGOUTS_WAITING_FOR_SIGNATURES("rskTxsWaitingFS"),
-    NEW_FEDERATION_KEY("newFederation"),
-    OLD_FEDERATION_KEY("oldFederation"),
-    PENDING_FEDERATION_KEY("pendingFederation"),
-    FEDERATION_ELECTION_KEY("federationElection"),
     LOCK_ONE_OFF_WHITELIST_KEY("lockWhitelist"),
     LOCK_UNLIMITED_WHITELIST_KEY("unlimitedLockWhitelist"),
     LOCKING_CAP_KEY("lockingCap"),
     RELEASE_REQUEST_QUEUE_WITH_TXHASH("releaseRequestQueueWithTxHash"),
     PEGOUTS_WAITING_FOR_CONFIRMATIONS_WITH_TXHASH_KEY("releaseTransactionSetWithTxHash"),
     RECEIVE_HEADERS_TIMESTAMP("receiveHeadersLastTimestamp"),
-    // Federation creation keys
-    ACTIVE_FEDERATION_CREATION_BLOCK_HEIGHT_KEY("activeFedCreationBlockHeight"),
-    NEXT_FEDERATION_CREATION_BLOCK_HEIGHT_KEY("nextFedCreationBlockHeight"),
-    LAST_RETIRED_FEDERATION_P2SH_SCRIPT_KEY("lastRetiredFedP2SHScript"),
     // Version keys and versions
-    NEW_FEDERATION_FORMAT_VERSION("newFederationFormatVersion"),
-    OLD_FEDERATION_FORMAT_VERSION("oldFederationFormatVersion"),
-    PENDING_FEDERATION_FORMAT_VERSION("pendingFederationFormatVersion"),
     NEXT_PEGOUT_HEIGHT_KEY("nextPegoutHeight"),
 
     // Compound keys
