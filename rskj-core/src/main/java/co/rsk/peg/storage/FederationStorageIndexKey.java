f8ff8376f6c5d4a1f9ad527ba86941d754c528ca
julia-zack <julia.zack@iovlabs.org>
2024-05-20 16:55:45 -0300

    Add blank lines to improve readability

--- rskj-core/src/main/java/co/rsk/peg/storage/FederationStorageIndexKey.java
+++ rskj-core/src/main/java/co/rsk/peg/storage/FederationStorageIndexKey.java
@@ -8,19 +8,22 @@ public enum FederationStorageIndexKey {
     NEW_FEDERATION_BTC_UTXOS_KEY_FOR_TESTNET_PRE_HOP("newFederationBtcUTXOsForTestnet"),
     NEW_FEDERATION_BTC_UTXOS_KEY_FOR_TESTNET_POST_HOP("newFedBtcUTXOsForTestnetPostHop"),
     OLD_FEDERATION_BTC_UTXOS_KEY("oldFederationBtcUTXOs"),
+
     NEW_FEDERATION_KEY("newFederation"),
     OLD_FEDERATION_KEY("oldFederation"),
     PENDING_FEDERATION_KEY("pendingFederation"),
+
     FEDERATION_ELECTION_KEY("federationElection"),
-    // Federation creation keys
+
     ACTIVE_FEDERATION_CREATION_BLOCK_HEIGHT_KEY("activeFedCreationBlockHeight"),
     NEXT_FEDERATION_CREATION_BLOCK_HEIGHT_KEY("nextFedCreationBlockHeight"),
+
     LAST_RETIRED_FEDERATION_P2SH_SCRIPT_KEY("lastRetiredFedP2SHScript"),
+
     // Format version keys
     NEW_FEDERATION_FORMAT_VERSION("newFederationFormatVersion"),
     OLD_FEDERATION_FORMAT_VERSION("oldFederationFormatVersion"),
-    PENDING_FEDERATION_FORMAT_VERSION("pendingFederationFormatVersion"),
-
+    PENDING_FEDERATION_FORMAT_VERSION("pendingFederationFormatVersion")
     ;
 
     private final String key;
