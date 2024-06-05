b968efdc2860a7f390f2d011b28bd7705e7cca96
julia-zack <julia.zack@iovlabs.org>
2024-06-05 14:37:46 -0300

    Remove unused constructors in SerializationException

--- rskj-core/src/main/java/co/rsk/peg/SerializationException.java
+++ rskj-core/src/main/java/co/rsk/peg/SerializationException.java
@@ -2,14 +2,6 @@ package co.rsk.peg;
 
 
 public class SerializationException extends RuntimeException {
-    public SerializationException() {
-        super();
-    }
-
-    public SerializationException(String message) {
-        super(message);
-    }
-
     public SerializationException(String message, Throwable cause) {
         super(message, cause);
     }
