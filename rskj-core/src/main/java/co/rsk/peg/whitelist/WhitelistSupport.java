915a16ce4f2a49ba4e86c1795585cd9e8438a4e4
Wilmer Rondon <wilmer.rondonbriceno@iovlabs.org>
2024-06-06 11:30:08 -0400

    Changing Variable Types On Return Value
    - Replacing Integer Wrapper Class With int primitive data type to avoid null values.

--- rskj-core/src/main/java/co/rsk/peg/whitelist/WhitelistSupport.java
+++ rskj-core/src/main/java/co/rsk/peg/whitelist/WhitelistSupport.java
@@ -4,21 +4,22 @@ import java.math.BigInteger;
 import org.ethereum.core.Transaction;
 
 /**
- * Interface for the whitelist support. This interface is used to add, remove and get whitelist entries.
+ * Interface for the whitelist support. This interface is used to add, remove and get whitelist
+ * entries.
  */
 public interface WhitelistSupport {
 
-    Integer getLockWhitelistSize();
+    int getLockWhitelistSize();
 
     LockWhitelistEntry getLockWhitelistEntryByIndex(int index);
 
     LockWhitelistEntry getLockWhitelistEntryByAddress(String addressBase58);
 
-    Integer addOneOffLockWhitelistAddress(Transaction tx, String addressBase58, BigInteger maxTransferValue);
+    int addOneOffLockWhitelistAddress(Transaction tx, String addressBase58, BigInteger maxTransferValue);
 
-    Integer addUnlimitedLockWhitelistAddress(Transaction tx, String addressBase58);
+    int addUnlimitedLockWhitelistAddress(Transaction tx, String addressBase58);
 
-    Integer removeLockWhitelistAddress(Transaction tx, String addressBase58);
+    int removeLockWhitelistAddress(Transaction tx, String addressBase58);
 
-    Integer setLockWhitelistDisableBlockDelay(Transaction tx, BigInteger disableBlockDelayBI);
+    int setLockWhitelistDisableBlockDelay(Transaction tx, BigInteger disableBlockDelayBI);
 }
