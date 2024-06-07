98577297850df429179f0270276a6307cb78c9c5
Wilmer Rondon <wilmer.rondonbriceno@iovlabs.org>
2024-06-07 17:28:09 -0400

    Whitelist Support Implementation
    Create the implementation of the interface called WhitelistSupport, and put all the method Whitelist-related on it. Take these methods from BridgeSupport. The plan is to remove all of them from BridgeSupport into their own WhitelistSupport class. This implementation class contains all the business logic related to Whitelist.
    
    JavaDoc for WhitelistSupportImpl.
    
    - Adding JavaDoc to document WhitelistSupportImpl.
    
    Addressing Review Comments
    
    - Moved verifyLockSenderIsWhitelisted method from BridgeSupport to WhitelistSupport.
    - UNAUTHORIZED_CALLER constant added to WhitelistResponseCode.
    - Replaced GENERIC_ERROR with UNAUTHORIZED_CALLER.
    - Removed PanicProcessor.
    - added some custom indentation to WhitelistSupportImpl constructor.
    - Removed whitelist prefix to variables.
    - isAuthorizedLockWhitelistChange changed name to isNotAuthorizedLockWhitelistChange. This is a IntelliJ suggestion to make more readable the code.
    -Added logger for AddressFormatException in removeLockWhitelistAddress method.
    
    isAuthorizedLockWhitelistChange method refactoring
    
    - Reverted improvement since at this stage it is not needed. The improvement was made based on avoiding negation statements into conditionals to make the code more readable.
    - Refactor reverted to leave it like it was before refactoring.
    
    Addressing Review Comments
    
    - Moving method JavaDocs from WhitelistSupportImpl implementation class to WhitelistSupport interface.
    - Removing Class JavaDoc on WhitelistSupportImpl implementation class since it doesn't add much value.
    - Improving logger messages to align to standard.
    - Removing "this" keyword since in these cases it doesn't add much value.
    - Removing generic Exception, IOException, BlockStoreException. They are since they are not thrown in any place.
    - Removing comments in verifyLockSenderIsWhitelisted. These comments are out of place.
    - Removed UNKNOWN_ERROR in WhitelistResponseCode.
    
    Moving addLockWhitelistAddress Method
    
    - To improve readability, we moved the addLockWhitelistAddress private method, right next to the public methods where it is being used.
    
    Refactoring Method Name
    
    Update WhitelistSupport javadocs
    
    Replace btcContext param for NetworkParameters in WhitelistSupportImpl
    
    Remove javadoc from whitelist related methods in BridgeSupport
    
    Remove unused field from BridgeDevNetConstants

---
+++ rskj-core/src/main/java/co/rsk/peg/whitelist/WhitelistSupportImpl.java
@@ -0,0 +1,161 @@
+package co.rsk.peg.whitelist;
+
+import static co.rsk.peg.whitelist.WhitelistResponseCode.*;
+
+import co.rsk.bitcoinj.core.*;
+import co.rsk.peg.vote.AddressBasedAuthorizer;
+import co.rsk.peg.whitelist.constants.WhitelistConstants;
+import java.math.BigInteger;
+import java.util.List;
+import org.ethereum.core.SignatureCache;
+import org.ethereum.core.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WhitelistSupportImpl implements WhitelistSupport {
+
+    private static final Logger logger = LoggerFactory.getLogger(WhitelistSupportImpl.class);
+    private static final String INVALID_ADDRESS_FORMAT_MESSAGE = "invalid address format";
+    private final WhitelistStorageProvider whitelistStorageProvider;
+    private final WhitelistConstants whitelistConstants;
+    private final SignatureCache signatureCache;
+    private final NetworkParameters networkParameters;
+
+    public WhitelistSupportImpl(
+        WhitelistStorageProvider whitelistStorageProvider,
+        WhitelistConstants whitelistConstants,
+        SignatureCache signatureCache,
+        NetworkParameters networkParameters) {
+        this.whitelistStorageProvider = whitelistStorageProvider;
+        this.whitelistConstants = whitelistConstants;
+        this.signatureCache = signatureCache;
+        this.networkParameters = networkParameters;
+    }
+
+    @Override
+    public int getLockWhitelistSize() {
+        return whitelistStorageProvider.getLockWhitelist().getSize();
+    }
+
+    @Override
+    public LockWhitelistEntry getLockWhitelistEntryByIndex(int index) {
+        List<LockWhitelistEntry> entries = whitelistStorageProvider.getLockWhitelist().getAll();
+
+        if (index < 0 || index >= entries.size()) {
+            return null;
+        }
+        return entries.get(index);
+    }
+
+    @Override
+    public LockWhitelistEntry getLockWhitelistEntryByAddress(String addressBase58) {
+        try {
+            Address address = Address.fromBase58(networkParameters, addressBase58);
+
+            return whitelistStorageProvider.getLockWhitelist().get(address);
+        } catch (AddressFormatException e) {
+            logger.warn("[getLockWhitelistEntryByAddress] {}", INVALID_ADDRESS_FORMAT_MESSAGE, e);
+            return null;
+        }
+    }
+
+    @Override
+    public int addOneOffLockWhitelistAddress(Transaction tx, String addressBase58, BigInteger maxTransferValue) {
+        try {
+            Address address = Address.fromBase58(networkParameters, addressBase58);
+            Coin maxTransferValueCoin = Coin.valueOf(maxTransferValue.longValueExact());
+            LockWhitelistEntry entry = new OneOffWhiteListEntry(address, maxTransferValueCoin);
+
+            return addLockWhitelistAddress(tx, entry);
+        } catch (AddressFormatException e) {
+            logger.warn("[addOneOffLockWhitelistAddress] {}", INVALID_ADDRESS_FORMAT_MESSAGE, e);
+            return INVALID_ADDRESS_FORMAT.getCode();
+        }
+    }
+
+    @Override
+    public int addUnlimitedLockWhitelistAddress(Transaction tx, String addressBase58) {
+        try {
+            Address address = Address.fromBase58(networkParameters, addressBase58);
+            LockWhitelistEntry entry = new UnlimitedWhiteListEntry(address);
+
+            return addLockWhitelistAddress(tx, entry);
+        } catch (AddressFormatException e) {
+            logger.warn("[addUnlimitedLockWhitelistAddress] {}", INVALID_ADDRESS_FORMAT_MESSAGE, e);
+            return INVALID_ADDRESS_FORMAT.getCode();
+        }
+    }
+
+    private Integer addLockWhitelistAddress(Transaction tx, LockWhitelistEntry entry) {
+        if (!isLockWhitelistChangeAuthorized(tx)) {
+            return UNAUTHORIZED_CALLER.getCode();
+        }
+
+        LockWhitelist whitelist = whitelistStorageProvider.getLockWhitelist();
+        if (whitelist.isWhitelisted(entry.address())) {
+            return ADDRESS_ALREADY_WHITELISTED.getCode();
+        }
+        whitelist.put(entry.address(), entry);
+        return SUCCESS.getCode();
+    }
+
+    @Override
+    public int removeLockWhitelistAddress(Transaction tx, String addressBase58) {
+        if (!isLockWhitelistChangeAuthorized(tx)) {
+            return UNAUTHORIZED_CALLER.getCode();
+        }
+
+        LockWhitelist whitelist = whitelistStorageProvider.getLockWhitelist();
+        try {
+            Address address = Address.fromBase58(networkParameters, addressBase58);
+            if (!whitelist.remove(address)) {
+                return ADDRESS_NOT_EXIST.getCode();
+            }
+            return SUCCESS.getCode();
+        } catch (AddressFormatException e) {
+            logger.error("[removeLockWhitelistAddress] {}", INVALID_ADDRESS_FORMAT_MESSAGE, e);
+            return INVALID_ADDRESS_FORMAT.getCode();
+        }
+    }
+
+    @Override
+    public int setLockWhitelistDisableBlockDelay(Transaction tx, BigInteger disableBlockDelayBI, int btcBlockchainBestChainHeight) {
+
+        if (!isLockWhitelistChangeAuthorized(tx)) {
+            return UNAUTHORIZED_CALLER.getCode();
+        }
+
+        LockWhitelist lockWhitelist = whitelistStorageProvider.getLockWhitelist();
+        if (lockWhitelist.isDisableBlockSet()) {
+            return DELAY_ALREADY_SET.getCode();
+        }
+
+        int disableBlockDelay = disableBlockDelayBI.intValueExact();
+        if (disableBlockDelay + btcBlockchainBestChainHeight <= btcBlockchainBestChainHeight) {
+            return DISABLE_BLOCK_DELAY_INVALID.getCode();
+        }
+
+        lockWhitelist.setDisableBlockHeight(btcBlockchainBestChainHeight + disableBlockDelay);
+
+        return SUCCESS.getCode();
+    }
+
+    @Override
+    public boolean verifyLockSenderIsWhitelisted(Address senderBtcAddress, Coin totalAmount, int height) {
+        final String ADDRESS_NOT_WHITELISTED_MESSAGE = "Rejected lock. Address is not whitelisted. Address: ";
+        LockWhitelist lockWhitelist = whitelistStorageProvider.getLockWhitelist();
+        if (!lockWhitelist.isWhitelistedFor(senderBtcAddress, totalAmount, height)) {
+            logger.info("[verifyLockSenderIsWhitelisted] {} {}", ADDRESS_NOT_WHITELISTED_MESSAGE, senderBtcAddress);
+            return false;
+        }
+        // Consume this whitelisted address
+        lockWhitelist.consume(senderBtcAddress);
+
+        return true;
+    }
+
+    private boolean isLockWhitelistChangeAuthorized(Transaction tx) {
+        AddressBasedAuthorizer authorizer = whitelistConstants.getLockWhitelistChangeAuthorizer();
+        return authorizer.isAuthorized(tx, signatureCache);
+    }
+}
