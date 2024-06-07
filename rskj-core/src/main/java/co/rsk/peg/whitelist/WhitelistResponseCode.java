53ed4b1c4560a77fbbb0b6080c0a4c9b9d01a188
Wilmer Rondon <wilmer.rondonbriceno@iovlabs.org>
2024-06-07 17:28:09 -0400

    Whitelist Support Implementation
    Create the implementation of the interface called WhitelistSupport, and put all the method Whitelist-related on it. Take these methods from BridgeSupport. The plan is to remove all of them from BridgeSupport into their own WhitelistSupport class. This implementation class contains all the business logic related to Whitelist.
    
    JavaDoc for WhitelistSupportImpl.
    
    - Adding JavaDoc to document WhitelistSupportImpl.
    
    Addressing Review Comments
    
    - Moved verifyLockSenderIsWhitelisted method from BridgeSupport to WhitelistSupport.
    - UNAUTHORIZED_CALLER constant added to WhitelistResponseCode.
    - Replaced GENERIC_ERROR with UNAUTHORIZED_CALLER.
    - Removed PanicProcessor.
    - added some custom indentation to WhitelistSupportImpl constructor.
    - Removed whitelist prefix to variables.
    - isAuthorizedLockWhitelistChange changed name to isNotAuthorizedLockWhitelistChange. This is a IntelliJ suggestion to make more readable the code.
    -Added logger for AddressFormatException in removeLockWhitelistAddress method.
    
    isAuthorizedLockWhitelistChange method refactoring
    
    - Reverted improvement since at this stage it is not needed. The improvement was made based on avoiding negation statements into conditionals to make the code more readable.
    - Refactor reverted to leave it like it was before refactoring.
    
    Addressing Review Comments
    
    - Moving method JavaDocs from WhitelistSupportImpl implementation class to WhitelistSupport interface.
    - Removing Class JavaDoc on WhitelistSupportImpl implementation class since it doesn't add much value.
    - Improving logger messages to align to standard.
    - Removing "this" keyword since in these cases it doesn't add much value.
    - Removing generic Exception, IOException, BlockStoreException. They are since they are not thrown in any place.
    - Removing comments in verifyLockSenderIsWhitelisted. These comments are out of place.
    - Removed UNKNOWN_ERROR in WhitelistResponseCode.
    
    Moving addLockWhitelistAddress Method
    
    - To improve readability, we moved the addLockWhitelistAddress private method, right next to the public methods where it is being used.
    
    Refactoring Method Name

--- rskj-core/src/main/java/co/rsk/peg/whitelist/WhitelistResponseCode.java
+++ rskj-core/src/main/java/co/rsk/peg/whitelist/WhitelistResponseCode.java
@@ -2,9 +2,12 @@ package co.rsk.peg.whitelist;
 
 public enum WhitelistResponseCode {
     GENERIC_ERROR(-10),
+    UNAUTHORIZED_CALLER(-10),
     INVALID_ADDRESS_FORMAT(-2),
+    DISABLE_BLOCK_DELAY_INVALID(-2),
     ADDRESS_ALREADY_WHITELISTED(-1),
-    UNKNOWN_ERROR(0),
+    ADDRESS_NOT_EXIST(-1),
+    DELAY_ALREADY_SET(-1),
     SUCCESS(1);
 
     private final int code;
