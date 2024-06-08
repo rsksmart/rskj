3f20b2f3562240ec639aed2431e81cac4326f320
Wilmer Rondon <wilmer.rondonbriceno@iovlabs.org>
2024-06-07 22:46:02 -0400

    Unit Tests for WhitelistConstants
    Following the Bridge refactors, we already migrated the WhitelistConstants to their classes to break the high coupling Whitelist had with the Bridge objects, however, it’s missing add coverage, so we need to create Unit Tests to get reliability your new code doesn't break existing functionality when a change is made to the application.
    
    Create Test Cases for all Networks such as MainNet, TestNet, and RegTest.
    
    Addressing Review Comments
    
    - Improving WhitelistConstants: test and production code for all Networks.

--- rskj-core/src/main/java/co/rsk/peg/whitelist/constants/WhitelistRegTestConstants.java
+++ rskj-core/src/main/java/co/rsk/peg/whitelist/constants/WhitelistRegTestConstants.java
@@ -1,9 +1,8 @@
 package co.rsk.peg.whitelist.constants;
 
 import co.rsk.peg.vote.AddressBasedAuthorizer;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.bouncycastle.util.encoders.Hex;
 import org.ethereum.crypto.ECKey;
 
@@ -12,9 +11,10 @@ public class WhitelistRegTestConstants extends WhitelistConstants {
     private static final WhitelistRegTestConstants instance = new WhitelistRegTestConstants();
 
     private WhitelistRegTestConstants() {
-        List<ECKey> lockWhitelistAuthorizedKeys = Arrays.stream(new String[]{
-            "04641fb250d7ca7a1cb4f530588e978013038ec4294d084d248869dd54d98873e45c61d00ceeaeeb9e35eab19fa5fbd8f07cb8a5f0ddba26b4d4b18349c09199ad"
-        }).map(hex -> ECKey.fromPublicOnly(Hex.decode(hex))).collect(Collectors.toList());
+        ECKey authorizerPublicKey = ECKey.fromPublicOnly(Hex.decode(
+            "04641fb250d7ca7a1cb4f530588e978013038ec4294d084d248869dd54d98873e45c61d00ceeaeeb9e35eab19fa5fbd8f07cb8a5f0ddba26b4d4b18349c09199ad"));
+        List<ECKey> lockWhitelistAuthorizedKeys = Collections.singletonList(
+            authorizerPublicKey);
 
         lockWhitelistChangeAuthorizer = new AddressBasedAuthorizer(
             lockWhitelistAuthorizedKeys,
