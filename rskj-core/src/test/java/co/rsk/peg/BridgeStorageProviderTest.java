afcad2d5e8250fc77be1688e054b5886532cf98e
julia-zack <julia.zack@iovlabs.org>
2024-06-10 13:55:15 -0300

    Add FederationSupportImpl tests. Refactor to reuse duplicated code
    Remove unused imports
    
    Improvements
    
    Add -removed by mistake- blank line
    
    Remove unused method
    
    Reorder arguments to have expected value at the end. Add blank lines between methods. Add test cases. Add precision to existing test cases
    
    Add test cases
    
    Improve tags and comments. Remove unnecessary variable declarations. Use camel case in source methods
    
    Remove duplicated tag from old test
    
    Remove unrealistic test case. Make test more realistic
    
    Add getActiveFederationSize tests
    
    Add getActiveFederationThreshold tests
    
    Add getActiveFederationCreationTime tests
    
    Add getActiveFederationCreationBlockNumber tests
    
    Renames FederationStorageProvider derived and test classes. Moves getNewFederationBtcUTXOs tests from BridgeStorageProviderTest to FedreationStorageProviderImplTest
    
    Adds parameterized tests
    
    Updated parameterized tests
    
    Simplifies parameterized test
    
    Remove unrealistic test cases. Rename tag to be more accurate
    
    Remove no more needed parameterized tests
    
    Move variables from setup to be global to replace before all for before each
    
    Remove now unused variable
    
    Adds getOldFederationBtcUTXOs tests
    
    Add getActiveFederatorBtcPublicKey test
    
    Remove unrealistic test case
    
    Remove unrealistic test cases
    
    Rename
    
    Reformat long lines
    
    Add getActiveFederatorPublicKeyOfType tests
    
    Remove replaced test and no more needed global setup
    
    Add explanatory comments
    
    Remove no needed federation support re-built
    
    Rename
    
    Add getRskPublicKeys and getMstPublicKeys methods to Federation class. Use them for testing
    
    Remove duplicated semicolons
    
    Remove unnecessary comments
    
    Correct comment
    
    Create new RskTestUtils class to have rsk-related stuff. Move method from BitcoinTestUtils to it
    
    Remove unused import
    
    Move getRskPublicKeys method from Federation class to RskTestUtils since its just being used for testing purposes. Refactor to improve readability
    
    Create getMstPubKeys method in test class. Use new methods in FederationSupportImplTest.
    
    Remove methods from Federation class
    
    Refactor
    
    Refactor
    
    Move public method from RskTestUtils to private one inside FederationSupportImplTest
    
    Rebase against integration branch

--- rskj-core/src/test/java/co/rsk/peg/BridgeStorageProviderTest.java
+++ rskj-core/src/test/java/co/rsk/peg/BridgeStorageProviderTest.java
@@ -2935,46 +2935,6 @@ class BridgeStorageProviderTest {
     }
 
     @Test
-    void getNewFederationBtcUTXOs_before_RSKIP284_before_RSKIP293_testnet() throws IOException {
-        testGetNewFederationBtcUTXOs(false, false, NetworkParameters.ID_TESTNET);
-    }
-
-    @Test
-    void getNewFederationBtcUTXOs_before_RSKIP284_before_RSKIP293_mainnet() throws IOException {
-        testGetNewFederationBtcUTXOs(false, false, NetworkParameters.ID_MAINNET);
-    }
-
-    @Test
-    void getNewFederationBtcUTXOs_before_RSKIP284_after_RSKIP293_testnet() throws IOException {
-        testGetNewFederationBtcUTXOs(false, true, NetworkParameters.ID_TESTNET);
-    }
-
-    @Test
-    void getNewFederationBtcUTXOs_before_RSKIP284_after_RSKIP293_mainnet() throws IOException {
-        testGetNewFederationBtcUTXOs(false, true, NetworkParameters.ID_MAINNET);
-    }
-
-    @Test
-    void getNewFederationBtcUTXOs_after_RSKIP284_before_RSKIP293_testnet() throws IOException {
-        testGetNewFederationBtcUTXOs(true, false, NetworkParameters.ID_TESTNET);
-    }
-
-    @Test
-    void getNewFederationBtcUTXOs_after_RSKIP284_before_RSKIP293_mainnet() throws IOException {
-        testGetNewFederationBtcUTXOs(true, false, NetworkParameters.ID_MAINNET);
-    }
-
-    @Test
-    void getNewFederationBtcUTXOs_after_RSKIP284_after_RSKIP293_testnet() throws IOException {
-        testGetNewFederationBtcUTXOs(true, true, NetworkParameters.ID_TESTNET);
-    }
-
-    @Test
-    void getNewFederationBtcUTXOs_after_RSKIP284_after_RSKIP293_mainnet() throws IOException {
-        testGetNewFederationBtcUTXOs(true, true, NetworkParameters.ID_MAINNET);
-    }
-
-    @Test
     void saveNewFederationBtcUTXOs_no_data() throws IOException {
         ActivationConfig.ForBlock activations = mock(ActivationConfig.ForBlock.class);
 
@@ -3203,68 +3163,6 @@ class BridgeStorageProviderTest {
             Assertions.assertEquals(2, storageBytesCalls.size());
             Assertions.assertEquals(1, serializeCalls.size());
         }
-    }
-
-    private void testGetNewFederationBtcUTXOs(boolean isRskip284Active, boolean isRskip293Active, String networkId) throws IOException {
-        ActivationConfig.ForBlock activations = mock(ActivationConfig.ForBlock.class);
-        when(activations.isActive(ConsensusRule.RSKIP284)).thenReturn(isRskip284Active);
-        when(activations.isActive(ConsensusRule.RSKIP293)).thenReturn(isRskip293Active);
-
-        NetworkParameters networkParameters = NetworkParameters.fromID(networkId);
-
-        Repository repository = mock(Repository.class);
-        List<UTXO> federationUtxos = Arrays.asList(
-            PegTestUtils.createUTXO(1, 0, Coin.COIN),
-            PegTestUtils.createUTXO(2, 2, Coin.COIN.divide(2)),
-            PegTestUtils.createUTXO(3, 0, Coin.COIN.multiply(3))
-        );
-        when(repository.getStorageBytes(
-            bridgeAddress,
-            NEW_FEDERATION_BTC_UTXOS_KEY.getKey()
-        )).thenReturn(BridgeSerializationUtils.serializeUTXOList(federationUtxos));
-
-        List<UTXO> federationUtxosAfterRskip284Activation = Arrays.asList(
-            PegTestUtils.createUTXO(4, 0, Coin.FIFTY_COINS),
-            PegTestUtils.createUTXO(5, 2, Coin.COIN.multiply(2))
-        );
-        when(repository.getStorageBytes(
-            bridgeAddress,
-            NEW_FEDERATION_BTC_UTXOS_KEY_FOR_TESTNET_PRE_HOP.getKey()
-        )).thenReturn(BridgeSerializationUtils.serializeUTXOList(federationUtxosAfterRskip284Activation));
-
-        List<UTXO> federationUtxosAfterRskip293Activation = Arrays.asList(
-            PegTestUtils.createUTXO(6, 1, Coin.valueOf(150_000)),
-            PegTestUtils.createUTXO(7, 3, Coin.COIN.multiply(3))
-        );
-        when(repository.getStorageBytes(
-            bridgeAddress,
-            NEW_FEDERATION_BTC_UTXOS_KEY_FOR_TESTNET_POST_HOP.getKey()
-        )).thenReturn(BridgeSerializationUtils.serializeUTXOList(federationUtxosAfterRskip293Activation));
-
-        FederationStorageProvider federationStorageProvider = createFederationStorageProvider(repository);
-
-        List<UTXO> obtainedUtxos = federationStorageProvider.getNewFederationBtcUTXOs(networkParameters, activations);
-
-        if (!networkId.equals(NetworkParameters.ID_TESTNET)) {
-            Assertions.assertEquals(federationUtxos, obtainedUtxos);
-            return;
-        }
-
-        // testnet
-        // rskip284 & rskip293 are not active
-        if (!isRskip284Active) {
-            Assertions.assertEquals(federationUtxos, obtainedUtxos);
-            return;
-        }
-
-        // rskip284 is active
-        if (!isRskip293Active) {
-            Assertions.assertEquals(federationUtxosAfterRskip284Activation, obtainedUtxos);
-            return;
-        }
-
-        // rskip293 is active
-        Assertions.assertEquals(federationUtxosAfterRskip293Activation, obtainedUtxos);
     }
 
     private void testSaveNewFederationBtcUTXOs(boolean isRskip284Active, String networkId) throws IOException {
