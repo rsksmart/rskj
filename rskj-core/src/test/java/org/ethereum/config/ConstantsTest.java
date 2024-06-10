c34cbc2db90a0542c1cff5716d89b2ea0b979748
julia-zack <julia.zack@iovlabs.org>
2024-06-10 18:00:33 -0300

    Fix broken tests.

--- rskj-core/src/test/java/org/ethereum/config/ConstantsTest.java
+++ rskj-core/src/test/java/org/ethereum/config/ConstantsTest.java
@@ -29,8 +29,9 @@ import co.rsk.peg.federation.Federation;
 import co.rsk.peg.federation.FederationTestUtils;
 import co.rsk.peg.federation.constants.FederationConstants;
 import co.rsk.peg.federation.constants.FederationTestNetConstants;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.bouncycastle.util.encoders.Hex;
 import org.ethereum.config.blockchain.upgrades.ActivationConfig;
 import org.ethereum.config.blockchain.upgrades.ConsensusRule;
@@ -39,12 +40,13 @@ import org.junit.jupiter.api.Test;
 
 class ConstantsTest {
 
-    private static final List<BtcECKey> TEST_FED_KEYS = Arrays.asList(
-        BtcECKey.fromPublicOnly(Hex.decode("039a060badbeb24bee49eb2063f616c0f0f0765d4ca646b20a88ce828f259fcdb9")),
-        BtcECKey.fromPublicOnly(Hex.decode("02afc230c2d355b1a577682b07bc2646041b5d0177af0f98395a46018da699b6da")),
-        BtcECKey.fromPublicOnly(Hex.decode("0344a3c38cd59afcba3edcebe143e025574594b001700dec41e59409bdbd0f2a09")),
-        BtcECKey.fromPublicOnly(Hex.decode("034844a99cd7028aa319476674cc381df006628be71bc5593b8b5fdb32bb42ef85"))
-    );
+    private static final List<BtcECKey> GENESIS_FEDERATION_PUBLIC_KEYS_TESTNET = Stream.of(
+        "039a060badbeb24bee49eb2063f616c0f0f0765d4ca646b20a88ce828f259fcdb9",
+        "02afc230c2d355b1a577682b07bc2646041b5d0177af0f98395a46018da699b6da",
+        "0344a3c38cd59afcba3edcebe143e025574594b001700dec41e59409bdbd0f2a09",
+        "034844a99cd7028aa319476674cc381df006628be71bc5593b8b5fdb32bb42ef85",
+        "032de868a99cf955cc1f9bfe9a869a611ec64707e8ea10709dee52dbfc688626eb" // public key derived from "random" word seed
+    ).map(hex -> BtcECKey.fromPublicOnly(Hex.decode(hex))).collect(Collectors.toList());
 
     private final ActivationConfig activationConfig = mock(ActivationConfig.class);
     private final ActivationConfig.ForBlock preRskip297Config = mock(ActivationConfig.ForBlock.class);
@@ -61,10 +63,11 @@ class ConstantsTest {
     void devnetWithFederationTest() {
         Federation genesisFederation = FederationTestUtils.getGenesisFederation(federationConstants);
 
-        assertThat(genesisFederation.hasBtcPublicKey(TEST_FED_KEYS.get(0)), is(true));
-        assertThat(genesisFederation.hasBtcPublicKey(TEST_FED_KEYS.get(1)), is(true));
-        assertThat(genesisFederation.hasBtcPublicKey(TEST_FED_KEYS.get(2)), is(true));
-        assertThat(genesisFederation.hasBtcPublicKey(TEST_FED_KEYS.get(3)), is(false));
+        assertThat(genesisFederation.hasBtcPublicKey(GENESIS_FEDERATION_PUBLIC_KEYS_TESTNET.get(0)), is(true));
+        assertThat(genesisFederation.hasBtcPublicKey(GENESIS_FEDERATION_PUBLIC_KEYS_TESTNET.get(1)), is(true));
+        assertThat(genesisFederation.hasBtcPublicKey(GENESIS_FEDERATION_PUBLIC_KEYS_TESTNET.get(2)), is(true));
+        assertThat(genesisFederation.hasBtcPublicKey(GENESIS_FEDERATION_PUBLIC_KEYS_TESTNET.get(3)), is(true));
+        assertThat(genesisFederation.hasBtcPublicKey(GENESIS_FEDERATION_PUBLIC_KEYS_TESTNET.get(4)), is(false));
     }
 
     @Test
