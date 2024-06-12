e299f568c66d2f2d8071c9e70b6739c9d5003b03
Nazaret Garcia <nazaret@iovlabs.org>
2024-06-12 20:59:19 -0300

    Updating test WIP

--- rskj-core/src/integrationTest/java/misc/PteIntegrationTest.java
+++ rskj-core/src/integrationTest/java/misc/PteIntegrationTest.java
@@ -16,13 +16,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import java.util.*;
+import java.util.concurrent.*;
 import java.util.stream.Stream;
 
 public class PteIntegrationTest {
+
+    // THIS TEST REQUIRES A JAR FILE TO BE IN THE CORRECT PLACE BEFORE RUNNING
 
     private static final int RPC_PORT = 9999;
     private final ObjectMapper objectMapper = new ObjectMapper();
@@ -83,6 +83,20 @@ public class PteIntegrationTest {
             0xec4ddeb4380ad69b3e509baad9f158cdf4e4681d
         */
 
+        List<String> accounts = Arrays.asList(
+                "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826",
+                "0x7986b3df570230288501eea3d890bd66948c9b79",
+                "0x0a3aa774752ec2042c46548456c094a76c7f3a79",
+                "0xcf7cdbbb5f7ba79d3ffe74a0bba13fc0295f6036",
+                "0x39b12c05e8503356e3a7df0b7b33efa4c054c409",
+                "0xc354d97642faa06781b76ffb6786f72cd7746c97",
+                "0xdebe71e1de41fc77c44df4b6db940026e31b0e71",
+                "0x7857288e171c6159c5576d1bd9ac40c0c48a771c",
+                "0xa4dea4d5c954f5fd9e87f0e9752911e83a3d18b3",
+                "0x09a1eda29f664ac8f68106f6567276df0c65d859",
+                "0xec4ddeb4380ad69b3e509baad9f158cdf4e4681d"
+        );
+
         Map<String, Response> txResponseMap = new HashMap<>();
 
         String cmd = String.format("%s -cp %s/%s co.rsk.Start --reset %s", baseJavaCmd, buildLibsPath, jarName, strBaseArgs);
@@ -93,6 +107,19 @@ public class PteIntegrationTest {
                 TimeUnit.MINUTES,
                 proc -> {
                     try {
+
+                        // Check balances
+
+                        List<String> balances = new ArrayList<>();
+
+                        for (String account : accounts) {
+                            balances.add(getBalance(account).body().string());
+                        }
+
+                        Assertions.assertEquals(null, balances);
+
+                        // Send bulk transactions
+
                         Response txResponse = sendBulkTransactions(
                                 "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826",
                                 "0x7986b3df570230288501eea3d890bd66948c9b79",
@@ -105,32 +132,21 @@ public class PteIntegrationTest {
                         );
                         txResponseMap.put("txResponse", txResponse);
 
-                        String number = "";
-                        for (int i = 0; i < 5; i++) {
-                            System.out.println(i);
-                            Response response = getBestBlock();
-                            Map<String, Response> blockResponseMap = new HashMap<>();
-                            blockResponseMap.put("latestProcessedBlock", response);
+                        // Await for n blocks to be mined and return them
 
-                            String blockResponseBody = blockResponseMap.get("latestProcessedBlock").body().string();
-                            JsonNode jsonRpcResponse = objectMapper.readTree(blockResponseBody);
-                            // JsonNode transactionsNode = jsonRpcResponse.get(0).get("result").get("transactions");
-                            JsonNode block = jsonRpcResponse.get(0).get("result");
-                            JsonNode txs = block.get("transactions");
+                        Future<Map<Integer, String>> future = getBlocksAsync();
 
-                            if (!number.equals(block.get("number").asText())) {
-                                number = block.get("number").asText();
-                                System.out.println("different");
-                            } else {
-                                System.out.println("same");
-                            }
+                        try {
 
-                            if (txs.size() > 1) {
-                                Assertions.assertEquals("", txs);
-                            }
+                            Map<Integer, String> result = future.get();
 
-                            // Sleep here, or wait for n blocks and then assert
+                            // TODO Check transaction edges on each block
 
+                            Assertions.assertEquals(null, result);
+
+                        } catch (ExecutionException | InterruptedException e) {
+                            e.printStackTrace();
+                            Assertions.fail(e);
                         }
 
                     } catch (IOException e) {
@@ -156,12 +172,12 @@ public class PteIntegrationTest {
         */
     }
 
-    private Response getBestBlock() throws IOException {
+    private Response getBalance(String account) throws IOException {
         String content = "[{\n" +
-                "    \"method\": \"eth_getBlockByNumber\",\n" +
+                "    \"method\": \"eth_getBalance\",\n" +
                 "    \"params\": [\n" +
-                "        \"latest\",\n" +
-                "        true\n" +
+                "        \"" + account + "\",\n" +
+                "        \"latest\"\n" +
                 "    ],\n" +
                 "    \"id\": 1,\n" +
                 "    \"jsonrpc\": \"2.0\"\n" +
@@ -170,10 +186,13 @@ public class PteIntegrationTest {
         return OkHttpClientTestFixture.sendJsonRpcMessage(content, RPC_PORT);
     }
 
-    private Response getAccounts() throws IOException {
+    private Response getBlockByNumber(String number) throws IOException {
         String content = "[{\n" +
-                "    \"method\": \"eth_accounts\",\n" +
-                "    \"params\": [],\n" +
+                "    \"method\": \"eth_getBlockByNumber\",\n" +
+                "    \"params\": [\n" +
+                "        \"" + number + "\",\n" +
+                "        true\n" +
+                "    ],\n" +
                 "    \"id\": 1,\n" +
                 "    \"jsonrpc\": \"2.0\"\n" +
                 "}]";
@@ -239,4 +258,63 @@ public class PteIntegrationTest {
         return OkHttpClientTestFixture.sendJsonRpcMessage(content, RPC_PORT);
     }
 
+    private Future<Map<Integer, String>> getBlocksAsync() {
+        CompletableFuture<Map<Integer, String>> completableFuture = new CompletableFuture<>();
+
+        Executors.newCachedThreadPool().submit(() -> {
+            Map<Integer, String> results = new HashMap<>();
+
+            for (int i = 0; i < 9; i++) {
+                String response = getBlockByNumber("0x" + i).body().string();
+
+                results.put(i, response);
+                Thread.sleep(500);
+            }
+
+            completableFuture.complete(results);
+            return null;
+        });
+
+        return completableFuture;
+    }
+
 }
+
+/*
+
+    Map<String, Response> blockResponseMap = new HashMap<>();
+    blockResponseMap.put("latestProcessedBlock", response);
+
+    String blockResponseBody = blockResponseMap.get("latestProcessedBlock").body().string();
+    JsonNode jsonRpcResponse = objectMapper.readTree(blockResponseBody);
+    JsonNode block = jsonRpcResponse.get(0).get("result");
+    JsonNode txs = block.get("transactions");
+
+    String number = "";
+    for (int i = 0; i < 5; i++) {
+        System.out.println(i);
+        Response response = getBestBlock();
+        Map<String, Response> blockResponseMap = new HashMap<>();
+        blockResponseMap.put("latestProcessedBlock", response);
+
+        String blockResponseBody = blockResponseMap.get("latestProcessedBlock").body().string();
+        JsonNode jsonRpcResponse = objectMapper.readTree(blockResponseBody);
+        // JsonNode transactionsNode = jsonRpcResponse.get(0).get("result").get("transactions");
+        JsonNode block = jsonRpcResponse.get(0).get("result");
+        JsonNode txs = block.get("transactions");
+
+        if (!number.equals(block.get("number").asText())) {
+            number = block.get("number").asText();
+            System.out.println("different");
+            System.out.println(number);
+        } else {
+            System.out.println("same");
+        }
+
+        if (txs.size() > 1) {
+            Assertions.assertEquals("", txs);
+        }
+
+        // Sleep here, or wait for n blocks and then assert
+     }
+ */
