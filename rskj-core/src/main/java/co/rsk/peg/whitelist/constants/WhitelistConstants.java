de5f0642e2613b48291d301009d5648cb6b6aae3
Marcos <marcos@iovlabs.org>
2024-06-13 15:59:18 -0300

    Add network parameters to WhitelistConstants

--- rskj-core/src/main/java/co/rsk/peg/whitelist/constants/WhitelistConstants.java
+++ rskj-core/src/main/java/co/rsk/peg/whitelist/constants/WhitelistConstants.java
@@ -1,10 +1,15 @@
 package co.rsk.peg.whitelist.constants;
 
+import co.rsk.bitcoinj.core.NetworkParameters;
 import co.rsk.peg.vote.AddressBasedAuthorizer;
 
 public class WhitelistConstants {
-
+    protected NetworkParameters btcParams;
     protected AddressBasedAuthorizer lockWhitelistChangeAuthorizer;
+
+    public NetworkParameters getBtcParams() {
+        return btcParams;
+    }
 
     public AddressBasedAuthorizer getLockWhitelistChangeAuthorizer() {
         return lockWhitelistChangeAuthorizer;
