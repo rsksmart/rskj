9b74a93d9112f6107faed829eb59f4486db474ae
Marcos <marcos@iovlabs.org>
2024-06-13 15:59:18 -0300

    Add network parameters to WhitelistConstants

--- rskj-core/src/main/java/co/rsk/peg/whitelist/constants/WhitelistMainNetConstants.java
+++ rskj-core/src/main/java/co/rsk/peg/whitelist/constants/WhitelistMainNetConstants.java
@@ -1,5 +1,6 @@
 package co.rsk.peg.whitelist.constants;
 
+import co.rsk.bitcoinj.core.NetworkParameters;
 import co.rsk.peg.vote.AddressBasedAuthorizer;
 import java.util.Collections;
 import java.util.List;
@@ -11,10 +12,12 @@ public class WhitelistMainNetConstants extends WhitelistConstants {
     private static final WhitelistMainNetConstants instance = new WhitelistMainNetConstants();
 
     private WhitelistMainNetConstants() {
+        btcParams = NetworkParameters.fromID(NetworkParameters.ID_MAINNET);
+
         ECKey authorizerPublicKey = ECKey.fromPublicOnly(Hex.decode(
-            "041a2449e9d63409c5a8ea3a21c4109b1a6634ee88fd57176d45ea46a59713d5e0b688313cf252128a3e49a0b2effb4b413e5a2525a6fa5894d059f815c9d9efa6"));
-        List<ECKey> lockWhitelistAuthorizedKeys = Collections.singletonList(
-            authorizerPublicKey);
+            "041a2449e9d63409c5a8ea3a21c4109b1a6634ee88fd57176d45ea46a59713d5e0b688313cf252128a3e49a0b2effb4b413e5a2525a6fa5894d059f815c9d9efa6"
+        ));
+        List<ECKey> lockWhitelistAuthorizedKeys = Collections.singletonList(authorizerPublicKey);
 
         lockWhitelistChangeAuthorizer = new AddressBasedAuthorizer(
             lockWhitelistAuthorizedKeys,
