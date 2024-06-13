06ced7cf253a94fdbecf9a0f4ded5f9e052ffc65
Marcos Irisarri <53787863+marcos-iov@users.noreply.github.com>
2024-06-13 13:31:15 -0300

    Update WhitelistSupport javadocs

--- rskj-core/src/main/java/co/rsk/peg/whitelist/WhitelistSupport.java
+++ rskj-core/src/main/java/co/rsk/peg/whitelist/WhitelistSupport.java
@@ -19,20 +19,27 @@ public interface WhitelistSupport {
     int getLockWhitelistSize();
 
     /**
-     * Returns the lock whitelist address stored at the given index, or null if the index is out of
+     * Returns the lock whitelist entry stored at the given index, or null if the index is out of
      * bounds
      *
-     * @param index the index at which to get the address
-     * @return the base58-encoded address stored at the given index, or null if index is out of
+     * @param index the index at which to get the entry
+     * @return the whitelist entry stored at the given index, or null if index is out of
      * bounds
      */
     LockWhitelistEntry getLockWhitelistEntryByIndex(int index);
 
+    /**
+     * Returns the lock whitelist entry for a given address, or null if the address is not whitelisted
+     *
+     * @param addressBase58 the address in base58 format to search for
+     * @return the whitelist entry for the given address, or null if the addrres is not whitelisted
+     */
     LockWhitelistEntry getLockWhitelistEntryByAddress(String addressBase58);
 
     /**
-     * Adds the given address to the lock whitelist.
+     * Adds the given address to the lock whitelist, allowing peg-ins up to certain max value
      *
+     * @param tx the RSK transaction where the call was made
      * @param addressBase58    the base58-encoded address to add to the whitelist
      * @param maxTransferValue the max amount of satoshis enabled to transfer for this address
      * @return 1 SUCCESS, -1 ADDRESS_ALREADY_WHITELISTED, -2 INVALID_ADDRESS_FORMAT,
@@ -40,11 +47,20 @@ public interface WhitelistSupport {
      */
     int addOneOffLockWhitelistAddress(Transaction tx, String addressBase58, BigInteger maxTransferValue);
 
+    /**
+     * Adds the given address to the lock whitelist, allowing unlimited peg-in value
+     *
+     * @param tx the RSK transaction where the call was made
+     * @param addressBase58    the base58-encoded address to add to the whitelist
+     * @return 1 SUCCESS, -1 ADDRESS_ALREADY_WHITELISTED, -2 INVALID_ADDRESS_FORMAT,
+     * -10 UNAUTHORIZED_CALLER.
+     */
     int addUnlimitedLockWhitelistAddress(Transaction tx, String addressBase58);
 
     /**
      * Removes the given address from the lock whitelist.
      *
+     * @param tx the RSK transaction where the call was made
      * @param addressBase58 the base58-encoded address to remove from the whitelist
      * @return 1 SUCCESS, -1 ADDRESS_NOT_EXIST, -2 INVALID_ADDRESS_FORMAT, -10 UNAUTHORIZED_CALLER.
      */
@@ -53,12 +69,20 @@ public interface WhitelistSupport {
     /**
      * Sets a delay in the BTC best chain to disable lock whitelist
      *
-     * @param tx                  current RSK transaction
-     * @param disableBlockDelayBI block since current BTC best chain height to disable lock
-     *                            whitelist
+     * @param tx the RSK transaction where the call was made
+     * @param disableBlockDelayBI block since current BTC best chain height to disable lock whitelist
+     * @param btcBlockchainBestChainHeight current btc blockchain best chain height
      * @return 1 SUCCESS, -1 DELAY_ALREADY_SET, -2 DISABLE_BLOCK_DELAY_INVALID, -10 UNAUTHORIZED_CALLER
      */
     int setLockWhitelistDisableBlockDelay(Transaction tx, BigInteger disableBlockDelayBI, int btcBlockchainBestChainHeight);
 
+    /**
+     * Checks that a given address is whitelisted and authorized to perform peg-in operations
+     *
+     * @param senderBtcAddress the Bitcoin address to check if it's whitelisted
+     * @param totalAmount the amount that is trying to peg
+     * @param height the Bitcoin network height where the peg-in transaction was included
+     * @return true if sender is authorized to peg-in the corresponding amount, false otherwise
+     */
     boolean verifyLockSenderIsWhitelisted(Address senderBtcAddress, Coin totalAmount, int height);
 }
