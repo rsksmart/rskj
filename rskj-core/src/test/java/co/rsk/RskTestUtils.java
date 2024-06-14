27bc91f381d60a220e0d17efbb70d4bd8b816ae5
julia-zack <julia.zack@iovlabs.org>
2024-06-14 11:17:11 -0300

    Move getRskPublicKeys method from Federation class to RskTestUtils since its just being used for testing purposes. Refactor to improve readability

--- rskj-core/src/test/java/co/rsk/RskTestUtils.java
+++ rskj-core/src/test/java/co/rsk/RskTestUtils.java
@@ -1,18 +1,31 @@
 package co.rsk;
 
+import co.rsk.peg.federation.Federation;
+import co.rsk.peg.federation.FederationMember;
 import org.ethereum.crypto.ECKey;
 import org.ethereum.crypto.HashUtil;
-
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class RskTestUtils {
     public static List<ECKey> getEcKeysFromSeeds(String[] seeds) {
-        return Arrays
-            .stream(seeds)
-            .map(seed -> ECKey.fromPrivate(HashUtil.keccak256(seed.getBytes(StandardCharsets.UTF_8))))
+        return Arrays.stream(seeds)
+            .map(seed -> seed.getBytes(StandardCharsets.UTF_8))
+            .map(HashUtil::keccak256)
+            .map(ECKey::fromPrivate)
             .collect(Collectors.toList());
+    }
+
+    public static List<ECKey> getRskPublicKeysFromFederation(Federation federation) {
+        List<ECKey> rskPublicKeys = new ArrayList<>();
+        List<FederationMember> members = federation.getMembers();
+        for (FederationMember member : members) {
+            rskPublicKeys.add(member.getRskPublicKey());
+        }
+
+        return rskPublicKeys;
     }
 }
