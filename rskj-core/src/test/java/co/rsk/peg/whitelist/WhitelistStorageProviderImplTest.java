0cd48d1b33ca0f6f1f9297b1f72dc746d0ebe0d1
Wilmer Rondon <wilmer.rondonbriceno@iovlabs.org>
2024-06-21 17:27:53 -0400

    Changing Return Object
    - Changing Return Object of getAddressFromUnlimitedWhiteListEntryInMemoryStorage method.

--- rskj-core/src/test/java/co/rsk/peg/whitelist/WhitelistStorageProviderImplTest.java
+++ rskj-core/src/test/java/co/rsk/peg/whitelist/WhitelistStorageProviderImplTest.java
@@ -97,7 +97,7 @@ class WhitelistStorageProviderImplTest {
         Map<Address, OneOffWhiteListEntry> oneOffWhiteListEntryMap = getAddressFromOneOffWhiteListEntryInMemoryStorage();
         assertTrue(oneOffWhiteListEntryMap.containsKey(firstBtcAddress));
 
-        Map<Address, LockWhitelistEntry> lockWhitelistEntryMap = getAddressFromUnlimitedWhiteListEntryInMemoryStorage();
+        Map<Address, UnlimitedWhiteListEntry> lockWhitelistEntryMap = getAddressFromUnlimitedWhiteListEntryInMemoryStorage();
         assertTrue(lockWhitelistEntryMap.containsKey(secondBtcAddress));
     }
 
@@ -174,12 +174,12 @@ class WhitelistStorageProviderImplTest {
         return oneOffWhitelistAndDisableBlockHeightData.getLeft();
     }
 
-    private Map<Address, LockWhitelistEntry> getAddressFromUnlimitedWhiteListEntryInMemoryStorage() {
-        Map<Address, LockWhitelistEntry> whitelistedAddresses = new HashMap<>();
-        whitelistedAddresses.putAll(inMemoryStorage.safeGetFromRepository(
+    private Map<Address, UnlimitedWhiteListEntry> getAddressFromUnlimitedWhiteListEntryInMemoryStorage() {
+        return inMemoryStorage.safeGetFromRepository(
             LOCK_UNLIMITED.getKey(),
-            data -> BridgeSerializationUtils.deserializeUnlimitedLockWhitelistEntries(data, networkParameters)
-        ));
-        return whitelistedAddresses;
+            data -> BridgeSerializationUtils.deserializeUnlimitedLockWhitelistEntries(
+                data,
+                networkParameters)
+        );
     }
 }
