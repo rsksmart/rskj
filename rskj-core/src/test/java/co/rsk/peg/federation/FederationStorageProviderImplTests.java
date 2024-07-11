b1653e821640cc568c01406e67fc11cb58650f49
jeremy-then <jeremy.then@iovlabs.org>
2024-07-11 16:20:02 -0400

    Add saveActiveFederationCreationBlockHeight test

--- rskj-core/src/test/java/co/rsk/peg/federation/FederationStorageProviderImplTests.java
+++ rskj-core/src/test/java/co/rsk/peg/federation/FederationStorageProviderImplTests.java
@@ -889,6 +889,31 @@ class FederationStorageProviderImplTests {
 
     }
 
+    @Test
+    void saveActiveFederationCreationBlockHeight_whenHeightIsNotNullAndRSKIP186IsActive_shouldSaveToStorage() {
+        // Arrange
+
+        ActivationConfig.ForBlock activations = mock(ActivationConfig.ForBlock.class);
+        when(activations.isActive(ConsensusRule.RSKIP186)).thenReturn(true);
+
+        StorageAccessor storageAccessor = new InMemoryStorage();
+        FederationStorageProvider federationStorageProvider = new FederationStorageProviderImpl(storageAccessor);
+
+        long expectedFederationCreationBlockHeight = 100;
+        federationStorageProvider.setActiveFederationCreationBlockHeight(expectedFederationCreationBlockHeight);
+
+        // Act
+        federationStorageProvider.save(networkParameters, activations);
+
+        // Assert
+
+        Optional<Long> actualFederationCreationBlockHeight = storageAccessor.getFromRepository(ACTIVE_FEDERATION_CREATION_BLOCK_HEIGHT_KEY.getKey(), BridgeSerializationUtils::deserializeOptionalLong);
+
+        assertTrue(actualFederationCreationBlockHeight.isPresent());
+        assertEquals(expectedFederationCreationBlockHeight, actualFederationCreationBlockHeight.get());
+
+    }
+
     private static Federation createFederation(int version) {
         List<FederationMember> members = FederationMember.getFederationMembersFromKeys(
             PegTestUtils.createRandomBtcECKeys(7)
