27cc35b158e3d1cb3564a4f681e3e2cabef9f343
Wilmer Rondon <wilmer.rondonbriceno@iovlabs.org>
2024-07-16 13:54:51 -0400

    Removing Arrays.stream From increaseAuthorizedKeys Variable
    Removing Arrays.stream From increaseAuthorizedKeys Variable and replacing it with Collections.unmodifiableList(Stream.of()

--- rskj-core/src/main/java/co/rsk/peg/lockingcap/constants/LockingCapTestNetConstants.java
+++ rskj-core/src/main/java/co/rsk/peg/lockingcap/constants/LockingCapTestNetConstants.java
@@ -2,9 +2,10 @@ package co.rsk.peg.lockingcap.constants;
 
 import co.rsk.bitcoinj.core.Coin;
 import co.rsk.peg.vote.AddressBasedAuthorizer;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.bouncycastle.util.encoders.Hex;
 import org.ethereum.crypto.ECKey;
 
@@ -13,11 +14,11 @@ public class LockingCapTestNetConstants extends LockingCapConstants {
     private static final LockingCapTestNetConstants instance = new LockingCapTestNetConstants();
 
     private LockingCapTestNetConstants() {
-        List<ECKey> increaseAuthorizedKeys = Arrays.stream(new String[]{
+        List<ECKey> increaseAuthorizedKeys = Collections.unmodifiableList(Stream.of(
             "04701d1d27f8c2ae97912d96fb1f82f10c2395fd320e7a869049268c6b53d2060dfb2e22e3248955332d88cd2ae29a398f8f3858e48dd6d8ffbc37dfd6d1aa4934",
             "045ef89e4a5645dc68895dbc33b4c966c3a0a52bb837ecdd2ba448604c4f47266456d1191420e1d32bbe8741f8315fde4d1440908d400e5998dbed6549d499559b",
             "0455db9b3867c14e84a6f58bd2165f13bfdba0703cb84ea85788373a6a109f3717e40483aa1f8ef947f435ccdf10e530dd8b3025aa2d4a7014f12180ee3a301d27"
-        }).map(hex -> ECKey.fromPublicOnly(Hex.decode(hex))).collect(Collectors.toList());
+        ).map(hex -> ECKey.fromPublicOnly(Hex.decode(hex))).collect(Collectors.toList()));
 
         increaseAuthorizer = new AddressBasedAuthorizer(
             increaseAuthorizedKeys,
