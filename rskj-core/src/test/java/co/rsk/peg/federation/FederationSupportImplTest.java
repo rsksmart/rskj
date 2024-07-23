641ba45c13af84d0fabd08aca240a570965f199b
jeremy-then <jeremy.then@iovlabs.org>
2024-07-23 00:25:32 -0400

    Using TransactionUtils and FederationChangeFunction Rebases

--- rskj-core/src/test/java/co/rsk/peg/federation/FederationSupportImplTest.java
+++ rskj-core/src/test/java/co/rsk/peg/federation/FederationSupportImplTest.java
@@ -2227,8 +2227,7 @@ class FederationSupportImplTest {
             ActivationConfig.ForBlock activations = ActivationConfigsForTest.all().forBlock(0L);
             signatureCache = mock(SignatureCache.class);
             logs = new ArrayList<>();
-            bridgeEventLogger = new BridgeEventLoggerImpl(BridgeMainNetConstants.getInstance(),
-                activations, logs, signatureCache);
+            bridgeEventLogger = new BridgeEventLoggerImpl(BridgeMainNetConstants.getInstance(), activations, logs, signatureCache);
             StorageAccessor storageAccessor = new InMemoryStorage();
             storageProvider = new FederationStorageProviderImpl(storageAccessor);
 
