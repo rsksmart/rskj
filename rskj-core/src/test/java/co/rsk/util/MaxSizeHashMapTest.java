026ce0bf8bc040796c38d220fba2d1191ec661ef
Volodymyr Kravets <volodymyr.kravets@outlook.com>
2024-07-24 14:37:53 +0300

    chore: add java21 support

--- rskj-core/src/test/java/co/rsk/util/MaxSizeHashMapTest.java
+++ rskj-core/src/test/java/co/rsk/util/MaxSizeHashMapTest.java
@@ -1,36 +1,39 @@
+/*
+ * This file is part of RskJ
+ * Copyright (C) 2019 RSK Labs Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package co.rsk.util;
 
-import org.ethereum.TestUtils;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 class MaxSizeHashMapTest {
 
     @Test
-    void maxSizeMap_Test() {
-        int maxSize = 50_000;
+    void maxSizeMapTest() {
+        int maxSize = 10;
         Map<Integer, Integer> maxSizeMap = new MaxSizeHashMap<>(maxSize, true);
 
-        Object[] table = TestUtils.getInternalState(maxSizeMap, "table");
-        assertNull(table);
-        for(int i=0; i < maxSize/2 ; i++) {
+        for(int i=0; i < 2*maxSize ; i++) {
             maxSizeMap.put(i, i);
         }
-        table = TestUtils.getInternalState(maxSizeMap, "table");
-        assertEquals(65_536, table.length);
-        for(int i=maxSize/2; i < maxSize ; i++) {
-            maxSizeMap.put(i, i);
-        }
-        table = TestUtils.getInternalState(maxSizeMap, "table");
-        assertEquals(131_072, table.length);
-        for(int i=maxSize; i < maxSize + maxSize ; i++) {
-            maxSizeMap.put(i, i);
-        }
-        table = TestUtils.getInternalState(maxSizeMap, "table");
-        assertEquals(131_072, table.length);
+        assertEquals(maxSize, maxSizeMap.size());
     }
 }
