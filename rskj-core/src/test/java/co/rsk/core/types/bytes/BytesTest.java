401204ac097f4aaf55f572feb713c75c306976d0
Volodymyr Kravets <volodymyr.kravets@outlook.com>
2024-07-26 15:16:53 +0300

    refactor: update Bytes and BytesSlice to 'mimic' System.arraycopy and Arrays.copyOfRange; add more tests

--- rskj-core/src/test/java/co/rsk/core/types/bytes/BytesTest.java
+++ rskj-core/src/test/java/co/rsk/core/types/bytes/BytesTest.java
@@ -19,9 +19,12 @@
 
 package co.rsk.core.types.bytes;
 
+import co.rsk.util.Functions;
 import org.ethereum.TestUtils;
 import org.ethereum.util.ByteUtil;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,6 +35,41 @@ class BytesTest {
         assertNull(Bytes.of(null));
         assertNotNull(Bytes.of(new byte[]{}));
         assertNotNull(Bytes.of(new byte[]{1}));
+    }
+
+    @Test
+    void testBytesLength() {
+        assertEquals(0, Bytes.of(new byte[]{}).length());
+        assertEquals(1, Bytes.of(new byte[]{1}).length());
+    }
+
+    @Test
+    void testBytesAt() {
+        assertThrows(IndexOutOfBoundsException.class, () -> Bytes.of(new byte[]{}).byteAt(0));
+        assertThrows(IndexOutOfBoundsException.class, () -> Bytes.of(new byte[]{1}).byteAt(1));
+        assertThrows(IndexOutOfBoundsException.class, () -> Bytes.of(new byte[]{1}).byteAt(-1));
+        assertEquals(1, Bytes.of(new byte[]{1}).byteAt(0));
+        assertEquals(2, Bytes.of(new byte[]{1,2}).byteAt(1));
+    }
+
+    @Test
+    void testBytesArraycopy() {
+        checkArraycopy((src, srcPos, dest, destPos, length) -> Bytes.of((byte[]) src).arraycopy(srcPos, (byte[]) dest, destPos, length));
+    }
+
+    @Test
+    void testBytesArraycopyMimicsSystemOne() {
+        checkArraycopy(System::arraycopy);
+    }
+
+    @Test
+    void testCopyArrayOfRange() {
+        checkCopyOfRange((original, from, to) -> Bytes.of(original).copyArrayOfRange(from, to));
+    }
+
+    @Test
+    void testCopyArrayOfRangeMimicsSystemOne() {
+        checkCopyOfRange(Arrays::copyOfRange);
     }
 
     @Test
@@ -83,13 +121,17 @@ class BytesTest {
 
     @Test
     void testLongBytesToString() {
-        byte[] bArray1 = TestUtils.generateBytes("hash1",15);
+        byte[] bArray1 = TestUtils.generateBytes("hash1",16);
         byte[] bArray2 = TestUtils.generateBytes("hash2",15);
         byte[] finalArray = ByteUtil.merge(bArray1, new byte[]{1, 2, 3}, bArray2);
 
-        assertEquals(33, finalArray.length);
+        assertEquals(34, finalArray.length);
 
-        String actualMessage = String.format("Some '%s' hex", Bytes.of(finalArray));
+        Bytes bytes = Bytes.of(finalArray);
+
+        assertEquals(64, String.format("%s", bytes).length());
+
+        String actualMessage = String.format("Some '%s' hex", bytes);
         String expectedMessage = "Some '"
                 + ByteUtil.toHexString(bArray1)
                 + ".."
@@ -115,5 +157,44 @@ class BytesTest {
         String expectedMessage = "Some '' hex";
 
         assertEquals(expectedMessage, actualMessage);
+    }
+
+    private static void checkArraycopy(Functions.Action5<Object, Integer, Object, Integer, Integer> fun) {
+        byte[] dest = new byte[5];
+        byte[] origin = new byte[]{1,2,3,4,5};
+
+        assertThrows(NullPointerException.class, () -> fun.apply(origin, 0, null, 0, 5));
+
+        assertThrows(IndexOutOfBoundsException.class, () -> fun.apply(origin, -1, dest, 0, 5));
+        assertThrows(IndexOutOfBoundsException.class, () -> fun.apply(origin, 0, dest, -1, 5));
+        assertThrows(IndexOutOfBoundsException.class, () -> fun.apply(origin, 0, dest, 0, -1));
+        assertThrows(IndexOutOfBoundsException.class, () -> fun.apply(origin, 0, dest, 0, 6));
+        assertThrows(IndexOutOfBoundsException.class, () -> fun.apply(origin, 1, dest, 0, 5));
+        assertThrows(IndexOutOfBoundsException.class, () -> fun.apply(origin, 0, dest, 1, 5));
+
+        assertArrayEquals(new byte[5], dest); // yet unmodified
+
+        fun.apply(origin, 0, dest, 0, 5);
+        assertArrayEquals(new byte[]{1,2,3,4,5}, dest);
+
+        byte[] dest2 = new byte[5];
+        fun.apply(origin, 1, dest2, 1, 3);
+        assertArrayEquals(new byte[]{0,2,3,4,0}, dest2);
+    }
+
+    private static void checkCopyOfRange(Functions.Function3<byte[], Integer, Integer, byte[]> fun) {
+        byte[] bArray =  new byte[]{1, 2, 3, 4, 5};
+
+        assertEquals(bArray.length, fun.apply(bArray, 0, 5).length);
+        assertNotSame(bArray, fun.apply(bArray, 0, 5));
+
+        assertArrayEquals(new byte[]{2, 3, 4}, fun.apply(bArray, 1, 4));
+        assertArrayEquals(new byte[]{2}, fun.apply(bArray, 1, 2));
+        assertArrayEquals(new byte[]{2, 3, 4, 5, 0, 0, 0}, fun.apply(bArray, 1, 8));
+        assertArrayEquals(new byte[]{}, fun.apply(bArray, 3, 3));
+
+        assertThrows(IllegalArgumentException.class, () -> fun.apply(bArray, 1, 0));
+        assertThrows(IndexOutOfBoundsException.class, () -> fun.apply(bArray, -1, 0));
+        assertThrows(IndexOutOfBoundsException.class, () -> fun.apply(bArray, 6, 6));
     }
 }
