cfbff8e6ed6962740ab5f98d56082c203c7eff18
Wilmer Rondon <wilmer.rondonbriceno@iovlabs.org>
2024-07-29 09:34:29 -0400

    Removing Blank Line

--- rskj-core/src/test/java/co/rsk/peg/lockingcap/LockingCapSupportImplTest.java
+++ rskj-core/src/test/java/co/rsk/peg/lockingcap/LockingCapSupportImplTest.java
@@ -1,6 +1,5 @@
 package co.rsk.peg.lockingcap;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
