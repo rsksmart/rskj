ac4c1c384ad8b1001c9d39a90b4cd3e2fb763ae9
Wilmer Rondon <wilmer.rondonbriceno@iovlabs.org>
2024-08-01 16:53:40 -0400

    Adding a new Customized LockingCapIllegalArgumentException
    - Using in LockingCap objects LockingCapIllegalArgumentException.
    - In Bridge objects VMException.
    
    Created INCREASE_LOCKING_CAP_LOG_TAG constant to avoid String repetition
    
    Bank line to improve readability
    
    When No Arguments Test Case, replaced encodeSignature with encodeArguments method
    
    Created a message string to avoid repetition
    
    Avoiding String Repetition
    
    Removed Unused Import
    
    Improving Log Message
    
    Improving Log Message for maxLockingCapVoteValueAllowed Case

--- rskj-core/src/main/java/co/rsk/peg/lockingcap/LockingCapSupport.java
+++ rskj-core/src/main/java/co/rsk/peg/lockingcap/LockingCapSupport.java
@@ -1,7 +1,6 @@
 package co.rsk.peg.lockingcap;
 
 import co.rsk.bitcoinj.core.Coin;
-import co.rsk.peg.BridgeIllegalArgumentException;
 import java.util.Optional;
 import org.ethereum.core.Transaction;
 
@@ -9,7 +8,7 @@ public interface LockingCapSupport {
 
     Optional<Coin> getLockingCap();
 
-    boolean increaseLockingCap(Transaction tx, Coin newCap) throws BridgeIllegalArgumentException;
+    boolean increaseLockingCap(Transaction tx, Coin newCap) throws LockingCapIllegalArgumentException;
 
     void save();
 }
