220725267b99f7e82837c9a72a61106a92042cbe
jeremy-then <jeremy.then@iovlabs.org>
2024-08-01 13:52:42 -0400

    Using voteToCreateFederation

--- rskj-core/src/test/java/co/rsk/peg/federation/FederationSupportImplTest.java
+++ rskj-core/src/test/java/co/rsk/peg/federation/FederationSupportImplTest.java
@@ -2656,11 +2656,7 @@ class FederationSupportImplTest {
             Transaction firstAuthorizedTx = TransactionUtils.getTransactionFromCaller(signatureCache, FederationChangeCaller.FIRST_AUTHORIZED.getRskAddress());
             Transaction secondAuthorizedTx = TransactionUtils.getTransactionFromCaller(signatureCache, FederationChangeCaller.SECOND_AUTHORIZED.getRskAddress());
 
-            ABICallSpec createFederationAbiCallSpec = new ABICallSpec(FederationChangeFunction.CREATE.getKey(), new byte[][]{});
-
-            // Voting with m of n authorizers to create the pending federation
-            federationSupport.voteFederationChange(firstAuthorizedTx, createFederationAbiCallSpec, signatureCache, bridgeEventLogger);
-            federationSupport.voteFederationChange(secondAuthorizedTx, createFederationAbiCallSpec, signatureCache, bridgeEventLogger);
+            voteToCreateFederation(firstAuthorizedTx, secondAuthorizedTx);
 
             ABICallSpec addMultiKeyAbiCallSpec1 = getAddMultiKeysVote(federator1);
 
