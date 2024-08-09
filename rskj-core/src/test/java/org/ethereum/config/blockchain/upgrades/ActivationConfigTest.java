5bb176549f89465de1f3f429a87406650aa29ecb
Reynold Morel <reynold.morel@iovlabs.org>
2024-08-09 08:22:48 -0400

    Addressing comments

--- rskj-core/src/test/java/org/ethereum/config/blockchain/upgrades/ActivationConfigTest.java
+++ rskj-core/src/test/java/org/ethereum/config/blockchain/upgrades/ActivationConfigTest.java
@@ -122,7 +122,7 @@ class ActivationConfigTest {
             "    rskip412: arrowhead600",
             "    rskip415: arrowhead600",
             "    rskip417: arrowhead600",
-            "    rskip00: tbd000",
+            "    rskip432: lovell700",
             "}"
     ));
 
