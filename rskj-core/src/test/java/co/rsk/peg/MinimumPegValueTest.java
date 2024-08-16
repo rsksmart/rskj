41339b3eed17507b78954d1d8b73469c3e9837cd
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2024-08-16 11:14:03 +0000

    feat(peg): add different feePerKb values for determining min peg values

--- rskj-core/src/test/java/co/rsk/peg/MinimumPegValueTest.java
+++ rskj-core/src/test/java/co/rsk/peg/MinimumPegValueTest.java
@@ -19,7 +19,6 @@ package co.rsk.peg;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import co.rsk.bitcoinj.core.Address;
 import co.rsk.bitcoinj.core.BtcECKey;
@@ -39,49 +38,51 @@ import java.util.List;
 import java.util.stream.Stream;
 import co.rsk.peg.federation.*;
 import org.ethereum.config.blockchain.upgrades.ActivationConfig;
-import org.ethereum.config.blockchain.upgrades.ConsensusRule;
+import org.ethereum.config.blockchain.upgrades.ActivationConfigsForTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class MinimumPegValueTest {
+
+  private static final Coin MINIMUM_PEGIN_VALUE = Coin.valueOf(100_000L);
+  private static final Coin MINIMUM_PEGOUT_VALUE = Coin.valueOf(80_000L);
+
   private ActivationConfig.ForBlock activations;
   private NetworkParameters networkParameters;
   private BridgeConstants bridgeMainNetConstants;
+  private Federation federation;
 
   @BeforeEach
   void setup() {
-    activations = mock(ActivationConfig.ForBlock.class);
+    activations = ActivationConfigsForTest.all().forBlock(0L);
     bridgeMainNetConstants = BridgeMainNetConstants.getInstance();
     networkParameters = bridgeMainNetConstants.getBtcParams();
+    federation = new P2shErpFederationBuilder().withNetworkParameters(networkParameters).build();
   }
 
   @ParameterizedTest()
   @MethodSource("providePegMinimumParameters")
-  void checkProposedPegValues_whenOneInputofMinPeginValueAndOneOutputOfMinPegoutValue_shouldBuildTransactionSuccessfully(
-      Coin feePerKb, Coin minPeginValue, Coin minPegoutValue) {
-    // build federation
-    Federation fed = new P2shErpFederationBuilder().withNetworkParameters(networkParameters).build();
+  void whenOneInputofMinPeginValueAndOneOutputOfMinPegoutValue_shouldBuildTransactionSuccessfully(Coin feePerKb) {
     // list of peg-out requests with the given minimum peg-out value
     List<ReleaseRequestQueue.Entry> entries = Arrays.asList(
-        createTestEntry(1000, minPegoutValue));
+        createTestEntry(1000, MINIMUM_PEGOUT_VALUE));
     // list of utxos that contains one utxo with the minimum peg-in value
     List<UTXO> utxos = Arrays.asList(
-        new UTXO(getUTXOHash("utxo"), 0, minPeginValue, 0, false, fed.getP2SHScript()));
+        new UTXO(getUTXOHash("utxo"), 0, MINIMUM_PEGIN_VALUE, 0, false, federation.getP2SHScript()));
     // the federation wallet, with flyover support
     Wallet fedWallet = BridgeUtils.getFederationSpendWallet(
         new Context(networkParameters),
-        fed,
+        federation,
         utxos,
         true,
         mock(BridgeStorageProvider.class));
     // build release transaction builder for current fed
-    when(activations.isActive(ConsensusRule.RSKIP201)).thenReturn(true);
     ReleaseTransactionBuilder releaseTransactionBuilder = new ReleaseTransactionBuilder(
         networkParameters,
         fedWallet,
-        fed.getAddress(),
+        federation.getAddress(),
         feePerKb,
         activations);
 
@@ -94,31 +95,28 @@ class MinimumPegValueTest {
 
   @ParameterizedTest()
   @MethodSource("providePegMinimumParameters")
-  void checkProposedPegValues_whenOneInputOfTenTimesMinPeginValueAndTenOutputsOfMinPegoutValue_shouldBuildTransactionSuccessfully(
-      Coin feePerKb, Coin minPeginValue, Coin minPegoutValue) {
-    // build federation
-    Federation fed = new P2shErpFederationBuilder().withNetworkParameters(networkParameters).build();
+  void whenOneInputOfTenTimesMinPeginValueAndTenOutputsOfMinPegoutValue_shouldBuildTransactionSuccessfully(
+      Coin feePerKb) {
     // list of peg-out requests with the given minimum peg-out value
     List<ReleaseRequestQueue.Entry> entries = new ArrayList<>();
     for (int i = 0; i < 10; i++) {
-      entries.add(createTestEntry(1000 + i, minPegoutValue));
+      entries.add(createTestEntry(1000 + i, MINIMUM_PEGOUT_VALUE));
     }
-    // list of utxos that contains one utxo with the minimum peg-in value
+    // list of utxos that contains one utxo with ten times the minimum peg-in value
     List<UTXO> utxos = Arrays.asList(
-        new UTXO(getUTXOHash("utxo"), 0, minPeginValue.times(10), 0, false, fed.getP2SHScript()));
+        new UTXO(getUTXOHash("utxo"), 0, MINIMUM_PEGIN_VALUE.times(10), 0, false, federation.getP2SHScript()));
     // the federation wallet, with flyover support
     Wallet fedWallet = BridgeUtils.getFederationSpendWallet(
         new Context(networkParameters),
-        fed,
+        federation,
         utxos,
         true,
         mock(BridgeStorageProvider.class));
     // build release transaction builder for current fed
-    when(activations.isActive(ConsensusRule.RSKIP201)).thenReturn(true);
     ReleaseTransactionBuilder releaseTransactionBuilder = new ReleaseTransactionBuilder(
         networkParameters,
         fedWallet,
-        fed.getAddress(),
+        federation.getAddress(),
         feePerKb,
         activations);
 
@@ -131,34 +129,30 @@ class MinimumPegValueTest {
 
   @ParameterizedTest()
   @MethodSource("providePegMinimumParameters")
-  void checkProposedPegValues_whenTenInputOfMinPeginValueAndTenOutputsOfMinPegoutValue_shouldBuildTransactionSuccessfully(
-      Coin feePerKb, Coin minPeginValue, Coin minPegoutValue) {
-    // build federation
-    Federation fed = new P2shErpFederationBuilder().withNetworkParameters(networkParameters).build();
+  void whenTenInputOfMinPeginValueAndTenOutputsOfMinPegoutValue_shouldBuildTransactionSuccessfully(Coin feePerKb) {
     // list of peg-out requests with the given minimum peg-out value
     List<ReleaseRequestQueue.Entry> entries = new ArrayList<>();
     for (int i = 0; i < 10; i++) {
-      entries.add(createTestEntry(1000 + i, minPegoutValue));
+      entries.add(createTestEntry(1000 + i, MINIMUM_PEGOUT_VALUE));
     }
-    // list of utxos that contains one utxo with the minimum peg-in value
+    // list of utxos that contains ten utxos with the minimum peg-in value
     List<UTXO> utxos = new ArrayList<>();
     for (int i = 0; i < 10; i++) {
       utxos.add(
-          new UTXO(getUTXOHash("utxo" + i), 0, minPeginValue, 0, false, fed.getP2SHScript()));
+          new UTXO(getUTXOHash("utxo" + i), 0, MINIMUM_PEGIN_VALUE, 0, false, federation.getP2SHScript()));
     }
     // the federation wallet, with flyover support
     Wallet fedWallet = BridgeUtils.getFederationSpendWallet(
         new Context(networkParameters),
-        fed,
+        federation,
         utxos,
         true,
         mock(BridgeStorageProvider.class));
     // build release transaction builder for current fed
-    when(activations.isActive(ConsensusRule.RSKIP201)).thenReturn(true);
     ReleaseTransactionBuilder releaseTransactionBuilder = new ReleaseTransactionBuilder(
         networkParameters,
         fedWallet,
-        fed.getAddress(),
+        federation.getAddress(),
         feePerKb,
         activations);
 
@@ -171,7 +165,10 @@ class MinimumPegValueTest {
 
   private static Stream<Arguments> providePegMinimumParameters() {
     return Stream.of(
-        Arguments.of(Coin.valueOf(24_000L), Coin.valueOf(100_000L), Coin.valueOf(80_000L)));
+        // current feePerKb value
+        Arguments.of(Coin.valueOf(24_000L)),
+        // maximum feePerKb value that allows all cases to succeed
+        Arguments.of(Coin.valueOf(81_423L)));
   }
 
   private Address getAddress(int pk) {
