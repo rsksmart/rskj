7e9278176df7adf4ce0c82008cc4f70366c26b8a
julia-zack <julia.zack@iovlabs.org>
2024-08-16 21:15:20 -0300

    Create flyover redeem script builder
    Remove unused import
    
    Remove mistaken reason
    
    Minor refactor
    
    Make flyover rs builder receive a keccak256 hash instead of sha256 one
    
    Refactors

---
+++ rskj-core/src/main/java/co/rsk/peg/bitcoin/FlyoverRedeemScriptBuilderImpl.java
@@ -0,0 +1,35 @@
+package co.rsk.peg.bitcoin;
+
+import co.rsk.bitcoinj.script.Script;
+import co.rsk.bitcoinj.script.ScriptBuilder;
+import co.rsk.bitcoinj.script.ScriptOpCodes;
+import co.rsk.crypto.Keccak256;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static co.rsk.peg.bitcoin.FlyoverRedeemScriptCreationException.Reason.INVALID_FLYOVER_DERIVATION_HASH;
+import static java.util.Objects.isNull;
+
+public class FlyoverRedeemScriptBuilderImpl implements FlyoverRedeemScriptBuilder {
+    private static final Logger logger = LoggerFactory.getLogger(FlyoverRedeemScriptBuilderImpl.class);
+
+    public Script addFlyoverDerivationHashToRedeemScript(Keccak256 flyoverDerivationHash, Script redeemScript) {
+        validateFlyoverDerivationHash(flyoverDerivationHash);
+
+        ScriptBuilder scriptBuilder = new ScriptBuilder();
+        byte[] flyoverDerivationHashSerialized = flyoverDerivationHash.getBytes();
+        return scriptBuilder
+            .data(flyoverDerivationHashSerialized)
+            .op(ScriptOpCodes.OP_DROP)
+            .addChunks(redeemScript.getChunks())
+            .build();
+    }
+
+    private void validateFlyoverDerivationHash(Keccak256 flyoverDerivationHash) {
+        if (isNull(flyoverDerivationHash) || flyoverDerivationHash.equals(Keccak256.ZERO_HASH)) {
+            String message = String.format("Provided flyover derivation hash %s is invalid.", flyoverDerivationHash);
+            logger.warn("[validateFlyoverDerivationHash] {}", message);
+            throw new FlyoverRedeemScriptCreationException(message, INVALID_FLYOVER_DERIVATION_HASH);
+        }
+    }
+}
---
+++ rskj-core/src/test/java/co/rsk/peg/bitcoin/FlyoverRedeemScriptBuilderImplTest.java
@@ -0,0 +1,74 @@
+package co.rsk.peg.bitcoin;
+
+import co.rsk.bitcoinj.script.Script;
+import co.rsk.bitcoinj.script.ScriptChunk;
+import co.rsk.bitcoinj.script.ScriptOpCodes;
+import co.rsk.crypto.Keccak256;
+import co.rsk.peg.federation.Federation;
+import co.rsk.peg.federation.P2shErpFederationBuilder;
+import org.ethereum.TestUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static co.rsk.peg.bitcoin.FlyoverRedeemScriptCreationException.Reason.INVALID_FLYOVER_DERIVATION_HASH;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlyoverRedeemScriptBuilderImplTest {
+    private Script redeemScript;
+    private FlyoverRedeemScriptBuilder flyoverRedeemScriptBuilder;
+
+    @BeforeEach
+    void setUp() {
+        Federation federation = new P2shErpFederationBuilder().build();
+        redeemScript = federation.getRedeemScript();
+        flyoverRedeemScriptBuilder = new FlyoverRedeemScriptBuilderImpl();
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidDerivationHashArgsProvider")
+    void addFlyoverDerivationHashToRedeemScript_withInvalidPrefix_shouldThrowFlyoverRedeemScriptCreationException(Keccak256 flyoverDerivationHash) {
+        FlyoverRedeemScriptCreationException exception = assertThrows(FlyoverRedeemScriptCreationException.class,
+            () -> flyoverRedeemScriptBuilder.addFlyoverDerivationHashToRedeemScript(flyoverDerivationHash, redeemScript));
+
+        assertEquals(INVALID_FLYOVER_DERIVATION_HASH, exception.getReason());
+
+        String expectedMessage = String.format("Provided flyover derivation hash %s is invalid.", flyoverDerivationHash);
+        assertEquals(expectedMessage, exception.getMessage());
+    }
+
+    private static Stream<Keccak256> invalidDerivationHashArgsProvider() {
+        return Stream.of(null, Keccak256.ZERO_HASH);
+    }
+
+    @Test
+    void addFlyoverDerivationHashToRedeemScript_shouldReturnRedeemScriptWithFlyoverDerivationHash() {
+        // arrange
+        Keccak256 flyoverDerivationHash = TestUtils.generateHash("hash");
+
+        // act
+        Script redeemScriptWithFlyoverDerivationHash = flyoverRedeemScriptBuilder.addFlyoverDerivationHashToRedeemScript(flyoverDerivationHash, redeemScript);
+
+        // assert
+        List<ScriptChunk> originalRedeemScriptChunks = getOriginalRedeemScriptChunks(redeemScriptWithFlyoverDerivationHash);
+        assertEquals(redeemScript.getChunks(), originalRedeemScriptChunks);
+
+        List<ScriptChunk> redeemScriptWithFlyoverDerivationHashChunks = redeemScriptWithFlyoverDerivationHash.getChunks();
+        ScriptChunk flyoverDerivationHashChunk = redeemScriptWithFlyoverDerivationHashChunks.get(0);
+        ScriptChunk opDropChunk = redeemScriptWithFlyoverDerivationHashChunks.get(1);
+        assertArrayEquals(flyoverDerivationHash.getBytes(), flyoverDerivationHashChunk.data);
+        assertEquals(ScriptOpCodes.OP_DROP, opDropChunk.opcode);
+    }
+
+    private List<ScriptChunk> getOriginalRedeemScriptChunks(Script redeemScript) {
+        List<ScriptChunk> redeemScriptChunks = redeemScript.getChunks();
+        int firstOriginalChunkIndex = 2;
+        int lastOriginalChunkIndex = redeemScriptChunks.size();
+
+        return redeemScriptChunks.subList(firstOriginalChunkIndex, lastOriginalChunkIndex);
+    }
+}
