7087d834f079c2cd744069a175beabefbc40fed7
julia-zack <julia.zack@iovlabs.org>
2024-08-19 10:25:27 -0300

    Remove mistaken reason

--- rskj-core/src/main/java/co/rsk/peg/bitcoin/FlyoverRedeemScriptCreationException.java
+++ rskj-core/src/main/java/co/rsk/peg/bitcoin/FlyoverRedeemScriptCreationException.java
@@ -4,8 +4,7 @@ public class FlyoverRedeemScriptCreationException extends RuntimeException {
     private final Reason reason;
 
     public enum Reason {
-        INVALID_FLYOVER_DERIVATION_HASH,
-        INVALID_CSV_VALUE
+        INVALID_FLYOVER_DERIVATION_HASH
     }
 
     public FlyoverRedeemScriptCreationException(String s, Reason reason) {
