8cb964f582ba3b4921d9040461bb363eed745f2d
Reynold Morel <reynold.morel@iovlabs.org>
2024-08-21 12:23:25 -0400

    modifying getConfigCapabilities()

--- rskj-core/src/main/java/org/ethereum/net/client/ConfigCapabilitiesImpl.java
+++ rskj-core/src/main/java/org/ethereum/net/client/ConfigCapabilitiesImpl.java
@@ -29,6 +29,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 import static org.ethereum.net.client.Capability.RSK;
 import static org.ethereum.net.client.Capability.SNAP;
@@ -79,7 +80,7 @@ public class ConfigCapabilitiesImpl implements ConfigCapabilities{
                 ret.add(capability);
             }
         }
-        return ret;
+        return allCapabilities.stream().collect(Collectors.toList());
     }
 
     /**
