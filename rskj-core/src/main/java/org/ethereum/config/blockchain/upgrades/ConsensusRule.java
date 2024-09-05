a1bfdfbb6616e4430a17e6fe942a0923879660a7
Nazaret Garcia <nazaret@iovlabs.org>
2024-09-05 10:57:34 -0300

    Adding basic structure

--- rskj-core/src/main/java/org/ethereum/config/blockchain/upgrades/ConsensusRule.java
+++ rskj-core/src/main/java/org/ethereum/config/blockchain/upgrades/ConsensusRule.java
@@ -98,7 +98,8 @@ public enum ConsensusRule {
     RSKIP427("rskip427"),
     RSKIP428("rskip428"),
     RSKIP434("rskip434"),
-    RSKIP438("rskip438")
+    RSKIP438("rskip438"),
+    RSKIP445("rskip445") // From EIP-5656 MCOPY instruction
     ;
 
     private final String configKey;
