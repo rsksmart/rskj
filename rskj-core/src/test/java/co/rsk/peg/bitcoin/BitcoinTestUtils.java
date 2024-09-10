63a4d3598fa3aa14b0629883b9c6e4f25c13f7a2
julia-zack <julia.zack@iovlabs.org>
2024-09-10 16:53:03 -0300

    Remove unused import

--- rskj-core/src/test/java/co/rsk/peg/bitcoin/BitcoinTestUtils.java
+++ rskj-core/src/test/java/co/rsk/peg/bitcoin/BitcoinTestUtils.java
@@ -7,7 +7,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import co.rsk.bitcoinj.script.ScriptChunk;
