6db07c5d1b0d2898aef8b013396f62b6dceb208e
julia-zack <julia.zack@iovlabs.org>
2024-09-10 11:17:09 -0300

    Add methods to create a real valid chain with stored blocks to avoid using mocks
    Refactor and renaming
    
    Modify method to receive a pmt since it is needed in registerBtcTransaction
    
    Improve comments
    
    Improve method name. Add transactionsInBlock argument for creating a pmt.
    
    Improve method to create a valid pmt for transactions following class implementation

--- rskj-core/src/test/java/co/rsk/peg/BridgeSupportTestUtil.java
+++ rskj-core/src/test/java/co/rsk/peg/BridgeSupportTestUtil.java
@@ -1,17 +1,19 @@
 package co.rsk.peg;
 
-import co.rsk.bitcoinj.core.BtcBlock;
-import co.rsk.bitcoinj.core.Sha256Hash;
-import co.rsk.bitcoinj.core.StoredBlock;
+import co.rsk.bitcoinj.core.*;
 import co.rsk.bitcoinj.store.BlockStoreException;
 import co.rsk.db.MutableTrieCache;
 import co.rsk.db.MutableTrieImpl;
+import co.rsk.peg.bitcoin.BitcoinTestUtils;
 import co.rsk.trie.Trie;
 import org.bouncycastle.util.encoders.Hex;
 import org.ethereum.core.Repository;
 import org.ethereum.db.MutableRepository;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
@@ -20,6 +22,55 @@ import static org.mockito.Mockito.when;
 public final class BridgeSupportTestUtil {
     public static Repository createRepository() {
         return new MutableRepository(new MutableTrieCache(new MutableTrieImpl(null, new Trie())));
+    }
+
+    public static PartialMerkleTree createValidPmtForTransactions(List<Sha256Hash> hashesToAdd, NetworkParameters networkParameters) {
+        byte[] relevantNodesBits = new byte[(int)Math.ceil(hashesToAdd.size() / 8.0)];
+        for (int i = 0; i < hashesToAdd.size(); i++) {
+            Utils.setBitLE(relevantNodesBits, i);
+        }
+
+        return PartialMerkleTree.buildFromLeaves(networkParameters, relevantNodesBits, hashesToAdd);
+    }
+
+    public static void recreateChainFromPmt(
+        BtcBlockStoreWithCache btcBlockStoreWithCache,
+        int chainHeight,
+        PartialMerkleTree partialMerkleTree,
+        int btcBlockWithPmtHeight,
+        NetworkParameters networkParameters
+    ) throws BlockStoreException {
+
+        // first create a block that has the wanted partial merkle tree
+        BtcBlock btcBlockWithPmt = createBtcBlockWithPmt(partialMerkleTree, networkParameters);
+        // store it on the chain at wanted height
+        StoredBlock storedBtcBlockWithPmt = new StoredBlock(btcBlockWithPmt, BigInteger.ONE, btcBlockWithPmtHeight);
+        btcBlockStoreWithCache.put(storedBtcBlockWithPmt);
+        btcBlockStoreWithCache.setMainChainBlock(btcBlockWithPmtHeight, btcBlockWithPmt.getHash());
+
+        // create and store a new chainHead at wanted chain height
+        Sha256Hash otherTransactionHash = Sha256Hash.of(Hex.decode("aa"));
+        PartialMerkleTree pmt = createValidPmtForTransactions(Collections.singletonList(otherTransactionHash), networkParameters);
+        BtcBlock chainHeadBlock = createBtcBlockWithPmt(pmt, networkParameters);
+        StoredBlock storedChainHeadBlock = new StoredBlock(chainHeadBlock, BigInteger.TEN, chainHeight);
+        btcBlockStoreWithCache.put(storedChainHeadBlock);
+        btcBlockStoreWithCache.setChainHead(storedChainHeadBlock);
+    }
+
+    private static BtcBlock createBtcBlockWithPmt(PartialMerkleTree pmt, NetworkParameters networkParameters) {
+        Sha256Hash prevBlockHash = BitcoinTestUtils.createHash(1);
+        Sha256Hash merkleRoot = pmt.getTxnHashAndMerkleRoot(new ArrayList<>());
+
+        return new co.rsk.bitcoinj.core.BtcBlock(
+            networkParameters,
+            1,
+            prevBlockHash,
+            merkleRoot,
+            1,
+            1,
+            1,
+            new ArrayList<>()
+        );
     }
 
     public static void mockChainOfStoredBlocks(BtcBlockStoreWithCache btcBlockStore, BtcBlock targetHeader, int headHeight, int targetHeight) throws BlockStoreException {
@@ -35,5 +86,4 @@ public final class BridgeSupportTestUtil {
         when(btcBlockStore.getChainHead()).thenReturn(currentStored);
         when(currentStored.getHeight()).thenReturn(headHeight);
     }
-
 }
