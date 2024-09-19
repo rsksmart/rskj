f451fa880799dc4aaa7868880625279ac03b72de
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2024-09-19 10:07:18 +0000

    refactor(peg): state for federator refactor including tests

--- rskj-core/src/test/java/co/rsk/peg/StateForFederatorTest.java
+++ rskj-core/src/test/java/co/rsk/peg/StateForFederatorTest.java
@@ -18,70 +18,65 @@
 
 package co.rsk.peg;
 
+import static co.rsk.peg.PegTestUtils.createHash3;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import co.rsk.bitcoinj.core.BtcTransaction;
 import co.rsk.bitcoinj.core.NetworkParameters;
 import co.rsk.crypto.Keccak256;
-import org.apache.commons.lang3.tuple.Pair;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
+import java.util.Arrays;
 import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
+import org.ethereum.util.RLP;
+import org.ethereum.util.RLPList;
+import org.junit.jupiter.api.Test;
 
-/**
- * Created by mario on 20/04/17.
- */
 class StateForFederatorTest {
 
-    private static final String SHA3_1 = "1111111111111111111111111111111111111111111111111111111111111111";
-    private static final String SHA3_2 = "2222222222222222222222222222222222222222222222222222222222222222";
-    private static final String SHA3_3 = "3333333333333333333333333333333333333333333333333333333333333333";
-    private static final String SHA3_4 = "4444444444444444444444444444444444444444444444444444444444444444";
-
-    private static final NetworkParameters NETWORK_PARAMETERS = NetworkParameters.fromID(NetworkParameters.ID_REGTEST);
+    private static final NetworkParameters NETWORK_PARAMETERS = NetworkParameters.fromID(NetworkParameters.ID_MAINNET);
 
     @Test
-    void serialize() {
-        Keccak256 hash1 = new Keccak256(SHA3_1);
-        Keccak256 hash2 = new Keccak256(SHA3_2);
-        Keccak256 hash3 = new Keccak256(SHA3_3);
-        Keccak256 hash4 = new Keccak256(SHA3_4);
+    void stateForFederator_whenSerializeAndDeserialize_shouldHaveEqualState() {
+        // Arrange
+        Keccak256 rskTxHash1 = createHash3(1);
+        Keccak256 rskTxHash2 = createHash3(2);
 
         BtcTransaction tx1 = new BtcTransaction(NETWORK_PARAMETERS);
         BtcTransaction tx2 = new BtcTransaction(NETWORK_PARAMETERS);
-        BtcTransaction tx3 = new BtcTransaction(NETWORK_PARAMETERS);
-        BtcTransaction tx4 = new BtcTransaction(NETWORK_PARAMETERS);
 
         SortedMap<Keccak256, BtcTransaction> rskTxsWaitingForSignatures = new TreeMap<>();
-        rskTxsWaitingForSignatures.put(hash1, tx1);
-        rskTxsWaitingForSignatures.put(hash2, tx2);
-
-        SortedMap<Keccak256, Pair<BtcTransaction, Long>> rskTxsWaitingForBroadcasting = new TreeMap<>();
-        rskTxsWaitingForBroadcasting.put(hash3, Pair.of(tx3, 3L));
-        rskTxsWaitingForBroadcasting.put(hash4, Pair.of(tx4, 4L));
+        rskTxsWaitingForSignatures.put(rskTxHash1, tx1);
+        rskTxsWaitingForSignatures.put(rskTxHash2, tx2);
 
         StateForFederator stateForFederator = new StateForFederator(rskTxsWaitingForSignatures);
 
-        byte[] encoded = stateForFederator.getEncoded();
+        // Act
+        byte[] rlpData = stateForFederator.encodeToRlp();
+        StateForFederator deserializedStateForFederator = fromRlpData(rlpData, NETWORK_PARAMETERS);
 
-        Assertions.assertTrue(encoded.length > 0);
-
-        StateForFederator reverseResult = new StateForFederator(encoded, NETWORK_PARAMETERS);
-
-        Assertions.assertNotNull(reverseResult);
-        Assertions.assertEquals(2, reverseResult.getRskTxsWaitingForSignatures().size());
-
-        Assertions.assertEquals(tx1, reverseResult.getRskTxsWaitingForSignatures().get(hash1));
-        Assertions.assertEquals(tx2, reverseResult.getRskTxsWaitingForSignatures().get(hash2));
-
-        Assertions.assertTrue(checkKeys(reverseResult.getRskTxsWaitingForSignatures().keySet(), hash1, hash2));
+        // Assert
+        assertNotNull(deserializedStateForFederator);
+        assertEquals(2, deserializedStateForFederator.getRskTxsWaitingForSignatures().size());
+        assertEquals(tx1, deserializedStateForFederator.getRskTxsWaitingForSignatures().get(rskTxHash1));
+        assertEquals(tx2, deserializedStateForFederator.getRskTxsWaitingForSignatures().get(rskTxHash2));
+        assertTrue(containsRskTxHashes(
+            deserializedStateForFederator.getRskTxsWaitingForSignatures().keySet(),
+            rskTxHash1, rskTxHash2));
     }
 
-    private boolean checkKeys(Set<Keccak256> keccak256s, Keccak256... keys) {
-        for(Keccak256 sha3 : keys)
-            if(!keccak256s.contains(sha3))
-                return false;
-        return true;
+    private static StateForFederator fromRlpData(byte[] rlpData, NetworkParameters parameters) {
+        RLPList rlpList = (RLPList) RLP.decode2(rlpData).get(0);
+        byte[] encodedWaitingForSign = rlpList.get(0).getRLPData();
+        SortedMap<Keccak256, BtcTransaction> rskTxsWaitingForSignatures = 
+            BridgeSerializationUtils.deserializeRskTxsWaitingForSignatures(encodedWaitingForSign, parameters, false);
+
+        return new StateForFederator(rskTxsWaitingForSignatures);
+    }
+
+    private static boolean containsRskTxHashes(Set<Keccak256> txHashes, Keccak256... requiredHashes) {
+        return Arrays.stream(requiredHashes).allMatch(txHashes::contains);
     }
 }
