3c8ac8d56c93c9409dab6fb652c2c93812ffe59e
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2024-09-23 10:03:40 +0000

    feat(peg): add getStateForSvpClient Bridge method

--- rskj-core/src/main/java/co/rsk/peg/BridgeMethods.java
+++ rskj-core/src/main/java/co/rsk/peg/BridgeMethods.java
@@ -465,6 +465,18 @@ public enum BridgeMethods {
         fixedPermission(true),
         CallTypeHelper.ALLOW_STATIC_CALL
     ),
+    GET_STATE_FOR_SVP_CLIENT(
+        CallTransaction.Function.fromSignature(
+                "getStateForSvpClient",
+                new String[]{},
+                new String[]{"bytes"}
+        ),
+        fixedCost(4000L),
+        (BridgeMethodExecutorTyped) Bridge::getStateForSvpClient,
+        activations -> activations.isActive(RSKIP419),
+        fixedPermission(true),
+        CallTypeHelper.ALLOW_STATIC_CALL
+    ),
     GET_STATE_FOR_DEBUGGING(
         CallTransaction.Function.fromSignature(
                 "getStateForDebugging",
