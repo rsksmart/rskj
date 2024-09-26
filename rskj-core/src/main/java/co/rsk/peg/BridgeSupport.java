729fca996a8a799206503287cefb7cc105e93bdc
julia-zack <julia.zack@iovlabs.org>
2024-09-26 16:50:13 -0300

    Rename method that calculates amount to be sent in svp spend tx. Replace redeem scripts being used in tests for some less shady and add tests for public method. Fix typo after rebase

--- rskj-core/src/main/java/co/rsk/peg/BridgeSupport.java
+++ rskj-core/src/main/java/co/rsk/peg/BridgeSupport.java
@@ -1073,7 +1073,7 @@ public class BridgeSupport {
         addSvpSpendTransactionInputs(svpSpendTransaction, svpFundTxSigned, proposedFederation);
 
         svpSpendTransaction.addOutput(
-            calculateAmountToSendToActiveFederation(proposedFederation),
+            calculateSvpSpendTxAmount(proposedFederation),
             federationSupport.getActiveFederationAddress()
         );
 
@@ -1088,7 +1088,7 @@ public class BridgeSupport {
         addInputFromMatchingOutputScript(svpSpendTransaction, svpFundTxSigned, flyoverProposedFederationOutputScript);
     }
 
-    private Coin calculateAmountToSendToActiveFederation(Federation proposedFederation) {
+    private Coin calculateSvpSpendTxAmount(Federation proposedFederation) {
         int svpSpendTransactionSize = calculatePegoutTxSize(activations, proposedFederation, 2, 1);
         long backupSizePercentage = (long) 1.2; // just to be sure the amount sent will be enough
 
