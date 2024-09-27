23d34205975d7067525f8c3c2cb3f971ba2ffc25
Marcos <marcos@iovlabs.org>
2024-09-27 13:16:52 -0300

    remove all test classes except for BitcoinUtilsTest

--- rskj-core/src/test/java/org/ethereum/crypto/signature/Secp256k1ServiceBCTest.java
+++
@@ -1,8 +0,0 @@
-package org.ethereum.crypto.signature;
-
-class Secp256k1ServiceBCTest extends Secp256k1ServiceTest {
-
-    public Secp256k1ServiceBCTest() {
-        super(new Secp256k1ServiceBC());
-    }
-}
