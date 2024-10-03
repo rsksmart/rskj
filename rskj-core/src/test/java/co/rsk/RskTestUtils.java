90eb9a6170dfa58f842df45c009a7f8e3c6eb6be
julia-zack <julia.zack@iovlabs.org>
2024-10-03 10:39:02 -0300

    Move rsk block creation method to RskTestUtils

--- rskj-core/src/test/java/co/rsk/RskTestUtils.java
+++ rskj-core/src/test/java/co/rsk/RskTestUtils.java
@@ -1,11 +1,17 @@
 package co.rsk;
 
 import co.rsk.crypto.Keccak256;
+import org.ethereum.config.blockchain.upgrades.ActivationConfig;
+import org.ethereum.core.Block;
+import org.ethereum.core.BlockHeader;
+import org.ethereum.core.BlockHeaderBuilder;
 import org.ethereum.crypto.ECKey;
 import org.ethereum.crypto.HashUtil;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
+
+import static org.mockito.Mockito.mock;
 
 public class RskTestUtils {
 
@@ -26,5 +32,14 @@ public class RskTestUtils {
         return Arrays.stream(seeds)
             .map(RskTestUtils::getEcKeyFromSeed)
             .toList();
+    }
+
+    public static Block createRskExecutionBlock(long rskExecutionBlockNumber, long rskExecutionBlockTimestamp) {
+        BlockHeader blockHeader = new BlockHeaderBuilder(mock(ActivationConfig.class))
+            .setNumber(rskExecutionBlockNumber)
+            .setTimestamp(rskExecutionBlockTimestamp)
+            .build();
+
+        return Block.createBlockFromHeader(blockHeader, true);
     }
 }
