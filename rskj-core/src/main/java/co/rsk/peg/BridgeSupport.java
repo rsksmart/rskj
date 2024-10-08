6ca9beca9564b455252666216a89c7a9e63037f5
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2024-10-08 10:04:10 +0000

    feat(peg): add getProposedFederatorPublicKeyOfType Bridge method
    feat(peg): add unit tests for getProposedFederatorPublicKeyOfType Bridge method
    
    feat(peg): add index out of bounds test for getting proposed fed pub key

--- rskj-core/src/main/java/co/rsk/peg/BridgeSupport.java
+++ rskj-core/src/main/java/co/rsk/peg/BridgeSupport.java
@@ -2263,6 +2263,10 @@ public class BridgeSupport {
         return federationSupport.getProposedFederationCreationBlockNumber();
     }
 
+    public Optional<byte[]> getProposedFederatorPublicKeyOfType(int index, FederationMember.KeyType keyType) {
+        return federationSupport.getProposedFederatorPublicKeyOfType(index, keyType);
+    }
+
     public Integer getLockWhitelistSize() {
         return whitelistSupport.getLockWhitelistSize();
     }
