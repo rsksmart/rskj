18e820642afacc2fe65cd4f5502b6e45154175bf
Nazaret Garcia <nazaret@iovlabs.org>
2024-11-29 15:27:22 -0300

    Updating opcode cost

--- rskj-core/src/main/java/org/ethereum/vm/OpCode.java
+++ rskj-core/src/main/java/org/ethereum/vm/OpCode.java
@@ -331,7 +331,7 @@ public enum OpCode {
     /**
      * (0x5e) Memory copying instruction
      */
-    MCOPY(0x5e, 3, 0, BASE_TIER),
+    MCOPY(0x5e, 3, 0, ZERO_TIER), // TODO UPDATE RSKIP
 
     /*  Push Operations */
     /**
