ecef8c4edac79b935727b47c4804d5ae0222b55b
Antonio Pancorbo <48168255+apancorb@users.noreply.github.com>
2024-12-02 10:24:56 +0000

    feat(peg): save svp spend tx signature back to Bridge storage

--- rskj-core/src/main/java/co/rsk/peg/BridgeSupport.java
+++ rskj-core/src/main/java/co/rsk/peg/BridgeSupport.java
@@ -1779,32 +1779,35 @@ public class BridgeSupport {
         BtcECKey proposedFederatorPublicKey,
         List<byte[]> signatures
     ) {
+        // This flow should never be reached. There should always be a proposed federation if svpIsOngoing.
         Federation proposedFederation = federationSupport.getProposedFederation()
-            // This flow should never be reached. There should always be a proposed federation if svpIsOngoing.
             .orElseThrow(() -> new IllegalStateException("Proposed federation must exist when trying to sign the svp spend transaction."));
+        // The svpSpendTxWFS should always be present at this point, since we already checked isTheSvpSpendTx.
+        Map.Entry<Keccak256, BtcTransaction> svpSpendTxWFS = provider.getSvpSpendTxWaitingForSignatures()
+            .orElseThrow(() -> new IllegalStateException("Svp spend tx waiting for signatures must exist"));
         FederationMember federationMember = proposedFederation.getMemberByBtcPublicKey(proposedFederatorPublicKey)
             .orElseThrow(() -> new IllegalStateException("Federator must belong to proposed federation to sign the svp spend transaction."));
 
-        provider.getSvpSpendTxWaitingForSignatures()
-            // The svpSpendTxWFS should always be present at this point, since we already checked isTheSvpSpendTx.
-            .ifPresent(svpSpendTxWFS -> {
-                Keccak256 svpSpendTxCreationRskTxHash = svpSpendTxWFS.getKey();
-                BtcTransaction svpSpendTx = svpSpendTxWFS.getValue();
+        Keccak256 svpSpendTxCreationRskTxHash = svpSpendTxWFS.getKey();
+        BtcTransaction svpSpendTx = svpSpendTxWFS.getValue();
 
-                if (!areSignaturesEnoughToSignAllTxInputs(svpSpendTx, signatures)) {
-                    return;
-                }
+        if (!areSignaturesEnoughToSignAllTxInputs(svpSpendTx, signatures)) {
+            return;
+        }
 
-                processSigning(federationMember, signatures, svpSpendTxCreationRskTxHash, svpSpendTx);
+        processSigning(federationMember, signatures, svpSpendTxCreationRskTxHash, svpSpendTx);
+       
+        // save current fed signature back in storage
+        svpSpendTxWFS.setValue(svpSpendTx);
+        provider.setSvpSpendTxWaitingForSignatures(svpSpendTxWFS);
 
-                if (!BridgeUtils.hasEnoughSignatures(btcContext, svpSpendTx)) {
-                    logMissingSignatures(svpSpendTx, svpSpendTxCreationRskTxHash, proposedFederation);
-                    return;
-                }
+        if (!BridgeUtils.hasEnoughSignatures(btcContext, svpSpendTx)) {
+            logMissingSignatures(svpSpendTx, svpSpendTxCreationRskTxHash, proposedFederation);
+            return;
+        }
 
-                logReleaseBtc(svpSpendTx, svpSpendTxCreationRskTxHash.getBytes());
-                provider.setSvpSpendTxWaitingForSignatures(null);
-            });
+        logReleaseBtc(svpSpendTx, svpSpendTxCreationRskTxHash.getBytes());
+        provider.setSvpSpendTxWaitingForSignatures(null);
     }
 
     private boolean areSignaturesEnoughToSignAllTxInputs(BtcTransaction releaseTx, List<byte[]> signatures) {
