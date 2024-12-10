71108ccd6df885ed1418a9707d972d4f8e73cff4
Nazaret Garcia <nazaret@iovlabs.org>
2024-12-09 22:36:26 -0300

    Applying sonar suggestions

--- rskj-core/src/test/java/co/rsk/vm/opcode/MCopyGasTest.java
+++ rskj-core/src/test/java/co/rsk/vm/opcode/MCopyGasTest.java
@@ -26,7 +26,7 @@ import static org.ethereum.config.blockchain.upgrades.ConsensusRule.RSKIP445;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class MCopyGasTest {
+class MCopyGasTest {
 
     private ActivationConfig.ForBlock activationConfig;
 
