2b6b9ff88158cb0523229f5521e57f923ed41689
nathanieliov <nathaniel.calderon@iovlabs.org>
2024-12-19 18:12:38 -0400

    Added RSKIP459 activation code

--- rskj-core/src/test/java/org/ethereum/config/blockchain/upgrades/ActivationConfigTest.java
+++ rskj-core/src/test/java/org/ethereum/config/blockchain/upgrades/ActivationConfigTest.java
@@ -131,6 +131,7 @@ class ActivationConfigTest {
         "    rskip446: lovell700",
         "    rskip453: lovell700",
         "    rskip454: lovell700",
+        "    rskip459: lovell700",
         "}"
     ));
 
