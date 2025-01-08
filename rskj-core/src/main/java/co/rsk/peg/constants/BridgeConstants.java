616a4bfbb7a1112461976599e6459f8930d45c10
julia-zack <julia.zack@iovlabs.org>
2025-01-08 19:10:56 -0300

    Remove spendableValueFromProposedFed constant and use minPegoutTxValue directly

--- rskj-core/src/main/java/co/rsk/peg/constants/BridgeConstants.java
+++ rskj-core/src/main/java/co/rsk/peg/constants/BridgeConstants.java
@@ -105,8 +105,6 @@ public abstract class BridgeConstants {
 
     public Coin getMinimumPegoutTxValue() { return minimumPegoutTxValue; }
 
-    public Coin getSpendableValueFromProposedFederation() { return minimumPegoutTxValue.multiply(2); }
-
     public Keccak256 getProposedFederationFlyoverPrefix() { return new Keccak256("0000000000000000000000000000000000000000000000000000000000000001"); }
 
     public Coin getMaxRbtc() { return Coin.valueOf(21_000_000, 0); }
