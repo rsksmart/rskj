b91418d7915a58a85f84f4972896a4c0994a77a1
Julian Len <julian@iovlabs.org>
2025-01-14 18:42:19 -0300

    fix: replace the variable fedECKeys by fedBtcECKeys in the BridgeCostsTest

--- rskj-core/src/test/java/co/rsk/net/handler/txvalidator/TxValidatorIntrinsicGasLimitValidatorTest.java
+++ rskj-core/src/test/java/co/rsk/net/handler/txvalidator/TxValidatorIntrinsicGasLimitValidatorTest.java
@@ -17,7 +17,7 @@
  */
 package co.rsk.net.handler.txvalidator;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import  static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import co.rsk.bitcoinj.core.BtcECKey;
