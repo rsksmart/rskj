ee76af95fa3206ceebcb4597fc3a047c55bd2cb6
Volodymyr Kravets <volodymyr.kravets@outlook.com>
2025-03-28 17:42:56 +0200

    feat(superchain): add posibility to store super chain data hash in header ext

--- rskj-core/src/main/java/org/ethereum/core/BlockHeaderV1.java
+++ rskj-core/src/main/java/org/ethereum/core/BlockHeaderV1.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of RskJ
+ * Copyright (C) 2024 RSK Labs Ltd.
+ * (derived from ethereumJ library, Copyright (c) 2016 <ether.camp>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.ethereum.core;
 
 import co.rsk.core.BlockDifficulty;
@@ -18,22 +37,40 @@ public class BlockHeaderV1 extends BlockHeader {
                          Coin paidFees, byte[] bitcoinMergedMiningHeader, byte[] bitcoinMergedMiningMerkleProof,
                          byte[] bitcoinMergedMiningCoinbaseTransaction, byte[] mergedMiningForkDetectionData,
                          Coin minimumGasPrice, int uncleCount, boolean sealed,
-                         boolean useRskip92Encoding, boolean includeForkDetectionData, byte[] ummRoot, byte[] superChainDataHash, SuperBlockResolver isSuper, short[] txExecutionSublistsEdges, boolean compressed) {
+                         boolean useRskip92Encoding, boolean includeForkDetectionData, byte[] ummRoot, byte[] superChainDataHash,
+                         SuperBlockResolver isSuper, short[] txExecutionSublistsEdges, boolean compressed) {
+        this(parentHash,unclesHash, coinbase, stateRoot,
+                txTrieRoot, receiptTrieRoot, compressed ? extensionData : null, difficulty,
+                number, gasLimit, gasUsed, timestamp, extraData,
+                paidFees, bitcoinMergedMiningHeader, bitcoinMergedMiningMerkleProof,
+                bitcoinMergedMiningCoinbaseTransaction, mergedMiningForkDetectionData,
+                minimumGasPrice, uncleCount, sealed,
+                useRskip92Encoding, includeForkDetectionData, ummRoot, isSuper,
+                makeExtension(compressed, extensionData, txExecutionSublistsEdges, superChainDataHash), compressed);
+    }
+
+    public BlockHeaderV1(byte[] parentHash, byte[] unclesHash, RskAddress coinbase, byte[] stateRoot,
+                         byte[] txTrieRoot, byte[] receiptTrieRoot, byte[] extensionData, BlockDifficulty difficulty,
+                         long number, byte[] gasLimit, long gasUsed, long timestamp, byte[] extraData,
+                         Coin paidFees, byte[] bitcoinMergedMiningHeader, byte[] bitcoinMergedMiningMerkleProof,
+                         byte[] bitcoinMergedMiningCoinbaseTransaction, byte[] mergedMiningForkDetectionData,
+                         Coin minimumGasPrice, int uncleCount, boolean sealed,
+                         boolean useRskip92Encoding, boolean includeForkDetectionData, byte[] ummRoot,
+                         SuperBlockResolver isSuper, BlockHeaderExtensionV1 extension, boolean compressed) {
         super(parentHash,unclesHash, coinbase, stateRoot,
                 txTrieRoot, receiptTrieRoot, compressed ? extensionData : null, difficulty,
                 number, gasLimit, gasUsed, timestamp, extraData,
                 paidFees, bitcoinMergedMiningHeader, bitcoinMergedMiningMerkleProof,
                 bitcoinMergedMiningCoinbaseTransaction, mergedMiningForkDetectionData,
                 minimumGasPrice, uncleCount, sealed,
-                useRskip92Encoding, includeForkDetectionData, ummRoot, superChainDataHash, isSuper);
-        this.extension = compressed
-                ? new BlockHeaderExtensionV1(null, null)
-                : new BlockHeaderExtensionV1(extensionData, txExecutionSublistsEdges);
-        if(!compressed) {
-            this.updateExtensionData(); // update after calculating
+                useRskip92Encoding, includeForkDetectionData, ummRoot, isSuper);
+        this.extension = extension;
+        if (!compressed) {
+            // update after calculating
+            this.extensionData = createExtensionData(extension);
         }
-
     }
+
     @Override
     public byte getVersion() { return 0x1; }
 
@@ -43,15 +80,23 @@ public class BlockHeaderV1 extends BlockHeader {
     public void setExtension(BlockHeaderExtension extension) { this.extension = (BlockHeaderExtensionV1) extension; }
 
     @VisibleForTesting
-    public static byte[] createExtensionData(byte[] extensionHash) {
+    public static byte[] createExtensionData(BlockHeaderExtension extension) {
         return RLP.encodeList(
-                RLP.encodeByte((byte) 0x1),
-                RLP.encodeElement(extensionHash)
+                RLP.encodeByte(extension.getVersion()),
+                RLP.encodeElement(extension.getHash())
         );
     }
 
-    private void updateExtensionData() {
-        this.extensionData = BlockHeaderV1.createExtensionData(this.extension.getHash());
+    private static BlockHeaderExtensionV1 makeExtension(boolean compressed,
+                                                        byte[] extensionData, short[] txExecutionSublistsEdges,
+                                                        byte[] superChainDataHash) {
+        return compressed
+                ? new BlockHeaderExtensionV1(null, null)
+                : new BlockHeaderExtensionV1(extensionData, txExecutionSublistsEdges);
+    }
+
+    protected void updateExtensionData() {
+        this.extensionData = createExtensionData(this.extension);
     }
 
     @Override
@@ -80,6 +125,18 @@ public class BlockHeaderV1 extends BlockHeader {
 
         this.extension.setTxExecutionSublistsEdges(edges != null ? Arrays.copyOf(edges, edges.length) : null);
         this.updateExtensionData();
+    }
+
+    @Override
+    public byte[] getSuperChainDataHash() {
+        return null;
+    }
+
+    @Override
+    public void setSuperChainDataHash(byte[] superChainDataHash) {
+        if (superChainDataHash != null) {
+            throw new UnsupportedOperationException("Block header v1 does not support super chain data hash");
+        }
     }
 
     @Override
