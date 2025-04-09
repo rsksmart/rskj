8aef4a6bc9d82afbb107df2ce81a6c0375b1d8aa
nathanieliov <nathaniel.calderon@iovlabs.org>
2025-04-09 13:50:57 -0400

    - Rearrange enum entries - Use unique code for each entry - Update comment descriptions

--- rskj-core/src/main/java/co/rsk/peg/union/UnionResponseCode.java
+++ rskj-core/src/main/java/co/rsk/peg/union/UnionResponseCode.java
@@ -1,20 +1,18 @@
 package co.rsk.peg.union;
 
 public enum UnionResponseCode {
-    GENERIC_ERROR(-10),
+    SUCCESS(0),
     UNAUTHORIZED_CALLER(-1),
-
-    // Environment restriction when prevent union bridge address being updated on production
-    ENVIRONMENT_DISABLED(-2),
-
-    // Invalid value when increasing union bridge locking cap
+    // Invalid value when The amount requested, combined with previously requested amounts, exceeds the current locking cap value.
+    // or invalid value when increasing union bridge locking cap
     INVALID_VALUE(-2),
-
-    // Response codes when disabling union bridge operations
-    ALREADY_PAUSED(-2),
-    ALREADY_UNPAUSED(-2),
-
-    SUCCESS(0);
+    // Response codes when enabling or disabling union bridge operations
+    ALREADY_PAUSED(-3),
+    ALREADY_UNPAUSED(-4),
+    // Environment restriction when prevent union bridge address being updated on production
+    ENVIRONMENT_DISABLED(-5),
+    GENERIC_ERROR(-10)
+    ;
 
     private final int code;
 
