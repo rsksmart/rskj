d852f58de2f546905d38dfcedc96ea512099c322
nathanieliov <nathaniel.calderon@iovlabs.org>
2025-04-10 12:38:51 -0400

    Add RSKIP502 activation code

--- rskj-core/src/test/java/org/ethereum/config/blockchain/upgrades/ActivationConfigsForTest.java
+++ rskj-core/src/test/java/org/ethereum/config/blockchain/upgrades/ActivationConfigsForTest.java
@@ -189,6 +189,7 @@ public class ActivationConfigsForTest {
     private static List<ConsensusRule> getReed800Rskips() {
         return new ArrayList<>(List.of(
             ConsensusRule.RSKIP305,
+            ConsensusRule.RSKIP502,
             ConsensusRule.RSKIP516
 
         ));
