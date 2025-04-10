ba92f6cf8dac3e69942ea3f7739903b3a79a8513
Julian Len <julian@iovlabs.org>
2025-04-10 22:48:04 +0200

    fix: modified so the fed has 9 member keys

--- rskj-core/src/test/java/co/rsk/peg/bitcoin/BitcoinUtilsTest.java
+++ rskj-core/src/test/java/co/rsk/peg/bitcoin/BitcoinUtilsTest.java
@@ -46,6 +46,8 @@ class BitcoinUtilsTest {
     // witness commitment output script in non-standard format
     private static final BtcTransaction coinbaseTxWithWitnessCommitmentOutputInNonStandardFormat = new BtcTransaction(btcMainnetParams, Hex.decode("010000000001010000000000000000000000000000000000000000000000000000000000000000ffffffff32030a0e250004fee5346404196a763b0cc3dd196400000000000000000a636b706f6f6c0e2f6d696e65642062792072736b2fffffffff039cce2a00000000001976a914ec2f9ffaba0d68ea6bd7c25cedfe2ae710938e6088ac0000000000000000266a24aa21a9ede46b6d3bc71412e8905cedfad91532bdccb693d93a1335fee0b6223a7ed1ee5b00000000000000002a6a24aa21a9ed4f434b3a8bc552daa25a7a473ac4640ba2b9d621c95652c61488143ca02bbf1b00392fb10120000000000000000000000000000000000000000000000000000000000000000000000000"));
 
+    private static final  List<BtcECKey> activeFederationMembersKeys = BitcoinTestUtils.getBtcEcKeysFromSeeds(new String[]{"fa01", "fa02", "fa03", "fa04", "fa05", "fa06", "fa07", "fa08", "fa09"}, true);
+
     private boolean isSigHashValid(Sha256Hash sigHash, List<BtcECKey> pubKeys, List<BtcECKey.ECDSASignature> signatures) {
         LinkedList<BtcECKey> keys = new LinkedList<>(pubKeys);
         LinkedList<BtcECKey.ECDSASignature> sigs = new LinkedList<>(signatures);
@@ -316,8 +318,7 @@ class BitcoinUtilsTest {
         Script retiringFedRedeemScript = retiringFederation.getRedeemScript();
         Script emptyScript = new Script(new byte[]{});
 
-        List<BtcECKey> newFederationMemberKey = BitcoinTestUtils.getBtcEcKeysFromSeeds(new String[]{"fa01"}, true);
-        Federation activeFederation = P2shP2wshErpFederationBuilder.builder().withMembersBtcPublicKeys(newFederationMemberKey).build();
+        Federation activeFederation = P2shP2wshErpFederationBuilder.builder().withMembersBtcPublicKeys(activeFederationMembersKeys).build();
         Address activeFederationAddress = activeFederation.getAddress();
 
         int numberOfInputs = 3;
@@ -405,15 +406,15 @@ class BitcoinUtilsTest {
 
         int outputIndex = 0;
         int nHash = 0;
-        List<BtcECKey> newFederationMemberKey = BitcoinTestUtils.getBtcEcKeysFromSeeds(new String[]{"fa01"}, true);
-        Federation retiringFederation = P2shP2wshErpFederationBuilder.builder().withMembersBtcPublicKeys(newFederationMemberKey).build();
+
+        Federation retiringFederation = P2shP2wshErpFederationBuilder.builder().build();
         Script retiringFedRedeemScript = retiringFederation.getRedeemScript();
         Script emptyScript = new Script(new byte[]{});
         migrationTx.addInput(BitcoinTestUtils.createHash(nHash), outputIndex, emptyScript);
         TransactionWitness witness = createBaseWitnessThatSpendsFromRedeemScript(retiringFedRedeemScript);
         migrationTx.setWitness(FIRST_INPUT_INDEX, witness);
 
-        Federation activeFederation = P2shP2wshErpFederationBuilder.builder().build();
+        Federation activeFederation = P2shP2wshErpFederationBuilder.builder().withMembersBtcPublicKeys(activeFederationMembersKeys).build();
         Address activeFederationAddress = activeFederation.getAddress();
         migrationTx.addOutput(Coin.COIN, activeFederationAddress);
 
