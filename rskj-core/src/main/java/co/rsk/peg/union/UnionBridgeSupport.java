dbe0bdc0ce330c90345a29a5d3dc13ac738c0e8d
nathanieliov <nathaniel.calderon@iovlabs.org>
2025-04-11 11:19:09 -0400

    - Add log for when entering the setUnionBridgeContractAddressForTestnet method - Add check to only store address when RSKIP502 is active - Update UnionBridgeStorageProvider tests - Add tests for UnionBridgeSupport

--- rskj-core/src/main/java/co/rsk/peg/union/UnionBridgeSupport.java
+++ rskj-core/src/main/java/co/rsk/peg/union/UnionBridgeSupport.java
@@ -1,8 +1,10 @@
 package co.rsk.peg.union;
 
 import co.rsk.core.RskAddress;
+import org.ethereum.config.blockchain.upgrades.ActivationConfig;
 import org.ethereum.core.Transaction;
 
 public interface UnionBridgeSupport {
     int setUnionBridgeContractAddressForTestnet(Transaction tx, RskAddress unionBridgeContractAddress);
+    void save(ActivationConfig.ForBlock activations);
 }
