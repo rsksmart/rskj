1473fa18bd69641f15a3874ef1b4502a522581b7
nathanieliov <nathaniel.calderon@iovlabs.org>
2025-04-11 00:20:59 -0400

    - Add UnionBridgeSupport - Implement setUnionBridgeContractAddressForTestnet method

---
+++ rskj-core/src/main/java/co/rsk/peg/union/UnionBridgeSupport.java
@@ -0,0 +1,8 @@
+package co.rsk.peg.union;
+
+import co.rsk.core.RskAddress;
+import org.ethereum.core.Transaction;
+
+public interface UnionBridgeSupport {
+    int setUnionBridgeContractAddressForTestnet(Transaction tx, RskAddress unionBridgeContractAddress);
+}
