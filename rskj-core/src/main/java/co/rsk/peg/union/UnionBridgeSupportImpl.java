5b12a49122601d9cbec27e1014aa265e59a82c9e
nathanieliov <nathaniel.calderon@iovlabs.org>
2025-04-11 11:19:09 -0400

    - Add log for when entering the setUnionBridgeContractAddressForTestnet method - Add check to only store address when RSKIP502 is active - Update UnionBridgeStorageProvider tests - Add tests for UnionBridgeSupport

--- rskj-core/src/main/java/co/rsk/peg/union/UnionBridgeSupportImpl.java
+++ rskj-core/src/main/java/co/rsk/peg/union/UnionBridgeSupportImpl.java
@@ -1,10 +1,10 @@
 package co.rsk.peg.union;
 
-import co.rsk.bitcoinj.core.Context;
 import co.rsk.bitcoinj.core.NetworkParameters;
 import co.rsk.core.RskAddress;
 import co.rsk.peg.union.constants.UnionBridgeConstants;
 import co.rsk.peg.vote.AddressBasedAuthorizer;
+import org.ethereum.config.blockchain.upgrades.ActivationConfig.ForBlock;
 import org.ethereum.core.SignatureCache;
 import org.ethereum.core.Transaction;
 import org.slf4j.Logger;
@@ -18,45 +18,55 @@ public class UnionBridgeSupportImpl implements UnionBridgeSupport {
     private final UnionBridgeConstants constants;
     private final UnionBridgeStorageProvider storageProvider;
     private final SignatureCache signatureCache;
-    private final Context btcContext;
 
     public UnionBridgeSupportImpl(UnionBridgeConstants constants, UnionBridgeStorageProvider storageProvider,
-        SignatureCache signatureCache, Context btcContext) {
+        SignatureCache signatureCache) {
         this.constants = constants;
         this.storageProvider = storageProvider;
         this.signatureCache = signatureCache;
-        this.btcContext = btcContext;
     }
 
     @Override
     public int setUnionBridgeContractAddressForTestnet(Transaction tx, RskAddress unionBridgeContractAddress) {
         final String SET_UNION_BRIDGE_ADDRESS_TAG = "setUnionBridgeContractAddressForTestnet";
 
-        // Check environment is not mainnet
-        if (btcContext.getParams().getId().equals(NetworkParameters.ID_MAINNET)) {
-            String baseMessage = String.format("Union Bridge Contract Address can only be set in Testnet and RegTest environments. Current network: %s", btcContext.getParams().getId());
-            logger.warn("[{}] {}", SET_UNION_BRIDGE_ADDRESS_TAG, baseMessage);
-            return UnionResponseCode.ENVIRONMENT_DISABLED.getCode();
-        }
+        logger.info("[{}] Setting new union bridge contract address: {}", SET_UNION_BRIDGE_ADDRESS_TAG, unionBridgeContractAddress);
+        try {
+            // Check if the network is mainnet as the contract address can only be set in testnet or regtest
+            String currentNetworkId = constants.getBtcParams().getId();
+            if (currentNetworkId.equals(NetworkParameters.ID_MAINNET)) {
+                String baseMessage = String.format("Union Bridge Contract Address can only be set in Testnet and RegTest environments. Current network: %s", currentNetworkId);
+                logger.warn("[{}] {}", SET_UNION_BRIDGE_ADDRESS_TAG, baseMessage);
+                return UnionResponseCode.ENVIRONMENT_DISABLED.getCode();
+            }
 
-        // Only pre-configured addresses can modify Locking Cap
-        AddressBasedAuthorizer authorizer = constants.getUnionBridgeChangeAuthorizer();
-        if (!authorizer.isAuthorized(tx, signatureCache)) {
-            String baseMessage = String.format("An unauthorized caller tried to set Union Bridge Contract Address. Caller address: %s", tx.getSender());
-            logger.warn("[{}] {}}", SET_UNION_BRIDGE_ADDRESS_TAG, baseMessage);
-            return UnionResponseCode.UNAUTHORIZED_CALLER.getCode();
-        }
+            // Check if the caller is authorized to set a new bridge contract address
+            AddressBasedAuthorizer authorizer = constants.getChangeAuthorizer();
+            if (!authorizer.isAuthorized(tx, signatureCache)) {
+                String baseMessage = String.format("Caller is not authorized to update union bridge contract address. Caller address: %s", tx.getSender());
+                logger.warn("[{}] {}}", SET_UNION_BRIDGE_ADDRESS_TAG, baseMessage);
+                return UnionResponseCode.UNAUTHORIZED_CALLER.getCode();
+            }
 
-        // Check address is not already set
-        RskAddress currentUnionBridgeAddress = storageProvider.getAddress().orElse(constants.getUnionBridgeAddress());
-        if (unionBridgeContractAddress.equals(currentUnionBridgeAddress)) {
-            String baseMessage = String.format("Union Bridge Contract Address is already set to the same value. Current address: %s", currentUnionBridgeAddress);
-            logger.warn("[{}] {}", SET_UNION_BRIDGE_ADDRESS_TAG, baseMessage);
+            // Check if the address is already set
+            RskAddress currentUnionBridgeAddress = storageProvider.getAddress().orElse(constants.getAddress());
+            if (unionBridgeContractAddress.equals(currentUnionBridgeAddress)) {
+                String baseMessage = String.format("The given union bridge contract address is already the current address. Current address: %s", currentUnionBridgeAddress);
+                logger.warn("[{}] {}", SET_UNION_BRIDGE_ADDRESS_TAG, baseMessage);
+                return UnionResponseCode.GENERIC_ERROR.getCode();
+            }
+
+            storageProvider.setAddress(unionBridgeContractAddress);
+            logger.info("[{}] Union Bridge Contract Address has been set to: {}", SET_UNION_BRIDGE_ADDRESS_TAG, unionBridgeContractAddress);
+            return UnionResponseCode.SUCCESS.getCode();
+        } catch (Exception e) {
+            logger.error("[{}] Unexpected error setting Union Bridge Contract Address: {}", SET_UNION_BRIDGE_ADDRESS_TAG, e.getMessage(), e);
             return UnionResponseCode.GENERIC_ERROR.getCode();
         }
+    }
 
-        storageProvider.setAddress(unionBridgeContractAddress);
-        logger.info("[{}] Union Bridge Contract Address set to: {}", SET_UNION_BRIDGE_ADDRESS_TAG, unionBridgeContractAddress);
-        return UnionResponseCode.SUCCESS.getCode();
+    @Override
+    public void save(ForBlock activations) {
+        this.storageProvider.save(activations);
     }
 }
