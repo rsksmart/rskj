448e50a8ece060e8668ac74c53ca3da75eeaf6b0
Volodymyr Kravets <volodymyr.kravets@outlook.com>
2025-04-14 12:59:32 +0300

    feat(snap): remove unused import

--- rskj-core/src/main/java/co/rsk/net/SnapshotProcessor.java
+++ rskj-core/src/main/java/co/rsk/net/SnapshotProcessor.java
@@ -49,7 +49,6 @@ import org.ethereum.util.RLPList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.math.BigInteger;
 import java.util.*;
