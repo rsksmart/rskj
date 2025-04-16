e81799670d906b033e0fc00d85bf9dbbf0c5ba1a
Reynold Morel <reynold.morel@iovlabs.org>
2025-04-16 10:25:21 -0400

    Refactored the code

--- validate-docker-container.sh
+++ validate-docker-container.sh
@@ -1,34 +1,40 @@
 #!/bin/sh
+set -e
 
-if [ "$(docker inspect -f '{{.State.Running}}' rskj-container)" = "true" ]; then
-    max_attempts=20
-    attempts=0
-    count=0
+docker build -t rsksmart/rskj:latest .
+container_id=$(docker run -d --name rskj-container rsksmart/rskj:latest --regtest)
+trap 'docker stop "$container_id"' EXIT INT QUIT TERM
 
-    while [ $attempts -lt $max_attempts ]; do
-        docker logs rskj-container > logs.txt
+# Give a couple of seconds for the container to start.
+sleep 5
 
-        count=$(grep -c "IMPORTED_BEST" logs.txt)
-
-        if [ $count -gt 0 ]; then
-        echo "✅ Found 'IMPORTED_BEST' block in logs!"
-        break
-        fi
-
-        echo "Waiting for 5 seconds for blocks to be imported..."
-        sleep 5
-        echo "Continuing to check imported blocks"
-
-        attempts=$(($attempts + 1))
-    done
-
-    if [ $count -le 0 ]; then
-        echo "❌ 'IMPORTED_BEST' block not found in logs after $max_attempts attempts"
-        exit 1
-    fi
-
-    echo "Container is running properly"
-else
+if [ "$(docker inspect -f '{{.State.Running}}' rskj-container)" != "true" ]; then
     echo "Container failed to start"
     exit 1
 fi
+
+max_attempts=20
+attempts=0
+count=0
+
+while [ $attempts -lt $max_attempts ]; do
+    count=$(docker logs rskj-container | grep -c "IMPORTED_BEST")
+
+    if [ $count -gt 0 ]; then
+      echo "✅ Found 'IMPORTED_BEST' block in logs!"
+      break
+    fi
+
+    echo "Waiting for 5 seconds for blocks to be imported..."
+    sleep 5
+    echo "Continuing to check imported blocks"
+
+    attempts=$(($attempts + 1))
+done
+
+if [ $count -le 0 ]; then
+    echo "❌ 'IMPORTED_BEST' block not found in logs after $max_attempts attempts"
+    exit 1
+fi
+
+echo "Container is running properly"
