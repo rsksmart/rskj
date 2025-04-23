097fab093cf3646d11c6f5beff1cc0e0fb45dbe0
nathanieliov <nathaniel.calderon@iovlabs.org>
2025-04-23 13:09:11 -0400

    - Fixed and refactor Union Bridge Support and provider tests. - Remove unneeded param from UnionBridgeSupportImpl.save method - Add missing test cases for null and empty given address

--- rskj-core/src/main/java/co/rsk/peg/union/UnionBridgeSupport.java
+++ rskj-core/src/main/java/co/rsk/peg/union/UnionBridgeSupport.java
@@ -1,10 +1,9 @@
 package co.rsk.peg.union;
 
 import co.rsk.core.RskAddress;
-import org.ethereum.config.blockchain.upgrades.ActivationConfig;
 import org.ethereum.core.Transaction;
 
 public interface UnionBridgeSupport {
     int setUnionBridgeContractAddressForTestnet(Transaction tx, RskAddress unionBridgeContractAddress);
-    void save(ActivationConfig.ForBlock activations);
+    void save();
 }
