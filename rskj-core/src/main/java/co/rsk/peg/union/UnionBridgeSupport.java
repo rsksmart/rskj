6e041b2d7eb5f2151ccc8dea3b0b70c29e1f737d
nathanieliov <nathaniel.calderon@iovlabs.org>
2025-04-29 23:30:12 -0400

    Add UnionBridgeSupport.getLockingCap implementation

--- rskj-core/src/main/java/co/rsk/peg/union/UnionBridgeSupport.java
+++ rskj-core/src/main/java/co/rsk/peg/union/UnionBridgeSupport.java
@@ -1,9 +1,12 @@
 package co.rsk.peg.union;
 
+import co.rsk.bitcoinj.core.Coin;
 import co.rsk.core.RskAddress;
+import java.util.Optional;
 import org.ethereum.core.Transaction;
 
 public interface UnionBridgeSupport {
     int setUnionBridgeContractAddressForTestnet(Transaction tx, RskAddress unionBridgeContractAddress);
+    Optional<Coin> getLockingCap();
     void save();
 }
