5018146db57a8264fb68179f9340a5f426331e43
nathanieliov <nathaniel.calderon@iovlabs.org>
2025-05-27 09:38:24 -0400

    Refactor test method name and fix new locking cap calculation

--- rskj-core/src/test/java/co/rsk/peg/BridgeSupportTest.java
+++ rskj-core/src/test/java/co/rsk/peg/BridgeSupportTest.java
@@ -865,7 +865,7 @@ class BridgeSupportTest {
         }
 
         @Test
-        void getUnionBridgeLockingCap_beforeRskip502_shouldReturnEmpty() {
+        void getUnionBridgeLockingCap_beforeRskip502_shouldReturnNegativeOne() {
             // arrange
             bridgeSupport = bridgeSupportBuilder
                 .withActivations(lovell)
@@ -935,7 +935,7 @@ class BridgeSupportTest {
                 .build();
 
             Coin initialLockingCap = unionBridgeConstants.getInitialLockingCap();
-            Coin newLockingCap = initialLockingCap.multiply(unionBridgeConstants.getLockingCapIncrementsMultiplier()).minus(Coin.SATOSHI);
+            Coin newLockingCap = initialLockingCap.multiply(unionBridgeConstants.getLockingCapIncrementsMultiplier());
 
             // act
             UnionResponseCode actualResult = bridgeSupport.increaseUnionBridgeLockingCap(
