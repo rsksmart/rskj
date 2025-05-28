cda8992714b169cf07926d2506e7c4023becc8d1
Nazaret Garcia <nazaret@iovlabs.org>
2025-05-27 23:01:59 -0300

    Extracting dependency

--- rskj-core/src/main/java/co/rsk/RskContext.java
+++ rskj-core/src/main/java/co/rsk/RskContext.java
@@ -261,6 +261,7 @@ public class RskContext implements NodeContext, NodeBootstrapper {
     private GasPriceTracker gasPriceTracker;
     private BlockChainFlusher blockChainFlusher;
     private MinGasPriceProvider minGasPriceProvider;
+    private StateOverrideApplier stateOverrideApplier;
     private final Map<String, DbKind> dbPathToDbKindMap = new HashMap<>();
 
     private volatile boolean closed;
@@ -519,6 +520,16 @@ public class RskContext implements NodeContext, NodeBootstrapper {
         return bridgeSupportFactory;
     }
 
+    public synchronized StateOverrideApplier getStateOverrideApplier() {
+        checkIfNotClosed();
+
+        if (stateOverrideApplier == null) {
+            stateOverrideApplier = new DefaultStateOverrideApplier();
+        }
+
+        return stateOverrideApplier;
+    }
+
     public synchronized BtcBlockStoreWithCache.Factory getBtcBlockStoreFactory() {
         checkIfNotClosed();
 
@@ -719,7 +730,8 @@ public class RskContext implements NodeContext, NodeBootstrapper {
                     getEthModuleTransaction(),
                     getBridgeSupportFactory(),
                     getRskSystemProperties().getGasEstimationCap(),
-                    getRskSystemProperties().getCallGasCap()
+                    getRskSystemProperties().getCallGasCap(),
+                    getStateOverrideApplier()
             );
         }
 
