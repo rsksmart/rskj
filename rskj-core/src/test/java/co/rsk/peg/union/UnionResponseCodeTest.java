370f042d7b3e613241d491425dfb14bf19bef16f
nathanieliov <nathaniel.calderon@iovlabs.org>
2025-06-13 14:22:55 -0400

    Rename response codes to request_disabled and release_disabled and update corresponding test method names

--- rskj-core/src/test/java/co/rsk/peg/union/UnionResponseCodeTest.java
+++ rskj-core/src/test/java/co/rsk/peg/union/UnionResponseCodeTest.java
@@ -6,43 +6,43 @@ import org.junit.jupiter.api.Test;
 class UnionResponseCodeTest {
 
     @Test
-    void getCode_whenSuccess_ok() {
+    void getCode_whenSuccess_shouldReturnCode() {
         // Arrange
         Assertions.assertEquals(0, UnionResponseCode.SUCCESS.getCode());
     }
 
     @Test
-    void getCode_whenGenericError_ok() {
+    void getCode_whenGenericError_shouldReturnCode() {
         // Arrange
         Assertions.assertEquals(-10, UnionResponseCode.GENERIC_ERROR.getCode());
     }
 
     @Test
-    void getCode_whenUnauthorizedCaller_ok() {
+    void getCode_whenUnauthorizedCaller_shouldReturnCode() {
         // Arrange
         Assertions.assertEquals(-1, UnionResponseCode.UNAUTHORIZED_CALLER.getCode());
     }
 
     @Test
-    void getCode_whenInvalidValue_ok() {
+    void getCode_whenInvalidValue_shouldReturnCode() {
         // Arrange
         Assertions.assertEquals(-2, UnionResponseCode.INVALID_VALUE.getCode());
     }
 
     @Test
-    void getCode_whenAlreadyPaused_ok() {
+    void getCode_whenRequestDisabled_shouldReturnCode() {
         // Arrange
-        Assertions.assertEquals(-3, UnionResponseCode.ALREADY_PAUSED.getCode());
+        Assertions.assertEquals(-3, UnionResponseCode.REQUEST_DISABLED.getCode());
     }
 
     @Test
-    void getCode_whenAlreadyUnpaused_ok() {
+    void getCode_whenReleaseDisabled_shouldReturnCode() {
         // Arrange
-        Assertions.assertEquals(-4, UnionResponseCode.ALREADY_UNPAUSED.getCode());
+        Assertions.assertEquals(-4, UnionResponseCode.RELEASE_DISABLED.getCode());
     }
 
     @Test
-    void getCode_whenEnvironmentDisabled_ok() {
+    void getCode_whenEnvironmentDisabled_shouldReturnCode() {
         // Arrange
         Assertions.assertEquals(-5, UnionResponseCode.ENVIRONMENT_DISABLED.getCode());
     }
