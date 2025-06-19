6c1cd3b359bfca549e2f0f138060b6682db2b459
Julian Len <julian@iovlabs.org>
2025-06-19 15:47:33 -0300

    feat: added tests to check the new validateRedeemScriptSize() at the beginning of the pending federation constructor

--- rskj-core/src/main/java/co/rsk/peg/bitcoin/ScriptValidations.java
+++ rskj-core/src/main/java/co/rsk/peg/bitcoin/ScriptValidations.java
@@ -6,11 +6,14 @@ import static co.rsk.peg.bitcoin.ScriptCreationException.Reason.ABOVE_MAX_SCRIPT
 import static co.rsk.peg.bitcoin.ScriptCreationException.Reason.ABOVE_MAX_SCRIPT_FOR_WITNESS_SIZE;
 
 public class ScriptValidations {
+
+    public static final int FLYOVER_SCRIPT_BYTES = 34;
+
     private ScriptValidations() {
     }
 
     public static void validateSizeOfRedeemScriptForScriptSig(Script redeemScript) throws ScriptCreationException {
-        int bytesCountFromScript = redeemScript.getProgram().length;
+        int bytesCountFromScript = getBytesCountFromScript(redeemScript);
         if (bytesCountFromScript > Script.MAX_SCRIPT_ELEMENT_SIZE) {
             String message = String.format("The size of the redeem script for scriptSig is %d, which is above the maximum allowed (%s).",
                 bytesCountFromScript,
@@ -21,7 +24,7 @@ public class ScriptValidations {
     }
 
     public static void validateSizeOfRedeemScriptForWitness(Script redeemScript) throws ScriptCreationException {
-        int bytesCountFromScript = redeemScript.getProgram().length;
+        int bytesCountFromScript = getBytesCountFromScript(redeemScript);
         if (bytesCountFromScript > Script.MAX_STANDARD_P2WSH_SCRIPT_SIZE) {
             String message = String.format("The size of the redeem script for witness is %d, which is above the maximum allowed (%s).",
                 bytesCountFromScript,
@@ -29,5 +32,9 @@ public class ScriptValidations {
             );
             throw new ScriptCreationException(message, ABOVE_MAX_SCRIPT_FOR_WITNESS_SIZE);
         }
+    }
+
+    private static int getBytesCountFromScript(Script redeemScript) {
+        return redeemScript.getProgram().length + FLYOVER_SCRIPT_BYTES;
     }
 }
