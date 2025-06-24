cc07368ecf6aa1bb981e7f1228eb9331d2ea76f2
fede <federico@rootstocklabs.com>
2025-06-24 12:07:47 -0300

    build clean scripts

--- benchmark_difficulty.sh
+++ benchmark_difficulty.sh
@@ -2,5 +2,10 @@
 
 set -eo pipefail
 
-./gradlew :rskj-core:test --tests "co.rsk.fasterblocks.DifficultyUpdateBenchmark.runBenchTest" -i > output.txt
-# python3 benchmark_difficulty_draw_chart.py
+./gradlew :rskj-core:test --tests "co.rsk.fasterblocks.DifficultyUpdateBenchmark.runBenchTest" -i
+mv ./rskj-core/bench.csv ./bench.csv
+clear
+python3 benchmark_difficulty_analysis.py
+open fp_vs_bigdecimal_time_per_scale.png &&\
+open rel_error_per_scale.png &&\
+open benchmark_rankings.txt
---
+++ clean_bench.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm bench.csv
+rm rel_error_per_scale.png
+rm fp_vs_bigdecimal_time_per_scale.png
+rm benchmark_rankings.txt
+
