2835e46700f2f89f9874e5cdb65c6f8348952a7a
Julian Len <julian@iovlabs.org>
2025-06-27 11:24:57 -0300

    fix: renamed constant MAX_REDEEM_SCRIPT_ELEMENT_SIZE to MAX_P2SH_REDEEM_SCRIPT_SIZE

--- rskj-core/src/test/java/co/rsk/peg/bitcoin/ScriptValidationsTest.java
+++ rskj-core/src/test/java/co/rsk/peg/bitcoin/ScriptValidationsTest.java
@@ -12,14 +12,14 @@ class ScriptValidationsTest {
 
     @Test
     void validateSizeOfRedeemScriptForScriptSig_withinLimit_shouldPass() {
-        byte[] program = new byte[(int) MAX_REDEEM_SCRIPT_ELEMENT_SIZE];
+        byte[] program = new byte[(int) MAX_P2SH_REDEEM_SCRIPT_SIZE];
         Script redeemScript = new Script(program);
         assertDoesNotThrow(() -> ScriptValidations.validateSizeOfRedeemScriptForScriptSig(redeemScript));
     }
 
     @Test
     void validateSizeOfRedeemScriptForScriptSig_exceedsLimit_shouldThrow() {
-        byte[] program = new byte[(int) MAX_REDEEM_SCRIPT_ELEMENT_SIZE + 1];
+        byte[] program = new byte[(int) MAX_P2SH_REDEEM_SCRIPT_SIZE + 1];
         Script redeemScript = new Script(program);
 
         ScriptCreationException ex = assertThrows(ScriptCreationException.class, () ->
