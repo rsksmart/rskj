54d16d60bb737fb591df9d7da7ad8ea1535c3133
nathanieliov <nathaniel.calderon@iovlabs.org>
2025-07-08 17:51:23 -0400

    Update union bridge contract address and authorizers to use zero address

--- rskj-core/src/main/java/co/rsk/peg/union/constants/UnionBridgeMainNetConstants.java
+++ rskj-core/src/main/java/co/rsk/peg/union/constants/UnionBridgeMainNetConstants.java
@@ -5,8 +5,8 @@ import co.rsk.core.Coin;
 import co.rsk.core.RskAddress;
 import co.rsk.peg.vote.AddressBasedAuthorizer;
 import java.math.BigInteger;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 import org.bouncycastle.util.encoders.Hex;
 import org.ethereum.crypto.ECKey;
 
@@ -19,8 +19,7 @@ public class UnionBridgeMainNetConstants extends UnionBridgeConstants {
         btcParams = NetworkParameters.fromID(NetworkParameters.ID_MAINNET);
 
         // TODO: Replace with actual address
-        // seed: UNION_BRIDGE_ADDRESS
-        unionBridgeAddress = new RskAddress("5988645d30cd01e4b3bc2c02cb3909dec991ae31");
+        unionBridgeAddress = new RskAddress("0000000000000000000000000000000000000000");
 
         // TODO: Replace with actual initial locking cap value and increments multiplier
         BigInteger oneRbtc = BigInteger.TEN.pow(18); // 1 RBTC = 1000000000000000000 wei
@@ -28,10 +27,9 @@ public class UnionBridgeMainNetConstants extends UnionBridgeConstants {
         lockingCapIncrementsMultiplier = 2;
 
         // TODO: Replace with actual authorizers
-        List<ECKey> changeUnionBridgeContractAddressAuthorizers = Collections.singletonList(
-            // seed: unionBridgeAuthorizer
-            ECKey.fromPublicOnly(Hex.decode("041fb6d4b421bb14d95b6fb79823d45b777f0e8fd07fe18d0940c0c113d9667911e354d4e8c8073f198d7ae5867d86e3068caff4f6bd7bffccc6757a3d7ee8024a"))
-        );
+        List<ECKey> changeUnionBridgeContractAddressAuthorizers = Stream.of(
+            "04bd1d5747ca6564ed860df015c1a8779a35ef2a9f184b6f5390bccb51a3dcace02f88a401778be6c8fd8ed61e4d4f1f508075b3394eb6ac0251d4ed6d06ce644d"
+        ).map(hex -> ECKey.fromPublicOnly(Hex.decode(hex))).toList();
 
         changeUnionBridgeContractAddressAuthorizer = new AddressBasedAuthorizer(
             changeUnionBridgeContractAddressAuthorizers,
@@ -39,20 +37,18 @@ public class UnionBridgeMainNetConstants extends UnionBridgeConstants {
         );
 
         // TODO: Replace with actual authorizers
-        List<ECKey> changeLockingCapAuthorizers = Collections.singletonList(
-            // seed: changeLockingCapAuthorizer
-            ECKey.fromPublicOnly(Hex.decode("049929eb3c107a65108830f4c221068f42301bd8b054f91bd594944e7fb488fd1c93a8921fb28d3494769598eb271cd2834a31c5bd08fa075170b3da804db00a5b"))
-        );
+        List<ECKey> changeLockingCapAuthorizers = Stream.of(
+            "040162aff21e78665eabe736746ed86ca613f9e628289438697cf820ed8ac800e5fe8cbca350f8cf0b3ee4ec3d8c3edec93820d889565d4ae9b4f6e6d012acec09"
+        ).map(hex -> ECKey.fromPublicOnly(Hex.decode(hex))).toList();
         changeLockingCapAuthorizer = new AddressBasedAuthorizer(
             changeLockingCapAuthorizers,
             AddressBasedAuthorizer.MinimumRequiredCalculation.ONE
         );
 
         // TODO: Replace with actual authorizers
-        List<ECKey> changeTransferPermissionsAuthorizers = Collections.singletonList(
-            // seed: changeTransferPermissionsAuthorizer
-            ECKey.fromPublicOnly(Hex.decode("04ea24f3943dff3b9b8abc59dbdf1bd2c80ec5b61f5c2c6dfcdc189299115d6d567df34c52b7e678cc9934f4d3d5491b6e53fa41a32f58a71200396f1e11917e8f"))
-        );
+        List<ECKey> changeTransferPermissionsAuthorizers = Stream.of(
+            "0458fdbe66a1eda5b94eaf3b3ef1bc8439a05a0b13d2bb9d5a1c6ea1d98ed5b0405fd002c884eed4aa1102d812c7347acc6dd172ad4828de542e156bd47cd90282"
+        ).map(hex -> ECKey.fromPublicOnly(Hex.decode(hex))).toList();
         changeTransferPermissionsAuthorizer = new AddressBasedAuthorizer(
             changeTransferPermissionsAuthorizers,
             AddressBasedAuthorizer.MinimumRequiredCalculation.ONE
--- rskj-core/src/test/java/co/rsk/peg/union/constants/UnionBridgeConstantsTest.java
+++ rskj-core/src/test/java/co/rsk/peg/union/constants/UnionBridgeConstantsTest.java
@@ -1,14 +1,12 @@
 package co.rsk.peg.union.constants;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.samePropertyValuesAs;
-
 import co.rsk.bitcoinj.core.NetworkParameters;
 import co.rsk.core.Coin;
 import co.rsk.core.RskAddress;
 import co.rsk.peg.vote.AddressBasedAuthorizer;
 import java.math.BigInteger;
 import java.util.Collections;
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.bouncycastle.util.encoders.Hex;
@@ -21,80 +19,106 @@ import org.junit.jupiter.params.provider.MethodSource;
 class UnionBridgeConstantsTest {
 
     private static Stream<Arguments> changeUnionAddressAuthorizerProvider() {
-        AddressBasedAuthorizer expectedUnionBridgeChangeAuthorizer = new AddressBasedAuthorizer(
-            Collections.singletonList(ECKey.fromPublicOnly(Hex.decode(
-                "041fb6d4b421bb14d95b6fb79823d45b777f0e8fd07fe18d0940c0c113d9667911e354d4e8c8073f198d7ae5867d86e3068caff4f6bd7bffccc6757a3d7ee8024a"
-            ))),
-            AddressBasedAuthorizer.MinimumRequiredCalculation.ONE
-        );
+        List<RskAddress> mainnetAuthorizers = mapToRskAddress(Collections.singletonList(
+            ECKey.fromPublicOnly(Hex.decode(
+                "04bd1d5747ca6564ed860df015c1a8779a35ef2a9f184b6f5390bccb51a3dcace02f88a401778be6c8fd8ed61e4d4f1f508075b3394eb6ac0251d4ed6d06ce644d"))));
+
+        List<RskAddress> testnetAuthorizers = mapToRskAddress(Collections.singletonList(
+            ECKey.fromPublicOnly(Hex.decode(
+                "041fb6d4b421bb14d95b6fb79823d45b777f0e8fd07fe18d0940c0c113d9667911e354d4e8c8073f198d7ae5867d86e3068caff4f6bd7bffccc6757a3d7ee8024a"))));
+
+        List<RskAddress> regtestAuthorizers = mapToRskAddress(Collections.singletonList(
+            ECKey.fromPublicOnly(Hex.decode(
+                "041fb6d4b421bb14d95b6fb79823d45b777f0e8fd07fe18d0940c0c113d9667911e354d4e8c8073f198d7ae5867d86e3068caff4f6bd7bffccc6757a3d7ee8024a"))));
 
         return Stream.of(
-            Arguments.of(UnionBridgeMainNetConstants.getInstance(), expectedUnionBridgeChangeAuthorizer),
-            Arguments.of(UnionBridgeTestNetConstants.getInstance(), expectedUnionBridgeChangeAuthorizer),
-            Arguments.of(UnionBridgeRegTestConstants.getInstance(), expectedUnionBridgeChangeAuthorizer)
+            Arguments.of(UnionBridgeMainNetConstants.getInstance(), mainnetAuthorizers),
+            Arguments.of(UnionBridgeTestNetConstants.getInstance(), testnetAuthorizers),
+            Arguments.of(UnionBridgeRegTestConstants.getInstance(), regtestAuthorizers)
         );
     }
 
     @ParameterizedTest
     @MethodSource("changeUnionAddressAuthorizerProvider")
     void getChangeUnionBridgeContractAddressAuthorizer_ok(UnionBridgeConstants unionBridgeConstants,
-                                            AddressBasedAuthorizer expectedUnionBridgeChangeAuthorizer) {
+                                            List<RskAddress> expectedSenderAddresses) {
         // Act
-        AddressBasedAuthorizer actualUnionBridgeChangeAuthorizer = unionBridgeConstants.getChangeUnionBridgeContractAddressAuthorizer();
+        AddressBasedAuthorizer actualUnionBridgeChangeAddressAuthorizer = unionBridgeConstants.getChangeUnionBridgeContractAddressAuthorizer();
 
         // Assert
-        assertThat(actualUnionBridgeChangeAuthorizer, samePropertyValuesAs(expectedUnionBridgeChangeAuthorizer));
+        for (RskAddress expectedSenderAddress : expectedSenderAddresses) {
+            Assertions.assertTrue(actualUnionBridgeChangeAddressAuthorizer.isAuthorized(expectedSenderAddress));
+        }
     }
 
     @ParameterizedTest
     @MethodSource("unionBridgeChangeLockingCapAuthorizerProvider")
-    void getChangeLockingCapAuthorizer_ok(UnionBridgeConstants unionBridgeConstants,
-        AddressBasedAuthorizer expectedUnionBridgeChangeLockingCapAuthorizer) {
+    void getChangeLockingCapAuthorizer_ok(UnionBridgeConstants unionBridgeConstants, List<RskAddress> expectedSenderAddresses) {
         // Act
         AddressBasedAuthorizer actualUnionBridgeChangeLockingCapAuthorizer = unionBridgeConstants.getChangeLockingCapAuthorizer();
 
         // Assert
-        assertThat(actualUnionBridgeChangeLockingCapAuthorizer, samePropertyValuesAs(expectedUnionBridgeChangeLockingCapAuthorizer));
+        for (RskAddress expectedSenderAddress : expectedSenderAddresses) {
+            Assertions.assertTrue(actualUnionBridgeChangeLockingCapAuthorizer.isAuthorized(expectedSenderAddress));
+        }
     }
 
     private static Stream<Arguments> unionBridgeChangeLockingCapAuthorizerProvider() {
-        AddressBasedAuthorizer expectedUnionBridgeChangeLockingCapAuthorizer = new AddressBasedAuthorizer(
-            Collections.singletonList(ECKey.fromPublicOnly(Hex.decode(
-                "049929eb3c107a65108830f4c221068f42301bd8b054f91bd594944e7fb488fd1c93a8921fb28d3494769598eb271cd2834a31c5bd08fa075170b3da804db00a5b"
-            ))),
-            AddressBasedAuthorizer.MinimumRequiredCalculation.ONE
-        );
+        List<RskAddress> mainnetAuthorizers = mapToRskAddress(Collections.singletonList(
+            ECKey.fromPublicOnly(Hex.decode(
+                "040162aff21e78665eabe736746ed86ca613f9e628289438697cf820ed8ac800e5fe8cbca350f8cf0b3ee4ec3d8c3edec93820d889565d4ae9b4f6e6d012acec09"))));
+
+        List<RskAddress> testnetAuthorizers = mapToRskAddress(Collections.singletonList(
+            ECKey.fromPublicOnly(Hex.decode(
+                "049929eb3c107a65108830f4c221068f42301bd8b054f91bd594944e7fb488fd1c93a8921fb28d3494769598eb271cd2834a31c5bd08fa075170b3da804db00a5b"))));
+
+        List<RskAddress> regtestAuthorizers = mapToRskAddress(Collections.singletonList(
+            ECKey.fromPublicOnly(Hex.decode(
+                "049929eb3c107a65108830f4c221068f42301bd8b054f91bd594944e7fb488fd1c93a8921fb28d3494769598eb271cd2834a31c5bd08fa075170b3da804db00a5b"))));
 
         return Stream.of(
-            Arguments.of(UnionBridgeMainNetConstants.getInstance(), expectedUnionBridgeChangeLockingCapAuthorizer),
-            Arguments.of(UnionBridgeTestNetConstants.getInstance(), expectedUnionBridgeChangeLockingCapAuthorizer),
-            Arguments.of(UnionBridgeRegTestConstants.getInstance(), expectedUnionBridgeChangeLockingCapAuthorizer)
+            Arguments.of(UnionBridgeMainNetConstants.getInstance(), mainnetAuthorizers),
+            Arguments.of(UnionBridgeTestNetConstants.getInstance(), testnetAuthorizers),
+            Arguments.of(UnionBridgeRegTestConstants.getInstance(), regtestAuthorizers)
         );
+    }
+
+    private static List<RskAddress> mapToRskAddress(List<ECKey> keys) {
+        return keys.stream()
+            .map(ECKey::getAddress)
+            .map(RskAddress::new)
+            .toList();
     }
 
     @ParameterizedTest
     @MethodSource("unionBridgeChangeTransferPermissionsAuthorizerProvider")
-    void getChangeTransferPermissionsAuthorizer_ok(UnionBridgeConstants unionBridgeConstants,
-        AddressBasedAuthorizer expectedUnionBridgeChangeTransferPermissionsAuthorizer) {
+    void getChangeTransferPermissionsAuthorizer_ok(UnionBridgeConstants unionBridgeConstants, List<RskAddress> expectedSenderAddresses) {
         // Act
         AddressBasedAuthorizer actualUnionBridgeChangeTransferPermissionsAuthorizer = unionBridgeConstants.getChangeTransferPermissionsAuthorizer();
 
         // Assert
-        assertThat(actualUnionBridgeChangeTransferPermissionsAuthorizer, samePropertyValuesAs(expectedUnionBridgeChangeTransferPermissionsAuthorizer));
+        for (RskAddress expectedSenderAddress : expectedSenderAddresses) {
+            Assertions.assertTrue(actualUnionBridgeChangeTransferPermissionsAuthorizer.isAuthorized(expectedSenderAddress));
+        }
     }
 
     private static Stream<Arguments> unionBridgeChangeTransferPermissionsAuthorizerProvider() {
-        AddressBasedAuthorizer expectedUnionBridgeChangeTransferPermissionsAuthorizer = new AddressBasedAuthorizer(
-            Collections.singletonList(ECKey.fromPublicOnly(Hex.decode(
-                "04ea24f3943dff3b9b8abc59dbdf1bd2c80ec5b61f5c2c6dfcdc189299115d6d567df34c52b7e678cc9934f4d3d5491b6e53fa41a32f58a71200396f1e11917e8f"
-            ))),
-            AddressBasedAuthorizer.MinimumRequiredCalculation.ONE
-        );
+        List<RskAddress> mainnetAuthorizers = mapToRskAddress(Collections.singletonList(
+            ECKey.fromPublicOnly(Hex.decode(
+                "0458fdbe66a1eda5b94eaf3b3ef1bc8439a05a0b13d2bb9d5a1c6ea1d98ed5b0405fd002c884eed4aa1102d812c7347acc6dd172ad4828de542e156bd47cd90282"))));
+
+        List<RskAddress> testnetAuthorizers = mapToRskAddress(Collections.singletonList(
+            ECKey.fromPublicOnly(Hex.decode(
+                "04ea24f3943dff3b9b8abc59dbdf1bd2c80ec5b61f5c2c6dfcdc189299115d6d567df34c52b7e678cc9934f4d3d5491b6e53fa41a32f58a71200396f1e11917e8f"))));
+
+        List<RskAddress> regtestAuthorizers = mapToRskAddress(Collections.singletonList(
+            ECKey.fromPublicOnly(Hex.decode(
+                "04ea24f3943dff3b9b8abc59dbdf1bd2c80ec5b61f5c2c6dfcdc189299115d6d567df34c52b7e678cc9934f4d3d5491b6e53fa41a32f58a71200396f1e11917e8f"))));
 
         return Stream.of(
-            Arguments.of(UnionBridgeMainNetConstants.getInstance(), expectedUnionBridgeChangeTransferPermissionsAuthorizer),
-            Arguments.of(UnionBridgeTestNetConstants.getInstance(), expectedUnionBridgeChangeTransferPermissionsAuthorizer),
-            Arguments.of(UnionBridgeRegTestConstants.getInstance(), expectedUnionBridgeChangeTransferPermissionsAuthorizer)
+            Arguments.of(UnionBridgeMainNetConstants.getInstance(), mainnetAuthorizers),
+            Arguments.of(UnionBridgeTestNetConstants.getInstance(), testnetAuthorizers),
+            Arguments.of(UnionBridgeRegTestConstants.getInstance(), regtestAuthorizers)
         );
     }
 
@@ -119,7 +143,7 @@ class UnionBridgeConstantsTest {
 
     private static Stream<Arguments> unionBridgeAddressProvider() {
         return Stream.of(
-            Arguments.of(UnionBridgeMainNetConstants.getInstance(), "5988645d30cd01e4b3bc2c02cb3909dec991ae31"),
+            Arguments.of(UnionBridgeMainNetConstants.getInstance(), "0000000000000000000000000000000000000000"),
             Arguments.of(UnionBridgeTestNetConstants.getInstance(), "5988645d30cd01e4b3bc2c02cb3909dec991ae31"),
             Arguments.of(UnionBridgeRegTestConstants.getInstance(), "5988645d30cd01e4b3bc2c02cb3909dec991ae31")
         );
