50dac60252a93d8487366dd5814a615aa060e937
nathanieliov <nathaniel.calderon@iovlabs.org>
2025-07-23 16:14:44 -0400

    - Add test for multi-vote transfer permissions logic and refactor enum key assertions - Remove no needed union change address testnet and regtest authorizers

--- rskj-core/src/test/java/co/rsk/peg/union/UnionBridgeStorageIndexKeyTest.java
+++ rskj-core/src/test/java/co/rsk/peg/union/UnionBridgeStorageIndexKeyTest.java
@@ -12,30 +12,16 @@ class UnionBridgeStorageIndexKeyTest {
     @EnumSource(UnionBridgeStorageIndexKey.class)
     void getKey_shouldReturnCorrectKey(UnionBridgeStorageIndexKey key) {
         // Testing that getKey() returns the correct DataWord for each enum value
-        switch (key) {
-            case UNION_BRIDGE_CONTRACT_ADDRESS:
-                assertEquals(DataWord.fromLongString("unionBridgeContractAddress"), key.getKey());
-                break;
-            case UNION_BRIDGE_LOCKING_CAP:
-                assertEquals(DataWord.fromLongString("unionBridgeLockingCap"), key.getKey());
-                break;
-            case UNION_BRIDGE_INCREASE_LOCKING_CAP_ELECTION:
-                assertEquals(DataWord.fromLongString("unionBridgeIncreaseLockingCapElection"), key.getKey());
-                break;
-            case WEIS_TRANSFERRED_TO_UNION_BRIDGE:
-                assertEquals(DataWord.fromLongString("weisTransferredToUnionBridge"), key.getKey());
-                break;
-            case UNION_BRIDGE_REQUEST_ENABLED:
-                assertEquals(DataWord.fromLongString("unionBridgeRequestEnabled"), key.getKey());
-                break;
-            case UNION_BRIDGE_RELEASE_ENABLED:
-                assertEquals(DataWord.fromLongString("unionBridgeReleaseEnabled"), key.getKey());
-                break;
-            case UNION_BRIDGE_TRANSFER_PERMISSIONS_ELECTION:
-                assertEquals(DataWord.fromLongString("unionBridgeTransferPermissionsElection"), key.getKey());
-                break;
-            default:
-                fail("Unexpected enum value: " + key);
-        }
+        DataWord expectedKey = switch (key) {
+            case UNION_BRIDGE_CONTRACT_ADDRESS -> DataWord.fromLongString("unionBridgeContractAddress");
+            case UNION_BRIDGE_LOCKING_CAP -> DataWord.fromLongString("unionBridgeLockingCap");
+            case UNION_BRIDGE_INCREASE_LOCKING_CAP_ELECTION -> DataWord.fromLongString("unionBridgeIncreaseLockingCapElection");
+            case WEIS_TRANSFERRED_TO_UNION_BRIDGE -> DataWord.fromLongString("weisTransferredToUnionBridge");
+            case UNION_BRIDGE_REQUEST_ENABLED -> DataWord.fromLongString("unionBridgeRequestEnabled");
+            case UNION_BRIDGE_RELEASE_ENABLED -> DataWord.fromLongString("unionBridgeReleaseEnabled");
+            case UNION_BRIDGE_TRANSFER_PERMISSIONS_ELECTION -> DataWord.fromLongString("unionBridgeTransferPermissionsElection");
+        };
+        
+        assertEquals(expectedKey, key.getKey());
     }
 }
