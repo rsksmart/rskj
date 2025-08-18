7da57c6cc5f878b5364db629d8859ff16fd5446a
Marcos <marcos@rootstocklabs.com>
2025-08-18 14:39:53 -0300

    Set base alphanet values
    - Add bitcoin checkpoints
    
    - Update peg-in/pegout waiting block values
    
    - Change genesis federation keys and creation time
    
    - Set federation change authorizers keys
    
    - Set federation change period values
    
    - Set mainnet min block difficulty to the minimum value
    
    - Pre-load RBTC for genesis pegnatories, authorizers and cow address
    
    - Set genesis fee per kb value and authorizers keys
    
    - Set locking cap authorizers keys and initial value
    
    - Set whitelist authorizer key
    
    - Update version modifier

--- rskj-core/src/main/java/org/ethereum/config/Constants.java
+++ rskj-core/src/main/java/org/ethereum/config/Constants.java
@@ -266,7 +266,7 @@ public class Constants {
             MAINNET_CHAIN_ID,
             false,
             MAINNET_DURATION_LIMIT,
-            new BlockDifficulty(BigInteger.valueOf(FALLBACK_MINING_DIFFICULTY / 2)),
+            new BlockDifficulty(BigInteger.valueOf(MINIMUM_DIFFICULTY)),
             new BlockDifficulty(BigInteger.valueOf(FALLBACK_MINING_DIFFICULTY)),
             DIFFICULTY_BOUND_DIVISOR,
             60,
