adb497079ac7a99bdbd0f632a12ac780c9d6ffdd
Marcos <marcos@rootstocklabs.com>
2025-08-21 15:34:52 -0300

    Set custom union bridge authorizer keys

--- rskj-core/src/main/java/co/rsk/peg/union/constants/UnionBridgeTestNetConstants.java
+++ rskj-core/src/main/java/co/rsk/peg/union/constants/UnionBridgeTestNetConstants.java
@@ -21,7 +21,7 @@ public class UnionBridgeTestNetConstants extends UnionBridgeConstants {
         btcParams = NetworkParameters.fromID(NetworkParameters.ID_TESTNET);
 
         // TODO: Replace with actual address
-        unionBridgeAddress = new RskAddress("5988645d30cd01e4b3bc2c02cb3909dec991ae31");
+        unionBridgeAddress = new RskAddress("0000000000000000000000000000000000000000");
 
         // TODO: Replace with actual initial value and increments multiplier
         BigInteger oneRbtc = BigInteger.TEN.pow(18); // 1 RBTC = 1000000000000000000 wei
@@ -29,7 +29,7 @@ public class UnionBridgeTestNetConstants extends UnionBridgeConstants {
         lockingCapIncrementsMultiplier = 3;
 
         // TODO: Replace with actual authorizers
-        ECKey changeUnionBridgeContractAddressAuthorizers = ECKey.fromPublicOnly(Hex.decode("041fb6d4b421bb14d95b6fb79823d45b777f0e8fd07fe18d0940c0c113d9667911e354d4e8c8073f198d7ae5867d86e3068caff4f6bd7bffccc6757a3d7ee8024a"));
+        ECKey changeUnionBridgeContractAddressAuthorizers = ECKey.fromPublicOnly(Hex.decode("03fcf11ef18d377b345571cb71d533aee40354020d3aa082354ee33a8df60cae2b"));
         changeUnionBridgeContractAddressAuthorizer = new AddressBasedAuthorizer(
             Collections.singletonList(changeUnionBridgeContractAddressAuthorizers),
             MinimumRequiredCalculation.ONE
@@ -37,9 +37,9 @@ public class UnionBridgeTestNetConstants extends UnionBridgeConstants {
 
         // TODO: Replace with actual authorizers
         List<ECKey> changeLockingCapAuthorizers = Stream.of(
-            "049929eb3c107a65108830f4c221068f42301bd8b054f91bd594944e7fb488fd1c93a8921fb28d3494769598eb271cd2834a31c5bd08fa075170b3da804db00a5b",
-            "04c8a5827bfadd2bce6fa782e6c48dd61503d38c86e29381781167cd6371eb56f50bc03c9e9c265ea7e07709b964e0b4b0f3d416955225fcb9202e6763ddd5ca91",
-            "0442329d63de5ec5b2f285da7e2f3eb484db3ee5e39066579244211021b81c32d7061922075e2272a8e8a633a5856071eef7e7f800b3d93c9acee91e0f0f37ac2f"
+            "03fcf11ef18d377b345571cb71d533aee40354020d3aa082354ee33a8df60cae2b",
+            "02eec0e71e7b459f2a20db8c06a06d1132ff1bec329d3cc2d761aec570cca4fe14",
+            "030b5baaac2550b527d94ea50881f4291c963cfa3638bfdec8a094cb86f6b96ed1"
         ).map(hex -> ECKey.fromPublicOnly(Hex.decode(hex))).toList();
 
         changeLockingCapAuthorizer = new AddressBasedAuthorizer(
@@ -49,9 +49,9 @@ public class UnionBridgeTestNetConstants extends UnionBridgeConstants {
 
         // TODO: Replace with actual authorizers
         List<ECKey> changeTransferPermissionsAuthorizers = Stream.of(
-            "04ea24f3943dff3b9b8abc59dbdf1bd2c80ec5b61f5c2c6dfcdc189299115d6d567df34c52b7e678cc9934f4d3d5491b6e53fa41a32f58a71200396f1e11917e8f",
-            "04cf42ec9eb287adc7196e8d3d2c288542b1db733681c22887e3a3e31eb98504002825ecbe0cd9b61aff3600ffd0ca4542094c75cb0bac5e93be0c7e00b2ead9ea",
-            "043a7510e39f8c406fb682c20d0e74e6f18f6ec6cb4bc9718a3c47f9bda741f3333ed39e9854b9ad89f16fccb52453975ff1039dd913addfa6a6c56bcacbd92ff9"
+            "03fcf11ef18d377b345571cb71d533aee40354020d3aa082354ee33a8df60cae2b",
+            "02eec0e71e7b459f2a20db8c06a06d1132ff1bec329d3cc2d761aec570cca4fe14",
+            "030b5baaac2550b527d94ea50881f4291c963cfa3638bfdec8a094cb86f6b96ed1"
         ).map(hex -> ECKey.fromPublicOnly(Hex.decode(hex))).toList();
 
         changeTransferPermissionsAuthorizer = new AddressBasedAuthorizer(
