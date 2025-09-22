ae56473620b89eeaf0f94d2f33dc34f6dba3d10b
nathanieliov <nathaniel.calderon@iovlabs.org>
2025-09-22 01:35:49 -0400

    Add unit tests for `AddressBasedAuthorizerFactory`, validate input constraints in `buildMajorityAuthorizer`

--- rskj-core/src/main/java/co/rsk/peg/vote/AddressBasedAuthorizerFactory.java
+++ rskj-core/src/main/java/co/rsk/peg/vote/AddressBasedAuthorizerFactory.java
@@ -12,6 +12,12 @@ public class AddressBasedAuthorizerFactory {
     }
 
     public static AddressBasedAuthorizer buildMajorityAuthorizer(Set<RskAddress> authorizedAddresses) {
+        if (authorizedAddresses.isEmpty()) {
+            throw new IllegalArgumentException("Cannot build an authorizer with no authorized addresses");
+        }
+        if (authorizedAddresses.size() == 1) {
+            throw new IllegalArgumentException("Cannot build a majority authorizer with a single authorized address");
+        }
         return AddressBasedAuthorizer.of(authorizedAddresses, MinimumRequiredCalculation.MAJORITY);
     }
 }
