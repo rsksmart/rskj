155892058e6bf62244527220e7dffc04c511b71b
nathanieliov <nathaniel.calderon@iovlabs.org>
2025-09-23 14:44:35 -0400

    Validate input constraints for null addresses in `buildMajorityAuthorizer` and update unit tests

--- rskj-core/src/main/java/co/rsk/peg/vote/AddressBasedAuthorizerFactory.java
+++ rskj-core/src/main/java/co/rsk/peg/vote/AddressBasedAuthorizerFactory.java
@@ -2,6 +2,7 @@ package co.rsk.peg.vote;
 
 import co.rsk.core.RskAddress;
 import co.rsk.peg.vote.AddressBasedAuthorizer.MinimumRequiredCalculation;
+import java.util.Objects;
 import java.util.Set;
 
 public class AddressBasedAuthorizerFactory {
@@ -17,6 +18,10 @@ public class AddressBasedAuthorizerFactory {
         }
         if (authorizedAddresses.size() == 1) {
             throw new IllegalArgumentException("Cannot build a majority authorizer with a single authorized address");
+        }
+        boolean hasNullAddress = authorizedAddresses.stream().anyMatch(Objects::isNull);
+        if (hasNullAddress) {
+            throw new IllegalArgumentException("Cannot build a majority authorizer with a null address");
         }
         return AddressBasedAuthorizer.of(authorizedAddresses, MinimumRequiredCalculation.MAJORITY);
     }
