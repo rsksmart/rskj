01932c3102287d75be830709afaf1439d72836df
julia-zack <julia.zack@iovlabs.org>
2025-10-01 17:00:47 -0300

    Add Bridge Methods

--- rskj-core/src/main/java/co/rsk/peg/Bridge.java
+++ rskj-core/src/main/java/co/rsk/peg/Bridge.java
@@ -1534,6 +1534,22 @@ public class Bridge extends PrecompiledContracts.PrecompiledContract {
         return bridgeSupport.setUnionBridgeTransferPermissions(rskTx, requestEnabled, releaseEnabled).getCode();
     }
 
+    public byte[] getSuperEvent(Object[] args) {
+        logger.trace("getSuperEvent");
+        return bridgeSupport.getSuperEvent();
+    }
+
+    public void setSuperEvent(Object[] args) {
+        logger.trace("setSuperEvent");
+        byte[] data = (byte[]) args[0];
+        bridgeSupport.setSuperEvent(data);
+    }
+
+    public void clearSuperEvent(Object[] args) {
+        logger.trace("clearSuperEvent");
+        bridgeSupport.clearSuperEvent();
+    }
+
     public static BridgeMethods.BridgeMethodExecutor executeIfAuthorized(AuthorizerProvider authorizerProvider, BridgeMethods.BridgeMethodExecutor decoratee, String funcName) {
         return (self, args) -> {
             AddressBasedAuthorizer addressBasedAuthorizer = authorizerProvider.provide(self.bridgeConstants);
