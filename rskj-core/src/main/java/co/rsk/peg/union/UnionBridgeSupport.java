ee9eb28c0ec15fcdb63373856772e12b14541940
julia-zack <julia.zack@iovlabs.org>
2025-10-01 16:59:13 -0300

    Add union bridge support impl

--- rskj-core/src/main/java/co/rsk/peg/union/UnionBridgeSupport.java
+++ rskj-core/src/main/java/co/rsk/peg/union/UnionBridgeSupport.java
@@ -21,5 +21,9 @@ public interface UnionBridgeSupport {
 
     UnionResponseCode setTransferPermissions(Transaction tx, boolean requestEnabled, boolean releaseEnabled);
 
+    byte[] getSuperEvent();
+    void setSuperEvent(byte[] data);
+    void clearSuperEvent();
+
     void save();
 }
