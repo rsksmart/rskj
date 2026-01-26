6ca7591479447f1fec61d2ef41b6f8b44b06cad1
Julian Len <julian@iovlabs.org>
2026-01-26 14:55:05 -0300

    extra line

--- rskj-core/src/test/java/co/rsk/peg/BridgeSupportTest.java
+++ rskj-core/src/test/java/co/rsk/peg/BridgeSupportTest.java
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 package co.rsk.peg;
 
 import static co.rsk.RskTestUtils.createRepository;
