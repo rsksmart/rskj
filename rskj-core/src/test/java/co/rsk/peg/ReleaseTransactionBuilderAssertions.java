2d852bfff740265511b2badf454b4c299cbb39b1
Julian Len <julian@iovlabs.org>
2026-03-06 17:21:43 -0300

    Added tests for buildAmountTo and simplified those methods repeated

--- rskj-core/src/test/java/co/rsk/peg/ReleaseTransactionBuilderAssertions.java
+++ rskj-core/src/test/java/co/rsk/peg/ReleaseTransactionBuilderAssertions.java
@@ -1,22 +1,25 @@
 package co.rsk.peg;
 
-import co.rsk.bitcoinj.core.BtcTransaction;
-import co.rsk.bitcoinj.core.TransactionInput;
-import co.rsk.bitcoinj.core.TransactionWitness;
-import co.rsk.bitcoinj.core.UTXO;
+import co.rsk.bitcoinj.core.*;
 import co.rsk.bitcoinj.script.Script;
+import co.rsk.peg.constants.BridgeConstants;
+import co.rsk.peg.constants.BridgeMainNetConstants;
 
 import java.util.List;
 import java.util.function.Predicate;
 
 import static co.rsk.peg.ReleaseTransactionBuilder.BTC_TX_VERSION_1;
 import static co.rsk.peg.ReleaseTransactionBuilder.BTC_TX_VERSION_2;
+import static co.rsk.peg.bitcoin.BitcoinTestUtils.MIN_NON_DUST_VALUE_FOR_P2SH_OUTPUT_SCRIPT;
 import static co.rsk.peg.bitcoin.BitcoinTestAssertions.assertScriptSigFromStandardMultisigWithoutSignaturesHasProperFormat;
 import static co.rsk.peg.bitcoin.BitcoinTestAssertions.assertScriptSigFromP2shErpWithoutSignaturesHasProperFormat;
 import static co.rsk.peg.bitcoin.BitcoinTestAssertions.assertP2shP2wshWitnessWithoutSignaturesHasProperFormat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public final class ReleaseTransactionBuilderAssertions {
+
+    private static final BridgeConstants BRIDGE_MAINNET_CONSTANTS = BridgeMainNetConstants.getInstance();
+    private static final NetworkParameters BTC_MAINNET_PARAMS = BRIDGE_MAINNET_CONSTANTS.getBtcParams();
 
     public static void assertReleaseTxInputsHasProperFormatAndBelongsToStandardMultisigFederation(
         BtcTransaction releaseTransaction,
@@ -87,4 +90,55 @@ public final class ReleaseTransactionBuilderAssertions {
     public static void assertBtcTxVersionIs2(BtcTransaction releaseTransaction) {
         assertEquals(BTC_TX_VERSION_2, releaseTransaction.getVersion());
     }
+
+    public static void assertDestinationAddress(List<TransactionOutput> releaseTransactionOutputs,
+                                                Address expectedDestinationAddress) {
+        for (TransactionOutput output : releaseTransactionOutputs) {
+            Address destinationAddress = output.getScriptPubKey().getToAddress(BTC_MAINNET_PARAMS);
+            assertEquals(expectedDestinationAddress, destinationAddress);
+        }
+    }
+
+    public static void assertReleaseTxHasChangeAndPegoutsAmountWithFeesProperly(BtcTransaction releaseTransaction,
+                                                                                List<TransactionOutput> releaseTransactionChangeOutputs,
+                                                                                Coin expectedSentPegoutAmount) {
+        Coin inputTotalAmount = releaseTransaction.getInputSum();
+        Coin expectedChangeAmount = inputTotalAmount.subtract(expectedSentPegoutAmount);
+
+        if (isDust(expectedChangeAmount)) {
+            Coin amountToGetNonDustValue = MIN_NON_DUST_VALUE_FOR_P2SH_OUTPUT_SCRIPT.subtract(expectedChangeAmount);
+            expectedSentPegoutAmount = expectedSentPegoutAmount.subtract(amountToGetNonDustValue);
+            expectedChangeAmount = MIN_NON_DUST_VALUE_FOR_P2SH_OUTPUT_SCRIPT;
+        }
+
+        int expectedNumberOfOutputs = 1;
+        assertEquals(expectedNumberOfOutputs, releaseTransactionChangeOutputs.size());
+        Coin changeOutputsAmount = releaseTransactionChangeOutputs.stream()
+            .map(TransactionOutput::getValue)
+            .reduce(Coin::add)
+            .orElse(Coin.ZERO);
+        assertEquals(expectedChangeAmount, changeOutputsAmount);
+
+        Coin pegoutOutputsAmount = releaseTransaction.getOutputSum().subtract(changeOutputsAmount);
+        Coin releaseTransactionFees = releaseTransaction.getFee();
+        Coin pegoutsAndFeesAmount = releaseTransactionFees.add(pegoutOutputsAmount);
+        assertEquals(expectedSentPegoutAmount, pegoutsAndFeesAmount);
+        assertEquals(inputTotalAmount, pegoutsAndFeesAmount.add(changeOutputsAmount));
+    }
+
+    private static boolean isDust(Coin expectedChangeAmount) {
+        return expectedChangeAmount.compareTo(MIN_NON_DUST_VALUE_FOR_P2SH_OUTPUT_SCRIPT) < 0;
+    }
+
+    public static void assertReleaseTxWithNoChangeHasPegoutsAmountWithFeesProperly(BtcTransaction releaseTransaction,
+                                                                                   Coin expectedSentAmount) {
+        Coin pegoutOutputsAmount = releaseTransaction.getOutputSum();
+        Coin releaseTransactionFees = releaseTransaction.getFee();
+        Coin outputTotalAmount = releaseTransactionFees.add(pegoutOutputsAmount);
+        assertEquals(expectedSentAmount, outputTotalAmount);
+
+        Coin inputTotalAmount = releaseTransaction.getInputSum();
+        assertEquals(inputTotalAmount, outputTotalAmount);
+    }
+
 }
