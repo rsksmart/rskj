400a8bdd0306e978797f7541b7b42f8673cd6476
meldsun <meldsun0@gmail.com>
2026-03-20 16:41:10 -0300

    adding locks metrics

--- rskj-core/src/main/java/org/ethereum/datasource/DataSourceWithCache.java
+++ rskj-core/src/main/java/org/ethereum/datasource/DataSourceWithCache.java
@@ -36,7 +36,7 @@ import java.util.stream.Stream;
 public class DataSourceWithCache implements KeyValueDataSource {
 
     private static final Logger logger = LoggerFactory.getLogger("datasourcewithcache");
-    private final DataSourceWithCacheMetrics metrics;
+    private final DataSourceWithLockMetrics metrics;
     private final int cacheSize;
     private final KeyValueDataSource base;
     private final Map<ByteArrayWrapper, byte[]> uncommittedCache;
@@ -63,61 +63,52 @@ public class DataSourceWithCache implements KeyValueDataSource {
         this.uncommittedCache = new LinkedHashMap<>(cacheSize / 8, (float) 0.75, false);
         this.committedCache = Collections.synchronizedMap(makeCommittedCache(cacheSize, cacheSnapshotHandler));
         this.cacheSnapshotHandler = cacheSnapshotHandler;
-        this.metrics = new DataSourceWithCacheMetrics(
-                logger,
-                getName() + "#" + instanceId,
-                owner,
-                () -> committedCache.size(),
-                () -> uncommittedCache.size(),
-                10_000 // emit every 10k ops
-        );
+        this.metrics = new DataSourceWithLockMetrics(logger, getName() + "#" + instanceId, owner, 10_000);
     }
 
     @Override
     public byte[] get(byte[] key) {
-        metrics.onUserReadGet();
         Objects.requireNonNull(key);
+        long methodStart = System.nanoTime();
 
         boolean traceEnabled = logger.isTraceEnabled();
         ByteArrayWrapper wrappedKey = ByteUtil.wrap(key);
+
         byte[] value;
 
+        long lockWaitStart = System.nanoTime();
         this.lock.readLock().lock();
+        metrics.onReadLockWait(System.nanoTime() - lockWaitStart);
+
+        long lockHeldStart = System.nanoTime();
 
         try {
-            metrics.onCacheCommittedReadContains();
+
             if (committedCache.containsKey(wrappedKey)) {
-                metrics.onCacheCommittedReadGet();
                 byte[] result = committedCache.get(wrappedKey);
-                metrics.onUserReadGetFromCommittedCache(result == null);
                 return result;
             }
-            metrics.onCacheUncommittedReadContains();
             if (uncommittedCache.containsKey(wrappedKey)) {
-                metrics.onCacheUncommittedReadGet();
                 byte[] result = uncommittedCache.get(wrappedKey);
-                metrics.onUserReadGetFromUncommittedCache(result == null);
                 return result;
             }
 
-            long start = System.nanoTime();
+
             value = base.get(key);
-            long nanos = System.nanoTime() - start;
-            metrics.onUserReadGetFromStore(nanos, value==null);
             if (traceEnabled) {
                 numOfGetsFromStore.incrementAndGet();
             }
-
             //null value, as expected, is allowed here to be stored in committedCache
-            metrics.onCacheCommittedWritePut();
             committedCache.put(wrappedKey, value);
-            metrics.onReadThroughFillCommittedFromStore(value == null);
         } finally {
+            metrics.onReadLockHeld( System.nanoTime() - lockHeldStart);
+
             if (traceEnabled) {
                 numOfGets.incrementAndGet();
             }
 
             this.lock.readLock().unlock();
+            metrics.onGetLatency(System.nanoTime() - methodStart);
         }
 
         return value;
@@ -131,40 +122,38 @@ public class DataSourceWithCache implements KeyValueDataSource {
 
     private byte[] put(ByteArrayWrapper wrappedKey, byte[] value) {
         Objects.requireNonNull(value);
-        metrics.onUserWritePut();
+        long methodStart = System.nanoTime();
 
+        long lockWaitStart = System.nanoTime();
         this.lock.writeLock().lock();
+        metrics.onWriteLockWait(System.nanoTime() - lockWaitStart);
 
+        long lockHeldStart = System.nanoTime();
         try {
             // here I could check for equal data or just move to the uncommittedCache.
-            metrics.onCacheCommittedReadGet();
             byte[] priorValue = committedCache.get(wrappedKey);
             if (priorValue != null && Arrays.equals(priorValue, value)) {
                 return value;
             }
-            metrics.onCacheCommittedWriteRemove(priorValue==null);
             committedCache.remove(wrappedKey);
             this.putKeyValue(wrappedKey, value);
         } finally {
+            metrics.onWriteLockHeld(System.nanoTime() - lockHeldStart);
             if (logger.isTraceEnabled()) {
                 numOfPuts.incrementAndGet();
             }
 
             this.lock.writeLock().unlock();
-        }
 
+            metrics.onPutLatency(System.nanoTime() - methodStart);
+        }
         return value;
     }
 
     private void putKeyValue(ByteArrayWrapper key, byte[] value) {
         uncommittedCache.put(key, value);
-        metrics.onCacheUncommittedWritePut();
-
         if (uncommittedCache.size() > cacheSize) {
-            long start = System.nanoTime();
-            flushNotManual();
-            long nanos = System.nanoTime() - start;
-            metrics.onUserWriteFlush(DataSourceWithCacheMetrics.FlushReason.SIZE, nanos);
+            flushNotManual(DataSourceWithCacheMetrics.FlushReason.SIZE);
         }
     }
 
@@ -174,28 +163,33 @@ public class DataSourceWithCache implements KeyValueDataSource {
     }
 
     private void delete(ByteArrayWrapper wrappedKey) {
-        this.lock.writeLock().lock();
-        metrics.onUserWriteDelete();
+        long methodStart = System.nanoTime();
+        long lockWaitStart = System.nanoTime();
 
+        this.lock.writeLock().lock();
+        metrics.onWriteLockWait(System.nanoTime() - lockWaitStart);
+
+        long lockHeldStart = System.nanoTime();
         try {
             // always mark for deletion if we don't know the state in the underlying store
-            metrics.onCacheCommittedReadContains();
+
             if (!committedCache.containsKey(wrappedKey)) {
                 this.putKeyValue(wrappedKey, null);
                 return;
             }
-            metrics.onCacheCommittedReadGet();
+
             byte[] valueToRemove = committedCache.get(wrappedKey);
 
             // a null value means we know for a fact that the key doesn't exist in the underlying store, so this is a noop
             if (valueToRemove != null) {
                 this.putKeyValue(wrappedKey, null);
-                metrics.onCacheCommittedWriteRemove(false);
                 committedCache.remove(wrappedKey);
 
             }
         } finally {
+            metrics.onWriteLockHeld(System.nanoTime() - lockHeldStart);
             this.lock.writeLock().unlock();
+            metrics.onDeleteLatency( System.nanoTime() - methodStart);
         }
     }
 
@@ -245,19 +239,21 @@ public class DataSourceWithCache implements KeyValueDataSource {
         if (rows.containsKey(null) || rows.containsValue(null)) {
             throw new IllegalArgumentException("Cannot update null values");
         }
-
+        long methodStart = System.nanoTime();
         // remove overlapping entries
         rows.keySet().removeAll(keysToRemove);
-        long start = System.nanoTime();
+        long lockWaitStart = System.nanoTime();
         this.lock.writeLock().lock();
-
+        metrics.onWriteLockWait( System.nanoTime() - lockWaitStart);
+        long lockHeldStart = System.nanoTime();
         try {
             rows.forEach(this::put);
             keysToRemove.forEach(this::delete);
         } finally {
+
+            metrics.onWriteLockHeld(System.nanoTime() - lockHeldStart);
             this.lock.writeLock().unlock();
-            long nanos = System.nanoTime() - start;
-            metrics.onUserWriteBatch(nanos);
+            metrics.onBatchLatency(System.nanoTime() - methodStart);
         }
     }
 
@@ -266,10 +262,12 @@ public class DataSourceWithCache implements KeyValueDataSource {
 
     @Override
     public void flush() {
-        long start = System.nanoTime();
+        long methodStart = System.nanoTime();
         Map<ByteArrayWrapper, byte[]> uncommittedBatch = new LinkedHashMap<>();
-
+        long lockWaitStart = System.nanoTime();
         this.lock.writeLock().lock();
+        metrics.onWriteLockWait(System.nanoTime() - lockWaitStart);
+        long lockHeldStart = System.nanoTime();
 
         try {
             long saveTime = System.nanoTime();
@@ -279,16 +277,12 @@ public class DataSourceWithCache implements KeyValueDataSource {
                     uncommittedBatch.put(key, value);
                 }
             });
-            long updateBatchTime = System.nanoTime();
             Set<ByteArrayWrapper> uncommittedKeysToRemove = uncommittedCache.entrySet().stream().filter(e -> e.getValue() == null).map(Map.Entry::getKey).collect(Collectors.toSet());
             base.updateBatch(uncommittedBatch, uncommittedKeysToRemove);
-            metrics.onStoreFlushBatchUpdate(uncommittedBatch.size(), uncommittedKeysToRemove.size(),  System.nanoTime() - updateBatchTime);
 
-            long updateCommittedAndUncommittedTime = System.nanoTime();
-            metrics.onCacheCommittedWritePutAll(uncommittedCache.size());
             committedCache.putAll(uncommittedCache);
             uncommittedCache.clear();
-            metrics.onStoreFlushCommitedAndUncommittedUpdate(System.nanoTime()-updateCommittedAndUncommittedTime);
+
 
             long totalTime = System.nanoTime() - saveTime;
 
@@ -297,17 +291,20 @@ public class DataSourceWithCache implements KeyValueDataSource {
             }
             base.flush();
         } finally {
-            long nanos = System.nanoTime() - start;
-           metrics.onUserWriteFlush(DataSourceWithCacheMetrics.FlushReason.MANUAL, nanos);
+            metrics.onWriteLockHeld(System.nanoTime() - lockHeldStart);
             this.lock.writeLock().unlock();
+            long methodNanos = System.nanoTime() - methodStart;
+            metrics.onFlushLatency(DataSourceWithCacheMetrics.FlushReason.MANUAL, methodNanos);
         }
     }
 
-    public void flushNotManual() {
+    public void flushNotManual(DataSourceWithCacheMetrics.FlushReason reason) {
+        long methodStart = System.nanoTime();
         Map<ByteArrayWrapper, byte[]> uncommittedBatch = new LinkedHashMap<>();
-
+        long lockWaitStart = System.nanoTime();
         this.lock.writeLock().lock();
-
+        metrics.onWriteLockWait(System.nanoTime() - lockWaitStart);
+        long lockHeldStart = System.nanoTime();
         try {
             long saveTime = System.nanoTime();
 
@@ -319,7 +316,6 @@ public class DataSourceWithCache implements KeyValueDataSource {
 
             Set<ByteArrayWrapper> uncommittedKeysToRemove = uncommittedCache.entrySet().stream().filter(e -> e.getValue() == null).map(Map.Entry::getKey).collect(Collectors.toSet());
             base.updateBatch(uncommittedBatch, uncommittedKeysToRemove);
-            metrics.onCacheCommittedWritePutAll(uncommittedCache.size());
             committedCache.putAll(uncommittedCache);
             uncommittedCache.clear();
 
@@ -330,7 +326,9 @@ public class DataSourceWithCache implements KeyValueDataSource {
             }
             base.flush();
         } finally {
+            metrics.onWriteLockHeld(System.nanoTime() - lockHeldStart);
             this.lock.writeLock().unlock();
+            metrics.onFlushLatency(reason,  System.nanoTime() - methodStart);
         }
     }
 
@@ -351,10 +349,7 @@ public class DataSourceWithCache implements KeyValueDataSource {
         this.lock.writeLock().lock();
 
         try {
-            long start = System.nanoTime();
-            flushNotManual();
-            long nanos = System.nanoTime() - start;
-            metrics.onUserWriteFlush(DataSourceWithCacheMetrics.FlushReason.CLOSE, nanos);
+            flushNotManual(DataSourceWithCacheMetrics.FlushReason.CLOSE);
             base.close();
             if (cacheSnapshotHandler != null) {
                 cacheSnapshotHandler.save(committedCache);
