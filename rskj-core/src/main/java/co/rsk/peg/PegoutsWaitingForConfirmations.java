01cf912ef5763209d42d538d23c3bace0547cf5d
Vlad <vladislav.zablotsky@rootstocklabs.com>
2026-04-09 21:50:55 +0300

    Preserve pegouts sorting order POC

--- rskj-core/src/main/java/co/rsk/peg/PegoutsWaitingForConfirmations.java
+++ rskj-core/src/main/java/co/rsk/peg/PegoutsWaitingForConfirmations.java
@@ -21,9 +21,20 @@ package co.rsk.peg;
 import co.rsk.bitcoinj.core.BtcTransaction;
 import co.rsk.crypto.Keccak256;
 import com.google.common.primitives.UnsignedBytes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
+
 
 /**
  * Representation of a queue of BTC release
@@ -33,6 +44,150 @@ import java.util.stream.Collectors;
  * @author Ariel Mendelzon
  */
 public class PegoutsWaitingForConfirmations {
+
+    private static final Logger logger = LoggerFactory.getLogger(PegoutsWaitingForConfirmations.class);
+
+    /**
+     * Wraps some Set functionality with preserving legacy entries order.
+     */
+    static class Entries {
+
+        /**
+         * From JDK.
+         */
+        static final int MAXIMUM_CAPACITY = 1 << 30;
+
+        /**
+         * From JDK.
+         */
+        static final float DEFAULT_LOAD_FACTOR = 0.75f;
+
+        private Set<Entry> entries = new HashSet<>();
+
+        /**
+         * Buckets size in HashSet used for legacy sorting.
+         */
+        private int legacyBuckets;
+
+        private Collection<LegacyEntry> legacyEntries = new ArrayList<>();
+
+        private Map<Integer, Integer> legacyIndexes = new HashMap<>();
+
+        /**
+         * From JDK.
+         */
+        static final int tableSizeFor(int cap) {
+            int n = -1 >>> Integer.numberOfLeadingZeros(cap - 1);
+            return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+        }
+
+        /**
+         * Reproducing steps for calculating initial capacity from JDK.
+         */
+        static final int calculateBuckets(int inputSize) {
+            // Core variables from HashMap constructor
+            var initialCapacity = Math.max((int) (inputSize/.75f) + 1, 16);
+            var threshold = tableSizeFor(initialCapacity);
+
+            // From HashMap resize() method
+            // Initially oldTab will be empty so oldCap = 0
+            // if oldCap == 0 and oldThr > -> newCap will be equal to threshold
+            //
+            // TODO: Will need a threshold logic only if there willI be
+            // bucket collisions (input order affects current sorting)
+            // var newCap = threshold;
+            // float ft = (float)newCap * threshold;
+            // var newThr = (newCap < MAXIMUM_CAPACITY && ft < (float)MAXIMUM_CAPACITY ?
+            //           (int)ft : Integer.MAX_VALUE);
+
+            return threshold;
+        }
+
+        public Entries(Collection<Entry> entries) {
+            if (entries.size() > 0) {
+                logger.warn("PEGOUTS storing {} entries", entries.size());
+            }
+
+            this.legacyBuckets = calculateBuckets(entries.size());
+
+            // Security issue for legacy ordering
+            // The actual order depends on input entries order :(
+            // But the issues could only appear if there will be hash collision in the bucket
+            // It will be ok as long as one entry sits in one bucket.
+            for (Entry entry : entries) {
+                this.addEntry(entry);
+            }
+
+            // possible ordering determinizm fix
+            // entries
+            //     .stream()
+            //     .sorted(Comparator.comparing(Entry::hashCode))
+            //     .forEachOrdered((e) ->{
+            //         this.addEntry(e);
+            //     });
+        }
+
+        public Collection<Entry> getEntries() {
+            return Collections.unmodifiableCollection(entries);
+        }
+
+        public Collection<Entry> getEntriesOrdered() {
+            return this.legacyEntries.stream()
+                .sorted(Comparator.comparing(LegacyEntry::bucketId).thenComparing(LegacyEntry::orderId))
+                .map(LegacyEntry::entry)
+                .collect(Collectors.toUnmodifiableList());
+        }
+
+        boolean remove(Entry e) {
+            var removed = this.entries.remove(e);
+            if (removed) {
+                // Meant for tests so no need to do it cool
+                this.legacyEntries.removeIf(item -> item.entry == e);
+            }
+            return removed;
+        }
+
+        /**
+         * The only valid way to add Entry into this structure.
+         */
+        public final void addEntry(Entry e) {
+            if (this.entries.contains(e)) {
+                // Entry exists nothing to do
+                return;
+            }
+
+            this.entries.add(e);
+
+            var hash = hash(e);
+
+            var bucketId = Integer.valueOf((this.legacyBuckets - 1) & hash);
+
+            // Mocking hash collision ordering
+            int orderId = 0;
+            if (this.legacyIndexes.containsKey(bucketId)) {
+                // I hope we will not have such events untill hardfork
+                logger.warn("PEGOUTS hash collision found for RSK block {} TxHash {}", e.getPegoutCreationRskBlockNumber(), e.getPegoutCreationRskTxHash() );
+                orderId = this.legacyIndexes.get(bucketId) + 1;
+            }
+            this.legacyIndexes.put(bucketId, orderId);
+
+
+            var legacyEntry = new LegacyEntry(e, bucketId, orderId);
+
+            this.legacyEntries.add(legacyEntry);
+        }
+
+        private static final int hash(Object key) {
+            int h;
+            return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+        }
+    }
+
+    /**
+     * Wraps entry with additional legacy sorting ordering information.
+     */
+    static record LegacyEntry(Entry entry, int bucketId, int orderId) {};
+
     public static class Entry {
         // Compares entries using the lexicographical order of the btc tx's serialized bytes
         public static final Comparator<Entry> BTC_TX_COMPARATOR = new Comparator<Entry>() {
@@ -45,7 +200,9 @@ public class PegoutsWaitingForConfirmations {
         };
 
         private BtcTransaction btcTransaction;
+
         private Long pegoutCreationRskBlockNumber;
+
         private Keccak256 pegoutCreationRskTxHash;
 
         public Entry(BtcTransaction btcTransaction, Long pegoutCreationRskBlockNumber, Keccak256 pegoutCreationRskTxHash) {
@@ -76,44 +233,44 @@ public class PegoutsWaitingForConfirmations {
             return otherEntry.getBtcTransaction().equals(getBtcTransaction()) &&
                 otherEntry.getPegoutCreationRskBlockNumber().equals(getPegoutCreationRskBlockNumber()) &&
                 (otherEntry.getPegoutCreationRskTxHash() == null && getPegoutCreationRskTxHash() == null ||
-                    otherEntry.getPegoutCreationRskTxHash() != null && otherEntry.getPegoutCreationRskTxHash().equals(getPegoutCreationRskTxHash()));
+                 otherEntry.getPegoutCreationRskTxHash() != null && otherEntry.getPegoutCreationRskTxHash().equals(getPegoutCreationRskTxHash()));
         }
 
         @Override
         public int hashCode() {
-            return Objects.hash(getBtcTransaction(), getPegoutCreationRskBlockNumber());
+            return java.util.Objects.hash(getBtcTransaction(), getPegoutCreationRskBlockNumber());
         }
     }
 
-    private Set<Entry> entries;
+    private Entries entries;
 
     public PegoutsWaitingForConfirmations(Set<Entry> entries) {
-        // This is a standart code for `new HashSet<>(entries);` in Java 17
-        // Coefficients were changed in Java 21
-        // Need to hardcode Java 17 init params here to preserve old behaviour in Java 21+
-        this.entries = new HashSet<>(Math.max((int) (entries.size()/.75f) + 1, 16));
-        this.entries.addAll(entries);
+        this.entries = new Entries(entries);
     }
 
     public Set<Entry> getEntriesWithoutHash() {
-        return entries.stream().filter(e -> e.getPegoutCreationRskTxHash() == null).collect(Collectors.toSet());
+        return this.entries.getEntries().stream().filter(e -> e.getPegoutCreationRskTxHash() == null).collect(Collectors.toSet());
     }
 
     public Set<Entry> getEntriesWithHash() {
-        return entries.stream().filter(e -> e.getPegoutCreationRskTxHash() != null).collect(Collectors.toSet());
+        return this.entries.getEntries().stream().filter(e -> e.getPegoutCreationRskTxHash() != null).collect(Collectors.toSet());
     }
 
     public Set<Entry> getEntries() {
-        return new HashSet<>(entries);
+        return new HashSet<>(this.entries.getEntries());
     }
 
     public void add(BtcTransaction transaction, Long blockNumber) {
         add(transaction, blockNumber, null);
     }
 
+    /**
+     * Assuming that this method should be used in tests only.
+     */
     public void add(BtcTransaction transaction, Long blockNumber, Keccak256 rskTxHash) {
-        if (entries.stream().noneMatch(e -> e.getBtcTransaction().equals(transaction))) {
-            entries.add(new Entry(transaction, blockNumber, rskTxHash));
+        var items = this.entries.getEntries();
+        if (items.stream().noneMatch(e -> e.getBtcTransaction().equals(transaction))) {
+            this.entries.addEntry(new Entry(transaction, blockNumber, rskTxHash));
         }
     }
 
@@ -129,9 +286,12 @@ public class PegoutsWaitingForConfirmations {
      * @return an optional with an entry with enough confirmations if found. If not, an empty optional.
      */
     public Optional<Entry> getNextPegoutWithEnoughConfirmations(Long currentBlockNumber, Integer minimumConfirmations) {
-        return entries.stream().filter(entry -> hasEnoughConfirmations(entry, currentBlockNumber, minimumConfirmations)).findFirst();
+        return this.entries.getEntriesOrdered().stream().filter(entry -> hasEnoughConfirmations(entry, currentBlockNumber, minimumConfirmations)).findFirst();
     }
 
+    /**
+     * WARNING! Units tests only relted method.
+     */
     public boolean removeEntry(Entry entry){
         return entries.remove(entry);
     }
