b52bd1026af3370d9cb7ca51c96f46e1b7c486ab
julia-zack <julia.zack@iovlabs.org>
2026-06-26 14:09:41 -0300

    Add max tx size check
    Fix tests, cleanup ReleaseTransactionBuilderTest
    
    Use RSKIP378
    
    Rename MAX_TX_SIZE_ALLOWED to VSIZE
    
    Redefine threshold constants in tests
    
    Use MAX_STANDARD_TX_SIZE from bitcoinj-thin
    
    Rollback test as it was before, keeping it as a regression test (fixed setup)
    
    Move tx vsize check right after completeTx

--- rskj-core/src/main/java/co/rsk/peg/ReleaseTransactionBuilder.java
+++ rskj-core/src/main/java/co/rsk/peg/ReleaseTransactionBuilder.java
@@ -18,10 +18,9 @@
 
 package co.rsk.peg;
 
+import static co.rsk.bitcoinj.core.BtcTransaction.MAX_STANDARD_TX_SIZE;
 import static co.rsk.peg.PegUtils.getFlyoverFederationAddress;
-import static co.rsk.peg.bitcoin.BitcoinUtils.BTC_TX_VERSION_1;
-import static co.rsk.peg.bitcoin.BitcoinUtils.BTC_TX_VERSION_2;
-import static co.rsk.peg.bitcoin.BitcoinUtils.addSpendingFederationBaseScript;
+import static co.rsk.peg.bitcoin.BitcoinUtils.*;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import co.rsk.bitcoinj.core.Address;
@@ -61,7 +60,7 @@ public class ReleaseTransactionBuilder {
 
     private final NetworkParameters params;
     private final Wallet wallet;
-    private final int federationFormatVersion;
+    private final Federation federation;
     private final Address changeAddress;
     private final Coin feePerKb;
     private final ActivationConfig.ForBlock activations;
@@ -69,24 +68,24 @@ public class ReleaseTransactionBuilder {
     /**
      * Creates a release transaction builder.
      *
-     * @param params                        network params
-     * @param wallet                        wallet to be used to build the release tx
-     * @param federationFormatVersion       needed to correctly set the redeem input data (script sig for legacy fed, witness for segwit fed)
-     * @param changeAddress                 address to send change to
-     * @param feePerKb                      fee per kb
-     * @param activations                   activations
+     * @param params        network params
+     * @param wallet        wallet to be used to build the release tx
+     * @param federation    needed to correctly create the tx depending on fed format
+     * @param changeAddress address to send change to
+     * @param feePerKb      fee per kb
+     * @param activations   activations
      */
     public ReleaseTransactionBuilder(
         NetworkParameters params,
         Wallet wallet,
-        int federationFormatVersion,
+        Federation federation,
         Address changeAddress,
         Coin feePerKb,
         ActivationConfig.ForBlock activations
     ) {
         this.params = params;
         this.wallet = wallet;
-        this.federationFormatVersion = federationFormatVersion;
+        this.federation = federation;
         this.changeAddress = changeAddress;
         this.feePerKb = feePerKb;
         this.activations = activations;
@@ -149,6 +148,7 @@ public class ReleaseTransactionBuilder {
 
         try {
             completeTx(sr);
+            checkTxVSize(btcTx);
 
             // Disconnect input from output because we don't need the reference and it interferes serialization
             for (TransactionInput transactionInput : btcTx.getInputs()) {
@@ -189,6 +189,28 @@ public class ReleaseTransactionBuilder {
         }
     }
 
+    private void checkTxVSize(BtcTransaction btcTx) {
+        if (!activations.isActive(ConsensusRule.RSKIP378)) {
+            return;
+        }
+
+        int inputsCount = btcTx.getInputs().size();
+        int outputsCount = btcTx.getOutputs().size();
+        if (inputsCount == 0 || outputsCount == 0) {
+            return;
+        }
+
+        int btcTxVSize = BridgeUtils.calculatePegoutTxSize(
+            activations,
+            federation,
+            inputsCount,
+            outputsCount
+        );
+        if (btcTxVSize > MAX_STANDARD_TX_SIZE) {
+            throw new Wallet.ExceededMaxTransactionSize();
+        }
+    }
+
     private BtcTransaction setDefaultReleaseTxConfig() {
         // Build a tx and send request and configure it
         BtcTransaction btcTx = new BtcTransaction(params);
@@ -206,7 +228,7 @@ public class ReleaseTransactionBuilder {
         // Specific settings
         sendRequestConfigurator.configure(sr);
 
-        if (federationFormatVersion == FederationFormatVersion.P2SH_P2WSH_ERP_FEDERATION.getFormatVersion()) {
+        if (federation.getFormatVersion() == FederationFormatVersion.P2SH_P2WSH_ERP_FEDERATION.getFormatVersion()) {
             sr.signInputs = false;
             sr.isSegwitCompatible = true;
         }
@@ -238,7 +260,7 @@ public class ReleaseTransactionBuilder {
             checkNotNull(redeemData, "Transaction exists in wallet that we cannot redeem: %s", txInput.getOutpoint().getHash());
             Script redeemScript = redeemData.redeemScript;
 
-            addSpendingFederationBaseScript(tx, i, redeemScript, federationFormatVersion);
+            addSpendingFederationBaseScript(tx, i, redeemScript, federation.getFormatVersion());
         }
     }
 
