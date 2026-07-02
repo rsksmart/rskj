b864d2653706a34a40d09a7f9d187db8a6cde7c1
Julian Len <julian@iovlabs.org>
2026-07-02 18:36:29 -0300

    refactor: improved assertions to pass the expected outputs instead of derive them within the assertions, this produced unified all multiple outputs assertions

--- rskj-core/src/test/java/co/rsk/peg/ReleaseTransactionAssertions.java
+++ rskj-core/src/test/java/co/rsk/peg/ReleaseTransactionAssertions.java
@@ -47,7 +47,7 @@ public class ReleaseTransactionAssertions {
         assertOutputsWithNoChange(migrationTransaction, migratedAmount);
     }
 
-    public static void assertFixedValueMigrationTxOutputs(
+    public static void assertMigrationTxWithMultipleOutputs(
         BtcTransaction migrationTransaction,
         List<Coin> expectedOutputsValues,
         Address destination,
@@ -58,10 +58,10 @@ public class ReleaseTransactionAssertions {
         assertDestinationAddress(migrationTransactionOutputs, destination, networkParameters);
         Coin expectedMigratedAmount = expectedOutputsValues.stream().reduce(Coin.ZERO, Coin::add);
         assertOutputsWithNoChange(migrationTransaction, expectedMigratedAmount);
-        assertEachOutputValueForFixedValueOutputs(migrationTransaction, expectedOutputsValues);
+        assertMigrationOutputsValues(migrationTransaction, expectedOutputsValues);
     }
 
-    private static void assertEachOutputValueForFixedValueOutputs(
+    private static void assertMigrationOutputsValues(
         BtcTransaction migrationTransaction,
         List<Coin> expectedOutputsValues
     ) {
@@ -80,47 +80,6 @@ public class ReleaseTransactionAssertions {
             Coin outputValue = outputs.get(i).getValue().add(feePerOutput);
             assertEquals(expectedOutputsValues.get(i), outputValue);
         }
-    }
-
-    public static void assertEvenlyDistributedMigrationTxOutputs(
-        BtcTransaction migrationTransaction,
-        Coin migratedAmount,
-        Address destination,
-        NetworkParameters networkParameters,
-        int maxOutputsPerMigrationTransaction) {
-        List<TransactionOutput> outputs = migrationTransaction.getOutputs();
-        assertEquals(maxOutputsPerMigrationTransaction, outputs.size());
-        assertDestinationAddress(outputs, destination, networkParameters);
-        assertOutputsWithNoChange(migrationTransaction, migratedAmount);
-        assertEachOutputValueEvenlyDistributed(migrationTransaction, migratedAmount, maxOutputsPerMigrationTransaction);
-    }
-
-    private static void assertEachOutputValueEvenlyDistributed(
-        BtcTransaction migrationTransaction,
-        Coin migratedAmount,
-        int maxOutputsPerMigrationTransaction
-    ) {
-        List<TransactionOutput> outputs = migrationTransaction.getOutputs();
-
-        Coin fees = migrationTransaction.getFee();
-        Coin[] feeDistribution = fees.divideAndRemainder(maxOutputsPerMigrationTransaction);
-        Coin feePerOutput = feeDistribution[0];
-        Coin feeRemainder = feeDistribution[1];
-
-        Coin[] valueDistribution = migratedAmount.divideAndRemainder(maxOutputsPerMigrationTransaction);
-        Coin valuePerOutput = valueDistribution[0];
-        Coin valueRemainder = valueDistribution[1];
-
-        assertEquals(valuePerOutput, outputs.get(0).getValue().add(feePerOutput).add(feeRemainder));
-
-        for (int i = 1; i < maxOutputsPerMigrationTransaction - 1; i++) {
-            assertEquals(valuePerOutput, outputs.get(i).getValue().add(feePerOutput));
-        }
-
-        assertEquals(
-            valuePerOutput.add(valueRemainder),
-            outputs.get(maxOutputsPerMigrationTransaction - 1).getValue().add(feePerOutput)
-        );
     }
 
     public static void assertDestinationAddress(List<TransactionOutput> outputs, Address expectedDestinationAddress, NetworkParameters networkParams) {
