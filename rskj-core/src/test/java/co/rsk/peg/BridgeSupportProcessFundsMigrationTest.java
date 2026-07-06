db31c2ee2a887b97c244e637f38a6468117e9182
Julian Len <julian@iovlabs.org>
2026-07-06 16:29:20 -0300

    renaming: withAboveMaxInputsPerPegoutTxUtxos -> aboveMaxInputsPerPegoutTx

--- rskj-core/src/test/java/co/rsk/peg/BridgeSupportProcessFundsMigrationTest.java
+++ rskj-core/src/test/java/co/rsk/peg/BridgeSupportProcessFundsMigrationTest.java
@@ -517,7 +517,7 @@ class BridgeSupportProcessFundsMigrationTest {
             }
 
             @Test
-            void updateCollections_duringMigration_withMaxInputsPerPegoutTxPlusOneUtxos_shouldCreateAMigrationTxEachTime() throws IOException {
+            void updateCollections_duringMigration_aboveMaxInputsPerPegoutTx_shouldCreateAMigrationTxEachTime() throws IOException {
                 // Arrange
                 List<UTXO> retiringUtxos = UTXOBuilder.builder()
                     .withValue(BELOW_MTMU_UTXO_BTC_VALUE)
@@ -649,7 +649,7 @@ class BridgeSupportProcessFundsMigrationTest {
             }
 
             @Test
-            void updateCollections_pastMigrationAge_withMaxInputsPerPegoutTxPlusOneUtxos_whenFirstUtxosSelectionSumIsBelowMTMUThreshold_shouldCreateMigrationTxAndClearRetiringFed() throws IOException {
+            void updateCollections_pastMigrationAge_aboveMaxInputsPerPegoutTx_whenFirstUtxosSelectionSumIsBelowMTMUThreshold_shouldCreateMigrationTxAndClearRetiringFed() throws IOException {
                 // Arrange
                 List<UTXO> retiringUtxos = UTXOBuilder.builder()
                     .withValue(BELOW_MTMU_UTXO_BTC_VALUE)
@@ -828,7 +828,7 @@ class BridgeSupportProcessFundsMigrationTest {
             }
 
             @Test
-            void updateCollections_duringMigration_withMaxInputsPerPegoutTxPlusOneUtxos_whenFirstBatchSumIsAboveMTMUThreshold_shouldCreateAMigrationTxEachTime() throws IOException {
+            void updateCollections_duringMigration_aboveMaxInputsPerPegoutTx_whenFirstBatchSumIsAboveMTMUThreshold_shouldCreateAMigrationTxEachTime() throws IOException {
                 // Arrange
                 List<UTXO> retiringUtxos = UTXOBuilder.builder()
                     .withValue(ABOVE_MTMU_UTXO_BTC_VALUE)
@@ -1004,7 +1004,7 @@ class BridgeSupportProcessFundsMigrationTest {
             }
 
             @Test
-            void updateCollections_pastMigrationAge_withMaxInputsPerPegoutTxPlusOneUtxos_whenFirstBatchSumIsAboveMTMUThreshold_shouldCreateMigrationTxWithMultipleOutputsAndClearRetiringFed() throws IOException {
+            void updateCollections_pastMigrationAge_aboveMaxInputsPerPegoutTx_whenFirstBatchSumIsAboveMTMUThreshold_shouldCreateMigrationTxWithMultipleOutputsAndClearRetiringFed() throws IOException {
                 // Arrange
                 List<UTXO> retiringUtxos = UTXOBuilder.builder()
                     .withValue(ABOVE_MTMU_UTXO_BTC_VALUE)
@@ -1227,7 +1227,7 @@ class BridgeSupportProcessFundsMigrationTest {
             }
 
             @Test
-            void updateCollections_duringMigration_withMaxInputsPerPegoutTxPlusOneUtxos_whenFirstBatchSumIsAboveLargeMTMUThreshold_shouldCreateAMigrationTxEachTime() throws IOException {
+            void updateCollections_duringMigration_aboveMaxInputsPerPegoutTx_whenFirstBatchSumIsAboveLargeMTMUThreshold_shouldCreateAMigrationTxEachTime() throws IOException {
                 // Arrange
                 List<UTXO> retiringUtxos = UTXOBuilder.builder()
                     .withValue(ABOVE_LARGE_MTMU_UTXO_BTC_VALUE)
@@ -1363,7 +1363,7 @@ class BridgeSupportProcessFundsMigrationTest {
             }
 
             @Test
-            void updateCollections_pastMigrationAge_withMaxInputsPerPegoutTxPlusOneUtxos_whenFirstBatchSumIsAboveLargeMTMUThreshold_shouldCreateMigrationTxWithMaxOutputsAndClearRetiringFed() throws IOException {
+            void updateCollections_pastMigrationAge_aboveMaxInputsPerPegoutTx_whenFirstBatchSumIsAboveLargeMTMUThreshold_shouldCreateMigrationTxWithMaxOutputsAndClearRetiringFed() throws IOException {
                 // Arrange
                 List<UTXO> retiringUtxos = UTXOBuilder.builder()
                     .withValue(ABOVE_LARGE_MTMU_UTXO_BTC_VALUE)
