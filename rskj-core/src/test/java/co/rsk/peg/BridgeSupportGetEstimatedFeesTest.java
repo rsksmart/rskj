b1e87d8e6df390b7d8f5fb249f26749ff122ee7c
julia-zack <julia.zack@iovlabs.org>
2026-07-07 12:15:37 -0300

    Rename tbd100 to tbd1000

--- rskj-core/src/test/java/co/rsk/peg/BridgeSupportGetEstimatedFeesTest.java
+++ rskj-core/src/test/java/co/rsk/peg/BridgeSupportGetEstimatedFeesTest.java
@@ -430,7 +430,7 @@ class BridgeSupportGetEstimatedFeesTest {
     }
 
     @Nested
-    class PostVetiverPreTBD100Activations {
+    class PostVetiverPreTBD1000Activations {
 
         @BeforeEach
         void setUp() {
@@ -474,7 +474,7 @@ class BridgeSupportGetEstimatedFeesTest {
     }
 
     @Nested
-    class PostTBD100Activations {
+    class PostTBD1000Activations {
         @BeforeEach
         void setUp() {
             setUpBridgeAndFederationSupport(ALL_ACTIVATIONS, FEE_PER_KB);
