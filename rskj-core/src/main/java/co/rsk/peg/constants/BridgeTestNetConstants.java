8892b8e548d1f687c1bf8d6666c032f541836605
Marcos <marcos@rootstocklabs.com>
2026-07-10 09:29:52 -0300

    Rename Bridge constant

--- rskj-core/src/main/java/co/rsk/peg/constants/BridgeTestNetConstants.java
+++ rskj-core/src/main/java/co/rsk/peg/constants/BridgeTestNetConstants.java
@@ -58,8 +58,8 @@ public class BridgeTestNetConstants extends BridgeConstants {
 
         minimumPegoutValuePercentageToReceiveAfterFee = 80;
 
-        legacyMaxInputsPerPegoutTransaction = 50;
-        maxInputsPerPegoutTransaction = 150;
+        legacyMaxInputsPerMigrationTransaction = 50;
+        maxInputsPerMigrationTransaction = 150;
 
         numberOfBlocksBetweenPegouts = 360; // 3 hours of RSK blocks (considering 1 block every 30 seconds)
 
