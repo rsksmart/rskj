782408984ae09d3d103ba651eea35300e45c9f1e
julia-zack <julia.zack@iovlabs.org>
2026-09-03 17:58:52 -0300

    Update method name when logging

--- rskj-core/src/main/java/co/rsk/peg/BridgeSupport.java
+++ rskj-core/src/main/java/co/rsk/peg/BridgeSupport.java
@@ -3474,7 +3474,7 @@ public class BridgeSupport {
             throw new RegisterBtcTransactionException(e.getMessage(), e);
         }
 
-        logger.trace("[validationsForRegisterBtcTransaction] Btc tx: {} successfully validated", btcTxHash);
+        logger.trace("[validateBtcTxRegistration] Btc tx: {} successfully validated", btcTxHash);
     }
 
     private void validatePMTSize(byte[] pmtSerialized) throws BridgeIllegalArgumentException {
