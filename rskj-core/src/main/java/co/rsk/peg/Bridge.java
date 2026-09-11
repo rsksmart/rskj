924b551ba79caeccb52170da6eb33a5fa6f900cf
Julian Len <julian@iovlabs.org>
2026-09-11 09:37:51 -0300

    feat: the args are obtained and logged

--- rskj-core/src/main/java/co/rsk/peg/Bridge.java
+++ rskj-core/src/main/java/co/rsk/peg/Bridge.java
@@ -604,6 +604,12 @@ public class Bridge extends PrecompiledContracts.PrecompiledContract {
 
     public void registerPegoutTransaction(Object[] args) {
         logger.trace("registerPegoutTransaction");
+
+        Sha256Hash btcTxHash = Sha256Hash.wrap((byte[]) args[0]);
+        int height = ((BigInteger)args[1]).intValue();
+        byte[] pmtSerialized = (byte[]) args[2];
+
+        logger.debug("[registerPegoutTransaction] btcTxHash: {}, height: {}, pmtSerialized: {}", btcTxHash, height, pmtSerialized);
     }
 
     public void releaseBtc(Object[] args) throws VMException {
