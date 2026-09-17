a51d22415be2fc7e386719ffc8e442add2218a25
Julian Len <julian@iovlabs.org>
2026-09-17 11:01:26 -0300

    test: Added VarIntUtils encode test case for an empty list

--- rskj-core/src/test/java/co/rsk/peg/bitcoin/VarIntUtilsTest.java
+++ rskj-core/src/test/java/co/rsk/peg/bitcoin/VarIntUtilsTest.java
@@ -2,6 +2,7 @@ package co.rsk.peg.bitcoin;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+import java.util.Collections;
 import org.junit.jupiter.api.Test;
 
 class VarIntUtilsTest {
@@ -10,6 +11,16 @@ class VarIntUtilsTest {
     void encode_withNull_shouldReturnEmptyArray() {
         // act
         byte[] encodedValues = VarIntUtils.encode(null);
+
+        // assert
+        byte[] expectedEncodedValues = new byte[]{};
+        assertArrayEquals(expectedEncodedValues, encodedValues);
+    }
+
+    @Test
+    void encode_withEmptyList_shouldReturnEmptyArray() {
+        // act
+        byte[] encodedValues = VarIntUtils.encode(Collections.emptyList());
 
         // assert
         byte[] expectedEncodedValues = new byte[]{};
