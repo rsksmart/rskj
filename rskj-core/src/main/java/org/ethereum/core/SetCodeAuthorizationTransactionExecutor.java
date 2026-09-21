bc27d4818d36f30d0dbdccab7d4b75d319808ea0
meldsun <meldsun0@gmail.com>
2026-09-21 15:52:51 -0300

    Adding comment on SetCodeAuthorizationTrsantionExecutor.processAuthorizationTuple

--- rskj-core/src/main/java/org/ethereum/core/SetCodeAuthorizationTransactionExecutor.java
+++ rskj-core/src/main/java/org/ethereum/core/SetCodeAuthorizationTransactionExecutor.java
@@ -32,6 +32,8 @@ public class SetCodeAuthorizationTransactionExecutor {
     public static final byte[] CODE_FOR_CLEANING_DELEGATED_ADDRESS = new byte[0];
     private static final BigInteger UNIVERSAL_CHAIN_ID = BigInteger.ZERO;
 
+    // Precondition: this method must only be called when RSKIP545 is active. Today that's enforced
+    // by TransactionExecutor.init() rejecting Type-4 txs before this method's only caller can run.
     public long processAuthorizationTuple(Repository repository, BigInteger outerTransactionChainId, SetCodeAuthorization authorization) {
         verifyChainId(authorization.getChainId(), outerTransactionChainId);
         authorization.verifyNonceRange();
