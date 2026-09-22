6a779617d189d3a19840a51553e5e5d83b1feb79
Julian Len <julian@iovlabs.org>
2026-09-22 18:52:17 -0300

    removed the unused InvalidOutputIndexException constructor

--- rskj-core/src/main/java/co/rsk/peg/bitcoin/InvalidOutputIndexException.java
+++ rskj-core/src/main/java/co/rsk/peg/bitcoin/InvalidOutputIndexException.java
@@ -2,10 +2,6 @@ package co.rsk.peg.bitcoin;
 
 public class InvalidOutputIndexException extends RuntimeException {
 
-    public InvalidOutputIndexException(String message) {
-        super(message);
-    }
-
     public InvalidOutputIndexException(String message, Throwable cause) {
         super(message, cause);
     }
